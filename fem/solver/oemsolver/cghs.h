d20c680ddd04423637958a9ffaeccb5169ae262a
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2005-09-09 07:48:30 +0000

    The iterative solvers from our home page with dune interface now.
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@56 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

---
+++ fem/solver/oemsolver/cghs.h
@@ -0,0 +1,250 @@
+// Emacs should recognise this header as -*- C++ -*-
+
+#ifndef CGHS_BLAS_H
+#define CGHS_BLAS_H
+
+// ============================================================================
+//
+//  CG nach Hestenes und Stiefel
+//
+//  siehe auch:
+//  Ashby, Manteuffel, Saylor
+//     A taxononmy for conjugate gradient methods
+//     SIAM J Numer Anal 27, 1542-1568 (1990)
+//
+//  oder:
+//  Willy D"orfler:
+//     Orthogonale Fehlermethoden
+//
+//                                                 ----------------------------
+//                                                 Christian Badura, Mai 1998
+//
+// ============================================================================
+
+
+// ohne Vorkonditionierer
+template< class MATRIX >
+inline int
+cghs( unsigned N, const MATRIX &A, const double *b, double *x, double eps );
+
+template< class MATRIX >
+inline int
+cghs( unsigned N, const MATRIX &A, const double *b, double *x, double eps,
+      bool detailed );
+
+template<class CommunicatorType, class MATRIX >
+inline int
+cghsParallel( CommunicatorType  & comm, unsigned N, const MATRIX &A, const double *b, double *x, double eps,
+      bool detailed );
+
+
+// mit Vorkonditionierer
+template< class MATRIX, class PC_MATRIX >
+inline int
+cghs( unsigned N, const MATRIX &A, const PC_MATRIX &C,
+      const double *b, double *x, double eps );
+
+template< class MATRIX, class PC_MATRIX >
+inline int
+cghs( unsigned N, const MATRIX &A, const PC_MATRIX &C,
+      const double *b, double *x, double eps, bool detailed );
+
+
+// ============================================================================
+
+#include "cblas.h"
+#include "tmpmem.hh"
+
+static OEMTmpMem cghsMem;
+
+template< class CommunicatorType ,class MATRIX >
+inline int
+cghsParallel( CommunicatorType & comm, unsigned N, const MATRIX &A, const double *b, double *x, double eps,
+      bool detailed ) 
+{
+  if ( N==0 )
+    return -1;
+#if 1
+  cghsMem.resize( 3*N ); 
+
+  double *p = cghsMem.getMem (N);
+  double *r = cghsMem.getMem (N);
+  double *g = cghsMem.getMem (N);
+#else 
+  double *g = new double[N];
+  double *r = new double[N];
+  double *p = new double[N];
+#endif
+  
+  int its=0;
+  //double t, tau, sig, rho, gam;
+  double t, gam;
+
+  // send and recive buffer for rho,tau and sig
+  double * commVal  = new double [3];
+  double * commBuff = new double [3];
+  
+  double & rho_s = commVal[0];
+  double & sig_s = commVal[1];
+  double & tau_s = commVal[2];
+  
+  double & rho = commBuff[0];
+  double & sig = commBuff[1];
+  double & tau = commBuff[2];
+  
+  double err=eps*eps* comm.globalSum( ddot(N,b,1,b,1) );
+  
+  mult(A,x,g);
+  daxpy(N,-1.,b,1,g,1);
+  dscal(N,-1.,g,1);
+  dcopy(N,g,1,r,1);
+  //comm.communicate();
+  double ddo = comm.globalSum( ddot(N,g,1,g,1) );
+  while ( ddo>err ) 
+  {
+    mult(A,r,p);
+
+    rho_s = ddot(N,p,1,p,1) ;
+    sig_s = ddot(N,r,1,p,1) ;
+    tau_s = ddot(N,g,1,r,1) ;
+
+    comm.globalSumVec ( commVal , 3 , commBuff );
+    
+    t=tau/sig;
+    daxpy(N,t,r,1,x,1);
+    daxpy(N,-t,p,1,g,1);
+    gam=(t*t*rho-tau)/tau;
+    dscal(N,gam,r,1);
+    daxpy(N,1.,g,1,r,1);
+    if ( detailed )
+      std::cout<<"cghs "<<its<<"\t"<<dnrm2(N,g,1)<<std::endl;
+    ++its;
+    ddo = comm.globalSum( ddot(N,g,1,g,1) );
+  }
+#if 1
+  cghsMem.reset();
+#else 
+  delete[] g;
+  delete[] r;
+  delete[] p;
+#endif
+  delete [] commVal ;
+  delete [] commBuff ;
+  comm.communicate();
+  return its;
+}
+
+template< class MATRIX >
+inline
+int
+cghs( unsigned N, const MATRIX &A, const double *b, double *x, double eps,
+      bool detailed ) 
+{
+  if ( N==0 )
+    return -1;
+#if 1
+  cghsMem.resize( 3*N ); 
+
+  double *p = cghsMem.getMem (N);
+  double *r = cghsMem.getMem (N);
+  double *g = cghsMem.getMem (N);
+#else 
+  double *g = new double[N];
+  double *r = new double[N];
+  double *p = new double[N];
+#endif
+  
+  int its=0;
+  double t, tau, sig, rho, gam;
+  double err=eps*eps*ddot(N,b,1,b,1);
+  
+  mult(A,x,g);
+  daxpy(N,-1.,b,1,g,1);
+  dscal(N,-1.,g,1);
+  dcopy(N,g,1,r,1);
+  while ( ddot(N,g,1,g,1)>err ) {
+    mult(A,r,p);
+    rho=ddot(N,p,1,p,1);
+    sig=ddot(N,r,1,p,1);
+    tau=ddot(N,g,1,r,1);
+    t=tau/sig;
+    daxpy(N,t,r,1,x,1);
+    daxpy(N,-t,p,1,g,1);
+    gam=(t*t*rho-tau)/tau;
+    dscal(N,gam,r,1);
+    daxpy(N,1.,g,1,r,1);
+    if ( detailed )
+      std::cout<<"cghs "<<its<<"\t"<<dnrm2(N,g,1)<<std::endl;
+    ++its;
+  }
+#if 1
+  cghsMem.reset();
+#else 
+  delete[] g;
+  delete[] r;
+  delete[] p;
+#endif
+  return its;
+}
+
+
+template< class MATRIX > inline int
+cghs( unsigned N, const MATRIX &A, const double *b, double *x, double eps ) {
+  return cghs(N,A,b,x,eps,false);
+}
+
+
+// ============================================================================
+
+
+template< class MATRIX, class PC_MATRIX >
+inline
+int
+cghs( unsigned N, const MATRIX &A, const PC_MATRIX &C,
+      const double *b, double *x, double eps, bool detailed ) {
+  if ( N==0 )
+    return 0;
+  double *r = new double[N];
+  double *d = new double[N];
+  double *h = new double[N];
+  double *Ad = h;
+  int its=0;
+  double rh, alpha, beta;
+  double err=eps*eps*ddot(N,b,1,b,1);
+  
+  assert(false);
+  
+  mult(A,x,r);
+  daxpy(N,-1.,b,1,r,1);
+  mult(A,r,d);
+  dcopy(N,d,1,h,1);
+  rh=ddot(N,r,1,h,1);
+  while ( ddot(N,r,1,r,1)>err ) {
+    mult(A,d,Ad);
+    alpha=rh/ddot(N,d,1,Ad,1);
+    daxpy(N,-alpha,d,1,x,1);
+    daxpy(N,-alpha,Ad,1,r,1);
+    mult(A,r,h);
+    beta=1./rh; rh=ddot(N,r,1,h,1); beta*=rh;
+    dscal(N,beta,d,1);
+    daxpy(N,1.,h,1,d,1);
+    if ( detailed )
+      std::cout<<"cghs "<<its<<"\t"<<dnrm2(N,r,1)<<std::endl;
+    ++its;
+  }
+  delete[] r;
+  delete[] d;
+  delete[] h;
+  return its;
+}
+
+template< class MATRIX, class PC_MATRIX > inline int
+cghs( unsigned N, const MATRIX &A, const PC_MATRIX &C,
+      const double *b, double *x, double eps ) {
+  return cghs(N,A,C,b,x,eps,false);
+}
+
+// ============================================================================
+
+
+#endif // CGHS_BLAS_H
---
+++ fem/solver/oemsolver/oemsolver.hh
@@ -0,0 +1,376 @@
+#ifndef __DUNE_OEMSOLVER_HH__
+#define __DUNE_OEMSOLVER_HH__
+
+#include <dune/common/operator.hh>
+
+namespace OEMSolver 
+{
+
+//! this method is called from all solvers and is only a wrapper
+template <class MatrixImp, class VectorType>
+void mult(const MatrixImp & m, const VectorType * x, VectorType * ret)
+{
+  // call multOEM of the matrix 
+  m.multOEM(x,ret);
+}
+  
+#include "bicgstab.h"
+#include "cghs.h"
+#include "gmres.h"
+#include "bicgsq.h"
+  
+} // end namespace OEMSolver 
+
+
+namespace Dune 
+{
+
+// CG scheme after Hestenes and Stiefel
+template <class DiscreteFunctionType, class OperatorType>
+class OEMCGOp : public Operator<
+      typename DiscreteFunctionType::DomainFieldType,
+      typename DiscreteFunctionType::RangeFieldType,
+            DiscreteFunctionType,DiscreteFunctionType> {
+
+private:
+  // no const reference, we make const later 
+  OperatorType &op_;
+  typename DiscreteFunctionType::RangeFieldType epsilon_;
+  int maxIter_;
+  bool verbose_ ;
+
+public:
+
+  OEMCGOp( OperatorType & op , double  redEps , double absLimit , int maxIter , bool verbose ) :
+        op_(op), epsilon_ ( absLimit ) ,
+        maxIter_ (maxIter ) , verbose_ ( verbose ) {
+  }
+
+  void prepare (const DiscreteFunctionType& Arg, DiscreteFunctionType& Dest) const
+  {
+    const_cast<OperatorType &> (op_).prepareGlobal(Arg,Dest);
+  }
+
+  void finalize () const
+  {
+    const_cast<OperatorType &> (op_).finalizeGlobal();
+  }
+
+  void apply( const DiscreteFunctionType& arg, DiscreteFunctionType& dest ) const
+  {
+    typedef typename DiscreteFunctionType::FunctionSpaceType FunctionSpaceType;
+    typedef typename FunctionSpaceType::RangeFieldType Field;
+    typedef typename DiscreteFunctionType::DofIteratorType DofIteratorType;
+    typedef typename DiscreteFunctionType::ConstDofIteratorType ConstDofIteratorType;
+    typedef typename FunctionSpaceType::GridType GridType;
+
+    // prepare operator 
+    prepare ( arg, dest );
+
+    int size = arg.getFunctionSpace().size();
+    
+    OEMSolver::cghs
+      (size,op_.myMatrix(),arg.leakPointer(),dest.leakPointer(),epsilon_,verbose_);
+
+    // finalize operator  
+    finalize ();
+  }
+
+  void operator ()( const DiscreteFunctionType& arg, DiscreteFunctionType& dest ) const
+  {
+    apply(arg,dest);
+  }
+
+};
+
+
+// cummunicate the unkown of the CG Method 
+template <class Writer , class Reader>
+class CommunicateVector
+{
+  Writer & dataWriter_;
+  Reader & dataReader_;
+public:
+  CommunicateVector ( Writer & w, Reader & r ) : dataWriter_(w), dataReader_(r) {}
+
+  template <class ObjectStreamType, class EntityType>
+  void inlineData ( ObjectStreamType & str, EntityType & en )
+  {
+    assert(false);
+  }
+
+  template <class ObjectStreamType, class EntityType>
+  void xtractData ( ObjectStreamType & str, EntityType & en )
+  {
+    assert(false);
+  }
+
+  template <class ObjectStreamType, class EntityType>
+  void scatter ( ObjectStreamType & str, EntityType & en )
+  {
+    std::pair < ObjectStreamType * , EntityType * > p (&str,&en);
+    dataWriter_.apply( p );
+  }
+
+  template <class ObjectStreamType, class EntityType>
+  void gather ( ObjectStreamType & str, EntityType & en )
+  {
+    std::pair < ObjectStreamType * , EntityType * > p (&str,&en);
+    dataReader_.apply( p );
+  }
+};
+
+// LocalCommType = CommunicateVector
+template <class GridCommType , class LocalCommType>
+class CommunicateCG
+{
+  GridCommType & gc_;
+  LocalCommType & comm_;
+public:
+  CommunicateCG ( GridCommType & gc , LocalCommType & comm )
+    : gc_(gc), comm_(comm) {}
+
+  void communicate ()
+  {
+    gc_.communicate(comm_);
+  }
+
+  double globalSum( double val )
+  {
+    return gc_.globalSum(val);
+  }
+
+  void globalSumVec ( double * send, int s , double * recv)
+  {
+    gc_.globalSum(send,s,recv);
+  }
+};
+
+//! Parallel version of above CG scheme
+template <class CommunicatorType , class DiscreteFunctionType, class OperatorType>
+class OEMCGOpParallel : public Operator<
+      typename DiscreteFunctionType::DomainFieldType,
+      typename DiscreteFunctionType::RangeFieldType,
+            DiscreteFunctionType,DiscreteFunctionType> {
+
+private:
+  // no const reference, we make const later 
+  OperatorType &op_;
+  typename DiscreteFunctionType::RangeFieldType epsilon_;
+  int maxIter_;
+  bool verbose_ ;
+  CommunicatorType & comm_;
+public:
+
+  OEMCGOpParallel( CommunicatorType & comm, OperatorType & op , double  redEps , double absLimit , int maxIter , bool verbose ) :
+        comm_(comm), op_(op), epsilon_ ( absLimit ) ,
+        maxIter_ (maxIter ) , verbose_ ( verbose ) {
+  }
+
+  void prepare (const DiscreteFunctionType& Arg, DiscreteFunctionType& Dest) const
+  {
+    const_cast<OperatorType &> (op_).prepareGlobal(Arg,Dest);
+  }
+
+  void finalize () const
+  {
+    const_cast<OperatorType &> (op_).finalizeGlobal();
+  }
+
+  void apply( const DiscreteFunctionType& arg, DiscreteFunctionType& dest ) const
+  {
+    typedef typename DiscreteFunctionType::FunctionSpace FunctionSpaceType;
+    typedef typename FunctionSpaceType::RangeField Field;
+    typedef typename DiscreteFunctionType::DofIteratorType DofIteratorType;
+    typedef typename DiscreteFunctionType::ConstDofIteratorType ConstDofIteratorType;
+    typedef typename FunctionSpaceType::GridType GridType;
+
+    // prepare operator 
+    prepare ( arg, dest );
+
+    int size = arg.getFunctionSpace().size();
+    
+    OEMSolver::cghsParallel
+      (comm_,size,op_.myMatrix(),arg.leakPointer(),dest.leakPointer(),epsilon_,verbose_);
+
+    // finalize operator  
+    finalize ();
+  }
+
+  void operator ()( const DiscreteFunctionType& arg, DiscreteFunctionType& dest ) const
+  {
+    apply(arg,dest);
+  }
+
+};
+
+
+// BICG STAB scheme 
+template <class DiscreteFunctionType, class OperatorType>
+class OEMBICGSTABOp : public Operator<
+      typename DiscreteFunctionType::DomainFieldType,
+      typename DiscreteFunctionType::RangeFieldType,
+            DiscreteFunctionType,DiscreteFunctionType> {
+
+private:
+  // no const reference, we make const later 
+  OperatorType &op_;
+  typename DiscreteFunctionType::RangeFieldType epsilon_;
+  int maxIter_;
+  bool verbose_ ;
+
+public:
+
+  OEMBICGSTABOp( OperatorType & op , double  redEps , double absLimit , int maxIter , bool verbose ) :
+        op_(op), epsilon_ ( absLimit ) ,
+        maxIter_ (maxIter ) , verbose_ ( verbose ) {
+  }
+
+  void prepare (const DiscreteFunctionType& Arg, DiscreteFunctionType& Dest) const
+  {
+    const_cast<OperatorType &> (op_).prepareGlobal(Arg,Dest);
+  }
+
+  void finalize () const
+  {
+    const_cast<OperatorType &> (op_).finalizeGlobal();
+  }
+
+  void apply( const DiscreteFunctionType& arg, DiscreteFunctionType& dest ) const
+  {
+    typedef typename DiscreteFunctionType::FunctionSpaceType FunctionSpaceType;
+
+    // prepare operator 
+    prepare ( arg, dest );
+
+    int size = arg.getFunctionSpace().size();
+
+    
+    //int iter = 
+    OEMSolver::bicgstab
+      (size,op_.myMatrix(),arg.leakPointer(),dest.leakPointer(),epsilon_,verbose_);
+
+    // finalize operator  
+    finalize ();
+  }
+
+  void operator ()( const DiscreteFunctionType& arg, DiscreteFunctionType& dest ) const
+  {
+    apply(arg,dest);
+  }
+
+};
+// BICG STAB scheme 
+template <class DiscreteFunctionType, class OperatorType>
+class OEMBICGSQOp : public Operator<
+      typename DiscreteFunctionType::DomainFieldType,
+      typename DiscreteFunctionType::RangeFieldType,
+            DiscreteFunctionType,DiscreteFunctionType> {
+
+private:
+  // no const reference, we make const later 
+  OperatorType &op_;
+  typename DiscreteFunctionType::RangeFieldType epsilon_;
+  int maxIter_;
+  bool verbose_ ;
+
+public:
+
+  OEMBICGSQOp( OperatorType & op , double  redEps , double absLimit , int maxIter , bool verbose ) :
+        op_(op), epsilon_ ( absLimit ) ,
+        maxIter_ (maxIter ) , verbose_ ( verbose ) {
+  }
+
+  void prepare (const DiscreteFunctionType& Arg, DiscreteFunctionType& Dest) const
+  {
+    const_cast<OperatorType &> (op_).prepareGlobal(Arg,Dest);
+  }
+
+  void finalize () const
+  {
+    const_cast<OperatorType &> (op_).finalizeGlobal();
+  }
+
+  void apply( const DiscreteFunctionType& arg, DiscreteFunctionType& dest ) const
+  {
+    typedef typename DiscreteFunctionType::FunctionSpaceType FunctionSpaceType;
+
+    // prepare operator 
+    prepare ( arg, dest );
+
+    int size = arg.getFunctionSpace().size();
+
+    
+    //int iter = 
+    OEMSolver::bicgsq
+      (size,op_.myMatrix(),arg.leakPointer(),dest.leakPointer(),epsilon_,verbose_);
+
+    // finalize operator  
+    finalize ();
+  }
+
+  void operator ()( const DiscreteFunctionType& arg, DiscreteFunctionType& dest ) const
+  {
+    apply(arg,dest);
+  }
+
+};
+
+
+template <class DiscreteFunctionType, class OperatorType>
+class OEMGMRESOp : public Operator<
+      typename DiscreteFunctionType::DomainFieldType,
+      typename DiscreteFunctionType::RangeFieldType,
+            DiscreteFunctionType,DiscreteFunctionType> {
+
+private:
+  // no const reference, we make const later 
+  OperatorType &op_;
+  typename DiscreteFunctionType::RangeFieldType epsilon_;
+  int maxIter_;
+  bool verbose_ ;
+
+public:
+
+  OEMGMRESOp( OperatorType & op , double  redEps , double absLimit , int maxIter , bool verbose ) :
+        op_(op), epsilon_ ( absLimit ) ,
+        maxIter_ (maxIter ) , verbose_ ( verbose ) {
+  }
+
+  void prepare (const DiscreteFunctionType& Arg, DiscreteFunctionType& Dest) const
+  {
+    const_cast<OperatorType &> (op_).prepareGlobal(Arg,Dest);
+  }
+
+  void finalize () const
+  {
+    const_cast<OperatorType &> (op_).finalizeGlobal();
+  }
+
+  void apply( const DiscreteFunctionType& arg, DiscreteFunctionType& dest ) const
+  {
+    typedef typename DiscreteFunctionType::FunctionSpaceType FunctionSpaceType;
+
+    // prepare operator 
+    prepare ( arg, dest );
+
+    int size = arg.getFunctionSpace().size();
+    int inner = (size > 1000) ? 1000 : size;
+
+    OEMSolver::gmres
+      (inner,size,op_.myMatrix(),arg.leakPointer(),dest.leakPointer(),epsilon_,verbose_);
+
+    // finalize operator  
+    finalize ();
+  }
+
+  void operator ()( const DiscreteFunctionType& arg, DiscreteFunctionType& dest ) const
+  {
+    apply(arg,dest);
+  }
+
+};
+ 
+} // end namespace Dune 
+
+#endif
