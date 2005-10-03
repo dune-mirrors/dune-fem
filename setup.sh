0356908a34712f6343247fe7beed84262b3ab86d
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2005-10-03 12:40:13 +0000

    *** empty log message ***
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@72 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Aufruf zum erzeugen der makefiles fuer Mario
+
+./autogen.sh --gnu --with-dune=$HOME/work/Dune/src/ \
+	--with-problem-dim=$1 \
+	--with-world-dim=$2 \
+  --with-grape=$HOME/DuneAdds/modules/GrapeNew \
+  --with-alberta=$HOME/DuneAdds/modules/alberta-1.2 \
+  --enable-blas \
+#  --with-alu3dgrid=$HOME/DuneAdds/modules/alu3dgrid \
+#  --nodebug \
+#  --optim
+#  --with-alberta=$HOME/DuneAdds/modules/alberta-1.2 \
+#	--with-blas-lib=/hosts/sauron/graid/robertk/Traube/liblinuxGL_dune/ \
+#./autogen.sh --intel --with-dune=/hosts/sauron/graid/robertk/Dune/robertk/ \
+#./autogen.sh --intel --with-dune=/tmp/robertk/Schrott  \
+  
