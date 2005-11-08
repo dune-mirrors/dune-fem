b2a44400b904749b3fc69484f619cca5aab43041
Bernard Haasdonk <haasdonk@mathematik.uni-stuttgart.de>
2005-11-08 17:34:03 +0000

    inserted precompiler selection possibilities for visualizing the data generated by applications in fuelcell/probs which are decomposed as suggested by Mario with the example ConvDiff
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@109 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/visual/grape/datadisp/datadisp.cc
+++ fem/visual/grape/datadisp/datadisp.cc
@@ -7,71 +7,103 @@
 #include <vector>
 #include <assert.h>
 #include <string.h>
-
 #include <config.h>
-#include <dune/common/stdstreams.cc>
-
-using namespace Dune;
-
-#define AGRID 1 
-#define BGRID 0 
-#define SGRID 0
-
-//#include <dune/grid/sgrid.hh>
 
 #define LARGE 1.0E308
 
-#if AGRID 
-#include <dune/grid/albertagrid.hh>
-
-static const int dim = DUNE_PROBLEM_DIM; 
-static const int dimworld = DUNE_WORLD_DIM; 
-
-typedef AlbertaGrid<dim,dimworld> GR_GridType;
-#endif
-
-#if BGRID 
-
-#include <dune/grid/alu3dgrid.hh>
-
-static const int dim = 3; 
-static const int dimworld = 3; 
-
-typedef ALU3dGrid<dim,dimworld,hexa> GR_GridType;
-#endif
-
-
-#if SGRID 
-#include <dune/grid/sgrid.hh>
-
-static const int dim = DIM; 
-static const int dimworld = DIM_OF_WORLD; 
-
-typedef SGrid <dim, dimworld> GR_GridType;
-#endif
-
+#include <dune/common/stdstreams.cc>
 #include <dune/fem/dfadapt.hh>
 #include <dune/fem/lagrangebase.hh>
 #include <dune/fem/dfadapt.hh>
 #include <dune/common/stack.hh>
-
 #include <dune/grid/common/leafindexset.hh>
 #include <dune/grid/common/gridpart.hh>
-
 #include <dune/io/file/grapedataio.hh>
-
-
-typedef double REAL;
-
 #include <dune/io/visual/grapedatadisplay.hh>
 #include <dune/io/visual/combinedgrapedisplay.hh>
 #include "printhelp.cc"
 
+using namespace Dune;
+
+// set one of the following to 1 depending on the source-application
+// e.g. ADVDIFF for use with problem-decomposition structure as proposed by 
+// mario, e.g. in fuelcell/probs/AdvDiff
+// otherwise OTHER_PROBLEM defines some parameters by hand 
+#define ADVDIFF_PROBLEM  0
+#define CONVDIFF_PROBLEM 1
+#define OTHER_PROBLEM    0
+
+// source-application includes assuming fuelcell and fuelcell/src are in the
+// compiler-include path
+#if ADVDIFF_PROBLEM
+#include <probs/AdvDiff/5_AdvDiffDescription/advdiffdescr.hh>
+#endif
+#if CONVDIFF_PROBLEM
+#include <probs/ConvDiff/5_ConvDiffDescription/convdiffdescr.hh>
+#endif
+
+typedef double REAL;
+
+// define FUELCELL_PROBLEM if ADVDIFF or CONVDIFF is set as they have common 
+// code parts in the sequel
+#if ADVDIFF_PROBLEM
+#define FUELCELL_PROBLEM
+#endif
+#if CONVDIFF_PROBLEM
+#define FUELCELL_PROBLEM
+#endif
+
+#ifdef FUELCELL_PROBLEM
+typedef GridName<dim,dimworld ELTYPE>   GridType;
+typedef Descr <double,GridType,ncomp,0> DescrType;  
+typedef DescrType::DiscrType   DiscrType;
+
+// get required types from the problem-specific typedefs
+typedef GridType                                  GR_GridType;
+typedef DiscrType::FuncSpaceType                  GR_FunctionSpaceType;
+typedef DiscrType::IndexSetType                   GR_IndexSetType;
+typedef DiscrType::GridPartType                   GR_GridPartType;
+typedef DiscrType::DiscFuncSpaceType              GR_DiscFuncSpaceType;
+typedef DiscrType::DiscreteFunctionType           GR_DiscFuncType;
+
+// application-independent defs:
+typedef DofManager<GR_GridType>                         GR_DofManagerType;
+typedef DofManagerFactory <GR_DofManagerType>           GR_DofManagerFactoryType;
+typedef GrapeDataDisplay<GR_GridType , GR_DiscFuncType> GrapeDispType;
+#endif
+// end of FUELCELL_PROBLEM block
+
+
+#if OTHER_PROBLEM
+
+#define AGRID 1 
+#define BGRID 0 
+#define SGRID 0
+//#include <dune/grid/sgrid.hh>
+
+#if AGRID 
+#include <dune/grid/albertagrid.hh>
+static const int dim = DUNE_PROBLEM_DIM; 
+static const int dimworld = DUNE_WORLD_DIM; 
+typedef AlbertaGrid<dim,dimworld> GR_GridType;
+#endif
+
+#if BGRID 
+#include <dune/grid/alu3dgrid.hh>
+static const int dim = 3; 
+static const int dimworld = 3; 
+typedef ALU3dGrid<dim,dimworld,hexa> GR_GridType;
+#endif
+
+#if SGRID 
+#include <dune/grid/sgrid.hh>
+static const int dim = DIM; 
+static const int dimworld = DIM_OF_WORLD; 
+typedef SGrid <dim, dimworld> GR_GridType;
+#endif
+
 typedef FunctionSpace <double ,double , dim, dim+2 >  GR_FunctionSpaceType;
-
-//typedef FunctionSpace <double ,double , dim, 1 >        GR_FunctionSpaceType;
-
-
+// typedef FunctionSpace <double ,double , dim, 1 >        GR_FunctionSpaceType;
 typedef DofManager<GR_GridType>                         GR_DofManagerType;
 typedef DofManagerFactory <GR_DofManagerType>           GR_DofManagerFactoryType;
 
@@ -82,19 +114,19 @@ typedef DofManagerFactory <GR_DofManagerType>           GR_DofManagerFactoryType
   typedef DefaultGridIndexSet<GR_GridType,GlobalIndex>   GR_IndexSetType;
 #endif
 
-
 typedef DefaultGridPart<GR_GridType,GR_IndexSetType>    GR_GridPartType;
-
 typedef LagrangeDiscreteFunctionSpace<GR_FunctionSpaceType,
 				     GR_GridPartType,0> GR_DiscFuncSpaceType;
 
 typedef DFAdapt < GR_DiscFuncSpaceType >                GR_DiscFuncType;
 typedef GrapeDataDisplay<GR_GridType , GR_DiscFuncType> GrapeDispType;
+#endif
+// end of OTHER_PROBLEM block
+
 
 #include "readdata.cc"
 #include "readparams.cc" 
 #include "readfile.cc" 
-
 
 int main(int argc, char **argv)
 {
