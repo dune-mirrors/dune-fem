3c4b98a1eea522947dba185a404da3546f5f76e2
Adrian Burri <burriad@mathematik.uni-freiburg.de>
2005-11-09 13:48:25 +0000

    Modified forward euler with TimeProvider class
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@112 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

---
+++ fem/misc/timenew.hh
@@ -0,0 +1,174 @@
+#ifndef NEW_TIMESTEPPING_HH
+#define NEW_TIMESTEPPING_HH
+
+#include <memory>
+#include "inverseoperatorfactory.hh"
+
+#include <strstream>
+#include "../visual/dx/dxdata.hh"
+
+using namespace Dune;
+
+class TimeProvider {
+public:
+  double time() const { return time_; }
+
+protected:
+  TimeProvider(double startTime = 0.0) : time_(startTime) {}
+  virtual ~TimeProvider() {}
+
+  void setTime(double time) { time_ = time; }
+  void augmentTime(double dt) { time_ += dt; }
+
+private:
+  double time_;
+};
+
+// Solve D_1 u' + D_0 u = f
+// Assume D_1 and D_0 to be linear operators, f independent from u
+template <class DiscreteFunctionImp>
+class Loop : public TimeProvider {
+public:
+  // Typedef
+  typedef DiscreteFunctionImp DiscreteFunctionType;
+
+public:  
+  // Constructor
+  Loop(DiscreteFunctionImp& sol,
+       double dt = 0.01,
+       double startTime = 0.0,
+       int startLevel = 0) :
+    TimeProvider(startTime),
+    sol_(sol), 
+    dt_(dt),
+    level_(startLevel) { 
+    std::cout << "Loop::Loop: dt = " << dt_ << std::endl;
+  }
+  
+  // Routines
+  void solve();
+  void solveUntil(int endLevel);
+  void solveUntil(double endTime);
+  
+  // Accessor functions
+  int level() const { return level_; }
+  DiscreteFunctionType& solution() { return sol_; }
+  
+protected:
+  virtual void next() = 0;
+  double dt() { return dt_; }
+  
+  DiscreteFunctionType& sol_;
+
+private: 
+  double dt_;
+  int level_;
+};
+
+template <class DiscreteFunctionImp>
+void Loop<DiscreteFunctionImp>::solve() {
+  // Preparations (nothing to do right now)
+  
+  // Call next()
+  next();
+  
+  // Update time and level
+  this->augmentTime(dt_);
+  ++level_;
+
+  // Output info
+  std::cout << this->time() << " time step finished! (dt = " << dt_ << ")" << std::endl;
+}
+
+template <class DiscreteFunctionImp>
+void Loop<DiscreteFunctionImp>::solveUntil(int endLevel) {
+  assert(endLevel > level_);
+  while (level_ <= endLevel) {
+    solve();
+  }
+}
+
+template <class DiscreteFunctionImp>
+void Loop<DiscreteFunctionImp>::solveUntil(double endTime) {
+  assert(endTime > this->time());
+  while (time() <= endTime) {
+    solve();
+  }
+}
+
+
+template <class DiscreteFunctionImp, class MappingImp>
+class ExplicitEuler : public Loop<DiscreteFunctionImp>
+{
+public:
+  typedef DiscreteFunctionImp DiscreteFunctionType;
+  typedef typename DiscreteFunctionType::DofIteratorType DofIteratorType;
+  typedef MappingImp MappingType;
+public:
+  ExplicitEuler(DiscreteFunctionImp& sol,
+                MappingType& timeOp,
+                MappingType& spaceOp,
+                InverseOperatorFactory<DiscreteFunctionType>& factory,
+                double dt,
+                double starttime = 0.0, 
+                int startlevel = 0) :
+    Loop<DiscreteFunctionImp>(sol, dt, starttime, startlevel),
+    timeOp_(timeOp),
+    spaceOp_(spaceOp),
+    factory_(factory)
+  {
+    std::cout << "ExplicitEuler::ExplicitEuler: dt = " << dt << std::endl;
+    // Set up inverse operator
+    std::auto_ptr<MappingType> tmp(factory.createOperator(timeOp));
+    inv_ = tmp;
+  }
+
+  virtual ~ExplicitEuler() {}
+
+  // do one timestep 
+  void next()
+  {
+    std::ostringstream filestream;
+    filestream << "upd" << this->level();
+
+    DiscreteFunctionType* temp = 
+      new DiscreteFunctionType("temp", this->sol_.getFunctionSpace());
+    DiscreteFunctionType* upd = 
+      new DiscreteFunctionType("upd", this->sol_.getFunctionSpace());
+
+    //DXWriter<typename DiscreteFunctionImp::DiscreteFunctionSpaceType, false> dx(upd->getFunctionSpace(), filestream.str());
+  
+
+
+    //DXWriter<FuncSpaceType,false> dx(pd_.space(), stream.str());
+
+    //dx.write(sol_, "bef");
+
+    spaceOp_(this->sol_, *upd);
+
+    //dx.write(*upd, "data");
+
+    (*(this->inv_))(*upd, *temp);
+
+    this->sol_.add(*temp, this->dt());
+
+    //dx.write(sol_, "aft");
+ 
+    delete temp;
+    delete upd;
+  }
+
+private:
+  ExplicitEuler();
+  ExplicitEuler(const ExplicitEuler&);
+  ExplicitEuler& operator= (const ExplicitEuler&);
+
+private:
+  MappingType& timeOp_;
+  MappingType& spaceOp_;
+
+  InverseOperatorFactory<DiscreteFunctionType>& factory_;
+  std::auto_ptr<MappingType> inv_;
+};
+
+#endif
