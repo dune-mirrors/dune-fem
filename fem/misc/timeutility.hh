4ddbfb0ba20e2bd85cf589a4b1cc7219a2b5fee9
Adrian Burri <burriad@mathematik.uni-freiburg.de>
2005-11-14 09:09:38 +0000

    Factored out TimeProvider in its own source file
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@127 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

---
+++ fem/misc/timeutility.hh
@@ -0,0 +1,44 @@
+#ifndef DUNE_TIMEUTILITY_HH
+#define DUNE_TIMEUTILITY_HH
+
+namespace Dune {
+
+  class TimeProvider {
+  public:
+    double time() const { return time_; }
+    
+    void provideTimeStepEstimate(double dtEstimate) {
+      dtEstimate_ = std::min(dtEstimate_, dtEstimate);
+    }
+  protected:
+    TimeProvider(double startTime = 0.0) : 
+      time_(startTime),
+      dtEstimate_()
+    {
+      resetTimeStepEstimate();
+    }
+    
+    virtual ~TimeProvider() {}
+
+    void setTime(double time) { time_ = time; }
+    void augmentTime(double dt) { time_ += dt; }
+    
+    void resetTimeStepEstimate() {
+      dtEstimate_ = std::numeric_limits<double>::max();
+    }
+    
+    double timeStepEstimate() const {
+      return dtEstimate_;
+    }
+  private:
+    TimeProvider(const TimeProvider&);
+    TimeProvider& operator=(const TimeProvider&);
+    
+  private:
+    double time_;
+    double dtEstimate_;
+  };
+  
+} // end namespace Dune
+
+#endif
