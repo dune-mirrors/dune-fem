6b76a0852b6d31eb3c318d0bfe15e3ace5baa777
Adrian Burri <burriad@mathematik.uni-freiburg.de>
2005-12-05 17:36:57 +0000

    Changed from TwistMapper to PointMapper
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@216 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/quadrature/cachingquad.hh
+++ fem/quadrature/cachingquad.hh
@@ -1,8 +1,14 @@
 #ifndef DUNE_CACHINGQUAD_HH
 #define DUNE_CACHINGQUAD_HH
 
+//- Dune includes
 #include <dune/common/misc.hh>
+#include <dune/grid/utility/structureutility.hh>
+
+//- Local includes
 #include "quadrature.hh"
+#include "../caching/pointmapper.hh"
+#include "../caching/twistprovider.hh"
 
 namespace Dune {
 
@@ -15,19 +21,15 @@ namespace Dune {
   template <typename GridImp>
   class CacheQuadrature<GridImp, 0> 
   {
-  private:
     typedef Quadrature<
       typename GridImp::ctype, GridImp::dimension> QuadratureType;
-    
-  private:
-    QuadratureType quad_;
-
-  public:
     typedef typename QuadratureType::CoordinateType CoordinateType;
     typedef typename GridImp::template Codim<0>::Entity Entity;
     typedef typename GridImp::ctype ct;
 
   public:
+    // * register quadrature points here?
+    //! Constructor which initializes the quadrature with a codim-0 entity
     CacheQuadrature(Entity& en, int order) :
       quad_(en.geometry().type(), order)
     {}
@@ -71,28 +73,39 @@ namespace Dune {
       return quad_.geo();
     }
 
+    GeometryType elementGeo() const {
+      return geo();
+    }
+
     //! Additional method to map quadrature points to caching points.
     //! For codim-0 entities, the quadrature points are the same as the
     //! caching points.
     int cachingPoint(int quadraturePoint) const {
       return quadraturePoint;
     }
+    
+  private:
+    QuadratureType quad_;
+
   };
 
+  // * Idea: to make the concepts sounder, why not transforming the 
+  // * the quadrature point into the reference element with
+  // * intersectionSelfLocal.global() ?
+  //! Specialisation for codim 1
   template <class GridImp>
   class CacheQuadrature<GridImp, 1> 
   {
   private:
     enum { codim = 1 };
-    typedef Quadrature<
-      typename GridImp::ctype, GridImp::dimension-codim> QuadratureType;
-
-  private:
-    QuadratureType quad_;
-    int faceTwist_;
-    int faceNumber_;
+    enum { dim = GridImp::dimension };
 
   public:
+    typedef Quadrature<
+      typename GridImp::ctype, dim-codim> QuadratureType;
+
+    //! Attributes the face either to the inside or the outside according
+    //! to the definition of the intersection iterator
     enum Side { INSIDE, OUTSIDE };
 
     typedef typename QuadratureType::CoordinateType CoordinateType;
@@ -100,6 +113,8 @@ namespace Dune {
     typedef typename GridImp::ctype ct;
 
   public:
+    //! Constructor
+    //! Constructs a quadrature on an intersection.
     CacheQuadrature(IntersectionIterator& it, 
                     int order, 
                     int twist, // Move inside once the twist is in interface
@@ -111,7 +126,9 @@ namespace Dune {
       faceTwist_(twist), // it.twistSelf(), it.twistNeighbor() later on
       faceNumber_(side == INSIDE ?
                   it.numberInSelf() :
-                  it.numberInNeighbor())
+                  it.numberInNeighbor()),
+      // ? replace by pointer?
+      mapper_(TwistProvider<ct, dim-codim>::getTwistMapper(quad_, twist))
     {}
 
    //! The total number of quadrature points.
@@ -148,9 +165,14 @@ namespace Dune {
       return quad_.order();
     }
 
-    //! The geometry type the quadrature points belong to.
+    //! The geometry type the face quadrature points belong to.
     GeometryType geo() const {
       return quad_.geo();
+    }
+
+    //! The geometry type of the attached element
+    GeometryType elementGeo() const {
+      return elementGeo_;
     }
 
     //! Additional method to map quadrature points to caching points.
@@ -161,89 +183,17 @@ namespace Dune {
       assert(false); // More to do
       return 0;
     }
-  };
-
-  // Alternative: Spezialisierung fuer dim, codim
-  /*
-  template <typename ct, int dim>
-  class CachingQuadrature {
-  public:
-    enum Side {INSIDE, OUTSIDE};
-  public:
-    template <class Entity>
-    CachingQuadrature(Entity& en, int order) :
-      quad_(en.geometry().type(), order),
-      side_(INSIDE),
-      twist_(0),
-      caching_(quad_.nop())
-    {}
-
-    template <class IntersectionIterator>
-    CachingQuadrature(IntersectionIterator& it, int order, 
-                      Side side, int twist) :
-      quad_(side == INSIDE ? 
-            it.inside()->geometry().type() : it.outside()->geometry().type(),
-            order),
-      side_(side),
-      twist_(twist),
-      caching_(quad_.nop())
-    {}
-
- 
-    //! The total number of quadrature points.
-    int nop() const {
-      return quad_.nop();
-    }
-
-    //! Access to the ith quadrature point.
-    const CoordinateType& point(size_t i) const {
-      return quad_.point(i);
-    }
-
-    //! Access to the weight of quadrature point i.
-    //! The quadrature weights sum up to the volume of the respective reference
-    //! element.
-    const ct& weight(size_t i) const {
-      return quad_.weight(i);
-    }
-
-    //! A unique id per quadrature type.
-    //! Quadratures are considered as distinct when they differ in the
-    //! following points: geometry type, order, dimension and implementation.
-    //! \note At the time of writing this, there is only one implementation
-    //! per geometry type, order and dimension provided, but the concept is
-    //! easily extendible beyond that.
-    size_t id() const {
-      return quad_.id();
-    }
-
-    //! The actual order of the quadrature.
-    //! The actual order can be higher as the desired order when no 
-    //! implementation for the desired order is found.
-    int order() const {
-      return quad_.order();
-    }
-
-    //! The geometry type the quadrature points belong to.
-    GeometryType geo() const {
-      return quad_.geo();
-    }
-
-    //! Additional method to map quadrature points to caching points.
-    int cachingPoint(int quadraturePoint) const {
-      assert(quadraturePoint >= 0 && quadraturePoint < nop());
-      return caching_[i];
-    }
 
   private:
-    Quadrature<ct, dim> quad_;
-    Side side_;
-    int twist_;
+    QuadratureType quad_;
+    int faceTwist_;
+    int faceNumber_;
+    GeometryType elementGeo_;
 
-    // To be replaced (Use caching provider here as well)
-    std::vector<int> caching_;
+    const PointMapper& mapper_;
+
   };
-  */
+
 }
 
 #endif
