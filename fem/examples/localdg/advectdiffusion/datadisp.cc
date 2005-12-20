a8c219645d4f537df3386241abc29fd4950db0fa
Andreas Dedner <A.S.Dedner@warwick.ac.uk>
2005-12-20 15:11:25 +0000

    *** empty log message ***
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@248 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

---
+++ fem/examples/localdg/advectdiffusion/datadisp.cc
@@ -0,0 +1,81 @@
+//************************************************************
+//
+//  (C) written and directed by Robert Kloefkorn 
+//
+//************************************************************
+#include <iostream>
+#include <vector>
+#include <assert.h>
+#include <string.h>
+
+#include <config.h>
+#include <dune/common/stdstreams.cc>
+
+using namespace Dune;
+
+
+#define LARGE 1.0E308
+
+static const int dim = DUNE_PROBLEM_DIM; 
+static const int dimworld = DUNE_WORLD_DIM; 
+
+
+#include <dune/fem/dfadapt.hh>
+#include <dune/fem/lagrangebase.hh>
+#include <dune/fem/dfadapt.hh>
+#include <dune/common/stack.hh>
+
+#include <dune/grid/common/leafindexset.hh>
+#include <dune/grid/common/gridpart.hh>
+
+#include <dune/io/file/grapedataio.hh>
+
+
+typedef double REAL;
+
+#include <dune/io/visual/grapedatadisplay.hh>
+#include <dune/io/visual/combinedgrapedisplay.hh>
+
+
+#include "../../../visual/grape/datadisp/printhelp.cc"
+
+#include "models.hh"
+
+typedef GridType                                        GR_GridType;
+typedef DgType::SpaceType                               GR_FunctionSpaceType;
+typedef DofManager<GR_GridType>                         GR_DofManagerType;
+typedef DofManagerFactory <GR_DofManagerType>           GR_DofManagerFactoryType;
+typedef DgType::IndexSetType                            GR_IndexSetType;
+typedef DgType::GridPartType                                GR_GridPartType;
+typedef DgType::DiscreteFunctionSpaceType               GR_DiscFuncSpaceType;
+typedef DgType::DestinationType                         GR_DiscFuncType;
+typedef GrapeDataDisplay<GR_GridType > GrapeDispType;
+
+#include "../../../visual/grape/datadisp/readdata.cc"
+#include "../../../visual/grape/datadisp/readparams.cc" 
+#include "../../../visual/grape/datadisp/readfile.cc" 
+
+
+int main(int argc, char **argv)
+{
+
+  if (argc < 2)
+  {
+    print_help("datadisp");
+    return(0);
+  }   
+
+  if (!strcmp(argv[1], "-h") || !strcmp(argv[1], "-help"))
+  {
+      print_help("dunedisp");
+      return(0);
+  }
+
+
+
+  if(argc > 2) 
+    return readParameterList(argc,argv);
+  else 
+    return readParameterFile(argc,argv);
+ 
+}
