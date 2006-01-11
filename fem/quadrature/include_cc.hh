f1d5372da09c72c4ca380da9cfd6e33d9971232d
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2006-01-11 18:19:50 +0000

    includes all cc
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@261 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

---
+++ fem/quadrature/include_cc.hh
@@ -0,0 +1,9 @@
+#ifndef QUADRATURE_INCLUDE_CC_HH
+#define QUADRATURE_INCLUDE_CC_HH
+
+#include "idprovider.cc"
+#include "gausspoints.cc"
+#include "prismpoints.cc"
+#include "pyramidpoints.cc"
+
+#endif 
