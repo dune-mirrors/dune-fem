2a707055ee911d5c88643320bfa45f62fbb0a3a5
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2006-03-24 15:20:21 +0000

    former dune/fem/common/base*
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@369 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

---
+++ fem/basefunctions/common/basefunctionfactory.hh
@@ -0,0 +1,35 @@
+#ifndef DUNE_BASEFUNCTIONFACTORY_HH
+#define DUNE_BASEFUNCTIONFACTORY_HH
+
+#include "basefunctions.hh"
+
+#include <dune/grid/common/grid.hh>
+
+namespace Dune {
+
+  //! \brief Interface class for the generation of base functions.
+  //! For every concrete set of base functions, derive your own concrete
+  //! base function factory.
+  template <class FunctionSpaceImp>
+  class BaseFunctionFactory 
+  {
+  public:
+    typedef BaseFunctionInterface<FunctionSpaceImp> BaseFunctionType;
+  public:
+    BaseFunctionFactory(GeometryType geo) :
+      geo_(geo)
+    {}
+
+    virtual BaseFunctionType* baseFunction(int num) const = 0;
+
+    virtual int numBaseFunctions() const = 0;
+
+    GeometryType geometry() const { return geo_; }
+  private:
+    GeometryType geo_;
+  };
+
+
+} // end namespace Dune
+
+#endif
