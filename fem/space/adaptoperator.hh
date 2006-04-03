a2d3ee5d99cde682ef8f9587ca62ad346f3ff965
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2006-04-03 16:02:40 +0000

    moved from dune/fem/transfer/ to here.
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@411 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/space/adaptoperator.hh
+++ fem/space/adaptoperator.hh
@@ -1,181 +1,314 @@
-#ifndef DUNE_DGADAPTOPERATORIMP_HH
-#define DUNE_DGADAPTOPERATORIMP_HH
+#ifndef DUNE_ADAPTOPERATORIMP_HH
+#define DUNE_ADAPTOPERATORIMP_HH
 
-#include <dune/fem/transfer/adaptoperator.hh>
+//- Dune includes 
+#include <dune/common/array.hh>
 
-#include "../quadrature/cachequad.hh"
-#include "dgspace.hh"
-
-/************************************************************
-1) Gewichte zwischen Vater/Sohn default Implementieren auf Gitter
-   (father.weight(son) oder so
-2) Caching der Basisfunktionen fuer Vater-BasisFunktionen fuer
-   Kinderquadraturen
-*************************************************************/
+//- local includes 
+#include "../operator/common/objpointer.hh"
+#include "../quadrature/elementquadrature.hh"
 
 namespace Dune{
 
-//***********************************************************************
-
-/** \brief This is a general restriction/prolongation operator 
+/** @defgroup AdaptOperatorImp AdaptOperatorImp
+    @ingroup Adaptation
+    
+  @{
  */
- template <class DiscreteFunctionType> class RestProlOperator;
-/** \brief This is a restriction/prolongation operator for DG data. 
- */
- template <template <class> class DiscFunc,
-  	  class FunctionSpaceImp, 
-   	  class GridPartImp, 
-   	  int polOrd, 
-   	  template <class> class StorageImp> 
- class RestProlOperator<DiscFunc<
-   DiscontinuousGalerkinSpace<FunctionSpaceImp, GridPartImp, polOrd,StorageImp> 
- > > 
- {
-   typedef DiscFunc<DiscontinuousGalerkinSpace<FunctionSpaceImp, 
-					       GridPartImp, 
-					      polOrd,
-					      StorageImp> > DiscreteFunctionType;
-  typedef typename DiscreteFunctionType::FunctionSpaceType FunctionSpaceType;
-  typedef typename DiscreteFunctionType::GridType GridType;
-  typedef typename DiscreteFunctionType::LocalFunctionType LocalFunctionType;
 
-  typedef typename DiscreteFunctionType::RangeFieldType RangeFieldType;
-  typedef typename DiscreteFunctionType::DomainType DomainType;
-  typedef CachingQuadrature<GridType,0> QuadratureType;
-   typedef typename GridType::template Codim<0>::Entity::Geometry Geometry;
-public:  
-  //! Constructor
-  RestProlOperator ( DiscreteFunctionType & df ) : 
-    df_ (df) , quadord_(2*df.getFunctionSpace().polynomOrder()),
-    weight_(-1.0)
+/*! This could be seen as a hack, but is not
+  With this method we define the class CombineRestProl which is only 
+  for combining of local grid operations without using virtual methods.
+  The interface of the two defined methods of the class (restrictLocal and
+  prolongLocal) is given by the implementation (see below ) and 
+  has to be the same for all local operators you want to combine 
+*/
+#define PARAM_CLASSNAME CombinedRestProl 
+#define PARAM_FUNC_1 restrictLocal 
+#define PARAM_FUNC_2 prolongLocal 
+#define PARAM_FUNC_3 calcFatherChildWeight 
+#include <dune/fem/common/combine.hh>
+
+/*! Combination of different AdaptOperators
+
+ This Class is the result of a combination of different
+ AdaptationOperators. It is the same principle as with Mapping and
+ DiscreteOperatorImp. 
+*/ 
+class AdaptMapping 
+{
+public:
+  //! default constructor 
+  AdaptMapping () : am_ (0) {}
+
+  //! destructor 
+  virtual ~AdaptMapping () {}
+  
+  //! all adaptation operators have this method which adapts the
+  //! corresponding grid and organizes the restriction prolongation process
+  //! of the underlying function spaces
+  virtual void adapt () const 
   {
-  }
-
-  //! restrict data to father 
-  template <class EntityType>
-  void restrictLocal ( EntityType &father, EntityType &son, 
-		       bool initialize ) const
-  {
-    typename FunctionSpaceType::RangeType ret (0.0);
-    typename FunctionSpaceType::RangeType phi (0.0);
-    assert( !father.isLeaf() );
-    const RangeFieldType weight = 
-      (weight_ < 0.0) ? (calcWeight(father,son)) : weight_; 
-
-    LocalFunctionType vati_ = df_.localFunction( father);
-    LocalFunctionType sohn_ = df_.localFunction( son   );
-
-    QuadratureType quad(son,quadord_);
-    const typename FunctionSpaceType::BaseFunctionSetType & baseset =
-      vati_.getBaseFunctionSet();
-    const int nop=quad.nop();
-    const Geometry& geometryInFather = son.geometryInFather();
-    if(initialize) {
-      for(int i=0; i<vati_.numDofs(); i++) {
-	vati_[i] = 0;
-      }
+    //std::cout << "called AdaptMapping::adapt()" << std::endl;
+    if(am_) am_->adapt();  
+    else 
+    {
+      std::cerr << "WARNING: adapt! \n";
     }
-    for(int qP = 0; qP < nop; qP++) {
-      sohn_.evaluate(son,quad,qP,ret);
-      for(int i=0; i<vati_.numDofs(); i++) {
-	baseset.eval(i,geometryInFather.global(quad.point(qP)),phi);
-	vati_[i] += quad.weight(qP) * weight * (ret * phi) ;
-      }
-    }
-  }
+  };
 
-  //! prolong data to children 
-  template <class EntityType>
-  void prolongLocal ( EntityType &father, EntityType &son, bool initialize ) const
+    //! Assignement operator
+  AdaptMapping & operator = (const AdaptMapping & am)
   {
-    //assert( son.state() == REFINED );
-    typename FunctionSpaceType::RangeType ret (0.0);
-    typename FunctionSpaceType::RangeType phi (0.0);
-    LocalFunctionType vati_ = df_.localFunction( father);
-    LocalFunctionType sohn_ = df_.localFunction( son   );
-    for(int i=0; i<sohn_.numDofs(); i++) 
-      sohn_[i] = 0.;
-
-    QuadratureType quad(son,quadord_);
-    const typename FunctionSpaceType::BaseFunctionSetType & baseset =
-      sohn_.getBaseFunctionSet();
-    const Geometry& geometryInFather = son.geometryInFather();
-    const int nop=quad.nop();
-    for(int qP = 0; qP < nop; qP++) {
-      vati_.evaluateLocal(father,geometryInFather.global(quad.point(qP)),ret);
-      for(int i=0; i<sohn_.numDofs(); i++) {
-	baseset.eval(i,quad,qP,phi);
-	sohn_[i] += quad.weight(qP) * (ret * phi) ;
-      }
-    }
+      /** \todo This const-casting seems strange to me! */
+    am_ = const_cast<AdaptMapping *> (&am);
+    return (*this);
   }
-
-private:
-  template <class EntityType>
-  RangeFieldType calcWeight (EntityType &father, EntityType &son) const
-  {
-    QuadratureType quad(father,1);
-    return std::abs(son.geometry().integrationElement(quad.point(0)) /
-              father.geometry().integrationElement(quad.point(0)));
-  }
-
-  mutable DiscreteFunctionType & df_;
-  int quadord_;
-  mutable RangeFieldType weight_;
+private: 
+  AdaptMapping *am_;
 };
 
 
-/** \brief This is a restriction/prolongation operator for DG data of order zero. 
+/*! \brief This class manages the adaptation process. 
+ If the method adapt is called, then the grid is adapted and also 
+ all the data belonging to the given dof manager will be rearranged 
+ for data set where it is necessary to keep the data.
  */
- template <template <class> class DiscFunc,
-  	  class FunctionSpaceImp, 
-   	  class GridPartImp, 
-   	  template <class> class StorageImp> 
- class RestProlOperator<DiscFunc<
-   DiscontinuousGalerkinSpace<FunctionSpaceImp,GridPartImp,0,StorageImp> 
- > > 
- {
-   typedef DiscFunc<DiscontinuousGalerkinSpace<FunctionSpaceImp, 
-					       GridPartImp, 
-					       0,
-					       StorageImp> > DiscreteFunctionType;
-  typedef typename DiscreteFunctionType::FunctionSpaceType FunctionSpaceType;
-  typedef typename DiscreteFunctionType::GridType GridType;
+template <class GridType, class RestProlOperatorImp >
+class AdaptOperator
+: 
+public AdaptMapping , public ObjPointerStorage 
+{  
+  typedef AdaptOperator<GridType,RestProlOperatorImp> MyType;
+  typedef DofManager< GridType > DofManagerType; 
+  typedef DofManagerFactory<DofManagerType> DofManagerFactoryType;
+public:
+  typedef typename GridType :: Traits :: LocalIdSet LocalIdSet;
+  //! create DiscreteOperator with a LocalOperator 
+  AdaptOperator (GridType & grid, RestProlOperatorImp & rpOp) 
+    : grid_(grid) 
+    , dm_ ( DofManagerFactoryType::getDofManager(grid_) )
+    , rpOp_ (rpOp) 
+  {}
+
+  virtual ~AdaptOperator () {}
+
+  /*! 
+   Add to AdaptOperators means that the RestProlOperators will be combined.
+   See DiscreteOperatorImp.
+   */
+  template <class RestProlOperatorType> 
+  AdaptOperator<GridType,
+  CombinedRestProl <RestProlOperatorImp,RestProlOperatorType> > & 
+  operator + (const AdaptOperator<GridType,RestProlOperatorType> &op)
+  {
+    //std::cout << "Operator + of AdaptOperator\n";
+    typedef AdaptOperator<GridType,RestProlOperatorType> CopyType;
+    typedef CombinedRestProl <RestProlOperatorImp,RestProlOperatorType> COType;
+     
+    COType *newRPOp = new COType ( rpOp_  , const_cast<CopyType &> (op).getRestProlOp() );
+
+    typedef AdaptOperator < GridType, COType > OPType;
+   
+    OPType *adaptOp = new OPType ( grid_ , *newRPOp );    
+
+    // memorize this new generated object because is represents this
+    // operator and is deleted if this operator is deleted
+    saveObjPointer( adaptOp , newRPOp );
+   
+    //std::cout << "Created " << adaptOp << " \n";
+    return *adaptOp;
+  }
+ 
+  //! no public method, but has to be public, because all AdaptOperators 
+  //! must be able to call this method and the template parameters are
+  //! allways different 
+  RestProlOperatorImp & getRestProlOp ()  
+  {
+    return rpOp_;
+  }
+  
+  //! adapt defines the grid walkthrough before and after grid adaptation.
+  //! Note that the LocalOperator can be an combined Operator 
+  //! Domain and Range are defined through class Operator
+  void adapt () const 
+  {
+    bool restr = grid_.preAdapt();  
+
+    if(restr)
+    {
+      dm_.resizeForRestrict();
+      
+      typedef typename DofManagerType :: IndexSetRestrictProlongType IndexSetRPType;
+      typedef CombinedRestProl <IndexSetRPType,RestProlOperatorImp> COType;
+      COType tmpop ( dm_.indexSetRPop() , rpOp_ );
+
+      typedef typename GridType::template Codim<0>::LevelIterator LevelIterator;
+
+      // make run through grid 
+      for(int l=0; l<grid_.maxLevel(); l++)
+      {
+        LevelIterator endit  = grid_.template lend<0>   ( l );
+        for(LevelIterator it = grid_.template lbegin<0> ( l );
+              it != endit; ++it )
+        {
+          hierarchicRestrict( *it , tmpop );
+        }
+      }
+    }
+    
+    bool ref = grid_.adapt();
+
+    if(ref)
+    {
+      dm_.resize();
+      typedef typename DofManagerType :: IndexSetRestrictProlongType IndexSetRPType;
+      typedef CombinedRestProl <IndexSetRPType,RestProlOperatorImp> COType;
+      COType tmpop ( dm_.indexSetRPop() , rpOp_ );
+      
+      typedef typename GridType::template Codim<0>::LevelIterator LevelIterator;
+
+      // make run through grid 
+      LevelIterator endit = grid_.template lend<0>   ( 0 );
+      for(LevelIterator it    = grid_.template lbegin<0> ( 0 );
+            it != endit; ++it )
+      {
+        hierarchicProlong( *it , tmpop );
+      }
+    }
+
+    // if grid was coarsend of refined, do dof compress 
+    if(restr || ref)
+      dm_.dofCompress();
+
+    // here the communicate and load-balancing should be called 
+/*
+#ifdef _ALU3DGRID_PARALLEL_
+    grid_.loadBalance(dm_);
+    grid_.communicate(dm_);
+#endif
+*/
+    
+    // do cleanup 
+    grid_.postAdapt();
+  }
+  
+private:
+  // make hierarchic walk trough 
+  template <class EntityType, class RestrictOperatorType  >
+  void hierarchicRestrict ( EntityType &en, RestrictOperatorType & restop ) const 
+  {
+    if(!en.isLeaf())
+    {
+      typedef typename EntityType::HierarchicIterator HierarchicIterator; 
+      HierarchicIterator it    = en.hbegin( en.level() + 1 );
+
+      // if the children have children then we have to go deeper 
+      HierarchicIterator endit = en.hend  ( en.level() + 1 );
+     
+      // ok because we checked en.isLeaf 
+      if(!it->isLeaf()) return;
+      
+      // true for first child, otherwise false 
+      bool initialize = true;
+      
+      for( ; it != endit; ++it)
+      {
+        if( (*it).mightBeCoarsened() )
+        {
+          restop.restrictLocal( en , *it, initialize);     
+          initialize = false;
+        }
+      }
+    }
+  }
+
+  template <class EntityType, class ProlongOperatorType >
+  void hierarchicProlong ( EntityType &en, ProlongOperatorType & prolop ) const 
+  {
+    typedef typename EntityType::HierarchicIterator HierarchicIterator; 
+    
+    bool initialize = true;
+    
+    HierarchicIterator endit  = en.hend  ( grid_.maxLevel() );
+    for(HierarchicIterator it = en.hbegin( grid_.maxLevel() ) ; 
+        it != endit; ++it)
+    {
+      assert( !en.isLeaf() );
+      if( (*it).wasRefined() )
+      {
+        prolop.prolongLocal( *(it->father()), *it , initialize );     
+        initialize = false;
+      }
+    }
+  }
+ 
+  //! corresponding grid 
+  mutable GridType & grid_;
+
+  //! DofManager corresponding to grid
+  mutable DofManagerType & dm_;
+  
+  //! Restriction and Prolongation Operator 
+  mutable RestProlOperatorImp & rpOp_;
+};
+
+
+//***********************************************************************
+
+/** \brief This is a simple restriction/prolongation operator for
+ piecewise constant data stored on elements. 
+*/
+template <class DiscreteFunctionType>
+class RestProlOperatorFV
+{
   typedef typename DiscreteFunctionType::LocalFunctionType LocalFunctionType;
+
+  typedef typename DiscreteFunctionType::GridType GridType;
 
   typedef typename DiscreteFunctionType::RangeFieldType RangeFieldType;
   typedef typename DiscreteFunctionType::DomainType DomainType;
-  typedef CachingQuadrature<GridType,0> QuadratureType;
+  typedef ElementQuadrature<GridType,0> BaryQuadType;
 public:  
   //! Constructor
-  RestProlOperator ( DiscreteFunctionType & df ) : 
-    df_ (df),
-    weight_(-1.0)
-  {}
+  RestProlOperatorFV ( DiscreteFunctionType & df ) : df_ (df), weight_(-1.0)
+  {
+  }
+
+  //! if weight is set, then ists assumend that we have always the same
+  //! proportion between fahter and son volume 
+  void setFatherChildWeight (const RangeFieldType& val) const
+  {
+    // volume of son / volume of father  
+    weight_ = val; 
+  }
   
   //! restrict data to father 
   template <class EntityType>
-  void restrictLocal ( EntityType &father, EntityType &son, 
-		       bool initialize ) const
+  void restrictLocal ( EntityType &father, EntityType &son, bool initialize ) const
   {
     assert( !father.isLeaf() );
 
-    // if weight < 0.0 , weight has not been calculated
-    const RangeFieldType weight = 
-      (weight_ < 0.0) ? (calcWeight(father,son)) : weight_; 
-    
-    LocalFunctionType vati_ =df_.localFunction( father);
-    LocalFunctionType sohn_ =df_.localFunction( son   );
+    const RangeFieldType weight = (weight_ < 0.0) ? (calcWeight(father,son)) : weight_; 
 
-    if(initialize) {
-      for(int i=0; i<vati_.numDofs(); i++) {
-        vati_[i] = weight * sohn_[i];
+    assert( weight > 0.0 );
+    
+    LocalFunctionType vati = df_.localFunction( father);
+    LocalFunctionType sohn = df_.localFunction( son   );
+
+    const int numDofs = vati.numDofs();
+    if(initialize)
+    {
+      for(int i=0; i<numDofs; ++i)
+      {
+        vati[i] = weight * sohn[i];
       }
     }
     else 
     {
-      for(int i=0; i<vati_.numDofs(); i++) {
-        vati_[i] += weight * sohn_[i];
+      for(int i=0; i<numDofs; ++i)
+      {
+        vati[i] += weight * sohn[i];
       }
     }
   }
@@ -184,22 +317,25 @@ public:
   template <class EntityType>
   void prolongLocal ( EntityType &father, EntityType &son, bool initialize ) const
   {
-    LocalFunctionType vati_ = df_.localFunction( father);
-    LocalFunctionType sohn_ = df_.localFunction( son   );
-    const int numDofs = vati_.numDofs();
-    for(int i=0; i<numDofs; i++) {
-      sohn_[i] = vati_[i];
+    LocalFunctionType vati = df_.localFunction( father);
+    LocalFunctionType sohn = df_.localFunction( son   );
+    const int numDofs = vati.numDofs();
+    for(int i=0; i<numDofs; ++i)
+    {
+      sohn[i] = vati[i];
     }
   }
 
 private:
+  //! calculates the weight, i.e. (volume son)/(volume father)
   template <class EntityType>
   RangeFieldType calcWeight (EntityType &father, EntityType &son) const
   {
-    QuadratureType quad(father,1);
+    const BaryQuadType quad(father,0);
     return std::abs(son.geometry().integrationElement(quad.point(0)) /
               father.geometry().integrationElement(quad.point(0)));
   }
+  
   mutable DiscreteFunctionType & df_;
   mutable RangeFieldType weight_;
 };
