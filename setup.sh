f15394f8f7f8069031db17ee283885dcdd0aae2d
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2006-05-04 13:52:54 +0000

    default settings.
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@465 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- setup.sh
+++ setup.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-./autogen.sh --gnu --with-dune=$HOME/work/Dune/src/ \
+DUNEDIR=/hosts/morgoth/raid5/dune/
+
+./autogen.sh --gnu \
+  --with-dunecommon=$HOME/dune-common \
+  --with-dunegrid=$HOME/dune-grid \
 	--with-problem-dim=$1 \
 	--with-world-dim=$2 \
-  --with-grape=$HOME/DuneAdds/modules/GrapeNew \
-  --with-alberta=$HOME/DuneAdds/modules/alberta-1.2 \
+  --with-grape=$DUNEDIR/modules/grape \
+  --x-includes=/usr/X11R6/include \
+  --x-libraries=/usr/X11R6/lib \
+  --with-alberta=$DUNEDIR/modules/alberta \
+  --with-alugrid=$DUNEDIR/modules/alugrid \
+  --with-amiramesh=$DUNEDIR/modules/amiramesh \
+  --with-ug=$DUNEDIR/modules/ug \
   --enable-blas \
-#  --with-alugrid=$HOME/DuneAdds/modules/alu3dgrid \
 #  --nodebug \
 #  --optim
-#  --with-alberta=$HOME/DuneAdds/modules/alberta-1.2 \
-#	--with-blas-lib=/hosts/sauron/graid/robertk/Traube/liblinuxGL_dune/ \
-#./autogen.sh --intel --with-dune=/hosts/sauron/graid/robertk/Dune/robertk/ \
-#./autogen.sh --intel --with-dune=/tmp/robertk/Schrott  \
-  
