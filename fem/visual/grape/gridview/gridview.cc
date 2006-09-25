be867b428acf94c39e924d4fe9a483f22cf7ed08
Bernard Haasdonk <haasdonk@mathematik.uni-stuttgart.de>
2006-09-25 13:42:57 +0000

    adopted to dfg-parser and grid-selection as make-arguments
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@607 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/visual/grape/gridview/gridview.cc
+++ fem/visual/grape/gridview/gridview.cc
@@ -1,205 +1,131 @@
-//***************************************************************
-//
-//  (C) written and directed by Robert Kloefkorn 
-//
-//***************************************************************
+/**************************************************************************
+**       Title: 
+**    $RCSfile$
+**   $Revision$$Name$
+**       $Date$
+**   Copyright: GPL Author: robertk
+** Description: View of an arbitrary grid.
+**              Either a DGF-File is required as parameter or a 
+**              Alberta/UG/ALUGrid macrofile. An addditional integer can be passed indicating the 
+**              additional refinement levels.  
+**
+**              Select gridtype at compile time by
+**
+**              make GRIDTYPE=ALBERTAGRID GRIDDIM=2
+**
+**              or one of UGGRID,ALUGRID_SIMPLEX,ALUGRID_CUBE, YASPGRID, SGRID
+**
+**              GRIDTYPE=YASPGRID:
+**                 compiles and displays dgf-file
+**              GRIDTYPE=SGRID:
+**                 compiles and displays dgf-file
+**              GRIDTYPE=ALBERTAGRID:
+**                 compiles and displays dgf-file and ALberta-macrofile
+**              GRIDTYPE=ALUGRID_SIMPLEX:
+**                 compiles and displays dgf-file and ALugrid-macrofile
+**              GRIDTYPE=ALUGRID_CUBE GRIDDIM=3:
+**                 compiles, but throws error in display of dgf-file, successful ALugrid-macrofile
+**              GRIDTYPE=UGGRID:
+**                 error in compilation
+** 
+**              parallel grids are not tested/supported
+**              
+**-------------------------------------------------------------------------
+**
+**  $Log$
+**  Revision 1.10  2006/09/25 13:42:57  haasdonk
+**  adopted to dfg-parser and grid-selection as make-arguments
+**
+**
+**************************************************************************/
+
 #include <iostream>
+#include <string>
 #include <config.h>
-
-// set exactly one of the following to 1, the others 0
-#define SGRID 0
-#define UGRID 0
-#define AGRID 0
-#define BGRID 1
-#define YGRID 0
-
-#if SGRID
-#include <dune/grid/sgrid.hh>
-using namespace Dune;
-
-#undef dim 
-#undef dim_OF_WORLD
-
-static const int dim = 3;
-static const int dimworld = 3;
-
-typedef SGrid <dim,dimworld> GridType;
-#endif
-
-#if UGRID
-
-#include <dune/grid/uggrid.hh>
-#include <dune/io/file/amirameshreader.hh>
-#include <dune/io/file/amirameshwriter.hh>
-
-#undef dim 
-#undef dimworld
-
-#ifdef _2 
-static const int dim = 2;
-static const int dimworld = 2;
-#else 
-static const int dim = 3;
-static const int dimworld = 3;
-#endif
-using namespace Dune;
-typedef UGGrid< dim,dimworld> GridType;
-#endif
-
-#if YGRID
-#include <dune/grid/yaspgrid.hh>
-static const int dim = 3;
-static const int dimworld = 3;
-using namespace Dune;
-typedef YaspGrid <dim,dimworld> GridType;
-#endif
-
-#if AGRID
-#include <dune/grid/albertagrid.hh>
-using namespace Dune;
-//static const int dim = 3;
-//static const int dimworld = 3;
-static const int dim = DUNE_PROBLEM_DIM;
-static const int dimworld = DUNE_WORLD_DIM;
-typedef AlbertaGrid<dim,dimworld> GridType;
-#endif
-
-#if BGRID
-
-static const int dim = 3;
-static const int dimworld = 3;
-
-#define PROBLEMdim 3
-#define WORLDdim 3
-
-// the following file does not exist... renamed or what?
-//#include <dune/grid/alu3dgrid/includecc.cc>
-//#include <dune/grid/alu3dgrid.hh>
-#include <dune/grid/alugrid.hh>
-using namespace Dune;
-typedef ALU3dGrid<dim,dimworld,tetra> GridType;
-//typedef ALU3dGrid<dim,dimworld,hexa> GridType;
-// the new syntax should be the following, but incurs error
-// typedef ALUCubeGrid<dim,dimworld> GridType; 
-
-#endif
-
-#include <dune/grid/io/visual/grapegriddisplay.hh>
 #include <dune/common/stdstreams.cc>
+#include <dune/grid/io/file/dgfparser/gridtype.hh>
+#include <dune/grid/io/visual/grapegriddisplay.hh>
+
+using namespace Dune;
+using namespace std;
 
 int main (int argc, char **argv)
 {
-  // this leads to the same number of points for SGrid and AlbertGrid
-#if SGRID
-  int n[dim];
-  double h[dim];
-  for(int i=0; i<dim; i++)
-  {
-    n[i] = 3; h[i] = 1.0;
-  }
-  GridType grid ((int *) &n, (double *) &h );
-  int level = 0;
-  if(argc == 2) level = atoi(argv[1]);
-  grid.globalRefine (level);
-#endif
- 
-#if UGRID
-  GridType grid(200,30);
+// check whether dgf-file
+
   if(argc < 2)
   {
-    fprintf(stderr,"usage: %s <AmiraMesh> \n",argv[0]);
+    fprintf(stderr,"usage: %s <MacroFile> \n",argv[0]);
     abort();
   }
-  int level = 0;
-  if(argc == 3)
-    level = atoi(argv[2]);
 
-  AmiraMeshReader< GridType > am;
-  am.read ( grid, argv[1] );
-  //grid.globalRefine (level);
-  AmiraMeshWriter< GridType > amw;
-  std::string fn (  "testgrid.am" );
-  amw.writeGrid ( grid ,fn );
-#endif
-
-#if YGRID
-  // initialize MPI
-  MPI_Init(&argc,&argv);
-       
-  std::cout << "We are using YaspGrid! \n";
-
-//  Vec<double,dim> lang = 1.0;
-//  Vec<int,dim>  anz = 4;
-//  Vec<bool,dim> per; 
-//  for(int i=0; i<dim; i++) per(i) = true;
-  FieldVector<double,dim> lang(1.0);
-  FieldVector<int,dim>  anz(4);
-  FieldVector<bool,dim> per(true); 
+  bool dgf_argument = false;
+  std::string macroGridName(argv[1]);
   
-  GridType grid (MPI_COMM_WORLD, lang, anz, per ,0 );
-  for(int i=0; i<6; i++)  grid.globalRefine (1);
+  string::size_type pos;
+  pos = macroGridName.find(".dgf",0);
+  if (pos!=string::npos)
+      dgf_argument = true;
+  
+  
+  if (dgf_argument) // all gridtypes allowed for dgf-file
+  { 
+    int level = 0;
+    if(argc == 3) level = atoi(argv[2]);
+    GridPtr<GridType> gridptr(macroGridName.c_str()); 
+    gridptr->globalRefine (level);    
+    GrapeGridDisplay < GridType > grape(*gridptr,-1);
+    grape.display();
+  }
+  
+  else // specialized macrofiles expected
+  {
+    
+#if defined SGRID || YASPGRID
+    fprintf(stderr,"usage: %s <DGF-Macrofile> in ase of SGRID or YASPGRID\n",
+            argv[0]);
+    abort();   
 #endif
 
-#if AGRID
-  if(argc < 2)
-  {
-    fprintf(stderr,"usage: %s <Albert Macro Triang> \n",argv[0]);
-    abort();
-  }
-  int level = 0;
-  if(argc == 3)
-    level = atoi(argv[2]);
-
-  GridType grid( argv[1] );
-  grid.globalRefine (level);
-
+#if defined UGGRID
+    GridType grid(200,30);
+    int level = 0;
+    if(argc == 3)
+        level = atoi(argv[2]);
+    
+    AmiraMeshReader< GridType > am;
+    am.read ( grid, argv[1] );
+    grid.globalRefine (level);
+    AmiraMeshWriter< GridType > amw;
+    std::string fn (  "testgrid.am" );
+    amw.writeGrid ( grid ,fn );
 #endif
-
-#if BGRID
-  if(argc < 2)
-  {
-    fprintf(stderr,"usage: %s <BSGrid Macro Triang> \n",argv[0]);
-    abort();
-  }
-  int level = 0;
-  if(argc == 3)
-    level = atoi(argv[2]);
- 
+    
+#if defined ALBERTAGRID
+    int level = 0;
+    if(argc == 3)
+        level = atoi(argv[2]);
+    GridType grid( argv[1] );
+    grid.globalRefine (level);
+#endif
+    
+#if defined ALUGRID_SIMPLEX || ALUGRID_CUBE
+    int level = 0;
+    if(argc == 3)
+        level = atoi(argv[2]); 
 #ifdef _ALU3DGRID_PARALLEL_
-  GridType grid( argv[1], MPI_COMM_WORLD );
+    GridType grid( argv[1], MPI_COMM_WORLD );
 #else
-  GridType grid( argv[1] );
+    GridType grid( argv[1] );
 #endif
-  grid.globalRefine (level);
- 
-  //GrapeDataIO< GridType > dataIO;
-  //dataIO.writeGrid(grid,xdr,"grid",0.0,0);
-  //AmiraMeshWriter< GridType > am;
-  //std::string fn (  "forward3d.am" );
-  //am.writeGrid ( grid ,fn );
+    grid.globalRefine (level);
 #endif
-
-  {
+    
+#if defined ALBERTAGRID || UGGRID || ALUGRID_SIMPLEX || ALUGRID_CUBE
     GrapeGridDisplay < GridType > grape(grid,-1);
     grape.display();
-
-//    GrapeDataDisplay < GridType > grape(grid,-1);
-//    typedef DofManager< GridType > DofManagerType;
-//    typedef DofManagerFactory < DofManagerType> DofManagerFactoryType; 
-//    IndexSetType iset ( grid );    
-//    LeafGridPart < GridType > gpart ( grid );
-//    FuncSpaceType  space( gpart ); 
-//    DFType df ( space );
-//    setFunc(grid,df);
-//    grape.dataDisplay(df,true);
-
-  }
-
-#if YGRID 
-  MPI_Finalize();
 #endif
-
+  }    
   return 0;
-  
 }
 
