852ffaa8bf806ed016dd8ff71889731bbdd4a8a5
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2006-11-09 11:02:34 +0000

    interface class for access to preconditioning matrix, to enable for OEMSolver, derive from this class.
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@676 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

---
+++ fem/solver/oemsolver/preconditioning.hh
@@ -0,0 +1,22 @@
+#ifndef DUNE_PRECONDITIONING_HH
+#define DUNE_PRECONDITIONING_HH
+
+namespace OEMSolver 
+{
+
+class PreconditionInterface 
+{
+public:
+  // type of this class 
+  typedef PreconditionInterface ThisType; 
+
+  //! return reference to precondition matrix 
+  const ThisType & preconditionMatrix() const { return *this; }
+  
+  //! returns true, if preconditioning should be used 
+  //! default is false 
+  bool hasPreconditionMatrix() const { return false; }
+};
+
+} // end namespace OEMSolver 
+#endif
