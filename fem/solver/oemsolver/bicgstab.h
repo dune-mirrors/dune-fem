0dd5099bda39e171d44c6ca0686105f86b3675a4
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2006-11-09 11:03:44 +0000

    enabled preconditioning.
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@679 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/solver/oemsolver/bicgstab.h
+++ fem/solver/oemsolver/bicgstab.h
@@ -84,11 +84,12 @@ bicgstab( unsigned N, const MATRIX &A,
   double *t   = new double[N];
   double *r   = new double[N];
 #endif
+  
   // f"ur's Abbruchkriterium (*)  -- r enth"alt immer das Residuum r=Ax-b
  
   double *s   = h;
   double rTh, rTAd, rTr, alpha, beta, omega, st, tt;
-  int its=0;
+  unsigned int its=0;
 
   double err=eps*eps*ddot(N,b,1,b,1);
 
@@ -99,7 +100,7 @@ bicgstab( unsigned N, const MATRIX &A,
   dcopy(N,h,1,rT,1);
   
   //std::cout << "Start defekt = " << ddot(N,rT,1,rT,1) << "\n";
-  //assert( ddot(N,rT,1,rT,1)>1e-40 );
+  assert( ddot(N,rT,1,rT,1)>1e-40 );
   rTh=ddot(N,rT,1,h,1);
   rTr=ddot(N,r,1,r,1);
   while ( rTr>err ) {
@@ -160,45 +161,71 @@ bicgstab( unsigned N, const MATRIX &A,
 
 // ============================================================================
 
-
-/*
 template< class MATRIX, class PC_MATRIX > inline
 int
-bicgstab( unsigned N, const MATRIX &A, const PC_MATRIX &C,
-	  const double *b, double *x, double eps, bool detailed ) {
+bicgstab( unsigned N, const MATRIX &A, const PC_MATRIX & C,
+	  const double *rhs, double *x, double eps, bool detailed ) 
+{
+
+#if 1
+  bicgMem.resize ( 9*N ); 
+  
+  double *r   = bicgMem.getMem( N );
+  double *t   = bicgMem.getMem( N );
+  double *Ad  = bicgMem.getMem( N );
+  double *u   = bicgMem.getMem( N );
+  double *h   = bicgMem.getMem( N );
+  double *d   = bicgMem.getMem( N );
+  double *rT  = bicgMem.getMem( N );
+  double *newRhs = bicgMem.getMem( N );
+  double *tmp = bicgMem.getMem( N );
+#else 
   double *rT  = new double[N];
   double *d   = new double[N];
   double *h   = new double[N];
   double *u   = new double[N];
-  double *Sd  = new double[N];
+  double *Ad  = new double[N];
   double *t   = new double[N];
-  double *aux = new double[N];
-  double *s   = h;
-  double rTh, rTSd, rTr, alpha, beta, omega, st, tt;
-  int its=0;
-  double err=eps*eps*ddot(N,b,1,b,1);
-  // f"ur's Abbruchkriterium (*)  -- r enth"alt immer das Residuum r=Ax-b
-  double *r = new double[N];
+  double *r   = new double[N];
+  double *newRhs = new double[N];
+  double * tmp = new double[N];
+#endif
 
-  mult(A,x,r);
+  for(register int i=0; i<N; ++i) newRhs[i] = 0.0;
+  // create new rhs 
+  mult(C,rhs,newRhs);
+  const double * b = newRhs;
+  
+  // f"ur's Abbruchkriterium (*)  -- r enth"alt immer das Residuum r=Ax-b
+ 
+  double *s   = h;
+  double rTh, rTAd, rTr, alpha, beta, omega, st, tt;
+  unsigned int its=0;
+
+  double err=eps*eps*ddot(N,b,1,b,1);
+
+  // mult(A,x,r);
+  mult_pc(A,C,x,r,tmp);
   daxpy(N,-1.,b,1,r,1);
-  mult(C,r,d);
+  dcopy(N,r,1,d,1);
   dcopy(N,d,1,h,1);
   dcopy(N,h,1,rT,1);
+  
+  //std::cout << "Start defekt = " << ddot(N,rT,1,rT,1) << "\n";
   assert( ddot(N,rT,1,rT,1)>1e-40 );
   rTh=ddot(N,rT,1,h,1);
   rTr=ddot(N,r,1,r,1);
   while ( rTr>err ) {
-    mult(A,d,aux);
-    mult(C,aux,Sd);
-    rTSd=ddot(N,rT,1,Sd,1);
-    assert( fabs(rTSd)>1e-40 );
-    alpha=rTh/rTSd;
-    daxpy(N,-alpha,aux,1,r,1);
+    //mult(A,d,Ad);
+    mult_pc(A,C,d,Ad,tmp);
+    rTAd=ddot(N,rT,1,Ad,1);
+    assert( fabs(rTAd)>1e-40 );
+    alpha=rTh/rTAd;
+    daxpy(N,-alpha,Ad,1,r,1);
     dcopy(N,h,1,s,1);
-    daxpy(N,-alpha,Sd,1,s,1);
-    mult(A,s,aux);
-    mult(C,aux,t);
+    daxpy(N,-alpha,Ad,1,s,1);
+    //mult(A,s,t);
+    mult_pc(A,C,s,t,tmp);
     daxpy(N,1.,t,1,u,1);
     dscal(N,alpha,u,1);
     st=ddot(N,s,1,t,1);
@@ -207,7 +234,7 @@ bicgstab( unsigned N, const MATRIX &A, const PC_MATRIX &C,
       omega = 0.;
     else
       omega = st/tt;
-    daxpy(N,-omega,aux,1,r,1);
+    daxpy(N,-omega,t,1,r,1);
     daxpy(N,-alpha,d,1,x,1);
     daxpy(N,-omega,s,1,x,1);
     dcopy(N,s,1,h,1);
@@ -215,33 +242,41 @@ bicgstab( unsigned N, const MATRIX &A, const PC_MATRIX &C,
     beta=(alpha/omega)/rTh; rTh=ddot(N,rT,1,h,1); beta*=rTh;
     dscal(N,beta,d,1);
     daxpy(N,1.,h,1,d,1);
-    daxpy(N,-beta*omega,Sd,1,d,1);
+    daxpy(N,-beta*omega,Ad,1,d,1);
     rTr=ddot(N,r,1,r,1);
     if ( detailed )
       std::cout<<"bicgstab "<<its<<"\t"<<sqrt(rTr)<<std::endl;
     ++its;
   }
 
+#if 1
+  bicgMem.reset();
+#else
   delete[] r;
   delete[] rT;
   delete[] d;
   delete[] h;
   delete[] u;
-  delete[] Sd;
+  delete[] Ad;
   delete[] t;
-  delete[] aux;
+  delete[] newRhs;
+  delete[] tmp;
+#endif
+  
   return its;
 }
 
 
-template< class MATRIX, class PC_MATRIX > inline
+
+template< class MATRIX , class PC_MATRIX > inline
 int
-bicgstab( unsigned N, const MATRIX &A, const PC_MATRIX &C,
+bicgstab( unsigned N, const MATRIX &A, const PC_MATRIX & C,
 	  const double *b, double *x, double eps ) {
   return bicgstab(N,A,C,b,x,eps,false);
 }
 
-*/
+
+
 // ============================================================================
 
 #endif // BICGSTAB_BLAS_H
--- fem/solver/oemsolver/gmres.h
+++ fem/solver/oemsolver/gmres.h
@@ -36,9 +36,10 @@ gmres( int m, int N, const Matrix &A, const double *b, double *x, double eps,
 template< class Matrix >
 inline int
 gmres( int m, int n, const Matrix &A, const double *b, double *x, double eps,
-       bool detailed ) {
-  if ( n<=0 )
-    return -1;
+       bool detailed ) 
+{
+  if ( n<=0 ) return -1;
+
   typedef double *doubleP;
   double *V  = new double[n*(m+1)];
   double *U  = new double[m*(m+1)/2];
@@ -93,7 +94,7 @@ gmres( int m, int n, const Matrix &A, const double *b, double *x, double eps,
 	}
 	++j;
 	if ( detailed )
-	  std::cout<<"gmres("<<m<<")\t"<<io<<"\t"<<j<<"\t"<<y[j]<<std::endl;
+	  std::cout<<"gmres("<<m<<")\t"<<io<<"\t"<<j<<"\t"<<std::abs(y[j])<<std::endl;
       } while ( j<m && fabs(y[j])>=eps*nrm2b );
       { // minimiere bzgl Y
 	dtpsv(UpperTriangle,NoTranspose,NotUnitTriangular,j,U,y,1);
@@ -113,6 +114,105 @@ gmres( int m, int n, const Matrix &A, const double *b, double *x, double eps,
   delete[] c;
   delete[] s;
   delete[] v;
+  return its;
+}
+
+template< class Matrix , class PC_Matrix >
+inline int
+gmres_pc ( int m, int n, const Matrix &A, const PC_Matrix & C, const double *rhs , double *x, double eps,
+       bool detailed ) 
+{
+  if ( n<=0 )
+  {
+    abort();
+    return -1;
+  }
+
+  double * newRhs = new double[n];
+  double * tmp = new double[n];
+  for(register int i=0; i<n; ++i) newRhs[i] = 0.0;
+
+  mult(C,rhs,newRhs);
+  const double * b = newRhs;
+  
+  typedef double *doubleP;
+  double *V  = new double[n*(m+1)];
+  double *U  = new double[m*(m+1)/2];
+  double *r  = new double[n];
+  double *y  = new double[m+1];
+  double *c  = new double[m];
+  double *s  = new double[m];
+  double **v = new doubleP[m+1];
+  for ( int i=0; i<=m; ++i ) v[i]=V+i*n;
+  int its=-1;
+  {
+    double beta, h, rd, dd, nrm2b;
+    int j, io, uij, u0j;
+    nrm2b=dnrm2(n,b,1);
+    
+    io=0;
+    do  { // "aussere Iteration
+      ++io;
+      for(register int k=0; k<n; ++k) tmp[k] = 0.0;
+      mult_pc(A,C,x,r,tmp);
+      daxpy(n,-1.,b,1,r,1);
+      beta=dnrm2(n,r,1);
+      dcopy(n,r,1,v[0],1);
+      dscal(n,1./beta,v[0],1);
+
+      y[0]=beta;
+      j=0;
+      uij=0;
+      do { // innere Iteration j=0,...,m-1
+	u0j=uij;
+  for(register int k=0; k<n; ++k) tmp[k] = 0.0;
+	mult_pc(A,C,v[j],v[j+1],tmp);
+	dgemv(Transpose,n,j+1,1.,V,n,v[j+1],1,0.,U+u0j,1);
+	dgemv(NoTranspose,n,j+1,-1.,V,n,U+u0j,1,1.,v[j+1],1);
+	h=dnrm2(n,v[j+1],1);
+	dscal(n,1./h,v[j+1],1);
+        for ( register int i=0; i<j; ++i ) { // rotiere neue Spalte
+	  double tmp = c[i]*U[uij]-s[i]*U[uij+1];
+	  U[uij+1]   = s[i]*U[uij]+c[i]*U[uij+1];
+	  U[uij]     = tmp;
+	  ++uij;
+	}
+	{ // berechne neue Rotation
+	  rd     = U[uij];
+	  dd     = sqrt(rd*rd+h*h);
+	  c[j]   = rd/dd;
+	  s[j]   = -h/dd;
+	  U[uij] = dd;
+	  ++uij;
+	}
+	{ // rotiere rechte Seite y (vorher: y[j+1]=0)
+	  y[j+1] = s[j]*y[j];
+	  y[j]   = c[j]*y[j];
+	}
+	++j;
+	if ( detailed )
+	  std::cout<<"gmres("<<m<<")\t"<<io<<"\t"<<j<<"\t"<<std::abs(y[j])<<std::endl;
+      } while ( j<m && fabs(y[j])>=eps*nrm2b );
+      { // minimiere bzgl Y
+	dtpsv(UpperTriangle,NoTranspose,NotUnitTriangular,j,U,y,1);
+	// korrigiere X
+	dgemv(NoTranspose,n,j,-1.,V,n,y,1,1.,x,1);
+      }
+    } while ( fabs(y[j])>=eps*nrm2b );
+
+    // R"uckgabe: Zahl der inneren Iterationen
+    its = m*(io-1)+j;
+  }
+
+  delete[] V;
+  delete[] U;
+  delete[] r;
+  delete[] y;
+  delete[] c;
+  delete[] s;
+  delete[] v;
+  delete[] newRhs;
+  delete[] tmp;
   return its;
 }
 
