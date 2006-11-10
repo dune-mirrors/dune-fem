a4dae764a03f03dda60da66b622083b43cba52cc
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2006-11-10 17:50:50 +0000

    use less memory.
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@696 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/solver/oemsolver/bicgstab.h
+++ fem/solver/oemsolver/bicgstab.h
@@ -179,16 +179,15 @@ bicgstab( unsigned N, const MATRIX &A, const PC_MATRIX & C,
   }
 
 #if 1
-  bicgMem.resize ( 9*N ); 
+  bicgMem.resize ( 8*N ); 
   
-  double *r   = bicgMem.getMem( N );
+  double *resid= bicgMem.getMem( N );
   double *t   = bicgMem.getMem( N );
   double *Ad  = bicgMem.getMem( N );
   double *u   = bicgMem.getMem( N );
   double *h   = bicgMem.getMem( N );
   double *d   = bicgMem.getMem( N );
   double *rT  = bicgMem.getMem( N );
-  double *newRhs = bicgMem.getMem( N );
   double *tmp = bicgMem.getMem( N );
 #else 
   double *rT  = new double[N];
@@ -197,46 +196,48 @@ bicgstab( unsigned N, const MATRIX &A, const PC_MATRIX & C,
   double *u   = new double[N];
   double *Ad  = new double[N];
   double *t   = new double[N];
-  double *r   = new double[N];
-  double *newRhs = new double[N];
+  double *resid= new double[N];
   double * tmp = new double[N];
 #endif
 
-  for(register size_t i=0; i<N; ++i) newRhs[i] = 0.0;
-  // create new rhs 
-  mult(C,rhs,newRhs);
-  const double * b = newRhs;
-  
   // f"ur's Abbruchkriterium (*)  -- r enth"alt immer das Residuum r=Ax-b
  
   double *s   = h;
   double rTh, rTAd, rTr, alpha, beta, omega, st, tt;
   unsigned int its=0;
 
-  double err=eps*eps*ddot(N,b,1,b,1);
+  // start error of system 
+  //double err=eps * eps * ddot(N,rhs,1,rhs,1);
 
-  // mult(A,x,r);
-  mult_pc(A,C,x,r,tmp);
-  daxpy(N,-1.,b,1,r,1);
-  dcopy(N,r,1,d,1);
+  mult_pc(A,C,x,resid,tmp);
+  mult(C,rhs,tmp);
+  daxpy(N,-1.,tmp,1,resid,1);
+  dcopy(N,resid,1,d,1);
+  
+  double err = eps * eps * ddot(N,tmp,1,tmp,1);
+  
+  //daxpy(N,-1.,rhs,1,r,1);
+  //mult(C,r,d); // apply pre cond matrix 
+
   dcopy(N,d,1,h,1);
   dcopy(N,h,1,rT,1);
   
-  //std::cout << "Start defekt = " << ddot(N,rT,1,rT,1) << "\n";
   assert( ddot(N,rT,1,rT,1)>1e-40 );
   rTh=ddot(N,rT,1,h,1);
-  rTr=ddot(N,r,1,r,1);
-  while ( rTr>err ) {
+  rTr=ddot(N,resid,1,resid,1);
+  while ( rTr>err ) 
+  {
     //mult(A,d,Ad);
     mult_pc(A,C,d,Ad,tmp);
     rTAd=ddot(N,rT,1,Ad,1);
     assert( fabs(rTAd)>1e-40 );
     alpha=rTh/rTAd;
-    daxpy(N,-alpha,Ad,1,r,1);
+    daxpy(N,-alpha,Ad,1,resid,1);
     dcopy(N,h,1,s,1);
     daxpy(N,-alpha,Ad,1,s,1);
     //mult(A,s,t);
     mult_pc(A,C,s,t,tmp);
+
     daxpy(N,1.,t,1,u,1);
     dscal(N,alpha,u,1);
     st=ddot(N,s,1,t,1);
@@ -245,18 +246,24 @@ bicgstab( unsigned N, const MATRIX &A, const PC_MATRIX & C,
       omega = 0.;
     else
       omega = st/tt;
-    daxpy(N,-omega,t,1,r,1);
+    daxpy(N,-omega,t,1,resid,1);
     daxpy(N,-alpha,d,1,x,1);
     daxpy(N,-omega,s,1,x,1);
     dcopy(N,s,1,h,1);
     daxpy(N,-omega,t,1,h,1);
-    beta=(alpha/omega)/rTh; rTh=ddot(N,rT,1,h,1); beta*=rTh;
+    
+    beta=(alpha/omega)/rTh; 
+    rTh=ddot(N,rT,1,h,1); 
+    beta*=rTh;
+    
     dscal(N,beta,d,1);
     daxpy(N,1.,h,1,d,1);
     daxpy(N,-beta*omega,Ad,1,d,1);
-    rTr=ddot(N,r,1,r,1);
+    rTr=ddot(N,resid,1,resid,1);
     if ( detailed )
+    {
       std::cout<<"bicgstab "<<its<<"\t"<<sqrt(rTr)<<std::endl;
+    }
     ++its;
   }
 
@@ -270,11 +277,15 @@ bicgstab( unsigned N, const MATRIX &A, const PC_MATRIX & C,
   delete[] u;
   delete[] Ad;
   delete[] t;
-  delete[] newRhs;
   delete[] tmp;
 #endif
   
-  return std::pair<int,double> (its,sqrt(rTr));
+  // calculate real error 
+  mult(A,x,tmp);
+  daxpy(N,-1.,rhs,1,tmp,1);
+  double factor = ddot(N,tmp,1,tmp,1);
+  
+  return std::pair<int,double> (its,sqrt(factor));
 }
 
 
