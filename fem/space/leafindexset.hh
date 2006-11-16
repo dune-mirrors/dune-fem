e98992f289ae8ff38236a7a7f963bc7e8b1218ac
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2006-11-16 13:07:27 +0000

    bug fix
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@733 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/space/leafindexset.hh
+++ fem/space/leafindexset.hh
@@ -1,5 +1,5 @@
 #ifndef DUNE_LEAFINDEXSET_HH
 #define DUNE_LEAFINDEXSET_HH
 #warning "Use <dune/fem/space/common/adaptiveleafgridpart.hh> instead! "
-#include "common/adaptiveleafgridpart.hh>
+#include "common/adaptiveleafgridpart.hh"
 #endif
