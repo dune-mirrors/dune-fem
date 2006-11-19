79f113ec7a591f194c7eae1e6ecb5080f28c427a
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2006-11-19 15:42:01 +0000

    IdBased leaf index sets. Only working for codim 0 right now.
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@773 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

---
+++ fem/space/common/idbasedleafgridpart.hh
@@ -0,0 +1,120 @@
+#ifndef DUNE_IDBASEDLEAFGRIDPART_HH
+#define DUNE_IDBASEDLEAFGRIDPART_HH
+
+//- Dune includes 
+#include <dune/grid/common/gridpart.hh>
+
+//- local includes 
+#include "idbasedleafindexset.hh"
+
+namespace Dune {
+
+/////////////////////////////////////////////////////////////////////////
+//
+//  --AdaptiveLeafIndexGridPart 
+//
+/////////////////////////////////////////////////////////////////////////
+
+// forward deklaration of grid part 
+template <class GridImp,PartitionIteratorType pitype>
+struct DefaultAdaptiveLeafGridPart;
+template <class GridImp>
+struct DefaultAdaptiveLeafIndexSet;
+
+//! Type definitions for the LeafGridPart class
+template <class GridImp,PartitionIteratorType pitype>
+struct DefaultAdaptiveLeafGridPartTraits {
+  typedef GridImp GridType;
+  typedef DefaultAdaptiveLeafGridPart<GridImp,pitype> GridPartType;
+  typedef DefaultAdaptiveLeafIndexSet<GridImp> IndexSetType;
+
+  typedef typename GridType::template Codim<0>::Entity::
+    LeafIntersectionIterator IntersectionIteratorType;
+  
+  template <int cd>
+  struct Codim {
+    typedef typename GridImp::template Codim<cd>::template Partition<pitype>::LeafIterator IteratorType;
+  };
+};
+
+/** \brief GridPart for AdaptiveLeafIndexSet. Used underlying index set is
+    singleton for each grid object.   
+*/
+template <class GridImp, PartitionIteratorType pitype = Interior_Partition > 
+class DefaultAdaptiveLeafGridPart
+: public GridPartDefault<DefaultAdaptiveLeafGridPartTraits<GridImp,pitype> > 
+{
+  typedef SingletonList<GridImp,typename DefaultAdaptiveLeafGridPartTraits<GridImp,pitype>::IndexSetType > IndexSetProviderType;  
+public:
+  //- Public typedefs and enums
+  //! Type definitions
+  typedef DefaultAdaptiveLeafGridPartTraits<GridImp,pitype> Traits;
+  //! Grid implementation type
+  typedef typename Traits::GridType GridType;
+  //! The leaf index set of the grid implementation
+  typedef typename Traits::IndexSetType IndexSetType;
+  
+  //! The corresponding IntersectionIterator 
+  typedef typename Traits::IntersectionIteratorType IntersectionIteratorType ;
+  
+  //! Struct providing types of the leaf iterators on codimension cd
+  template <int cd>
+  struct Codim {
+    typedef typename Traits::template Codim<cd>::IteratorType IteratorType;
+  };
+
+private:
+  typedef typename GridType::template Codim<0>::Entity EntityCodim0Type;
+
+public:
+  //- Public methods
+  //! Constructor
+  DefaultAdaptiveLeafGridPart(const GridType& grid) :
+    GridPartDefault<Traits>(grid, IndexSetProviderType::getObject(grid) )
+  {}
+
+  /** \brief Destrcutor removeing index set, if only one reference left, index set
+      removed.  */
+  ~DefaultAdaptiveLeafGridPart() 
+  { 
+    IndexSetProviderType::removeObject(this->indexSet());
+  }
+
+  //! Begin iterator on the leaf level
+  template <int cd>
+  typename Traits::template Codim<cd>::IteratorType begin() const {
+    return this->grid().template leafbegin<cd,pitype>();
+  }
+
+  //! End iterator on the leaf level
+  template <int cd>
+  typename Traits::template Codim<cd>::IteratorType end() const {
+    return this->grid().template leafend<cd,pitype>();
+  }
+
+  //! ibegin of corresponding intersection iterator for given entity
+  IntersectionIteratorType ibegin(const EntityCodim0Type & en) const 
+  {
+    return en.ileafbegin();
+  }
+  
+  //! iend of corresponding intersection iterator for given entity
+  IntersectionIteratorType iend(const EntityCodim0Type & en) const 
+  {
+    return en.ileafend();
+  }
+
+  //! Returns maxlevel of the grid
+  int level() const { return this->grid().maxLevel(); }
+
+  //! corresponding communication method for this grid part
+  template <class DataHandleImp,class DataType>
+  void communicate(CommDataHandleIF<DataHandleImp,DataType> & data, 
+                   InterfaceType iftype, CommunicationDirection dir) const 
+  {
+    this->grid().communicate(data,iftype,dir);
+  }
+};
+
+} // end namespace Dune 
+#endif
