477fdc0a145a6246f3111b44e7b1e6a113fc5361
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2006-11-20 19:23:16 +0000

    finally got the right upper estimate for non-zeros ;)
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@808 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/operator/matrix/matrixhandler.hh
+++ fem/operator/matrix/matrixhandler.hh
@@ -194,20 +194,13 @@ namespace Dune {
         singleMaxNumbers_ = singleSpace_.getBaseFunctionSet(*(singleSpace_.begin())).numBaseFunctions();
         gradMaxNumbers_   = gradientSpace_.getBaseFunctionSet(*(gradientSpace_.begin())).numBaseFunctions();
 
-        singleMaxNumbers_ *= singleMaxNumbers_;
-        gradMaxNumbers_   *= gradMaxNumbers_;
-
-        //std::cout << singleMaxNumbers_ << " " << gradMaxNumbers_ << "\n";
-        
         assert( singleMaxNumbers_ > 0 );
         assert( gradMaxNumbers_ > 0 );
 
         // upper estimate for number of neighbors 
         enum { dim = SpaceType :: GridType :: dimension };
-        singleMaxNumbers_ *= (dim * 2) + 1; // e.g. 6 for dim = 3
+        singleMaxNumbers_ *= (dim * 2) + 1; // e.g. 7 for dim = 3
         gradMaxNumbers_   *= (dim * 2) + 1;
-
-        //std::cout << singleMaxNumbers_ << " " << gradMaxNumbers_ << "\n";
 
         stabMatrix_.reserve(singleSpace_.size(),singleSpace_.size(),singleMaxNumbers_,0.0);
         gradMatrix_.reserve(gradientSpace_.size(),singleSpace_.size(),singleMaxNumbers_,0.0);
