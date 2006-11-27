53c59e67a0d6091b964ff1bcf77f543851ccf0bc
Andreas Dedner <A.S.Dedner@warwick.ac.uk>
2006-11-27 21:39:43 +0000

    now it works
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@921 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/examples/localdg/advectdiffusion/models.hh
+++ fem/examples/localdg/advectdiffusion/models.hh
@@ -1,9 +1,8 @@
-#undef HAVE_MPI
 #include <dune/grid/io/file/dgfparser/gridtype.hh>
 #include "advectdiff.hh"
 
-// #include <dune/fem/solver/odesolver.hh>
-#include "odesolver.hh"
+#include <dune/fem/solver/odesolver.hh>
+// #include "odesolver.hh"
 // Approximations Ordnung
 enum {order=POLORDER,rksteps=POLORDER+1}; 
 // Gitterauswahl
