bc4973012be04dbc6ce1d82cebf254a042800fd0
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2006-12-18 12:46:20 +0000

    class returning all geom types for a given grid. Use the index set geomTypes method except for UGGrid. Here we have a specialisation.
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@996 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

---
+++ fem/space/common/allgeomtypes.hh
@@ -0,0 +1,98 @@
+#ifndef DUNE_ALLGEOMTYPES_HH
+#define DUNE_ALLGEOMTYPES_HH
+
+//- system includes 
+#include <vector>
+
+//- Dune includes 
+#include <dune/common/geometrytype.hh>
+#if HAVE_UG 
+#include <dune/grid/uggrid.hh>
+#endif
+
+namespace Dune {
+
+  /** \brief default implementation uses method geomTypes of given index
+       set. 
+   */
+  template <class IndexSetImp, class GridImp> 
+  class AllGeomTypes 
+  {
+    typedef IndexSetImp IndexSetType;
+    const IndexSetType & indexSet_;
+  public:
+    //! constructor storing index set reference 
+    AllGeomTypes(const IndexSetType & indexSet) 
+      : indexSet_(indexSet) 
+    {}                  
+    
+    //! returns vector with geometry tpyes this index set has indices for
+    const std::vector <GeometryType> & geomTypes (int codim) const
+    {
+      return indexSet_.geomTypes(codim);
+    }
+  };
+
+#if HAVE_UG 
+  /** \brief specialisation fir UGGrid, because geomTypes method of index
+      sets not usable in this case. 
+  */
+  template <class IndexSetImp, int dimworld> 
+  class AllGeomTypes<IndexSetImp, UGGrid<dimworld> >
+  {
+    typedef IndexSetImp IndexSetType;
+    enum { ncodim = dimworld + 1 };
+    
+    std::vector< GeometryType > geomTypes_[ncodim]; 
+  public:
+    //! constructor storing index set reference 
+    AllGeomTypes(const IndexSetType & indexSet) 
+    {
+      // UGGrid only working for dimension 2 or 3 
+      assert( (dimworld == 2) || (dimworld == 3) ); 
+      // vertices 
+      {
+        geomTypes_[dimworld].push_back( GeometryType(GeometryType::simplex,0)); 
+      }
+      
+      {
+        if( dimworld == 2 )
+        {
+          // elements 
+          geomTypes_[0].push_back( GeometryType(GeometryType::simplex,2)); 
+          geomTypes_[0].push_back( GeometryType(GeometryType::cube,2)); 
+
+          // faces 
+          geomTypes_[1].push_back( GeometryType(GeometryType::cube,1));
+        }
+
+        if( dimworld == 3 )
+        {
+          // elements 
+          geomTypes_[0].push_back( GeometryType(GeometryType::simplex,3)); 
+          geomTypes_[0].push_back( GeometryType(GeometryType::cube,3)); 
+          geomTypes_[0].push_back( GeometryType(GeometryType::prism,3)); 
+          geomTypes_[0].push_back( GeometryType(GeometryType::pyramid,3)); 
+
+          // faces 
+          geomTypes_[1].push_back( GeometryType(GeometryType::simplex,2)); 
+          geomTypes_[1].push_back( GeometryType(GeometryType::cube,2)); 
+
+          // edges 
+          geomTypes_[2].push_back( GeometryType(GeometryType::cube,1));
+        }
+      }
+    }                  
+    
+    //! returns vector with geometry tpyes this index set has indices for
+    const std::vector <GeometryType> & geomTypes (int codim) const
+    {
+      assert( codim >= 0 );
+      assert( codim < ncodim );
+      return geomTypes_[codim];
+    }
+  };
+#endif
+
+} // end namespace Dune 
+#endif
