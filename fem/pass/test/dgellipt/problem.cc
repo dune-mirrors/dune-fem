d86853d960fe9ebc83358ad46e86e766b628a180
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2007-01-11 17:46:53 +0000

    bug fix in rhsNeumann.
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@1061 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/pass/test/dgellipt/problem.cc
+++ fem/pass/test/dgellipt/problem.cc
@@ -10,8 +10,8 @@ static const double globalShift = 2.0;
 
 double exactFactor() 
 {
-  //return 1.0/(5.24*1e5);
-  return 2.0;
+  return 1.0/(5.24*1e5);
+  //return 2.0;
   //return 1.;
   //return (5.24*1e5);
 }
@@ -235,12 +235,12 @@ void exactGradient(const double x[dim], double grad[dim])
   grad[1] = 0.5*M_PI*cos(0.5*M_PI*x[0])*-sin(0.5*M_PI*x[1]);
 #endif
 
-  //for(int i=0; i<dim; ++i) grad[i] *= exactFactor();
 }
 
 void rhsNeumann(const double x[dim], double grad[dim])
 {
   exactGradient(x,grad);
+  for(int i=0; i<dim; ++i) grad[i] *= exactFactor();
 }
 
 bool boundaryDataFunction(const double x[dim], double & val)
