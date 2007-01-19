b41c85e4727e766ae320d463ff7a4563d13f6134
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2007-01-19 15:19:11 +0000

    moved from ../../basefunctions to here.
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@1208 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

---
+++ fem/space/basefunctions/test/include.cc
@@ -0,0 +1,5 @@
+#include "../../quadrature/idprovider.cc"
+#include "../../quadrature/gausspoints.cc"
+#include "../../quadrature/pyramidpoints.cc"
+#include "../../quadrature/prismpoints.cc"
+//#include "../../quadrature/
