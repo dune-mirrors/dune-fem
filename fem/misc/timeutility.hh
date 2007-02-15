0b3f289fa4b1f3212451230ff87d95c0261b889e
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2007-02-15 13:46:06 +0000

    minor change.
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@1334 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/misc/timeutility.hh
+++ fem/misc/timeutility.hh
@@ -5,14 +5,20 @@
 
 namespace Dune {
 
-  class TimeProvider {
+  //! class for time and time step estimate handling
+  class TimeProvider 
+  {
   public:
+    //! return internal time 
     double time() const { return time_; }
     
+    //! set time step estimate to minimum of given value and
+    //! internal time step estiamte 
     void provideTimeStepEstimate(double dtEstimate) {
       dtEstimate_ = std::min(dtEstimate_, dtEstimate);
     }
   public:
+    //! constructor taking initial time, default is zero
     TimeProvider(double startTime = 0.0) : 
       time_(startTime),
       dtEstimate_()
@@ -20,19 +26,27 @@ namespace Dune {
       resetTimeStepEstimate();
     }
     
+    //! destructor 
     virtual ~TimeProvider() {}
 
+    //! set internal time to given time 
     void setTime(double time) { time_ = time; }
+
+    //! increase internal time by dt 
     void augmentTime(double dt) { time_ += dt; }
     
+    //! set time step estimate to big value 
     void resetTimeStepEstimate() {
       dtEstimate_ = std::numeric_limits<double>::max();
     }
     
+    //! return time step estimate 
     double timeStepEstimate() const {
       return dtEstimate_;
     }
+    
   private:
+    //! do not copy this class 
     TimeProvider(const TimeProvider&);
     TimeProvider& operator=(const TimeProvider&);
     
