3581f5deb97623f70c04727de2858c44e65777cf
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2007-04-26 14:04:48 +0000

    objects files for lib.
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@1548 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

---
+++ fem/solver/ode/odeobjectfiles.cc
@@ -0,0 +1,41 @@
+//- system includes 
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <pthread.h>
+#include <cassert>
+#include <sys/times.h>
+
+#include <config.h>
+
+#if HAVE_MPI
+#include <mpi.h>
+#endif
+
+namespace DuneODE {
+
+// include first, because of typedef 
+#if HAVE_MPI 
+#include "communicator.cpp"
+#include "buffer.cpp"
+#else
+#include "emptycommunicator.hpp"
+#endif
+  
+#include "bulirsch_stoer.cpp"  
+#include "iterative_solver.cpp"  
+#include "ode_solver.cpp"     
+#include "sirk.cpp"   
+
+#include "matrix.cpp"            
+#include "qr_solver.cpp"   
+#include "lu_solver.cpp"
+#include "ssp.cpp"
+#include "dirk.cpp"     
+#include "runge_kutta.cpp"
+#include "gmres.cpp"
+#include "fgmres.cpp"
+#include "bicgstab.cpp"
+#include "cg.cpp"
+#include "vector.cpp"
+}
