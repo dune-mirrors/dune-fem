235ea3289f5d1ed98cf83702199bccc74f9c402e
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2007-05-03 11:35:48 +0000

    slight change in L2Projection.
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@1555 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/operator/projection/l2projection.hh
+++ fem/operator/projection/l2projection.hh
@@ -6,35 +6,18 @@
 #include <dune/fem/operator/common/operator.hh>
 namespace Dune 
 {
-/*======================================================================*/
-/*!
- *  \class L2Projection
- *  \brief The L2Projection class provides methods for projecting a function
- *         unto a given discrete function space. Note that this implementation
- *         assumes orthorgonal base functions!
- */
-/*======================================================================*/
 
-template <typename DFieldType, typename RFieldType,
-          typename DType , typename RType>
- class L2Projection : public Operator<DFieldType, RFieldType,DType , RType> {
- public:
-  typedef DType DomainType;
-  typedef RType  RangeType;
-  typedef DFieldType DomainFieldType;
-  typedef RFieldType RangeFieldType;
-
-  //! Constructor taking degree for quadrature rule
-  //! if no argument given a default value is chosen depending on the order
-  //! in the discrete function space
-  L2Projection(int polOrd = -1) : polOrd_(polOrd) {}
-
-  virtual void operator() (const DomainType& f, RangeType& discFunc) const {
-    typedef typename RangeType::FunctionSpaceType DiscreteFunctionSpaceType;
-    typedef typename RangeType::LocalFunctionType LocalFuncType;
+struct L2ProjectionImpl
+{
+  template <class FunctionImp, class DiscreteFunctionImp>
+  static void project(const FunctionImp& f, DiscreteFunctionImp& discFunc, int polOrd_ = -1) 
+  {
+    typedef typename DiscreteFunctionImp::FunctionSpaceType DiscreteFunctionSpaceType;
+    typedef typename DiscreteFunctionImp::LocalFunctionType LocalFuncType;
     typedef typename DiscreteFunctionSpaceType::Traits::GridPartType GridPartType;
     typedef typename DiscreteFunctionSpaceType::Traits::IteratorType Iterator;
     typedef typename DiscreteFunctionSpaceType::BaseFunctionSetType BaseFunctionSetType ; 
+    typedef typename GridPartType::GridType GridType;
 
     typename DiscreteFunctionSpaceType::RangeType ret (0.0);
     typename DiscreteFunctionSpaceType::RangeType phi (0.0);
@@ -49,29 +32,65 @@ template <typename DFieldType, typename RFieldType,
     Iterator endit = space.end();
     for(Iterator it = space.begin(); it != endit ; ++it) 
     {
+      typename GridType::template Codim<0>::Entity& en = *it; 
+      
       // Get quadrature rule
-      CachingQuadrature<GridPartType,0> quad(*it, polOrd);
-      LocalFuncType lf = discFunc.localFunction(*it);
+      CachingQuadrature<GridPartType,0> quad(en, polOrd);
+      LocalFuncType lf = discFunc.localFunction(en);
+
       //! Note: BaseFunctions must be ortho-normal!!!!
       const BaseFunctionSetType & baseset = lf.baseFunctionSet();
 
       const typename GridType::template Codim<0>::Entity::Geometry& 
-        itGeom = (*it).geometry();
+        itGeom = en.geometry();
      
       const int quadNop = quad.nop();
       const int numDofs = lf.numDofs();
+
       for(int qP = 0; qP < quadNop ; ++qP) 
       {
         f.evaluate(itGeom.global(quad.point(qP)), ret);
-        for(int i=0; i<numDofs; ++i) {
+        for(int i=0; i<numDofs; ++i) 
+        {
           baseset.evaluate(i,quad,qP,phi);
           lf[i] += quad.weight(qP) * (ret * phi) ;
         }
       }
     }
   }
-private:
-  int polOrd_;
 };
+
+/*======================================================================*/
+/*!
+ *  \class L2Projection
+ *  \brief The L2Projection class provides methods for projecting a function
+ *         unto a given discrete function space. Note that this implementation
+ *         assumes orthorgonal base functions!
+ */
+/*======================================================================*/
+template <typename DFieldType, typename RFieldType,
+          typename DType , typename RType>
+class L2Projection : public Operator<DFieldType, RFieldType,DType , RType> {
+ public:
+  typedef DType DomainType;
+  typedef RType  RangeType;
+  typedef DFieldType DomainFieldType;
+  typedef RFieldType RangeFieldType;
+
+  //! Constructor taking degree for quadrature rule
+  //! if no argument given a default value is chosen depending on the order
+  //! in the discrete function space
+  L2Projection(int polOrd = -1) : polOrd_(polOrd) {}
+
+  //! apply L2 projection
+  virtual void operator() (const DomainType& f, RangeType& discFunc) const 
+  {
+    L2ProjectionImpl::project(f,discFunc,polOrd_);
+  }
+
+private:
+  const int polOrd_;
+};
+
 }
 #endif
