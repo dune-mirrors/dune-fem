3af18e7b8fb118bb3b341c9db9a5af5af4d7b936
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2007-05-28 14:40:31 +0000

    adjusted to new interface.
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@1628 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/space/dgspace/dgmapper.hh
+++ fem/space/dgspace/dgmapper.hh
@@ -45,11 +45,6 @@ public:
     return ((indexSet_.template index<0>(en,0) * numberOfDofs_) + localNum);
   };
 
-  //** size methods needed by the dofmanager ***
-  //! calc new start point for element dof , vertex dof ... 
-  //not needed here at the moment  
-  void calcInsertPoints () {};
-
   //! default implementation if not overlaoded 
   int numDofs () const
   {
@@ -63,37 +58,31 @@ public:
   }
 
   //! return old index, for dof manager only 
-  int oldIndex (int num) const
+  int oldIndex (const int hole, int ) const
   {
     // corresponding number of set is newn 
-    const int newn  = static_cast<int> (num / numberOfDofs_);
+    const int newn  = static_cast<int> (hole / numberOfDofs_);
     // local number of dof is local 
-    const int local = (num % numberOfDofs_);
+    const int local = (hole % numberOfDofs_);
     return (numberOfDofs_ * indexSet_.oldIndex(newn,0)) + local;
   }
 
   //! return new index, for dof manager only 
-  int newIndex (int num) const
+  int newIndex (const int hole, int ) const
   {
     // corresponding number of set is newn 
-    const int newn = static_cast<int> (num / numberOfDofs_);
+    const int newn = static_cast<int> (hole / numberOfDofs_);
     // local number of dof is local 
-    const int local = (num % numberOfDofs_);
+    const int local = (hole % numberOfDofs_);
     return (numberOfDofs_ * indexSet_.newIndex(newn,0)) + local;
   }
 
   //! return size of grid entities per level and codim 
   //! for dof mapper 
-  int numberOfHoles () const
+  int numberOfHoles ( int ) const
   {
     // this index set works only for codim = 0 at the moment
     return numberOfDofs_ * indexSet_.numberOfHoles(0);
-  }
-
-  // is called once and calcs the insertion points too
-  int additionalSizeEstimate () const
-  {
-    return numberOfDofs_ * indexSet_.additionalSizeEstimate();
   }
 
   //! return whether the data has to be compressed or not
