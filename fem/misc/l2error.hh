46abbaebf55800f2682375731fbee6411af3ce62
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2007-06-11 12:46:54 +0000

    some face lifting.
    Who implemented the template parameter n=0 ?? 
    That just does nothing. 
    
    
    
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@1668 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/misc/l2error.hh
+++ fem/misc/l2error.hh
@@ -34,22 +34,20 @@ namespace Dune
   template <class DiscreteFunctionType,int n=0> 
   class L2Error
   {
-    typedef typename DiscreteFunctionType::FunctionSpaceType FunctionSpaceType;
-    typedef typename DiscreteFunctionType::FunctionSpaceType DiscFuncSpaceType;
+    typedef typename DiscreteFunctionType::DiscreteFunctionSpaceType DiscreteFunctionSpaceType;
     typedef typename DiscreteFunctionType::RangeType         RangeType;
-    typedef typename DiscFuncSpaceType::IteratorType         IteratorType;
-    typedef typename DiscreteFunctionType::LocalFunctionType LocalFunctionType;
-    typedef typename DiscFuncSpaceType::GridType             GridType;
-    typedef typename DiscFuncSpaceType::GridPartType         GridPartType;
+    typedef typename DiscreteFunctionSpaceType::IteratorType         IteratorType;
+    typedef typename DiscreteFunctionSpaceType::GridType             GridType;
+    typedef typename DiscreteFunctionSpaceType::GridPartType         GridPartType;
     typedef typename GridType::template Codim<0>::Entity     EntityType; 
+    typedef typename GridType::template Codim<0>::Geometry   EnGeometryType; 
     typedef typename EntityType::ctype                       coordType; 
     
-    enum {dimrange = DiscFuncSpaceType::DimRange};
-    enum {dim = GridType::dimension};
-    typedef typename DiscreteFunctionType::DiscreteFunctionSpaceType 
-    DiscreteFunctionSpaceType;
+    typedef typename DiscreteFunctionType::LocalFunctionType LocalFunctionType;
+      
+    enum { dim = GridType::dimension};
     enum { spacePolOrd = DiscreteFunctionSpaceType :: polynomialOrder }; 
-    enum { dimR = RangeType :: dimension };
+    enum { dimRange = RangeType :: dimension };
 
   public:  
 /*======================================================================*/
@@ -76,66 +74,70 @@ namespace Dune
     RangeType norm (const FunctionType &f, DiscreteFunctionType &discFunc,
                     int polOrd = (2 * spacePolOrd + 2), 
                     double time = 0.0)
+    {
+      // get function space
+      const DiscreteFunctionSpaceType & space = discFunc.space();  
+      
+      const GridPartType & gridPart = space.gridPart();
+      typedef typename GridPartType :: GridType :: Traits :: 
+          CollectiveCommunication
+          CommunicatorType; 
+      
+      const CommunicatorType & comm = gridPart.grid().comm();
+      
+      RangeType ret (0.0);
+      RangeType phi (0.0);
+      
+      RangeType error(0.0);
+
+      IteratorType endit = space.end();
+      for(IteratorType it = space.begin(); it != endit ; ++it)
+      {
+        // entity
+        const EntityType& en = *it;
+        
+        // create quadrature for given geometry type 
+        CachingQuadrature <GridPartType , 0 > quad(en,polOrd); 
+        
+        // get local function 
+        LocalFunctionType lf = discFunc.localFunction(en); 
+
+        // get geoemetry of entity
+        const EnGeometryType& geo = en.geometry();
+        
+        // integrate 
+        const int quadNop = quad.nop();
+        for(int qP = 0; qP < quadNop; ++qP)
+        {
+          const double det = quad.weight(qP) * 
+              geo.integrationElement(quad.point(qP));
+
+          if (n==0) {
+            f.evaluate(geo.global(quad.point(qP)),time,ret);
+            lf.evaluate(quad,qP,phi);
+            for(int k=0; k<dimRange; ++k) {
+              error[k] += det * SQR(ret[k] - phi[k]);
+            }
+          } 
+          else if (n==1) 
           {
-            const DiscreteFunctionSpaceType & space = discFunc.space();  
-            
-            typedef typename DiscreteFunctionSpaceType::GridPartType GridPartType;
-            typedef typename DiscreteFunctionSpaceType::IteratorType IteratorType;
-            typedef typename DiscreteFunctionType::LocalFunctionType LocalFuncType;
-            
-            const GridPartType & gridPart = space.gridPart();
-            typedef typename GridPartType :: GridType :: Traits :: 
-                CollectiveCommunication
-                CommunicatorType; 
-            
-            const CommunicatorType & comm = gridPart.grid().comm();
-            
-            RangeType ret (0.0);
-            RangeType phi (0.0);
-            
-            RangeType error(0.0);
-            
-            IteratorType endit = space.end();
-            for(IteratorType it = space.begin(); it != endit ; ++it)
-            {
-              // create quadrature for given geometry type 
-              CachingQuadrature <GridPartType , 0 > quad(*it,polOrd); 
-              // get local function 
-              LocalFuncType lf = discFunc.localFunction(*it); 
-              
-              // integrate 
-              const int quadNop = quad.nop();
-              for(int qP = 0; qP < quadNop; ++qP)
-              {
-                double det = quad.weight(qP) * 
-                    (*it).geometry().integrationElement(quad.point(qP));
-                if (n==0) {
-                  f.evaluate((*it).geometry().global(quad.point(qP)),time,ret);
-                  lf.evaluate(quad,qP,phi);
-                  for(int k=0; k<dimR; ++k) {
-                    error[k] += det * SQR(ret[k] - phi[k]);
-                  }
-                } else if (n==1) {
-                  
-                  f.evaluate((*it).geometry().global(quad.point(qP)),time,ret);
-                  lf.evaluate(quad,qP,phi);
-                  for(int k=0; k<dimR; ++k) {
-                    error[k] += det * SQR(ret[k] - phi[k]);
-                  }
-                }
-              }
+            f.evaluate(geo.global(quad.point(qP)),time,ret);
+            lf.evaluate(quad,qP,phi);
+            for(int k=0; k<dimRange; ++k) {
+              error[k] += det * SQR(ret[k] - phi[k]);
             }
-            
-            //return sqrt(sum);
-            
-            for(int k=0; k<dimR; ++k)
-            {
-              error[k] = comm.sum( error[k] );
-              error[k] = sqrt(error[k]);
-            }
-            
-            return error;
-          } // end of method
+          }
+        }
+      }
+      
+      for(int k=0; k<dimRange; ++k)
+      {
+        error[k] = comm.sum( error[k] );
+        error[k] = sqrt(error[k]);
+      }
+      
+      return error;
+    } // end of method
 
 /*======================================================================*/
 /*! 
@@ -170,47 +172,52 @@ namespace Dune
     const CommunicatorType & comm = gridPart.grid().comm();
     
     double ret=0;  
+
+    if( dimRange > 1 ) 
+    {
+      std::cout << "L2Error::norm2: only implemented for dimRange = 1! \n";
+      abort();
+    }
     
-    assert( dimrange == 1); // currently only scalar functions supported
+    // get function space 
+    const DiscreteFunctionSpaceType& dfsp = f1.space();  
     
-//    const DiscFuncSpaceType& dfsp = f1.getFunctionSpace();  
-    const DiscFuncSpaceType& dfsp = f1.space();  
-    
-    IteratorType it = dfsp.begin();  
-    IteratorType eit = dfsp.end();  
     RangeType lv1,lv2;
-    
-    // get quadrature rule for first entity and hope that the geometry 
-    // does not change. Get rule which is exact for difference of df1,df2, 
-    // assuming them to be polynomial
     
     // for product:
     //int quadOrd = (polOrd*dim)*(polOrd*dim);
     int quadOrd = polOrd;
                     
     // iterate over all elements defining the function
+    IteratorType eit = dfsp.end();  
     for (IteratorType it = dfsp.begin(); it!=eit; ++it)
     {
-      CachingQuadrature <GridPartType , 0 > quad(*it,quadOrd); 
+      const EntityType& en = *it;
+      
+      CachingQuadrature <GridPartType , 0 > quad(en,quadOrd); 
       // get local functions on current element
-      LocalFunctionType lf1 = f1.localFunction( *it ); 
-      LocalFunctionType lf2 = f2.localFunction( *it );
-      for (int qp = 0; qp < quad.nop(); qp++)
+      LocalFunctionType lf1 = f1.localFunction( en ); 
+      LocalFunctionType lf2 = f2.localFunction( en );
+
+      // get geoemetry of entity
+      const EnGeometryType& geo = en.geometry();
+      
+      const int quadNop = quad.nop();
+      for (int qp = 0; qp < quadNop; ++qp)
       {
         const double det = 
-            it->geometry().integrationElement(quad.point(qp));
+            geo.integrationElement(quad.point(qp));
         
-      // the following demonstrates a very strange effect:
-        lf1.evaluate(quad.point(qp), lv1);
-        lf2.evaluate(quad.point(qp), lv2);
-        ret += det * quad.weight( qp) * (lv1 - lv2) * (lv1 - lv2);
+        lf1.evaluate(quad, qp, lv1);
+        lf2.evaluate(quad, qp, lv2);
+        ret += det * quad.weight(qp) * (lv1 - lv2) * (lv1 - lv2);
       } // end qp iteration
       
     } // end element iteration
     
     ret = comm.sum(ret);
     return sqrt(ret);
-  }; // end method
+  } // end method
   
 }; // end of class L2Error
 
