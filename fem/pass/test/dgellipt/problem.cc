18e2159cfa50b8703bf9e95d6591e3be8d2f843b
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2007-06-30 10:59:59 +0000

    minor change
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@1752 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/pass/test/dgellipt/problem.cc
+++ fem/pass/test/dgellipt/problem.cc
@@ -1,9 +1,7 @@
 #ifndef ELLIPTPROBLEM_CC
 #define ELLIPTPROBLEM_CC
 
-#include <math.h>
-
-#define PROBLEM 1
+#include <cmath>
 
 template<int dim> 
 class DataFunctionIF
@@ -44,7 +42,7 @@ public:
     : globalShift_(globalShift)
     , factor_(factor) 
   {
-    assert(dim == 2);
+    //assert(dim == 2);
   }
 
   virtual double factor () const { return factor_; }
@@ -91,7 +89,7 @@ public:
     : globalShift_(globalShift)
     , factor_(factor) 
   {
-    assert(dim == 2);
+    //assert(dim == 2);
   }
 
   virtual double factor () const { return factor_; }
@@ -291,250 +289,4 @@ public:
     return true; 
   }
 };
-
-#if 0
-// shift solutions to avoid dirichlet 0 bnd 
-static const double globalShift = 2.0;
-
-double exactFactor() 
-{
-  return 1.0/(5.24*1e5);
-  //return 12.0;
-  //return 1.;
-  //return (5.24*1e5);
-}
-
-double frhs(const double x[dim]) 
-{
-  double ret = 0.0;
-  for(int i=0; i<dim; i++)
-  {
-    double tmp = 2.0;
-    for(int j=1; j<dim; j++)
-    {
-      int idx = (i+j) % dim;
-      tmp *= (x[idx] - SQR(x[idx]));
-    } 
-    ret += tmp;
-  } 
-  return ret;
-} 
-
-double coscosHalf(const double x[dim]) 
-{
-  double cos_x = cos(0.5*M_PI*x[0]);
-  double cos_y = cos(0.5*M_PI*x[1]);
-       
-  double val = -0.5 * M_PI*M_PI* cos_x * cos_y ;
-  return -val;
-}
-
-double expProblemRhs( const double x[dim]) 
-{
-  double ret = 0.0;
-  double tmp =-23+7*SQR(x[1])-24*x[0]+24*x[0]*SQR(x[1])+7*SQR(x[0])+9*SQR(x[0])*SQR(x[1])-24
-              *x[1]+24*x[1]*SQR(x[0]);
-
-  ret=-0.5*exp(0.75*(x[0]+x[1]));
-  ret*=tmp;
-  return ret;
-}
-
-double rhsFunction(const double x[dim])
-{
-  double val = 0.0;
-#if PROBLEM == 3
-  val = M_PI * M_PI * cos( M_PI * x[0] );
-#endif
-
-#if PROBLEM == 4 
-  val = frhs( x );
-#endif
-  
-#if PROBLEM == 5 
-  val = 0.0;
-#endif
-
-#if PROBLEM == 6 
-  val = expProblemRhs( x ); 
-#endif
-
-#if PROBLEM == 7
-  val = coscosHalf( x );
-#endif
-
-  val *= exactFactor();
-  return val;
-}
-
-double exactPol(const double x[dim])
-{
-  double ret = 1.0;
-  for(int i=0; i<dim; i++)
-    ret *= ( x[i] - SQR(x[i]) );
-  return ret;
-}
-
-double inSpringingCorner(const double x[dim])
-{
-  double ret = 0.0;
-  const double Pi= M_PI; 
-  double tmp2=0.0;
-
-  double s = 0.0 ;
-  for(int i=0; i<dim; ++i) s += x[i]*x[i];
-  double r = sqrt(s); 
- 
-  double phi=0.0;
-  double fac=2.0;
-  fac/=3;
- 
-  ret=pow(r,fac);
-  if(x[1]>=0.0)
-  {
-    tmp2=x[0];
-    tmp2/=r;
-    phi=acos(tmp2);
-    phi*=fac;
-    ret*=sin(phi);
-  }
-  else
-  {
-    tmp2=-x[0];
-    tmp2/=r;
-    phi=acos(tmp2);
-    phi+=Pi;
-    phi*=fac;
-    ret*=sin(phi);
-  }
-  return ret;
-}
-
-double exactExpProblem(const double x[dim])
-{
-  double ret = 4.0 *(1.-SQR(x[0]))*(1.-SQR(x[1]))*exp(0.75*(x[0]+x[1]));
-  return ret;
-}
-
-double exactSolution(const double x[dim])
-{
-  double val = 0.0;
-#if PROBLEM == 2
-#endif
-
-#if PROBLEM == 3
-  val = cos(M_PI * x[0]);
-#endif
-
-#if PROBLEM == 4 
-  val = exactPol( x );
-#endif
-
-#if PROBLEM == 5 
-  val = inSpringingCorner( x );
-#endif
-
-#if PROBLEM == 6 
-#endif
-
-#if PROBLEM == 7
-  val = cos(0.5*M_PI*x[0]) * cos(0.5*M_PI*x[1]);
-#endif
-
-  val += globalShift;
-  return val;
-}
-
-
-void exactPol(const double x[dim], double grad[dim])
-{
-  for(int j=0; j<dim; ++j) grad[j] = 1.0;
-
-  for(int j=0; j<dim; ++j)
-  {
-    for(int i=0; i<dim; ++i)
-    {
-      if(i == j) grad[j] *= (1.0-2.0*x[i]);
-      else       grad[j] *= ( x[i] - SQR(x[i]) );
-    }
-  }
-}
-
-void exactGradient(const double x[dim], double grad[dim])
-{
-#if PROBLEM == 2
-#endif
-
-#if PROBLEM == 3
-  grad[0] = - M_PI * sin( M_PI * x[0] ); // -2.0;
-  grad[1] = 0.0;
-#endif
-
-#if PROBLEM == 4 
-  exactPol( x , grad );
-#endif
-  
-#if PROBLEM == 5 
-  grad[0] = grad[1] = 0.0; 
-#endif
-  
-#if PROBLEM == 7
-  grad[0] = 0.5*M_PI*-sin(0.5*M_PI*x[0])*cos(0.5*M_PI*x[1]);
-  grad[1] = 0.5*M_PI*cos(0.5*M_PI*x[0])*-sin(0.5*M_PI*x[1]);
-#endif
-
-}
-
-void rhsNeumann(const double x[dim], double grad[dim])
-{
-  exactGradient(x,grad);
-  for(int i=0; i<dim; ++i) grad[i] *= exactFactor();
-}
-
-bool boundaryDataFunction(const double x[dim], double & val)
-{
-  abort();
-#if PROBLEM == 3
-  if(x[0] <= 0.0) 
-  {
-    val = globalShift + 1.0; 
-    return true; 
-  }
-
-  if(x[0] >= 1.0)
-  {
-    val = globalShift - 1.0; 
-    return true;
-  }
-
-  val = globalShift;
-  return false;
-#endif
-
-#if PROBLEM == 4 
-  val = globalShift;
-  return true; 
-#endif
-
-#if PROBLEM == 5 
-  val = inSpringingCorner( x ); 
-  val += globalShift;
-  return true; 
-#endif
-
-#if PROBLEM == 6 
-  val = exactSolution( x ); 
-  return true; 
-#endif
-
-#if PROBLEM == 7
-  val = exactSolution( x ); 
-  if(x[0] <= 0.0) return false;
-  return true; 
-#endif
-
-  return true;
-}
-#endif
-
 #endif
