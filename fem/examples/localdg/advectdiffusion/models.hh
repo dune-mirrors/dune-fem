9cd752cb18fe690e8e8805405fb27fbb02a8c0d3
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2007-07-12 15:02:19 +0000

    minor change.
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@1825 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/examples/localdg/advectdiffusion/models.hh
+++ fem/examples/localdg/advectdiffusion/models.hh
@@ -3,33 +3,53 @@
 #include "advectdiff.hh"
 
 #include <dune/fem/solver/odesolver.hh>
-// #include "odesolver.hh"
 // Approximations Ordnung
 enum {order=POLORDER,rksteps=POLORDER+1}; 
-// Gitterauswahl
 
+// Gitter view Auswahl
 //typedef LeafGridPart<GridType> GridPartType;
 typedef HierarchicGridPart<GridType> GridPartType;
-// typedef DGAdaptiveLeafGridPart<GridType> GridPartType;
+//typedef DGAdaptiveLeafGridPart<GridType> GridPartType;
+
 // Modell- und Flussauswahl
 // Skalar
-#if PROBLEM == 1
+#if PROBLEM == 0
+   #include "scalarmodels.hh"
+   #include "initpulse.cc"
+   typedef U0<GridType> InitialDataType;
+   //typedef AdvectionDiffusionModel<GridPartType,InitialDataType> ModelType;
+   typedef AdvectionDiffusionModel<GridPartType,InitialDataType> ModelType;
+   //typedef LLFFlux<ModelType> FluxType;
+   typedef UpwindFlux<ModelType> FluxType;
+   //typedef DGAdvectionDiffusionOperator<ModelType,UpwindFlux,order> DgType;
+   //typedef DGAdvectionOperator<ModelType,UpwindFlux,order> DgType;
+   typedef DGLimitedAdvectionOperator<ModelType,UpwindFlux,order> DgType;
+   //typedef DuneODE::ExplRungeKutta<DgType> ODEType;
+   typedef DuneODE::ExplTimeStepper<DgType> ODEType;
+   //typedef DuneODE::ImplTimeStepper<DgType> ODEType;
+#elif PROBLEM == 1
    #include "scalarmodels.hh"
    #include "initadvectdiff.cc"
    typedef U0<GridType> InitialDataType;
+   //typedef U0Disc<GridType> InitialDataType;
+   //typedef AdvectionDiffusionModel<GridPartType,InitialDataType> ModelType;
    typedef AdvectionDiffusionModel<GridPartType,InitialDataType> ModelType;
    // typedef LLFFlux<ModelType> FluxType;
    typedef UpwindFlux<ModelType> FluxType;
    typedef DGAdvectionDiffusionOperator<ModelType,UpwindFlux,order> DgType;
-   typedef DuneODE::ImplTimeStepper<DgType> ODEType;
+   //typedef DGAdvectionOperator<ModelType,UpwindFlux,order> DgType;
+   //typedef DGLimitedAdvectionOperator<ModelType,UpwindFlux,order> DgType;
+   typedef DuneODE::ExplRungeKutta<DgType> ODEType;
+   //typedef DuneODE::ExplTimeStepper<DgType> ODEType;
+   //typedef DuneODE::ImplTimeStepper<DgType> ODEType;
 #elif PROBLEM == 2
    #include "scalarmodels.hh"
    #include "initburgers.cc"
    typedef U0<GridType> InitialDataType;
    typedef BurgersModel<GridPartType,InitialDataType > ModelType;
    typedef LLFFlux<ModelType> FluxType;
-   //typedef DGLimitedAdvectionOperator<ModelType,LLFFlux,order> DgType;
-   typedef DGAdvectionDiffusionOperator<ModelType,LLFFlux,order> DgType;
+   typedef DGLimitedAdvectionOperator<ModelType,LLFFlux,order> DgType;
+   //typedef DGAdvectionDiffusionOperator<ModelType,LLFFlux,order> DgType;
    typedef DuneODE::ExplRungeKutta<DgType> ODEType;
 #elif PROBLEM == 3
 #include "scalarmodels.hh"
@@ -39,7 +59,8 @@ typedef HierarchicGridPart<GridType> GridPartType;
    // typedef LLFFlux<ModelType> FluxType;
    typedef UpwindFlux<ModelType> FluxType;
    typedef DGAdvectionDiffusionOperator<ModelType,UpwindFlux,order> DgType;
-   typedef DuneODE::ExplTimeStepper<DgType> ODEType;
+//   typedef DuneODE::ExplTimeStepper<DgType> ODEType;
+   typedef DuneODE::ImplTimeStepper<DgType> ODEType;
 #elif PROBLEM == 4
 #include "scalarmodels.hh"
 #include "initadvectdiff.cc"
@@ -56,6 +77,7 @@ typedef HierarchicGridPart<GridType> GridPartType;
    typedef HLLNumFlux<ModelType> FluxType;
    // typedef DWNumFlux<ModelType> FluxType;
    typedef DGAdvectionOperator<ModelType,HLLNumFlux,order> DgType;
+   //typedef DuneODE::ImplTimeStepper<DgType> ODEType;
    typedef DuneODE::ExplTimeStepper<DgType> ODEType;
-   // typedef DuneODE::ExplRungeKutta<DgType> ODEType;
+   //typedef DuneODE::ExplRungeKutta<DgType> ODEType;
 #endif
