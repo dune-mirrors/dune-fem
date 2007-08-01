54365edd5b51813f9d0f877b238372de15b720a6
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2007-08-01 16:10:37 +0000

    reverted changes from yesterday.
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@1971 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/space/common/restrictprolonginterface.hh
+++ fem/space/common/restrictprolonginterface.hh
@@ -8,9 +8,15 @@
 #include <dune/fem/misc/combineinterface.hh>
 
 namespace Dune{
-/** @defgroup RestrictProlongInterface Restrict Prolong Interface
+/** @defgroup RestrictProlongInterface Restriction and Prolongation Interface
     @ingroup Adaptation
-    Interface for restrict prolong operation on single elements.
+    Interface for restriction and prolongation operation of data 
+    on single elements.
+
+    \remarks The Interface for a restriction and prolongation operation 
+    is defined by the class RestrictProlongInterface.
+
+    
   @{
  */
 
@@ -98,6 +104,155 @@ private:
   }
 };
 
+/** \brief Traits class for derivation from RestrictProlongInterface. */
+template <class Base>
+struct RestrictProlongTraits {
+  typedef Base RestProlImp;
+  typedef double RangeFieldType;
+};
+
+/*! \brief Allow the combination of two restrict/prolong instances
+ */
+template <class I1,class I2>
+class RestrictProlongPair : 
+    public RestrictProlongInterface<RestrictProlongTraits<RestrictProlongPair<I1,I2> > >,
+    public PairOfInterfaces<I1,I2> {
+public:  
+  typedef PairOfInterfaces<I1,I2> BaseType;
+  typedef typename BaseType::T1Type::RangeFieldType RangeFieldType;
+  typedef typename BaseType::T2Type::RangeFieldType RangeFieldType2;
+
+  RestrictProlongPair(I1 i1, I2 i2) : PairOfInterfaces<I1,I2>(i1,i2) {
+    //IsTrue<SameType<RangeFieldType,RangeFieldType2>::value>::yes();
+  }
+  
+  //! if weight is set, then ists assumend that we have always the same
+  //! proportion between fahter and son volume 
+  void setFatherChildWeight (const RangeFieldType& val) const {
+    this->first().setFatherChildWeight(val);
+    this->second().setFatherChildWeight(val);    
+  }
+  //! restrict data to father 
+  template <class EntityType>
+  void restrictLocal ( EntityType &father, EntityType &son, 
+		       bool initialize ) const {
+    this->first().restrictLocal(father,son,initialize);
+    this->second().restrictLocal(father,son,initialize);    
+  }
+  //! prolong data to children 
+  template <class EntityType>
+  void prolongLocal ( EntityType &father, EntityType &son, 
+		      bool initialize ) const {
+    this->first().prolongLocal(father,son,initialize);
+    this->second().prolongLocal(father,son,initialize);    
+  }
+  
+  //! prolong data to children 
+  template <class CommunicatorImp>
+  void addToList(CommunicatorImp& comm) 
+  {
+    this->first().addToList(comm); 
+    this->second().addToList(comm);    
+  }
+};
+/** \brief This is a general restriction/prolongation operator
+    which is speciallized for some discreteFunctionSpaces (e.g. DG)
+ */
+template <class DiscreteFunctionType> class RestrictProlongDefault :
+    public RestrictProlongInterface<RestrictProlongTraits<RestrictProlongDefault<DiscreteFunctionType> > > {
+};
+
+/** \brief This is a simple restriction/prolongation operator for
+ piecewise constant data stored on elements. 
+*/
+template <class DiscreteFunctionType>
+class RestrictProlongPieceWiseConstantData : 
+public RestrictProlongInterface<
+  RestrictProlongTraits<RestrictProlongPieceWiseConstantData< DiscreteFunctionType > > >
+{
+public:  
+  typedef typename DiscreteFunctionType::LocalFunctionType LocalFunctionType;
+
+  typedef typename DiscreteFunctionType::DiscreteFunctionSpaceType SpaceType; 
+  typedef typename SpaceType :: GridPartType GridPartType;
+
+  typedef typename DiscreteFunctionType::RangeFieldType RangeFieldType;
+  typedef typename DiscreteFunctionType::DomainType DomainType;
+public:  
+  //! Constructor
+  RestrictProlongPieceWiseConstantData( DiscreteFunctionType & df ) : df_ (df), weight_(-1.0)
+  {
+    // make sure index set can be used for adaptive computations 
+  }
+
+  //! if weight is set, then ists assumend that we have always the same
+  //! proportion between fahter and son volume 
+  void setFatherChildWeight (const RangeFieldType& val) const
+  {
+    // volume of son / volume of father  
+    weight_ = val; 
+  }
+  
+  //! restrict data to father 
+  template <class EntityType>
+  void restrictLocal ( EntityType &father, EntityType &son, bool initialize ) const
+  {
+    assert( df_.space().indexSet().adaptive() );
+
+    assert( !father.isLeaf() );
+
+    const RangeFieldType weight = (weight_ < 0.0) ? (this->calcWeight(father,son)) : weight_; 
+
+    assert( weight > 0.0 );
+    
+    LocalFunctionType vati = df_.localFunction( father);
+    LocalFunctionType sohn = df_.localFunction( son   );
+
+    const int numDofs = vati.numDofs();
+    if(initialize)
+    {
+      for(int i=0; i<numDofs; ++i)
+      {
+        vati[i] = weight * sohn[i];
+      }
+    }
+    else 
+    {
+      for(int i=0; i<numDofs; ++i)
+      {
+        vati[i] += weight * sohn[i];
+      }
+    }
+  }
+
+  //! prolong data to children 
+  template <class EntityType>
+  void prolongLocal ( EntityType &father, EntityType &son, bool initialize ) const
+  {
+    assert( df_.space().indexSet().adaptive() );
+    
+    LocalFunctionType vati = df_.localFunction( father);
+    LocalFunctionType sohn = df_.localFunction( son   );
+    const int numDofs = vati.numDofs();
+    for(int i=0; i<numDofs; ++i)
+    {
+      sohn[i] = vati[i];
+    }
+  }
+
+  //! add discrete function to communicator 
+  template <class CommunicatorImp> 
+  void addToList(CommunicatorImp& comm) 
+  {
+    comm.addToList(df_);
+  }
+
+private:
+  mutable DiscreteFunctionType & df_;
+  mutable RangeFieldType weight_;
+};
+
+///@} 
 } // end namespace Dune 
 #include <dune/fem/space/common/restrictprolongimpl.hh>
 #endif
