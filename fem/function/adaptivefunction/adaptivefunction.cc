afd8f655877bedfb4084e7af6e6e5c4117e02f91
Andreas Dedner <A.S.Dedner@warwick.ac.uk>
2007-08-08 01:06:05 +0000

    added const in subFunction for space
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@2093 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/function/adaptivefunction/adaptivefunction.cc
+++ fem/function/adaptivefunction/adaptivefunction.cc
@@ -355,7 +355,7 @@ namespace Dune {
   AdaptiveDiscreteFunction<CombinedSpace<ContainedFunctionSpaceImp, N, p> >::
   subFunction(int component) 
   {
-    SubSpaceType& subSpace = this->spc_.subSpace(component);
+    const SubSpaceType& subSpace = this->space().subSpace(component);
     return SubDiscreteFunctionType(std::string("Subfunction of ")+this->name(),
                                    subSpace,
                                    this->dofStorage());
