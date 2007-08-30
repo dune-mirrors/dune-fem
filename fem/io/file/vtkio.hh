9b6a97e2a9b04302d720241115f395362a42f54d
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2007-08-30 09:40:48 +0000

    VTKIO can be constructed from const GridPart!
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@2162 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/io/file/vtkio.hh
+++ fem/io/file/vtkio.hh
@@ -1,67 +1,87 @@
 #ifndef VTKIO_HH
 #define VTKIO_HH
+
 #include <dune/grid/io/file/vtk/vtkwriter.hh>
-namespace Dune {
-template <class DF>
-class VTKFunctionWrapper : 
-  public VTKWriter<typename DF::FunctionSpaceType::GridPartType::GridType,
-                   typename DF::FunctionSpaceType::GridPartType::IndexSetType>::VTKFunction {
-public:
-  typedef DF DiscreteFunctionType;
-  typedef typename DF::LocalFunctionType LocalFunctionType;
-  typedef typename DF::FunctionSpaceType FunctionSpaceType;
-  typedef typename FunctionSpaceType::DomainType DomainType;
-  typedef typename FunctionSpaceType::RangeType RangeType;
-  typedef typename FunctionSpaceType::GridPartType GridPartType;
-  typedef typename FunctionSpaceType::IteratorType::Entity EntityType;
-  enum {DimRange=FunctionSpaceType::DimRange,
-        DimDomain=FunctionSpaceType::DimDomain};
-  //! constructor taking discrete function 
-  VTKFunctionWrapper(const DiscreteFunctionType& df,
-                     int component)
-    : discFunc_(df),
-      component_(component) {}
-  //! return number of components
-  virtual int ncomps () const {
-    return (component_>=0) ? 1 : DimRange;
-  }
-  //! evaluate single component comp in
-  //! the entity
-  virtual double evaluate (int comp, const EntityType& e, const DomainType& xi) const {
-    const LocalFunctionType lf = discFunc_.localFunction(e);
-    RangeType val;
-    lf.evaluate(xi,val);
-    if (component_<0)
-      return val[comp];
-    else 
-      return val[component_];
-  }
-  //! get name
-  virtual std::string name () const {
-    if (component_<0) 
-      return discFunc_.name();
-    std::stringstream ret;
-    ret << discFunc_.name() << "-" << component_;
-    return ret.str();
-  }
-  //! virtual destructor
-  virtual ~VTKFunctionWrapper () {}
-private:
-  const DF& discFunc_;
-  const int component_;
-};
-template <class GridPartType>
-class VTKIO : 
-  public VTKWriter<typename GridPartType::GridType,
-                   typename GridPartType::IndexSetType> {
-  typedef VTKWriter<typename GridPartType::GridType,
-                    typename GridPartType::IndexSetType> BaseType;
+
+namespace Dune
+{
+
+  template <class DF>
+  class VTKFunctionWrapper : 
+    public VTKWriter<typename DF::FunctionSpaceType::GridPartType::GridType,
+                     typename DF::FunctionSpaceType::GridPartType::IndexSetType>::VTKFunction {
   public:
-    VTKIO(GridPartType& part) : 
-      BaseType(part.grid(),part.indexSet()) {
-      }
-    template <class DF>
-    void addCellData(DF& df,bool vector=false) {
+    typedef DF DiscreteFunctionType;
+    typedef typename DF::LocalFunctionType LocalFunctionType;
+    typedef typename DF::FunctionSpaceType FunctionSpaceType;
+    typedef typename FunctionSpaceType::DomainType DomainType;
+    typedef typename FunctionSpaceType::RangeType RangeType;
+    typedef typename FunctionSpaceType::GridPartType GridPartType;
+    typedef typename FunctionSpaceType::IteratorType::Entity EntityType;
+    enum {DimRange=FunctionSpaceType::DimRange,
+          DimDomain=FunctionSpaceType::DimDomain};
+    //! constructor taking discrete function 
+    VTKFunctionWrapper(const DiscreteFunctionType& df,
+                       int component)
+      : discFunc_(df),
+        component_(component) {}
+    //! return number of components
+    virtual int ncomps () const {
+      return (component_>=0) ? 1 : DimRange;
+    }
+    //! evaluate single component comp in
+    //! the entity
+    virtual double evaluate (int comp, const EntityType& e, const DomainType& xi) const {
+      const LocalFunctionType lf = discFunc_.localFunction(e);
+      RangeType val;
+      lf.evaluate(xi,val);
+      if (component_<0)
+        return val[comp];
+      else 
+        return val[component_];
+    }
+    //! get name
+    virtual std::string name () const {
+      if (component_<0) 
+        return discFunc_.name();
+      std::stringstream ret;
+      ret << discFunc_.name() << "-" << component_;
+      return ret.str();
+    }
+    //! virtual destructor
+    virtual ~VTKFunctionWrapper () {}
+  private:
+    const DF& discFunc_;
+    const int component_;
+  };
+
+
+
+  template< class GridPartImp >
+  class VTKIO
+  : public VTKWriter< typename GridPartImp :: GridType,
+                      typename GridPartImp :: IndexSetType >
+  {
+  public:
+    typedef GridPartImp GridPartType;
+
+    typedef typename GridPartType :: GridType GridType;
+    typedef typename GridPartType :: IndexSetType IndexSetType;
+
+  private:
+    typedef VTKIO< GridPartType > ThisType;
+    typedef VTKWriter< GridType, IndexSetType > BaseType;
+    
+  public:
+    VTKIO( const GridPartType &gridPart )
+    : BaseType( gridPart.grid(), gridPart.indexSet() )
+    {
+    }
+
+    template< class DF >
+    void addCellData( DF &df,
+                      bool vector=false )
+    {
       typedef typename DF::FunctionSpaceType FunctionSpaceType;
       enum {DimRange=FunctionSpaceType::DimRange};
       if (vector) {
@@ -71,8 +91,11 @@ class VTKIO :
           BaseType::addCellData(new VTKFunctionWrapper<DF>(df,i)); 
       }
     }
-    template <class DF>
-    void addVertexData(DF& df,bool vector=false) {
+    
+    template< class DF >
+    void addVertexData( DF &df,
+                        bool vector=false )
+    {
       typedef typename DF::FunctionSpaceType FunctionSpaceType;
       enum {DimRange=FunctionSpaceType::DimRange};
       if (vector) {
@@ -82,6 +105,8 @@ class VTKIO :
           BaseType::addVertexData(new VTKFunctionWrapper<DF>(df,i)); 
       }
     }
-};
+  };
+  
 }
+
 #endif
