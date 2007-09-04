5508aa1752d308bcd1a5afbd54740701207a26fc
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2007-09-04 15:14:42 +0000

    Made constructors explicit, added ThisType declarations
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@2174 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/space/common/allgeomtypes.hh
+++ fem/space/common/allgeomtypes.hh
@@ -12,50 +12,73 @@
 #include <dune/grid/uggrid.hh>
 #endif
 
-namespace Dune {
- 
+namespace Dune
+{
 
   /**  \brief default implementation uses method geomTypes of given index
        set. Used in DiscreteFunctionSpaces.
    */
-  template <class IndexSetImp, class GridImp> 
+  template< class IndexSetImp, class GridImp >
   class AllGeomTypes 
   {
+  public:
     typedef IndexSetImp IndexSetType;
-    const IndexSetType & indexSet_;
+    typedef GridImp GridType;
+
+  private:
+    typedef AllGeomTypes< IndexSetType, GridType > ThisType;
+
+  protected:
+    const IndexSetType &indexSet_;
+    
   public:
     //! constructor storing index set reference 
-    AllGeomTypes(const IndexSetType & indexSet) 
-      : indexSet_(indexSet) 
-    {}                  
+    inline explicit AllGeomTypes( const IndexSetType &indexSet )
+    : indexSet_( indexSet )
+    {
+    }
     
     //! returns vector with geometry tpyes this index set has indices for
-    const std::vector <GeometryType> & geomTypes (int codim) const
+    const std :: vector< GeometryType > &geomTypes ( int codim ) const
     {
-      return indexSet_.geomTypes(codim);
+      return indexSet_.geomTypes( codim );
     }
 
     //! UGGrid might have different geom types 
-    static bool multipleGeomTypes () { return false; }
+    static bool multipleGeomTypes ()
+    {
+      return false;
+    }
   };
+
+
 
 #if USE_UG 
   /** \brief specialisation fir UGGrid, because geomTypes method of index
       sets not usable in this case. 
   */
-  template <class IndexSetImp, int dimworld> 
-  class AllGeomTypes<IndexSetImp, UGGrid<dimworld> >
+  template< class IndexSetImp, int dimworld >
+  class AllGeomTypes< IndexSetImp, UGGrid< dimworld > >
   {
+  public:
     typedef IndexSetImp IndexSetType;
+    typedef UGGrid< dimworld > GridType;
+
+  private:
+    typedef AllGeomTypes< IndexSetType, GridType > ThisType;
+
+    CompileTimeChecker< (dimworld == 2) || (dimworld == 3) >
+      __UGGrid_Works_Only_for_Dimensions_2_and_3__;
+   
+  protected:
     enum { ncodim = dimworld + 1 };
     
-    std::vector< GeometryType > geomTypes_[ncodim]; 
+    std::vector< GeometryType > geomTypes_[ ncodim ];
+
   public:
     //! constructor storing index set reference 
-    AllGeomTypes(const IndexSetType & indexSet) 
+    inline explicit AllGeomTypes( const IndexSetType &indexSet )
     {
-      // UGGrid only working for dimension 2 or 3 
-      assert( (dimworld == 2) || (dimworld == 3) ); 
       // vertices 
       {
         geomTypes_[dimworld].push_back( GeometryType(GeometryType::simplex,0)); 
@@ -91,18 +114,22 @@ namespace Dune {
     }                  
     
     //! returns vector with geometry types this index set has indices for
-    const std::vector <GeometryType> & geomTypes (int codim) const
+    const std :: vector< GeometryType > &geomTypes ( int codim ) const
     {
-      assert( codim >= 0 );
-      assert( codim < ncodim );
-      return geomTypes_[codim];
+      assert( (codim >= 0) && (codim < ncodim) );
+      return geomTypes_[ codim ];
     }
 
     //! UGGrid might have different geom types 
-    static bool multipleGeomTypes () { return true; }
+    static bool multipleGeomTypes ()
+    {
+      return true;
+    }
   };
 #endif
+
 #undef USE_UG
 
-} // end namespace Dune 
+} // end namespace Dune
+
 #endif
