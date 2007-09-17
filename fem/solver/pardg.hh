35cf6b22680874ea68d2607db1621ea18dcb4862
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2007-09-17 10:52:03 +0000

    also vector needs to be in parDG namespace, otherwise conflicts with Dune::Vector.
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@2212 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/solver/pardg.hh
+++ fem/solver/pardg.hh
@@ -1,7 +1,9 @@
 #ifndef PARDG_INCLUDE_HH
 #define PARDG_INCLUDE_HH
 
-#define USE_PARDG_ODE_SOLVER
+//#ifdef ENABLE_PARDG 
+#define USE_PARDG_ODE_SOLVER 
+//#endif
 
 // if the preprocessor variable is defined, the ODE Solver from Dennis
 // are used.
@@ -18,17 +20,21 @@ namespace pardg {
 // if pardg library was found 
 #ifdef ENABLE_PARDG 
 
+#include <blas.hpp>
+// we also need vector to be in namespace parDG 
+namespace pardg {
+#include <vector.hpp>
+}
+
 #include <ode_solver.hpp>
 #include <linear_solver.hpp>
 
 // else use build in ode solver (may be outdated)
 #else
-
 #include "ode/blas.hpp"
 #include "ode/function.hpp"
 #include "ode/ode_solver.hpp"
 #include "ode/linear_solver.hpp"
-
 #endif
 
 #endif // end USE_DENNIS 
