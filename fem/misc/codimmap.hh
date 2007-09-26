89251ab727f35b938e87eff66d9208c6725b6810
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2007-09-26 07:33:30 +0000

    Documented CodimMap
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@2254 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/misc/codimmap.hh
+++ fem/misc/codimmap.hh
@@ -4,10 +4,44 @@
 namespace Dune
 {
 
+  /** \class CodimMap
+   *  \brief Simple array-like map for templates parameterized over codimensions
+   *
+   *  In some cases we need to call functions that depend on the codimension
+   *  as template parameter. Now, if the codimension is actually a variable,
+   *  the fastest way of accessing the functions is through an array mapping
+   *  the codimension of the function. Building this array has to be done by
+   *  template metaprogramming and hence is tedious and error prone. So, this
+   *  is exactly what CodimMap does implicitly. Even the destruction of these
+   *  objects is done implicitly be the destructor; you only need to destroy
+   *  the CodimMap.
+   *
+   *  To use CodimMap you provide a class template (CodimObjectImp) depending
+   *  only on the codimension as template parameter. All instances of this
+   *  template have to have a common, virtual base class and a constructor
+   *  requiring no arguments. CodimMap will then create an array of numCodims
+   *  instances of CodimObjectImp. To the outside world it presents these
+   *  instances as an array of objects of the common base class.
+   *
+   *  For example, a CodimMap could be created as follows:
+   *  \code
+   *    CodimMap< dimension+1, MyCodimObjectImp > map;
+   *  \endcode
+   *  Then, accessing the objects is similar to accessing an array:
+   *  \code
+   *    MyCodimBaseType &object = map[ codim ];
+   *  \endcode
+   *
+   *  \param  nCodims         number of codimensions to create (usually
+   *                          dimension+1)
+   *  \param  CodimObjectImp  class template (parameterized by the codimension)
+   *                          of which nCodims instances shall be created
+   */
   template< unsigned int nCodims, template< unsigned int > class CodimObjectImp >
   class CodimMap
   {
   public:
+    //! number of codimensions (= size of the array)
     enum { numCodims = nCodims };
 
   private:
@@ -16,29 +50,44 @@ namespace Dune
     CompileTimeChecker< (numCodims > 0) > __numCodims_Must_Be_Positive__;
 
   public:
+    //! common base type of all the instances of CodimObjectImp
     typedef typename CodimObjectImp< 0 > :: BaseType CodimObjectBaseType;
 
   protected:
     CodimObjectBaseType *codimObjects_[ numCodims ];
 
   public:
+    //! constructor building the CodimMap
     inline CodimMap ()
     {
       Factory< ThisType, numCodims - 1 > :: getObjects( *this );
     }
     
+    //! destructor freeing all the instances of CodimObjectImp
     inline ~CodimMap ()
     {
       for( unsigned int codim = 0; codim < numCodims; ++codim )
         delete codimObjects_[ codim ];
     }
     
+    /** \brief access the instance for one codimension
+     *
+     *  \param[in]  codim  codimension, the instance is to be obtained for
+     *
+     *  \returns instance of CodimObjectImp with the given codimension
+     */
     inline const CodimObjectBaseType &operator[] ( unsigned int codim ) const
     {
       assert( codim < numCodims );
       return *(codimObjects_[ codim ]);
     }
 
+    /** \brief access the instance for one codimension
+     *
+     *  \param[in]  codim  codimension, the instance is to be obtained for
+     *
+     *  \returns instance of CodimObjectImp with the given codimension
+     */
     inline CodimObjectBaseType &operator[] ( unsigned int codim )
     {
       assert( codim < numCodims );
