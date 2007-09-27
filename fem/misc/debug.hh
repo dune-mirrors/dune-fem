05c23cd5d5a39cd5dca3fe08dd963b87e57f793d
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2007-09-27 08:33:46 +0000

    Documented DebugCounter
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@2255 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/misc/debug.hh
+++ fem/misc/debug.hh
@@ -4,10 +4,23 @@
 namespace Dune
 {
 
+  /** \class DebugCounter
+   *  \brief A counter only present if NDEBUG is not defined
+   *
+   *  There are several cases, where we need a counter for debugging purposes
+   *  that shall only be present, if NDEBUG is not defined. Since it is quite
+   *  tedious to write the #ifndefs repeatedly, it is provided here.
+   *
+   *  In debug mode, this counter wraps a standard integer type, otherwise its
+   *  size is zero.
+   *
+   *  \Note The comparison operators always return true, if NDEBUG is defined!
+   */
   template< class CounterImp = unsigned int >
   class DebugCounter
   {
   public:
+    //! integral type for the actual counting
     typedef CounterImp CounterType;
     
   private:
@@ -19,6 +32,13 @@ namespace Dune
 #endif
     
   public:
+    /** \brief constructor
+     *
+     *  \note This constructor implicitly defines a conversion from CounterType
+     *        to DebugCounter. This is very useful in comparison statements.
+     *
+     *  \param[in]  count  value to initialize the counter with (defaults to 0)
+     */
     inline DebugCounter ( const CounterType count = 0 )
 #ifndef NDEBUG
     : count_( count )
@@ -26,6 +46,8 @@ namespace Dune
     {
     }
 
+    /** \brief copy constructor
+     */
     inline DebugCounter ( const ThisType &other )
 #ifndef NDEBUG
     : count_( other.count_ )
@@ -33,6 +55,12 @@ namespace Dune
     {
     }
 
+    /** \brief increment operator
+     *
+     *  If NDEBUG is not defined, the counter is incremented by 1. Otherwise
+     *  nothing happens (and the entire call will be removed during
+     *  oprimization).
+     */
     inline ThisType &operator++ ()
     {
 #ifndef NDEBUG
@@ -41,6 +69,12 @@ namespace Dune
       return *this;
     }
 
+    /** \brief decrement operator
+     *
+     *  If NDEBUG is not defined, the counter is decremented by 1. Otherwise
+     *  nothing happens (and the entire call will be removed during
+     *  oprimization).
+     */
     inline ThisType &operator-- ()
     {
 #ifndef NDEBUG
@@ -49,6 +83,18 @@ namespace Dune
       return *this;
     }
 
+    /** \brief comparison for equality
+     *
+     *  Compares to DebugCounters for equality. If NDEBUG is defined, the
+     *  result will be true.
+     *
+     *  \Note Due to the implicit conversion, the second argument may also be
+     *        of CounterType.
+     *
+     *  \param[in]  other  DebugCounter to compare this one to
+     *
+     *  \returns true, if the counters equal or NDEBUG is defined
+     */
     inline bool operator== ( const ThisType &other )
     {
 #ifndef NDEBUG
@@ -58,6 +104,18 @@ namespace Dune
 #endif
     }
 
+    /** \brief comparison for inequality
+     *
+     *  Compares to DebugCounters for inequality. If NDEBUG is defined, the
+     *  result will be true.
+     *
+     *  \Note Due to the implicit conversion, the second argument may also be
+     *        of CounterType.
+     *
+     *  \param[in]  other  DebugCounter to compare this one to
+     *
+     *  \returns true, if the counters differ or NDEBUG is defined
+     */
     inline bool operator!= ( const ThisType &other )
     {
 #ifndef NDEBUG
@@ -68,7 +126,6 @@ namespace Dune
     }
   };
 
-  
 };
 
 #endif
