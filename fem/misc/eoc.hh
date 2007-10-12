4180f693b905c5523c85e75a27b1299bc5f54cf6
Andreas Dedner <A.S.Dedner@warwick.ac.uk>
2007-10-12 03:12:44 +0000

    also gnu output in eoc.hh (starting point)
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@2338 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/misc/eoc.hh
+++ fem/misc/eoc.hh
@@ -1,3 +1,17 @@
+#ifndef DUNE_EOCOUTPUT_HH
+#define DUNE_EOCOUTPUT_HH
+
+namespace Dune{
+
+/*======================================================================*/
+/*!  @ingroup HelperClasses
+ *   Write a self contained tex table and a gnuplot
+ *   file for eoc runs with timing information.
+ *   Constructor takes base name (filename) of file and
+ *   generates two files:
+ *   filename.tex and filename.gnu
+ */
+/*======================================================================*/
 class EocOutput {
   std::string outputFile;
   int level;
@@ -11,47 +25,48 @@ class EocOutput {
   , prevError(0)
   , initial(true)
   , finalized(false) {
-    outputFile = name;
-    std::ostringstream filestream;
-    filestream << outputFile;
-    std::ofstream ofs(filestream.str().c_str(), std::ios::out);
+    {
+      std::ostringstream filestream;
+      filestream << outputFile << ".tex";
+      std::ofstream ofs(filestream.str().c_str(), std::ios::out);
 
-    ofs << "\\documentclass[12pt,english]{article}\n"
-       	<< "\\usepackage[T1]{fontenc}\n"
-	      << "\\usepackage[latin1]{inputenc}\n"
-	      << "\\usepackage{setspace}\n"
-	      << "\\onehalfspacing\n"
-	      << "\\makeatletter\n"
-	      << "\\providecommand{\\boldsymbol}[1]{\\mbox{\\boldmath $#1$}}\n"
-	      << "\\providecommand{\\tabularnewline}{\\\\}\n"
-	      << "\\usepackage{babel}\n"
-	      << "\\makeatother\n"
-	      << "\\begin{document}\n";
-
-    ofs.close();	
+      ofs << "\\documentclass[12pt,english]{article}\n"
+         	<< "\\usepackage[T1]{fontenc}\n"
+	        << "\\usepackage[latin1]{inputenc}\n"
+	        << "\\usepackage{setspace}\n"
+	        << "\\onehalfspacing\n"
+	        << "\\makeatletter\n"
+	        << "\\providecommand{\\boldsymbol}[1]{\\mbox{\\boldmath $#1$}}\n"
+	        << "\\providecommand{\\tabularnewline}{\\\\}\n"
+	        << "\\usepackage{babel}\n"
+	        << "\\makeatother\n"
+	        << "\\begin{document}\n";
+      ofs.close();	
+    }
+    {
+      std::ostringstream filestream;
+      filestream << outputFile << ".gnu";
+      std::ofstream ofs(filestream.str().c_str(), std::ios::out);
+      ofs.close();
+    }
   }
   ~EocOutput() {
     if (!finalized) {
       std::ostringstream filestream;
-      filestream << outputFile;
+      filestream << outputFile << ".tex";
       std::ofstream ofs(filestream.str().c_str(), std::ios::app);
-		
       ofs  << "\\end{tabular}\\\\\n\n"
 	         << "\\end{document}\n" << std::endl;
-		
       ofs.close();
     }
   }
   void printTexEnd(double totaltime) {
     std::ostringstream filestream;
-    filestream << outputFile;
-
+    filestream << outputFile << ".tex";
     std::ofstream ofs(filestream.str().c_str(), std::ios::app);
-		
     ofs  << "\\end{tabular}\\\\\n\n"
 	       << "Total time: " << totaltime << "\n"
 	       << "\\end{document}\n" << std::endl;
-		
     ofs.close();
     finalized = true;
   }
@@ -62,9 +77,10 @@ class EocOutput {
                   const GridType& grid,
                   const SolverType& solver) {
     std::ostringstream filestream;
-    filestream << outputFile;
-
+    filestream << outputFile << ".tex";
     std::ofstream ofs(filestream.str().c_str(), std::ios::app);
+    filestream << outputFile << ".gnu";
+    std::ofstream ofsGnu(filestream.str().c_str(), std::ios::app);
 
     ofs  << "Grid: "      << grid.name() << "\\\\"
 	       << "\n";
@@ -76,8 +92,11 @@ class EocOutput {
                         double time, 
                         int counter) {
     std::ostringstream filestream;
-    filestream << outputFile;
+    filestream << outputFile << ".tex";
     std::ofstream ofs(filestream.str().c_str(), std::ios::app);
+    std::ostringstream filestreamGnu;
+    filestreamGnu << outputFile << ".gnu";
+    std::ofstream ofsGnu(filestreamGnu.str().c_str(), std::ios::app);
 		
     if (initial) {
 	    ofs << "\\begin{tabular}{|c|c|c|";
@@ -100,21 +119,33 @@ class EocOutput {
 	      << level << " & "
         << time << " & " 
         << counter;
+	  ofsGnu << level << "  "
+           << time << "  " 
+           << counter;
     for (int i=0;i<error.size();++i) {
       ofs << " & " << error[i] << " & ";
       if (initial)
         ofs << " --- ";
       else 
         ofs << log(prevError[i]/error[i])/M_LN2;
+      ofsGnu << "    " << error[i] << " ";
+      if (initial)
+        ofsGnu << " 1/0 ";
+      else 
+        ofsGnu << log(prevError[i]/error[i])/M_LN2;
     }
 	  ofs << "\n"
         << "\\tabularnewline\n"
 	      << "\\hline \n";
+    ofsGnu << "\n";
     prevError = error;
     level++;
     initial = false;
     ofs.close();
+    ofsGnu.close();
   }
 
 };
 
+}
+#endif
--- fem/misc/example.cc
+++ fem/misc/example.cc
@@ -1,3 +1,6 @@
+#include <cmath>
+#include <config.h>
+#include <dune/common/tuples.hh>
 #include <iostream>
 #include "combineinterface.hh"
 
@@ -7,9 +10,9 @@ struct InterfaceA {
   InterfaceA(const InterfaceA<Impl>& a);
   InterfaceA() {}
   const InterfaceA& operator=(const InterfaceA&);
+  Impl& asImp() {return static_cast<Impl&>(*this);}
  public:
   int a(int i) {return asImp().a(i);}
-  Impl& asImp() {return static_cast<Impl&>(*this);}
 };
 struct InterfaceAImpl1 : public InterfaceA<InterfaceAImpl1> {
   int a(int i) {return i*i;}
@@ -18,15 +21,27 @@ struct InterfaceAImpl2 : public InterfaceA<InterfaceAImpl2> {
   int a(int i) {return -i*i;}
 };
 // now how to combine two interface implementations 
+
 template <class I1,class I2>
-struct InterfaceAPair : 
-  public InterfaceA<InterfaceAPair<I1,I2> >,
+struct InterfaceAPairPlus : 
+  public InterfaceA<InterfaceAPairPlus<I1,I2> >,
   public Dune::PairOfInterfaces<I1,I2> {
   // always use the following interface
-  InterfaceAPair(I1 i1, I2 i2) : Dune::PairOfInterfaces<I1,I2>(i1,i2) {}
+  InterfaceAPairPlus(I1 i1, I2 i2) : Dune::PairOfInterfaces<I1,I2>(i1,i2) {}
   // the result of the interface method should be summed up...
   int a(int i) {
     return this->first().a(i) + this->second().a(i);
+  }
+};
+template <class I1,class I2>
+struct InterfaceAPairPow : 
+  public InterfaceA<InterfaceAPairPow<I1,I2> >,
+  public Dune::PairOfInterfaces<I1,I2> {
+  // always use the following interface
+  InterfaceAPairPow(I1 i1, I2 i2) : Dune::PairOfInterfaces<I1,I2>(i1,i2) {}
+  // the result of the interface method should be summed up...
+  int a(int i) {
+    return this->first().a(i)*this->second().a(i);
   }
 };
 template <class A>
@@ -39,7 +54,7 @@ int test2(InterfaceA<A1>& impl1,
   typedef InterfaceA<A1> I1Type;
   typedef InterfaceA<A2> I2Type;
   // Can now further combine the interfaces also using reference and pointer
-  typedef Dune::CombineInterface<InterfaceAPair,I1Type*,I1Type&,I1Type&,
+  typedef Dune::CombineInterface<InterfaceAPairPlus,I1Type*,I1Type&,I1Type&,
     InterfaceAImpl1&,InterfaceAImpl1&,InterfaceAImpl2&,InterfaceAImpl2&,
     I2Type&,I2Type&> CIType;
   InterfaceAImpl1 a1;
@@ -48,13 +63,13 @@ int test2(InterfaceA<A1>& impl1,
   return ci.a(10);
 }
 int main() {
-  typedef Dune::CombineInterface<InterfaceAPair,
+  typedef Dune::CombineInterface<InterfaceAPairPlus,
     InterfaceAImpl1&,InterfaceAImpl2&,InterfaceAImpl2&> InterfaceAImpl122;
   InterfaceAImpl1 i1;
   InterfaceAImpl2 i2,i3;
   InterfaceAImpl122 i122(i1,i2,i3);
   // InterfaceAImpl122 is again an interface implementation...
-  typedef Dune::CombineInterface<InterfaceAPair,
+  typedef Dune::CombineInterface<InterfaceAPairPlus,
     InterfaceAImpl1&,InterfaceAImpl122&,InterfaceAImpl1&,InterfaceAImpl122&> 
     InterfaceAImpl11221122;
   InterfaceAImpl11221122 i11221122(i1,i122,i1,i122);
@@ -64,4 +79,11 @@ int main() {
   std::cout << "Example of recombination: " 
 	    << "result is A11111221122112211221122 (13xA1 + 10xA2)" << std::endl;
   std::cout << test2(i1,i11221122) << std::endl;
+  typedef Dune::CombineInterface<InterfaceAPairPow,
+    InterfaceAImpl1&,InterfaceAImpl122&,InterfaceAImpl11221122&> 
+    InterfaceAImpl122Pow11221122;
+  InterfaceAImpl122Pow11221122 i122Pow11221122(i1,i122,i11221122);
+  std::cout << "Combined with power function: "
+    << "a^" << i11221122.a(10) << " = "
+    << i122Pow11221122.a(10) << std::endl;
 }
