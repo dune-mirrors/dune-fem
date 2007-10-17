0217649a1c42f3dd073299d22ac3783ed91c5503
Andreas Dedner <A.S.Dedner@warwick.ac.uk>
2007-10-17 15:00:10 +0000

    seperate table from main tex file
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@2359 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/misc/eoc.hh
+++ fem/misc/eoc.hh
@@ -3,32 +3,45 @@
 
 namespace Dune{
 
-/*======================================================================*/
-/*!  @ingroup HelperClasses
- *   Write a self contained tex table and a gnuplot
- *   file for eoc runs with timing information.
- *   Constructor takes base name (filename) of file and
- *   generates two files:
- *   filename.tex and filename.gnu
+/**  
+    @ingroup HelperClasses
+    \brief Write a self contained tex table and a gnuplot
+    file for eoc runs with timing information.
+    
+    Constructor takes base name (filename) of file and
+    generates two files:
+    filename.tex, filename_body.tex and filename.gnu
+    The file filename_body.tex hold the actual body
+    of the tex file which is included in filename.tex
+    but can also be used to combine runs with different
+    parameters.
  */
-/*======================================================================*/
 class EocOutput {
-  std::string outputFile;
+ std::string outputFile;
   int level;
   std::vector<double> prevError;
+  double prevSize;
   bool initial;
   bool finalized;
  public:
-  EocOutput(std::string name) :
+  //! Constructor taking base name of file and
+  //! and a header for the page which
+  //! should be valid tex code. 
+  EocOutput(std::string name,
+            std::string descript) :
     outputFile(name)
   , level(0)
   , prevError(0)
+  , prevSize(0)
   , initial(true)
   , finalized(false) {
     {
       std::ostringstream filestream;
       filestream << outputFile << ".tex";
       std::ofstream ofs(filestream.str().c_str(), std::ios::out);
+      std::ostringstream filestreamBody;
+      filestreamBody << outputFile << "_body.tex";
+      std::ofstream ofsBody(filestreamBody.str().c_str(), std::ios::out);
 
       ofs << "\\documentclass[12pt,english]{article}\n"
          	<< "\\usepackage[T1]{fontenc}\n"
@@ -40,8 +53,15 @@ class EocOutput {
 	        << "\\providecommand{\\tabularnewline}{\\\\}\n"
 	        << "\\usepackage{babel}\n"
 	        << "\\makeatother\n"
-	        << "\\begin{document}\n";
+	        << "\\begin{document}\n"
+          << "\\begin{center}\\large\n"
+          << descript 
+          << "\n\\end{center}\n\n"
+          << "\\include{"
+          << filestreamBody.str() 
+          << "}\n";
       ofs.close();	
+      ofsBody.close();	
     }
     {
       std::ostringstream filestream;
@@ -50,98 +70,143 @@ class EocOutput {
       ofs.close();
     }
   }
+  //! Destrucor writes the end document section to file
+  //! if not done explicitly in the printTexEnd method
   ~EocOutput() {
     if (!finalized) {
       std::ostringstream filestream;
       filestream << outputFile << ".tex";
       std::ofstream ofs(filestream.str().c_str(), std::ios::app);
-      ofs  << "\\end{tabular}\\\\\n\n"
-	         << "\\end{document}\n" << std::endl;
+      std::ostringstream filestreamBody;
+      filestreamBody << outputFile << "_body.tex";
+      std::ofstream ofsBody(filestreamBody.str().c_str(), std::ios::app);
+      ofsBody  << "\\end{tabular}\\\\\n\n";
+	    ofs << "\\end{document}\n" << std::endl;
       ofs.close();
+      ofsBody.close();
     }
   }
+  //! write end of document information 
+  //! and add the total runtime of the simulation
   void printTexEnd(double totaltime) {
     std::ostringstream filestream;
     filestream << outputFile << ".tex";
     std::ofstream ofs(filestream.str().c_str(), std::ios::app);
-    ofs  << "\\end{tabular}\\\\\n\n"
-	       << "Total time: " << totaltime << "\n"
-	       << "\\end{document}\n" << std::endl;
+    std::ostringstream filestreamBody;
+    filestreamBody << outputFile << "_body.tex";
+    std::ofstream ofsBody(filestreamBody.str().c_str(), std::ios::app);
+    ofsBody  << "\\end{tabular}\\\\\n\n"
+	       << "Total time: " << totaltime << "\n";
+	  ofs << "\\end{document}\n" << std::endl;
     ofs.close();
+    ofsBody.close();
     finalized = true;
   }
-	
+  //! header information on setting for this simulation
+  //! at the moment a grid class and 
+  //! two other clases are passed to this method
+  //! which should implement a method
+  //!   printTexInfo(std::ostream&) const
   template <class GridType,class ProblemType,
             class SolverType>
   void printInput(const ProblemType& problem, 
                   const GridType& grid,
                   const SolverType& solver) {
-    std::ostringstream filestream;
-    filestream << outputFile << ".tex";
-    std::ofstream ofs(filestream.str().c_str(), std::ios::app);
-    filestream << outputFile << ".gnu";
-    std::ofstream ofsGnu(filestream.str().c_str(), std::ios::app);
-
-    ofs  << "Grid: "      << grid.name() << "\\\\"
-	       << "\n";
-	  solver.printTexInfo(ofs);
-    problem.printTexInfo(ofs);
-    ofs.close();
+    std::ostringstream filestreamBody;
+    filestreamBody << outputFile << "_body.tex";
+    std::ofstream ofsBody(filestreamBody.str().c_str(), std::ios::app);
+    ofsBody  << "Grid: "      << grid.name() << "\\\\"
+	           << "\n";
+	  solver.printTexInfo(ofsBody);
+    problem.printTexInfo(ofsBody);
+    ofsBody.close();
   }
-  void printTexAddError(std::vector<double> error, 
+  //! \brief Write tabular for error and eoc output
+  //!
+  //! The eoc is computed using the size parameter
+  //! and the error - previous size and errors are
+  //! saved. The formular used is
+  //! \f{eqnarray*} 
+  //! \frac{\log(e_{\rm old}/e_{\rm new})}
+  //!         {\log(S_{\rm old}/S_{\rm new})} 
+  //! \f}
+  //! where \f$ S \f$ is the size parameter. 
+  //! 
+  //! If for example the number of element \f$ N \f$
+  //! in the grid is to be used to compute the eoc, then 
+  //! \f{eqnarray*}
+  //! S=\frac{1}{N^{\frac{1}{d}}}
+  //! \f}
+  //! where \f$ d \f$ is the dimension of the grid.
+  //!
+  //! \param size: the size of the grid which is used to compute the EOC
+  //! \param error: a vector of computed error
+  //! \param errDescr: a vector of column headers for the errors
+  //! \param time: run time for this step of the simulation
+  //! \param counter: could be time step counter or number of refinment
+  //!                 steps for example
+  void printTexAddError(double size,
+                        std::vector<double> error, 
+                        std::vector<std::string> errDescr,
                         double time, 
                         int counter) {
-    std::ostringstream filestream;
-    filestream << outputFile << ".tex";
-    std::ofstream ofs(filestream.str().c_str(), std::ios::app);
+    std::ostringstream filestreamBody;
+    filestreamBody << outputFile << "_body.tex";
+    std::ofstream ofsBody(filestreamBody.str().c_str(), std::ios::app);
     std::ostringstream filestreamGnu;
     filestreamGnu << outputFile << ".gnu";
     std::ofstream ofsGnu(filestreamGnu.str().c_str(), std::ios::app);
 		
     if (initial) {
-	    ofs << "\\begin{tabular}{|c|c|c|";
+      assert(errDescr.size()==error.size());
+	    ofsBody << "\\begin{tabular}{|c|c|c|c|";
       for (int i=0;i<error.size();i++) {
-        ofs << "|cc|";
+        ofsBody << "|cc|";
       }
-      ofs << "}\n"
+      ofsBody << "}\n"
 	        << "\\hline \n"
-          << "level & CPU-time & counter";
+          << "level & size & CPU-time & counter";
       for (int i=0;i<error.size();i++) {
-        ofs << " & error & eoc ";
+        ofsBody << " & " << errDescr[i]
+                << " & eoc ";
       }
-	    ofs << "\n \\tabularnewline\n"
-	        << "\\hline\n"
-	        << "\\hline\n";
+	    ofsBody << "\n \\tabularnewline\n"
+	            << "\\hline\n"
+	            << "\\hline\n";
     } else {
       assert(error.size()==prevError.size());
     }
-	  ofs <<  "\\hline \n"
-	      << level << " & "
-        << time << " & " 
-        << counter;
+	  ofsBody <<  "\\hline \n"
+	          << level << " & "
+            << size << " & "
+            << time << " & " 
+            << counter;
 	  ofsGnu << level << "  "
+           << size << "  " 
            << time << "  " 
            << counter;
     for (int i=0;i<error.size();++i) {
-      ofs << " & " << error[i] << " & ";
-      if (initial)
-        ofs << " --- ";
-      else 
-        ofs << log(prevError[i]/error[i])/M_LN2;
+      ofsBody << " & " << error[i] << " & ";
       ofsGnu << "    " << error[i] << " ";
-      if (initial)
+      if (initial) {
+        ofsBody << " --- ";
         ofsGnu << " 1/0 ";
-      else 
-        ofsGnu << log(prevError[i]/error[i])/M_LN2;
+      }
+      else {
+        double factor = prevSize/size;
+        ofsBody << log(prevError[i]/error[i])/log(factor);
+        ofsGnu << log(prevError[i]/error[i])/log(factor);
+      }
     }
-	  ofs << "\n"
-        << "\\tabularnewline\n"
-	      << "\\hline \n";
+	  ofsBody << "\n"
+            << "\\tabularnewline\n"
+	          << "\\hline \n";
     ofsGnu << "\n";
     prevError = error;
+    prevSize = size;
     level++;
     initial = false;
-    ofs.close();
+    ofsBody.close();
     ofsGnu.close();
   }
 
