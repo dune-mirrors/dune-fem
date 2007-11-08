2463272ab94dd180c6642d993798bce756799faf
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2007-11-08 08:18:45 +0000

    Added header file declaring dune-fem version.
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@2506 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

---
+++ fem/version.hh
@@ -0,0 +1,38 @@
+#ifndef DUNE_FEM_VERSION_HH
+#define DUNE_FEM_VERSION_HH
+
+// These two lines define the dune-fem version.
+#define DUNE_FEM_VERSION_MAJOR 0
+#define DUNE_FEM_VERSION_MINOR 9
+
+#include <sstream>
+
+namespace Dune
+{
+  
+  class DuneFEM
+  {
+  private:
+    typedef DuneFEM ThisType;
+
+  public:
+    enum { MajorVersion = DUNE_FEM_VERSION_MAJOR };
+    enum { MinorVersion = DUNE_FEM_VERSION_MINOR };
+
+  private:
+    DuneFEM ();
+    DuneFEM ( const ThisType &other );
+
+  public:
+    static inline std :: string version ()
+    {
+      std :: ostringstream s;
+      s << "dune-fem " << DUNE_FEM_VERSION_MAJOR << "." << DUNE_FEM_VERSION_MINOR;
+      return s.str();
+    }
+    
+  };
+  
+};
+
+#endif
