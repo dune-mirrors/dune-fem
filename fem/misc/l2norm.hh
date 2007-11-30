3cc1a5c2982ee9dd0f0d200c50499fc434da3d5a
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2007-11-30 07:35:28 +0000

    added a weighted L2Norm
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@2687 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/misc/l2norm.hh
+++ fem/misc/l2norm.hh
@@ -23,7 +23,7 @@ namespace Dune
     typedef CachingQuadrature< GridPartType, 0 > QuadratureType;
 
   protected:
-    GridPartType gridPart_;
+    const GridPartType &gridPart_;
 
   public:
     inline explicit L2Norm ( const GridPartType &gridPart );
@@ -43,7 +43,56 @@ namespace Dune
     distance ( const UDiscreteFunctionType &u,
                const VDiscreteFunctionType &v ) const;
   };
+
+
   
+  template< class WeightFunctionImp >
+  class WeightedL2Norm
+  {
+  public:
+    typedef WeightFunctionImp WeightFunctionType;
+    
+  private:
+    typedef WeightedL2Norm< WeightFunctionType > ThisType;
+
+  public:
+    typedef typename WeightFunctionType :: DiscreteFunctionSpaceType
+      WeightFunctionSpaceType;
+    typedef typename WeightFunctionSpaceType :: GridPartType GridPartType;
+
+  protected:
+    typedef typename WeightFunctionType :: LocalFunctionType
+      LocalWeightFunctionType;
+    typedef typename WeightFunctionType :: RangeType WeightType;
+    typedef typename GridPartType :: template Codim< 0 > :: IteratorType
+      GridIteratorType;
+    typedef typename GridIteratorType :: Entity EntityType;
+    typedef typename EntityType :: Geometry GeometryType;
+    typedef CachingQuadrature< GridPartType, 0 > QuadratureType;
+
+  protected:
+    const WeightFunctionType &weightFunction_;
+    const GridPartType &gridPart_;
+
+  public:
+    inline explicit WeightedL2Norm ( const WeightFunctionType &weightFunction );
+    inline WeightedL2Norm ( const ThisType &other );
+
+  private:
+    // prohibit assignment
+    ThisType operator= ( const ThisType &other );
+
+  public:
+    template< class DiscreteFunctionType >
+    inline typename DiscreteFunctionType :: RangeFieldType
+    norm ( const DiscreteFunctionType &u ) const;
+    
+    template< class UDiscreteFunctionType, class VDiscreteFunctionType >
+    inline typename UDiscreteFunctionType :: RangeFieldType
+    distance ( const UDiscreteFunctionType &u,
+               const VDiscreteFunctionType &v ) const;
+  };
+
 }
 
 #include "l2norm_inline.hh"
--- fem/misc/l2norm_inline.hh
+++ fem/misc/l2norm_inline.hh
@@ -1,5 +1,8 @@
 namespace Dune
 {
+
+  // L2 Norm
+  // -------
   
   template< class GridPartImp >
   inline L2Norm< GridPartImp > :: L2Norm ( const GridPartType &gridPart )
@@ -101,6 +104,133 @@ namespace Dune
 
         uqp -= vqp;
         sum += factor * (uqp * uqp);
+      }
+    }
+
+    sum = gridPart_.grid().comm().sum( sum );
+    return sqrt( sum );
+  }
+  
+  
+  
+  // Weighted L2 Norm
+  // ----------------
+  
+  template< class WeightFunctionImp >
+  inline WeightedL2Norm< WeightFunctionImp >
+    :: WeightedL2Norm ( const WeightFunctionType &weightFunction )
+  : weightFunction_( weightFunction ),
+    gridPart_( weightFunction_.space().gridPart )
+  {
+    CompileTimeChecker< (WeightFunctionSpaceType :: dimRange == 1) >
+      __WEIGHTFUNCTION_MUST_BE_SCALAR__;
+  }
+
+
+
+  template< class WeightFunctionImp >
+  inline WeightedL2Norm< WeightFunctionImp >
+    :: WeightedL2Norm ( const ThisType &other )
+  : weightFunction_( other.weightFunction_ ),
+    gridPart_( other.gridPart_ )
+  {
+  }
+
+
+  
+  template< class WeightFunctionImp >
+  template< class DiscreteFunctionType >
+  inline typename DiscreteFunctionType :: RangeFieldType
+  WeightedL2Norm< WeightFunctionImp >
+    :: norm ( const DiscreteFunctionType &u ) const
+  {
+    typedef typename DiscreteFunctionType :: RangeFieldType RangeFieldType;
+    typedef typename DiscreteFunctionType :: RangeType RangeType;
+
+    typedef typename DiscreteFunctionType :: LocalFunctionType LocalFunctionType;
+
+    RangeFieldType sum( 0 );
+    unsigned int order = 2 * u.space().order() + weightFunction_.space().order();
+
+    const GridIteratorType end = gridPart_.template end< 0 >();
+    for( GridIteratorType it = gridPart_.template begin< 0 >(); it != end; ++it )
+    {
+      const EntityType &entity = *it;
+      const GeometryType &geometry = entity.geometry();
+
+      LocalWeightFunctionType wflocal = weightFunction_.localFunction( entity );
+      LocalFunctionType ulocal = u.localFunction( entity );
+      
+      QuadratureType quadrature( entity, order );
+      const unsigned int numQuadraturePoints = quadrature.nop();
+      for( unsigned int qp = 0; qp < numQuadraturePoints; ++qp )
+      {
+        const RangeFieldType factor
+          = quadrature.weight( qp )
+            * geometry.integrationElement( quadrature.point( qp ) );
+        WeightType weight;
+        wflocal.evaluate( quadrature[ qp ], weight );
+        
+        RangeType uqp;
+        ulocal.evaluate( quadrature[ qp ], uqp );
+
+        sum += factor * weight[ 0 ] * (uqp * uqp);
+      }
+    }
+
+    sum = gridPart_.grid().comm().sum( sum );
+    return sqrt( sum );
+  }
+
+ 
+  
+  template< class WeightFunctionImp >
+  template< class UDiscreteFunctionType, class VDiscreteFunctionType >
+  inline typename UDiscreteFunctionType :: RangeFieldType
+  WeightedL2Norm< WeightFunctionImp >
+    :: distance ( const UDiscreteFunctionType &u,
+                  const VDiscreteFunctionType &v ) const
+  {
+    typedef typename UDiscreteFunctionType :: RangeFieldType RangeFieldType;
+    typedef typename UDiscreteFunctionType :: RangeType RangeType;
+
+    typedef typename UDiscreteFunctionType :: LocalFunctionType ULocalFunctionType;
+    typedef typename VDiscreteFunctionType :: LocalFunctionType VLocalFunctionType;
+
+    RangeFieldType sum( 0 );
+    const unsigned int uorder = 2 * u.space().order();
+    const unsigned int vorder = 2 * v.space().order();
+    const unsigned int order
+      = (uorder >= vorder ? uorder : vorder) + weightFunction_.space().order();
+
+    const GridIteratorType end = gridPart_.template end< 0 >();
+    for( GridIteratorType it = gridPart_.template begin< 0 >(); it != end; ++it )
+    {
+      const EntityType &entity = *it;
+      const GeometryType &geometry = entity.geometry();
+
+      LocalWeightFunctionType wflocal = weightFunction_.localFunction( entity );
+      ULocalFunctionType ulocal = u.localFunction( entity );
+      VLocalFunctionType vlocal = v.localFunction( entity );
+      
+      QuadratureType quadrature( entity, order );
+      const unsigned int numQuadraturePoints = quadrature.nop();
+      for( unsigned int qp = 0; qp < numQuadraturePoints; ++qp )
+      {
+        const RangeFieldType factor
+          = quadrature.weight( qp )
+            * geometry.integrationElement( quadrature.point( qp ) );
+        WeightType weight;
+        wflocal.evaluate( quadrature[ qp ], weight );
+        
+        RangeType uqp;
+        ulocal.evaluate( quadrature[ qp ], uqp );
+        
+        RangeType vqp;
+        vlocal.evaluate( quadrature[ qp ], vqp );
+
+        uqp -= vqp;
+        sum += factor * weight[ 0 ] * (uqp * uqp);
       }
     }
 
