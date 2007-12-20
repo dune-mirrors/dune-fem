d1a2aa3295da875e5c3b8b84849393c3ada44f34
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2007-12-20 18:13:00 +0000

    removed warning.
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@2856 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/storage/subarray.hh
+++ fem/storage/subarray.hh
@@ -166,7 +166,7 @@ namespace Dune
       typedef CheckVectorInterface< BaseVectorType > __CheckBaseVectorType__;
       typedef CheckIndexMapperInterface< IndexMapperType > __CheckIndexMapperType__;
 
-      assert( baseVector_.size() == indexMapper_.range() );
+      assert( baseVector_.size() == (int) indexMapper_.range() );
     }
     
 
