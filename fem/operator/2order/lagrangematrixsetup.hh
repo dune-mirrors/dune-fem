c194bd7ee7be4bc2efc729ff544fb578094bf362
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2008-01-17 14:01:28 +0000

    Make LagrangeMatrixSetup compile
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@2975 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/operator/2order/lagrangematrixsetup.hh
+++ fem/operator/2order/lagrangematrixsetup.hh
@@ -1,104 +1,121 @@
 #ifndef DUNE_LAGRANGEMATRIXSETUP_HH
 #define DUNE_LAGRANGEMATRIXSETUP_HH
 
-#include <dune/fem/space/common/gridpartutility.hh>
+//#include <dune/fem/space/common/gridpartutility.hh>
 
-namespace Dune {
-
-////////////////////////////////////////////////////////////
-//
-//  Setup of matrix structure 
-//
-////////////////////////////////////////////////////////////
-/**  \brief Setup Matrix structure for Lagrange operators by including
- all lagrange nodes of an element.  
-*/
-class LagrangeMatrixSetup
+namespace Dune
 {
-public:
-  //! get number of entries per row for a block matrix, 
-  //! i.e. here number of neighboring nodes + 1 
-  template <class GridPartImp>
-  static inline int stencilSizeEstimate(const GridPartImp& gridPart)        
+
+  /** \class LagrangeMatrixSetup
+   *  \brief Setup Matrix structure for Lagrange operators by including all
+   *         Lagrange nodes of an element.
+   */
+  class LagrangeMatrixSetup
   {
-    return 15;//(GridPartImp :: GridType :: dimension * 2) + 1;
-  }
-
-  //! create entries for element and neighbors 
-  template <class SpaceImp, 
-            class RowMapperType,
-            class ColMapperType,
-            class MatrixStructureMapImp,
-            class DiscreteFunctionType>
-  static inline void setup(const SpaceImp& space, 
-                           const RowMapperType& rowMapper,
-                           const ColMapperType& colMapper,
-                           MatrixStructureMapImp& indices,
-                           const DiscreteFunctionType*)
-  {
-    typedef typename SpaceImp :: GridPartType GridPartImp;
-    GridPartImp& gridP = const_cast<GridPartImp&> (space.gridPart());
-           
-    
-    typedef typename GridPartNewPartitionType<
-      GridPartImp,All_Partition> :: NewGridPartType GridPartType; 
-
-    GridPartType gridPart ( gridP.grid() );
-
-    // define used types 
-    typedef typename GridPartType :: GridType GridType;
-    typedef typename GridType :: template Codim<0> :: Entity EntityType;
-    typedef typename GridPartType :: template Codim<0> :: IteratorType  IteratorType;
-
-    // clear map 
-    indices.clear();
-
-    // only for diagonal 
-    IteratorType endit = gridPart.template end<0>(); 
-    for(IteratorType it = gridPart.template begin<0>(); it != endit; ++it)
+  public:
+    //! get number of entries per row for a block matrix, 
+    //! i.e. here number of neighboring nodes + 1 
+    template< class GridPart >
+    static inline int stencilSizeEstimate ( const GridPart &gridPart )
     {
-      const EntityType & en = *it;
-      // add all column entities to row  
-      fill(gridPart,en,rowMapper,colMapper,indices);
+      return 15;//(GridPartImp :: GridType :: dimension * 2) + 1;
     }
-  }
 
-protected:
-  //! create entries for element and neighbors 
-  template <class GridPartImp,
-            class EntityImp,
-            class RowMapperImp,
-            class ColMapperImp,
-            class OverlapVectorImp>
-  static inline void fill(const GridPartImp& gridPart,
-                   const EntityImp& en,
-                   const RowMapperImp& rowMapper,
-                   const ColMapperImp& colMapper,
-                   std::map< int , std::set<int> >& indices)
-  {
-    // type of local indices storage 
-    typedef std::set< int >  LocalIndicesType; 
-
-    const int numBaseFunctions = rowMapper.numDofs(); 
-
-    for(int i=0; i<numBaseFunctions; ++i)
+    //! create entries for element and neighbors 
+    template< class Space, class RowMapper, class ColMapper,
+              class MatrixStructureMap, class DiscreteFunction >
+    static inline void setup ( const Space &space, 
+                               const RowMapper &rowMapper,
+                               const ColMapper &colMapper,
+                               MatrixStructureMap &indices,
+                               const DiscreteFunction*)
     {
-      const int rowIdx = rowMapper.mapToGlobal(en , i );
-      LocalIndicesType& localIndices = indices[rowIdx];
+      typedef typename Space :: IteratorType IteratorType;
+
+      indices.clear();
+
+      const IteratorType end = space.end();
+      for( IteratorType it = space.begin(); it != end; ++it )
+        fill( space.gridPart(), *it, rowMapper, colMapper, indices );
+
+#if 0
+      typedef typename Space :: GridPartType GridPartImp;
+      GridPartImp &gridP = const_cast< GridPartImp & >( space.gridPart() );
       
-      // insert diagonal for node  
-      localIndices.insert( rowIdx );
+      typedef typename GridPartNewPartitionType< GridPartImp, All_Partition >
+        :: NewGridPartType GridPartType;
 
-      for(int j=0; j<numBaseFunctions; ++j)
+      GridPartType gridPart ( gridP.grid() );
+
+      // define used types 
+      typedef typename GridPartType :: GridType GridType;
+      typedef typename GridPartType :: template Codim< 0 > :: IteratorType
+        IteratorType;
+      typedef typename IteratorType :: Entity EntityType;
+
+      // clear map 
+      indices.clear();
+
+      // only for diagonal 
+      const IteratorType endit = gridPart.template end< 0 >();
+      for( IteratorType it = gridPart.template begin< 0 >(); it != endit; ++it )
       {
-        const int colIdx = colMapper.mapToGlobal(en , j );
-
-        // insert column each node  
-        localIndices.insert( colIdx );
+        const EntityType &entity = *it;
+        // add all column entities to row  
+        fill( gridPart, entity, rowMapper, colMapper, indices );
       }
+#endif
     }
-  }
-};
+
+  protected:
+    //! create entries for element and neighbors 
+    template< class GridPart, class Entity,
+              class RowMapper, class ColMapper >
+    static inline void fill ( const GridPart &gridPart,
+                              const Entity &entity,
+                              const RowMapper &rowMapper,
+                              const ColMapper &colMapper,
+                              std :: map< int, std :: set< int > > &indices )
+    {
+      // type of local indices storage 
+      typedef std :: set< int >  LocalIndicesType;
+
+      typedef typename RowMapper :: DofMapIteratorType RowDofMapIteratorType;
+      typedef typename ColMapper :: DofMapIteratorType ColDofMapIteratorType;
+
+      const RowDofMapIteratorType rowEnd = rowMapper.end( entity );
+      RowDofMapIteratorType rowIt = rowMapper.begin( entity );
+      for( ; rowIt != rowEnd; ++rowIt )
+      {
+        LocalIndicesType &localIndices = indices[ rowIt.global() ];
+
+        const ColDofMapIteratorType colEnd = colMapper.end( entity );
+        ColDofMapIteratorType colIt = colMapper.begin( entity );
+        for( ; colIt != colEnd; ++colIt )
+          localIndices.insert( colIt.global() );
+      }
+#if 0
+      const int numBaseFunctions = rowMapper.numDofs( entity ); 
+
+      for( int i=0; i < numBaseFunctions; ++i )
+      {
+        const int rowIdx = rowMapper.mapToGlobal( entity, i );
+        LocalIndicesType &localIndices = indices[ rowIdx ];
+        
+        // insert diagonal for node  
+        localIndices.insert( rowIdx );
+
+        for( int j = 0; j < numBaseFunctions; ++j )
+        {
+          const int colIdx = colMapper.mapToGlobal( entity, j );
+
+          // insert column each node  
+          localIndices.insert( colIdx );
+        }
+      }
+#endif
+    }
+  };
 
 } // end namespace Dune 
 #endif
