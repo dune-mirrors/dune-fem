7891ad3e52af3155d09f07bdcec41fe214ed46a4
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2008-01-23 10:51:09 +0000

    added an envelope (pointer wrapping the object it points to)
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@3030 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

---
+++ fem/storage/envelope.hh
@@ -0,0 +1,49 @@
+#ifndef DUNE_FEM_ENVELOPE_HH
+#define DUNE_FEM_ENVELOPE_HH
+
+namespace Dune
+{
+
+  template< class Object >
+  class Envelope
+  {
+  public:
+    typedef Object ObjectType;
+
+  protected:
+    ObjectType object_;
+
+  public:
+    template< class ParamType >
+    inline explicit Envelope ( ParamType param )
+    : object_( param )
+    {}
+
+    inline Envelope ( const Envelope &other )
+    : object_( other.object_ )
+    {}
+
+    inline const ObjectType &operator* () const
+    {
+      return object_;
+    }
+
+    inline ObjectType &operator* ()
+    {
+      return object_;
+    }
+
+    inline const ObjectType *operator-> () const
+    {
+      return &object_;
+    }
+
+    inline ObjectType *operator-> ()
+    {
+      return &object_;
+    }
+  };
+
+}
+
+#endif
