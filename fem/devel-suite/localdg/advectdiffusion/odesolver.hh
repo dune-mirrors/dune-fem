48df73c4de1353716cceea389207580fc5775437
Christoph Gersbacher <gersbach@mathematik.uni-freiburg.de>
2008-03-12 16:07:17 +0000

    removed unused (and empty) file
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@3260 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/devel-suite/localdg/advectdiffusion/odesolver.hh
+++
@@ -1,5 +0,0 @@
-#ifndef OLD_RUNGEKUTTA_ODE_SOLVER_HH
-#define OLD_RUNGEKUTTA_ODE_SOLVER_HH
-#warning "use <dune/fem/solver/odesolver.hh> now, instead of this file."
-#include <dune/fem/solver/odesolver.hh>
-#endif
