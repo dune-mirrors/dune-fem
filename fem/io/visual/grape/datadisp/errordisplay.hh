582bc0323d5aa931a5391d83ff977e4a412c3368
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2008-03-13 12:13:31 +0000

    enable DisplayErrorFunction to handle non-timedependent functions
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@3273 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/io/visual/grape/datadisp/errordisplay.hh
+++ fem/io/visual/grape/datadisp/errordisplay.hh
@@ -1,73 +1,232 @@
-template <class ConsType,class SolutionType>
-class DisplayErrorFunction {
-  struct Error {
-    typedef typename ConsType::DiscreteFunctionSpaceType::GridPartType 
-      GridPartType;
-    typedef typename GridPartType :: GridType :: 
-      template Codim<0> :: Entity EntityType;
-    typedef typename EntityType :: Geometry GeometryImp;
-    typedef typename ConsType :: DiscreteFunctionSpaceType :: FunctionSpaceType
-                     ConsFunctionSpaceType;
-    typedef typename ConsFunctionSpaceType :: DomainType ConsDomainType;
-    typedef typename ConsFunctionSpaceType :: RangeType ConsRangeType;
-    typedef FunctionSpace<double,double,
-			  ConsDomainType::dimension,ConsRangeType::dimension>
-			  FunctionSpaceType;
+#ifndef DUNE_FEM_ERRORDISPLAY_HH
+#define DUNE_FEM_ERRORDISPLAY_HH
+
+#include <dune/fem/function/common/discretefunctionadapter.hh>
+
+namespace Dune
+{
+
+  template< class DiscreteFunction, class SolutionType, bool withTime = true >
+  class DisplayErrorFunction;
+
+  
+  template< class DiscreteFunction, class SolutionType >
+  class DisplayErrorFunction< DiscreteFunction, SolutionType, true >
+  {
+    typedef DisplayErrorFunction< DiscreteFunction, SolutionType > ThisType;
+
+  public:
+    typedef DiscreteFunction DiscreteFunctionType;
+
+    typedef typename DiscreteFunctionType :: LocalFunctionType LocalFunctionType;
+
+    typedef typename DiscreteFunctionType :: DiscreteFunctionSpaceType
+      DiscreteFunctionSpaceType;
+
+    typedef typename DiscreteFunctionSpaceType :: GridPartType GridPartType;
+
+  protected:
+    struct Error;
+
+    typedef LocalFunctionAdapter< Error > ErrorFunctionType;
+    
+  protected:
+    const GridPartType &gridPart_;
+    Error error_;
+    ErrorFunctionType errorFunction_;
+    
+  public:
+    template< class GrapeDispType >
+    DisplayErrorFunction ( GrapeDispType &disp,
+                           const DiscreteFunctionType &Uh,
+                           const SolutionType &solution,
+                           const double time = 0 )
+    : gridPart_( Uh.space().gridPart() ),
+      error_( Uh, solution ),
+      errorFunction_( "error", error_, gridPart_ )
+    {
+      disp.addData( errorFunction_, "error", time );
+    }
+
+  private:
+    DisplayErrorFunction ( const ThisType & );
+    ThisType &operator= ( const ThisType & );
+  };
+
+
+  template< class DiscreteFunctionType, class SolutionType >
+  struct DisplayErrorFunction< DiscreteFunctionType, SolutionType, true >
+    :: Error
+  {
+    typedef typename DiscreteFunctionSpaceType :: GridPartType GridPartType;
+    
+    typedef typename DiscreteFunctionSpaceType :: FunctionSpaceType
+      FunctionSpaceType;
     typedef typename FunctionSpaceType :: DomainType DomainType;
     typedef typename FunctionSpaceType :: RangeType RangeType;
     typedef typename FunctionSpaceType :: JacobianRangeType JacobianRangeType;
-    Error(const ConsType& Uh,double time) : 
-      lUh_(Uh), 
-      initU0_(),
-      time_(time),
-      geometry_(0),
-      initialized_(false) {
-    }
-    ~Error() {
-    }
+
+    static const int dimDomain = DiscreteFunctionSpaceType :: dimDomain;
+    static const int dimRange = DiscreteFunctionSpaceType :: dimRange;
+
+  protected:
+    typedef typename GridPartType :: template Codim< 0 > :: IteratorType :: Entity
+      EntityType;
+    typedef typename EntityType :: Geometry GeometryType;
+
+  protected:
+    LocalFunctionType lUh_;
+    const SolutionType &initU0_;
+    const GeometryType *geometry_;
+    const double time_;
+    bool initialized_;
+
+  public:
+    Error ( const DiscreteFunctionType &Uh,
+            const SolutionType &solution,
+            double time = 0 )
+    : lUh_( Uh ), 
+      initU0_( solution ),
+      geometry_( 0 ),
+      time_( time ),
+      initialized_( false )
+    {}
+
     template< class PointType >
-    void evaluate ( const PointType &x, RangeType& ret) const {
+    void evaluate ( const PointType &x, RangeType& ret) const
+    {
       assert(initialized_);
-      ConsRangeType u;
-      lUh_.evaluate(x,ret);
-      ConsRangeType u0;
+      lUh_.evaluate( x, ret );
       DomainType global = geometry_->global( coordinate( x ) );
-      initU0_.evaluate(time_,global,u0);
-      ret -= u0;
+      RangeType phi;
+      initU0_.evaluate( time_, global, phi );
+      ret -= phi;
     }
+
     template< class PointType >
-    void jacobian ( const PointType &x, JacobianRangeType& ret) const {
+    void jacobian ( const PointType &x, JacobianRangeType &ret ) const
+    {
       abort();
     }
-    //! init local function
-    void init(const EntityType& en)
+
+    inline void init ( const EntityType &entity )
     {
-      lUh_.init(en);
-      geometry_ = &(en.geometry());
+      lUh_.init( entity );
+      geometry_ = &( entity.geometry() );
       initialized_ = true;
     }
-  private:
-    typedef typename ConsType::LocalFunctionType ConsLocalFunctionType;
-    ConsLocalFunctionType lUh_;
-    SolutionType initU0_;
-    double time_;
-    const GeometryImp* geometry_;
-    bool initialized_;
   };
-public:
-  // Add the error function to the display 
-  template <class GrapeDispType, 
-          class GR_GridType,
-          class DestinationType>
-  static void apply(GrapeDispType& disp,
-		    const GR_GridType& grid,
-		    const double time,
-		    const DestinationType& Uh)
+
+
+  template< class DiscreteFunction, class SolutionType >
+  class DisplayErrorFunction< DiscreteFunction, SolutionType, false >
   {
-    typedef LocalFunctionAdapter<Error> ErrorFunction;
-    Error* evalError = new Error(Uh,time);
-    ErrorFunction* error = new 
-      ErrorFunction("error",*evalError,Uh.space().gridPart()); 
-    disp.addData(*error,"error",time);  
-  }
-};
+    typedef DisplayErrorFunction< DiscreteFunction, SolutionType > ThisType;
+
+  public:
+    typedef DiscreteFunction DiscreteFunctionType;
+
+    typedef typename DiscreteFunctionType :: LocalFunctionType LocalFunctionType;
+
+    typedef typename DiscreteFunctionType :: DiscreteFunctionSpaceType
+      DiscreteFunctionSpaceType;
+
+    typedef typename DiscreteFunctionSpaceType :: GridPartType GridPartType;
+
+  protected:
+    struct Error;
+
+    typedef LocalFunctionAdapter< Error > ErrorFunctionType;
+    typedef DiscreteFunctionAdapter< SolutionType, GridPartType >
+      GridSolutionType;
+    
+  protected:
+    const GridPartType &gridPart_;
+    GridSolutionType gridSolution_;
+    Error error_;
+    ErrorFunctionType errorFunction_;
+    
+  public:
+    template< class GrapeDispType >
+    DisplayErrorFunction ( GrapeDispType &disp,
+                           const DiscreteFunctionType &Uh,
+                           const SolutionType &solution )
+    : gridPart_( Uh.space().gridPart() ),
+      gridSolution_( "exact solution", solution, gridPart_ ),
+      error_( Uh, solution ),
+      errorFunction_( "error", error_, gridPart_ )
+    {
+      disp.addData( errorFunction_ );
+      disp.addData( gridSolution_ );
+    }
+
+  private:
+    DisplayErrorFunction ( const ThisType & );
+    ThisType &operator= ( const ThisType & );
+  };
+
+
+  template< class DiscreteFunctionType, class SolutionType >
+  struct DisplayErrorFunction< DiscreteFunctionType, SolutionType, false >
+    :: Error
+  {
+    typedef typename DiscreteFunctionSpaceType :: GridPartType GridPartType;
+    
+    typedef typename DiscreteFunctionSpaceType :: FunctionSpaceType
+      FunctionSpaceType;
+    typedef typename FunctionSpaceType :: DomainType DomainType;
+    typedef typename FunctionSpaceType :: RangeType RangeType;
+    typedef typename FunctionSpaceType :: JacobianRangeType JacobianRangeType;
+
+    static const int dimDomain = DiscreteFunctionSpaceType :: dimDomain;
+    static const int dimRange = DiscreteFunctionSpaceType :: dimRange;
+
+  protected:
+    typedef typename GridPartType :: template Codim< 0 > :: IteratorType :: Entity
+      EntityType;
+    typedef typename EntityType :: Geometry GeometryType;
+
+  protected:
+    LocalFunctionType lUh_;
+    const SolutionType &initU0_;
+    const GeometryType *geometry_;
+    bool initialized_;
+
+  public:
+    Error ( const DiscreteFunctionType &Uh,
+            const SolutionType &solution )
+    : lUh_( Uh ),
+      initU0_( solution ),
+      geometry_( 0 ),
+      initialized_( false )
+    {}
+
+    template< class PointType >
+    void evaluate ( const PointType &x, RangeType& ret) const
+    {
+      assert(initialized_);
+      lUh_.evaluate( x, ret );
+      DomainType global = geometry_->global( coordinate( x ) );
+      RangeType phi;
+      initU0_.evaluate( global, phi );
+      ret -= phi;
+    }
+
+    template< class PointType >
+    void jacobian ( const PointType &x, JacobianRangeType &ret ) const
+    {
+      abort();
+    }
+
+    inline void init ( const EntityType &entity )
+    {
+      lUh_.init( entity );
+      geometry_ = &( entity.geometry() );
+      initialized_ = true;
+    }
+  };
+
+}
+
+#endif // DUNE_FEM_ERRORDISPLAY_HH
+
