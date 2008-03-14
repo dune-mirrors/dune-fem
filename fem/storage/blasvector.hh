9f9056ee8736aaa6f308fd8f0477408bfd9ccc27
Andreas Dedner <A.S.Dedner@warwick.ac.uk>
2008-03-14 21:32:58 +0000

    a double* Wrapper with blas routin and resize - taken from pardg
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@3300 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

---
+++ fem/storage/blasvector.hh
@@ -0,0 +1,194 @@
+#ifndef FEM_BLASVECTOR_HH
+#define FEM_BLASVECTOR_HH
+
+#include <iostream>
+#include <cassert>
+#include <cmath>
+#include <dune/fem/solver/pardg.hh>
+#include <dune/fem/storage/vector.hh>
+namespace Dune {
+
+class BlasVector : 
+public VectorDefault<double,BlasVector> {
+  bool owner_;
+public:
+  typedef double FieldType;
+  BlasVector(int n);
+  BlasVector(int n, const double *a);
+  BlasVector(const BlasVector &v); // copy constructor
+  ~BlasVector();
+  
+  // element access
+  double& operator()(int i);
+  double operator()(int i) const;
+  double& operator[](int i);
+  double operator[](int i) const;
+
+  // conversion operators
+  operator double*();
+  operator const double *() const;
+
+  // assignment operators
+  BlasVector& operator=(const BlasVector& v);
+  BlasVector& operator+=(const BlasVector& v);
+  BlasVector& operator-=(const BlasVector& v);
+  BlasVector& operator*=(double alpha);
+  BlasVector& operator=(double alpha);
+
+  unsigned int size() const;
+
+  double* leakPointer() {
+    return data;
+  }
+  const double* leakPointer() const {
+    return data;
+  }
+  void reserve(unsigned int newSize) {
+    assert(owner_);
+    if (newSize<n) return;
+    unsigned int useSize = newSize;
+    double* newData = new double[useSize];
+    if (!data) data = newData;
+    else {
+      double* oldData;
+      pardg::cblas_dcopy(n,data,1,newData,1);
+      data = newData;
+      delete [] oldData;
+    }
+    totalSize = useSize;
+
+  }
+  void resize(unsigned int newSize) {
+    assert(owner_);
+    if (newSize>totalSize) 
+      reserve(newSize);
+    else 
+      n = newSize;
+  }
+private:
+  unsigned int n,totalSize;
+  double *data;
+};
+
+
+// class BlasVector inline implementation
+
+inline 
+BlasVector::BlasVector(int pn) : owner_(true),
+n(pn), totalSize(pn), data(new double[pn])
+{
+  assert(data);
+  pardg::dset(n, 0.0, data, 1);
+}
+
+
+inline 
+BlasVector::BlasVector(int pn, const double *a) : owner_(false),
+n(pn), totalSize(pn), data(const_cast<double*>(a))
+{
+  assert(data);
+  // pardg::cblas_dcopy(n, a, 1, data, 1);
+}
+
+
+inline 
+BlasVector::BlasVector(const BlasVector &v) : owner_(true),
+n(v.n), totalSize(v.n), data(new double[v.n])
+{
+  assert(data);
+  pardg::cblas_dcopy(n, v.data, 1, data, 1);
+}
+
+
+inline 
+BlasVector::~BlasVector()
+{
+  if (owner_)
+    delete[] data;
+}
+
+
+inline
+double& BlasVector::operator()(int i)
+{
+  assert(i>=0 && i<n);
+  return data[i];
+}
+
+
+inline
+double BlasVector::operator()(int i) const
+{
+  assert(i>=0 && i<n);
+  return data[i];
+}
+
+
+inline
+double& BlasVector::operator[](int i)
+{
+  assert(i>=0 && i<n);
+  return data[i];
+}
+
+
+inline
+double BlasVector::operator[](int i) const
+{
+  assert(i>=0 && i<n);
+  return data[i];
+}
+
+
+inline
+BlasVector& BlasVector::operator=(const BlasVector& v)
+{
+  assert(n==v.n);
+  pardg::cblas_dcopy(n, v.data, 1, data, 1);
+  return *this;
+}
+
+
+inline
+BlasVector& BlasVector::operator+=(const BlasVector& v)
+{
+  assert(n==v.n);
+  pardg::cblas_daxpy(n, 1.0, v.data, 1, data, 1);
+  return *this;
+}
+
+
+inline
+BlasVector& BlasVector::operator-=(const BlasVector& v)
+{
+  assert(n==v.n);
+  pardg::cblas_daxpy(n, -1.0, v.data, 1, data, 1);
+  return *this;
+}
+
+
+inline
+BlasVector& BlasVector::operator*=(double alpha)
+{
+  pardg::cblas_dscal(n, alpha, data, 1);
+  return *this;
+}
+
+
+inline
+BlasVector& BlasVector::operator=(double alpha)
+{
+  pardg::dset(n, alpha, data, 1);
+  return *this;
+}
+
+
+inline
+unsigned int BlasVector::size() const
+{
+  return n;
+}
+
+}
+
+#endif
