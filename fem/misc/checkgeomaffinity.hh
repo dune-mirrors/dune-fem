ad93becb1ed209f8e13b0aa22996f031d6e1c251
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2008-04-09 16:22:59 +0000

    added cartesian check for grids.
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@3402 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/misc/checkgeomaffinity.hh
+++ fem/misc/checkgeomaffinity.hh
@@ -1,6 +1,9 @@
 #ifndef DUNE_CHECKGEOMETRYAFFINITY_HH
 #define DUNE_CHECKGEOMETRYAFFINITY_HH
 
+#include <dune/common/fvector.hh>
+
+
 namespace Dune {
 
 /*! @addtogroup HelperClasses
@@ -35,6 +38,112 @@ struct GeometryAffinityCheck
       }
     }
     return affinity;
+  }
+};
+
+//! Helper class to check whether grid is structured or not 
+struct CheckCartesian 
+{
+protected:  
+  //! check whether this is a cartesian grid or not 
+  template <class GridType, class IndexSetType>
+  static inline bool doCheck(const GridType& grid, const IndexSetType& indexSet)
+  {
+    typedef typename GridType :: template Codim<0> :: LevelIterator MacroIteratorType; 
+    typedef typename GridType :: template Codim<0> :: EntityPointer  EntityPointerType; 
+    typedef typename GridType :: template Codim<0> :: Entity  EntityType; 
+    typedef typename GridType :: template Codim<0> :: Geometry Geometry; 
+    
+    const MacroIteratorType endit = grid.template lend<0> (0);
+    MacroIteratorType it = grid.template lbegin<0> (0);
+
+    // empty grids are considerd as cartesian 
+    if( it == endit ) return true;
+
+    typedef AllGeomTypes< IndexSetType, GridType> GeometryInformationType;
+    GeometryInformationType geoInfo( indexSet );
+
+    // if we have more than one geometry Type return false 
+    if( geoInfo.geomTypes(0).size() > 1 ) return false;
+
+    // if this type is not cube return false 
+    if( ! geoInfo.geomTypes(0)[0].isCube() ) return false; 
+
+    typedef typename GridType :: ctype ctype;
+    enum { dimension = GridType :: dimension };
+    enum { dimworld  = GridType :: dimensionworld };
+    
+    // grid width 
+    FieldVector<ctype, dimension> h(0);
+    FieldVector<ctype, dimworld> enBary;
+    FieldVector<ctype, dimension-1> mid(0.5);
+    
+    const int map[3] = {1, 2, 4};
+    {
+      const Geometry& geo = it->geometry();
+      if ( ! geo.type().isCube() ) return false;
+
+      // calculate grid with 
+      for(int i=0; i<dimension; ++i) 
+      {
+        h[i] = (geo[0] - geo[map[i]]).two_norm();
+      }
+    }
+
+    // loop over all macro elements 
+    for(MacroIteratorType it = grid.template lbegin<0> (0); 
+        it != endit; ++it)
+    {
+      const EntityType& en = *it;
+      const Geometry& geo = en.geometry();
+      
+      const FieldVector<ctype, dimworld> enBary = 
+        geo.global( geoInfo.localCenter( geo.type() ));
+
+      typedef typename GridType :: Traits :: LevelIntersectionIterator IntersectionIteratorType;
+
+      // if geometry is not cube, it's not a cartesian grid 
+      if ( ! geo.type().isCube() ) return false;
+
+      for(int i=0; i<dimension; ++i) 
+      {
+        const ctype w = (geo[0] - geo[map[i]]).two_norm();
+        if( std::abs( h[i] - w ) > 1e-15 ) return false; 
+      }
+
+      IntersectionIteratorType endnit = en.ilevelend();
+      for(IntersectionIteratorType nit = en.ilevelbegin();
+          nit != endnit; ++nit)
+      {
+        if( nit.neighbor() )
+        {
+          EntityPointerType ep = nit.outside();
+          const EntityType& nb = *ep;
+          const Geometry& nbGeo = nb.geometry();
+          
+          FieldVector<ctype, dimworld> diff = nbGeo.global( geoInfo.localCenter( nbGeo.type() ));
+          diff -= enBary;
+          assert( diff.two_norm() > 1e-15 );
+          diff /= diff.two_norm();
+
+          // scalar product should be 1 or -1 
+          if( std::abs(std::abs((diff * nit.unitOuterNormal(mid))) - 1.0) > 1e-12 ) return false;
+        }
+      }
+    }
+    return true;
+  }
+  
+public:  
+  //! check whether all the is grid is a cartesian grid 
+  template <class GridType, class IndexSetType>
+  static inline bool check(const GridType& grid, const IndexSetType& indexSet)
+  {
+    bool cartesian = doCheck( grid , indexSet );
+    int val = (cartesian) ? 1 : 0;
+    // take global minimum  
+    val = grid.comm().min( val );
+    return (val == 1) ? true : false;
   }
 };
 
