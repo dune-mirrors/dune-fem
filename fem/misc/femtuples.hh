c6b68b804685fd87fef518175ff2bc1e7ad31d6e
Nina Shokina <shokina@mathematik.uni-freiburg.de>
2008-05-05 16:45:38 +0000

    operator>> not implemented in dune-common tuples -> implement it femtuples.hh anyway
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@3620 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/misc/femtuples.hh
+++ fem/misc/femtuples.hh
@@ -612,22 +612,6 @@ namespace Dune{
   }
 
   /**
-   * @brief Read a pair or  Tuple.
-   */
-  template<typename T1, typename T2>
-  inline std::istream& operator>>(std::istream& os, Pair<T1,T2>& pair)
-  {
-    os>>pair.first()>>pair.second();
-    return os;
-  }
-
-  template<typename T1>
-  inline std::istream& operator>>(std::istream& os, Pair<T1,Nil>& pair)
-  {
-    os>>pair.first();
-    return os;
-  }
-  /**
    * @brief Print aa pair or  Tuple.
    */
   template<typename T1, typename T2>
@@ -772,7 +756,26 @@ namespace Dune{
 #endif // end if HAVE_TUPLE || HAVE_TR1_TUPLE
 #endif // end ifdef Tuple 
 
-namespace Dune { 
+namespace Dune
+{
+
+  /**
+   * @brief Read a pair or  Tuple.
+   */
+  template<typename T1, typename T2>
+  inline std::istream& operator>>(std::istream& os, Pair<T1,T2>& pair)
+  {
+    os>>pair.first()>>pair.second();
+    return os;
+  }
+
+  template<typename T1>
+  inline std::istream& operator>>(std::istream& os, Pair<T1,Nil>& pair)
+  {
+    os>>pair.first();
+    return os;
+  }
+
   ///////////////////////////////////////
   //  
   //  TupleLength
