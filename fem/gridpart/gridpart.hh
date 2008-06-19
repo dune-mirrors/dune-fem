17848888bcec13540dc489fffd5d8ece278dc961
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2008-06-19 11:37:00 +0000

    added unified way to include gridparts
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@3772 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

---
+++ fem/gridpart/gridpart.hh
@@ -0,0 +1,11 @@
+#ifndef DUNE_FEM_GRIDPART_HH
+#define DUNE_FEM_GRIDPART_HH
+
+#ifdef DUNE_GRIDPART_HH
+#warning "Including <dune/grid/common/gridpart.hh> directly is deprecated."
+#warning "Include <dune/fem/gridpart/gridpart.hh> instead."
+#endif
+
+#include <dune/grid/common/gridpart.hh>
+
+#endif
