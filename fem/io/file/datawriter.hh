61a8ad59ef247131de66aec831aef4ed8e6e016d
Nina Shokina <shokina@mathematik.uni-freiburg.de>
2008-06-26 09:48:03 +0000

    updated include path
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@3788 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/io/file/datawriter.hh
+++ fem/io/file/datawriter.hh
@@ -8,7 +8,7 @@
 #include <dune/fem/io/file/iotuple.hh>
 #include <dune/fem/io/parameter.hh>
 #include <dune/fem/space/common/loadbalancer.hh>
-#include <dune/fem/space/common/adaptiveleafgridpart.hh>
+#include <dune/fem/gridpart/adaptiveleafgridpart.hh>
 #include <dune/fem/space/lagrangespace.hh>
 #include <dune/fem/function/adaptivefunction.hh>
 #include <dune/fem/quadrature/cachequad.hh>
