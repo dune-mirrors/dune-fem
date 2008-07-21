dbebffcb0828fd8ad71b372f667bd5ac82848211
Christoph Gersbacher <gersbach@mathematik.uni-freiburg.de>
2008-07-21 16:37:03 +0000

    added SemiImplicitOdeSolver
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@3819 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/solver/odesolver.hh
+++ fem/solver/odesolver.hh
@@ -627,6 +627,203 @@ private:
 #endif
 
 
+//////////////////////////////////////////////////////////////////
+//
+//  --SemiImplTimeStepperBase
+//
+//////////////////////////////////////////////////////////////////
+template<class OperatorExpl, class OperatorImpl>
+class SemiImplTimeStepperBase
+{
+  typedef typename OperatorExpl :: DestinationType DestinationType; 
+public:
+  SemiImplTimeStepperBase(OperatorExpl& explOp, OperatorImpl & implOp, Dune :: TimeProviderBase &tp, 
+                      int pord, bool verbose) :
+    ord_(pord),
+    comm_(pardg::Communicator::instance()),
+    explOp_(explOp),
+    implOp_(implOp),
+    expl_(explOp),
+    impl_(implOp),
+    ode_(0),
+    linsolver_(comm_,cycle),
+    initialized_(false)
+  {
+    //linsolver_.set_tolerance(1.0e-8,false);
+    linsolver_.set_tolerance(1.0e-6,false);
+    //linsolver_.set_max_number_of_iterations(10000);
+    linsolver_.set_max_number_of_iterations(30);
+
+    switch (pord) {
+      case 1: ode_=new pardg::SemiImplicitEuler(comm_,impl_,expl_); break;
+      case 2: ode_=new pardg::IMEX_SSP222(comm_,impl_,expl_); break;
+      case 3: ode_=new pardg::SIRK33(comm_,impl_,expl_); break;
+      default : std::cerr << "Runge-Kutta method of this order not implemented" 
+                          << std::endl;
+                abort();
+    }
+    ode_->set_linear_solver(linsolver_);
+    //ode_->set_tolerance(1.0e-6);
+    ode_->set_tolerance(1.0e-8);
+    ode_->set_max_number_of_iterations(15);
+    
+    if( verbose ) 
+    {
+      ode_->IterativeSolver::set_output(cout);
+      ode_->DynamicalObject::set_output(cout);
+    }
+  }
+  
+  // initialize time step size 
+  bool initialize (const DestinationType& U0) 
+  {
+    // initialized dt on first call
+    if ( ! initialized_ )     
+    {
+      DestinationType tmp(U0);
+      this->explOp_(U0,tmp);
+      initialized_ = true;
+      return true;
+    }
+    return false;
+  }
+  //! destructor 
+  ~SemiImplTimeStepperBase() {delete ode_;}
+  
+  // return reference to ode solver 
+  pardg::SIRK& odeSolver() 
+  {
+    assert( ode_ );
+    return *ode_;
+  }
+  void printmyInfo(string filename) const {
+    std::ostringstream filestream;
+    filestream << filename;
+    std::ofstream ofs(filestream.str().c_str(), std::ios::app);
+    ofs << "Semi Implicit ODE solver, steps: " << this->ord_ << "\n\n";
+    ofs.close();
+    // this->op_.printmyInfo(filename);
+  }
+  
+protected:
+  int ord_;
+  pardg::Communicator & comm_;   
+  const OperatorExpl& explOp_;
+  const OperatorImpl& implOp_;
+  OperatorWrapper<OperatorExpl> expl_;
+  OperatorWrapper<OperatorImpl> impl_;
+  pardg::SIRK* ode_;
+  //pardg::CG linsolver_;
+  pardg::GMRES linsolver_;
+  //enum { cycle = 20 };
+  enum { cycle = 15 };
+  bool initialized_;
+};
+
+
+///////////////////////////////////////////////////////
+//
+//  --SemiImplicitOdeSolver 
+//
+///////////////////////////////////////////////////////
+template<class DestinationImp>
+class SemiImplicitOdeSolver : 
+  public OdeSolverInterface<DestinationImp> ,
+  public SemiImplTimeStepperBase<SpaceOperatorInterface<DestinationImp>, SpaceOperatorInterface<DestinationImp> > 
+{
+  typedef DestinationImp DestinationType;
+  typedef SpaceOperatorInterface<DestinationImp> OperatorType;
+  typedef SemiImplTimeStepperBase<OperatorType, OperatorType> BaseType;
+
+private:
+  Dune :: TimeProviderBase &timeProvider_;
+  double cfl_;
+
+public:
+  SemiImplicitOdeSolver(OperatorType& explOp, OperatorType& implOp, Dune::TimeProviderBase& tp,
+                    int pord, bool verbose = false) :
+    BaseType(explOp,implOp,tp,pord,verbose),
+    timeProvider_(tp),
+    cfl_(1.0)
+  {
+  }
+
+  virtual ~SemiImplicitOdeSolver() {}
+  
+  //! initialize solver 
+  void initialize(const DestinationType& U0)
+  {
+    // initialize solver 
+    BaseType :: initialize (U0);
+
+    // set time step estimate of operator 
+    timeProvider_.provideTimeStepEstimate( cfl_ * this->explOp_.timeStepEstimate() );
+  }
+
+  //! solve 
+  void solve(DestinationType& U0) 
+  {
+    // initialize 
+    if( ! this->initialized_ ) 
+    {
+      DUNE_THROW(InvalidStateException,"ImplicitOdeSolver wasn't initialized before first call!");
+    }
+
+    
+    const int min_it = 14;
+    const int max_it = 16;
+    const double sigma = 1.1;
+    
+    const double dt   = timeProvider_.deltaT();
+    assert( dt > 0.0 );
+    const double time = timeProvider_.time();
+
+     // get pointer to solution
+    double* u = U0.leakPointer();
+      
+    const bool convergence = this->odeSolver().step(time , dt , u);
+    const int iter = this->linsolver_.number_of_iterations();
+    // set time step estimate of operator 
+
+    if (convergence) {
+    // control the number of iterations of the linear solver
+    // the values for min_it and max_it has to be determined by experience
+      if (iter < min_it) {
+         cfl_ *= sigma;
+         cfl_ = std::min(1.,cfl_); 
+        // output only on rank 0
+        if(U0.space().grid().comm().rank() == 0 )
+        {
+          derr << " New cfl number is: "<< cfl_ << "\n";
+        }
+      }
+      else if (iter > max_it) {
+        cfl_ *= (double)max_it/(sigma*(double)iter);
+        // output only on rank 0
+         if(U0.space().grid().comm().rank() == 0 )
+         {
+           derr << " New cfl number is: "<< cfl_ << "\n";
+         }
+      }
+      timeProvider_.provideTimeStepEstimate( cfl_ * this->explOp_.timeStepEstimate() );
+    
+     this->linsolver_.reset_number_of_iterations();
+     std::cout << "number of iterations of linear solver  " << iter << std::endl;
+   }
+   else {
+     cfl_ *= 0.5;
+     timeProvider_.provideTimeStepEstimate( cfl_ * dt );
+     timeProvider_.invalidateTimeStep();
+     // output only on rank 0
+     if(U0.space().grid().comm().rank() == 0 )
+     {
+       derr << "No convergence: New cfl number is "<< cfl_ << std :: endl;
+     }
+   }
+ }
+
+};
+
 #if 0
 //////////////////////////////////////////////////////////////
 //
