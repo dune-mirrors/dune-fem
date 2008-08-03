f470b9a860d5d49276ccbcb8a53843554ce2c37a
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2008-08-03 06:05:44 +0000

    remove deprececated header
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@3853 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/space/leafindexset.hh
+++
@@ -1,5 +0,0 @@
-#ifndef DUNE_LEAFINDEXSET_HH
-#define DUNE_LEAFINDEXSET_HH
-#warning "Use <dune/fem/space/common/adaptiveleafgridpart.hh> instead! "
-#include "common/adaptiveleafgridpart.hh"
-#endif
