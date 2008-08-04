f70a88c4bb7ca9f6a9f87339939161d42a02cf9e
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2008-08-04 10:04:59 +0000

    wrap GridParts as GridViews
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@3857 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

---
+++ fem/gridpart/gridpartview.hh
@@ -0,0 +1,137 @@
+#ifndef DUNE_FEM_GRIDVIEW_HH
+#define DUNE_FEM_GRIDVIEW_HH
+
+namespace Dune
+{
+
+#if DUNE_GRID_VERSION_NEWER(1,2)
+  template< class GridPart >
+  class GridPartView;
+
+
+  template< class GridPart >
+  struct GridPartViewTraits
+  {
+    typedef GridPartGridView< GridPart > GridViewImp;
+    
+    typedef typename GridPart :: GridType Grid;
+    typedef typename GridPart :: IndexSetType IndexSet;
+    typedef typename GridPart :: IntersectionIteratorType IntersectionIterator;
+
+    typedef typename IntersectionIterator :: Intersection Intersection;
+
+    typedef typename Grid :: Traits :: CollectiveCommunication CollectiveCommunication;
+
+    template< int codim >
+    struct Codim
+    : public Grid :: Traits :: template Codim< codim >
+    {
+      typedef typename GridPart :: template Codim< codim > :: IteratorType Iterator;
+    };
+
+    enum { conforming = GridPart :: conforming };
+  };
+
+
+  template< class GridPart >
+  class GridPartView
+  : public GridView< GridPartViewTraits< GridPart > >
+  {
+    typedef GridPartView< GridPart > ThisType;
+    typedef GridView< GridPartViewTraits< GridPart > > BaseType;
+
+  public:
+    typedef GridPart GridPartType;
+    
+    typedef GridPartViewTraits< GridPartType > Traits;
+
+    /** \brief type of the grid */
+    typedef typename Traits :: Grid Grid;
+
+    /** \brief type of the index set */
+    typedef typename Traits :: IndexSet IndexSet;
+
+    /** \brief type of the intersection */
+    typedef typename Traits :: Intersection Intersection;
+
+    /** \brief type of the intersection iterator */
+    typedef typename Traits :: IntersectionIterator IntersectionIterator;
+
+    /** \brief type of the collective communication */
+    typedef typename Traits :: CollectiveCommunication CollectiveCommunication;
+
+    /** \brief Codim Structure */
+    template< int cd >
+    struct Codim : public Traits :: template Codim<cd> {};
+ 
+    enum { conforming = Traits :: conforming };
+
+    enum { dimension = Grid :: dimension };
+    enum { dimensionworld = Grid :: dimensionworld };
+
+  private:
+    const GridPartType &gridPart_;
+    
+  public:
+    explicit GridPartView ( const GridPartType &gridPart )
+    : gridPart_( gridPart )
+    {}
+
+    GridPartView ( const ThisType &other )
+    : gridPart_( other.gridPart_ )
+    {}
+
+  private:
+    ThisType &operator= ( const ThisType & );
+
+  public:
+    const Grid &grid () const
+    {
+      return gridPart_.grid();
+    }
+    
+    const IndexSet &indexSet () const
+    {
+      return gridPart_.indexSet();
+    }
+
+    template< int codim >
+    typename Codim< codim > :: Iterator begin () const
+    {
+      return gridPart_.template begin< codim >();
+    }
+    
+    template< int codim >
+    typename Codim< codim > :: Iterator end () const
+    {
+      return gridPart_.template end< codim >();
+    }
+
+    IntersectionIterator ibegin ( const typename Codim< 0 > :: Entity &entity ) const
+    {
+      return gridPart_.ibegin( entity );
+    }
+    
+    IntersectionIterator iend ( const typename Codim< 0 > :: Entity &entity ) const
+    {
+      return gridPart_.iend( entity );
+    }
+
+    const CollectiveCommunication &comm () const
+    {
+      return grid().comm();
+    }
+
+    template< class DataHandleImp, class DataType >
+    void communicate ( CommDataHandleIF< DataHandleImp, DataType > &data,
+                       InterfaceType iftype,
+                       CommunicationDirection dir ) const
+    {
+      gridPart_.communicate( data, iftype, dir );
+    }
+  };
+#endif
+  
+}
+
+#endif
