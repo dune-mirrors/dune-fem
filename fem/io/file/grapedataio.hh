e3949187550bb1a6de4eefa869e65a7010baf9f1
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2008-08-09 13:23:36 +0000

    enable PGM support only in compatibility mode (removes a deprecated warning otherwise)
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@3895 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/io/file/grapedataio.hh
+++ fem/io/file/grapedataio.hh
@@ -449,9 +449,13 @@ const GrapeIOFileFormatType ftype, const GrapeIOStringType filename, int timeste
     return df.write_xdr(fn);
   if(ftype == ascii)
     return df.write_ascii(fn);
+#if DUNE_FEM_COMPATIBILITY
   if(ftype == pgm)
     return df.write_pgm(fn);
+#endif
 
+  DUNE_THROW( NotImplemented, "GrapeIOFileFormatType " << ftype
+                               << " currently not supported." );
   return false;
 }
 
@@ -530,12 +534,13 @@ readData(DiscreteFunctionType & df, const GrapeIOStringType filename, int timest
     return df.read_xdr(fn);
   if(ftype == ascii)
     return df.read_ascii(fn);
+#if DUNE_FEM_COMPATIBILITY
   if(ftype == pgm)
     return df.read_pgm(fn);
+#endif
 
-  std::cerr << ftype << " GrapeIOFileFormatType not supported at the moment! in file " << __FILE__ << " line " << __LINE__ << "\n"; 
-  abort();
-
+  DUNE_THROW( NotImplemented, "GrapeIOFileFormatType " << ftype
+                               << " currently not supported." );
   return false;
 }
 
