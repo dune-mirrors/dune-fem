9f78d9f4c0e72f10b5352c4f50668f6e114bf423
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2008-10-15 07:48:28 +0000

    make compile again
    Note: Please verify your code before comittig. This bug was obvious.
    
    
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@4042 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/operator/1order/dgmasspass.hh
+++ fem/operator/1order/dgmasspass.hh
@@ -26,10 +26,12 @@ namespace Dune
   /** \brief Implementation of operator to calculate gradient of 
       a given discrete function using the pass concept.
   */
-  template <class DiscreteModelImp, class PreviousPassImp>
-  class LocalDGMassPass :
-    public LocalDGPass<DiscreteModelImp, PreviousPassImp> 
+  template< class DiscreteModelImp, class PreviousPassImp >
+  class LocalDGMassPass
+  : public LocalDGPass< DiscreteModelImp, PreviousPassImp >
   {
+    typedef LocalDGMassPass< DiscreteModelImp, PreviousPassImp > ThisType;
+
   public:
     //- Typedefs and enums
     //! Base class
@@ -69,9 +71,9 @@ namespace Dune
     // Various other types
     typedef typename DestinationType::LocalFunctionType LocalFunctionType;
     typedef typename DiscreteModelType::SelectorType SelectorType;
-    typedef CombinedSelectorType< ThisType , SelectorType > CombinedSelectorType;
-    typedef DiscreteModelCaller
-      < DiscreteModelType, ArgumentType, Combined SelectorType > DiscreteModelCallerType;
+    typedef CombinedSelector< ThisType, SelectorType > CombinedSelectorType;
+    typedef DiscreteModelCaller< DiscreteModelType, ArgumentType, CombinedSelectorType >
+      DiscreteModelCallerType;
 
     // type of Communication Manager 
     typedef CommunicationManager<DiscreteFunctionSpaceType> CommunicationManagerType;
