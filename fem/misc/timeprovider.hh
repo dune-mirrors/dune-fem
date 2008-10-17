d842175c2234e8b23b4ede50718fc06ed7ba4eda
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2008-10-17 14:13:24 +0000

    * remove deprecated headers * added forloop.hh (nice helper for template loops)
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@4054 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/misc/timeprovider.hh
+++
@@ -1,2 +0,0 @@
-#warning "'timeprovider.hh' has been moved. Please include 'dune/fem/solver/timeprovider.hh' instead."
-#include <dune/fem/solver/timeprovider.hh>
--- fem/misc/timeutility.hh
+++
@@ -1,2 +0,0 @@
-#warning "timeutility.hh is deprecated, use <dune/fem/misc/timeprovider.hh> instead!"
-#include "timeprovider.hh"
