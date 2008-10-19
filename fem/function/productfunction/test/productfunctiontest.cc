6b31622c0d5e8b62f5a77148844319bceb3d1a31
Andreas Dedner <A.S.Dedner@warwick.ac.uk>
2008-10-19 08:39:07 +0000

    gridparts moved
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@4063 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/function/productfunction/test/productfunctiontest.cc
+++ fem/function/productfunction/test/productfunctiontest.cc
@@ -21,8 +21,7 @@ static const int dim2 = 3;
 #include <dune/fem/space/dgspace.hh>
 #include <dune/fem/space/lagrangespace.hh>
 #include <dune/fem/quadrature/cachequad.hh>
-#include <dune/fem/space/common/adaptiveleafgridpart.hh> 
-#include <dune/fem/gridpart/gridpart.hh> 
+#include <dune/fem/gridpart/adaptiveleafgridpart.hh> 
 
 #include <dune/grid/common/referenceelements.hh>
  
