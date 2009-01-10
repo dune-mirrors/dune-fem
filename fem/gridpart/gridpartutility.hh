4d560df9af927108b67990d825c322ca5a7f5129
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2009-01-10 11:34:03 +0000

    * only warn about old gridpartutility.hh (otherwise nightly build fails) * remove the code in gridpartutility.hh
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@4304 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/gridpart/gridpartutility.hh
+++ fem/gridpart/gridpartutility.hh
@@ -1,49 +1,7 @@
 #ifndef DUNE_GRIDPARTUTILITY_HH
 #define DUNE_GRIDPARTUTILITY_HH
 
-//- System includes
-#error "Adapt code to new gridparts" 
+#warning "dune/gem/gridpart/gridpartutility.hh is not required anymore."
+#warning "GridParts now export all iterators. Please adapt your code accordingly-" 
 
-//- Dune includes
-#include <dune/fem/gridpart/gridpart.hh>
-
-/** @file
-  @brief Provides other partition types of the same grid part 
-*/
-
-namespace Dune {
-  /** 
-   * @addtogroup GridPart
-   *
-   * @{ 
-   */
-
-  //! @ingroup GridPart
-  //! \brief Helper class to get GridPart with different partition type
-  template <class GridPartImp, PartitionIteratorType piType> 
-  struct GridPartNewPartitionType;
-
-  //! \brief Helper class to get GridPart with different partition type
-  template <class GridImp, PartitionIteratorType old_pitype, 
-            template <class, PartitionIteratorType> class GridPartImp, 
-            PartitionIteratorType pitype> 
-  struct GridPartNewPartitionType< GridPartImp<GridImp, old_pitype> , pitype >
-  {
-    //! type of grid part with new partition type 
-    typedef GridPartImp<GridImp,pitype> NewGridPartType;
-  };
-
-  /** \brief Helper class to get GridPart with different partition type
-    here specialization for filtered grid part   */
-  template <class GridImp, class FilterImp, PartitionIteratorType old_pitype, 
-            template <class, class, PartitionIteratorType> class GridPartImp, 
-            PartitionIteratorType pitype> 
-  struct GridPartNewPartitionType<GridPartImp<GridImp, FilterImp,old_pitype> , pitype>
-  {
-    //! type of grid part with new partition type 
-    typedef GridPartImp<GridImp,FilterImp,pitype> NewGridPartType;
-  };
-  /** @} */
-
-} // end namespace Dune
 #endif
