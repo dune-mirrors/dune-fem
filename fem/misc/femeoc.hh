d8319f5b9650a88babd0b2f1d2c386a5e846bbd5
Andreas Dedner <A.S.Dedner@warwick.ac.uk>
2009-01-23 22:09:20 +0000

    added some docu...
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@4395 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/misc/femeoc.hh
+++ fem/misc/femeoc.hh
@@ -14,16 +14,25 @@ namespace Dune
 
 /**  
     @ingroup HelperClasses
-    \brief Write a self contained tex table and a gnuplot
-    file for eoc runs with timing information.
+    \brief Write a self contained tex table 
+    for eoc runs with timing information.
     
     Constructor takes base name (filename) of file and
     generates two files:
-    filename.tex, filename_body.tex and filename.gnu
+    filename.tex and filename_body.tex.
     The file filename_body.tex hold the actual body
-    of the tex file which is included in filename.tex
-    but can also be used to combine runs with different
-    parameters.
+    of the eoc table which is included in filename.tex
+    but can also be used to combine e.g. runs with different
+    parameters or for plotting using gnuplot.
+
+    The class is singleton and thus new errors for eoc
+    computations can be added in any part of the program.
+    To add a new entry for eoc computations use one of the
+    addEntry methods. These return a unique unsinged int
+    which can be used to add error values to the table
+    with the setErrors methods.
+    The method write is used to write a single line
+    to the eoc table.
  */
 class FemEoc
 {
@@ -160,6 +169,7 @@ class FemEoc
         outputFile_ << log(prevError_[i]/error_[i])/log(factor);
       }
       prevError_[i]=error_[i];
+      error_[i] = -1;  // uninitialized
     }
 	  outputFile_ << "\n"
                 << "\\tabularnewline\n"
@@ -174,41 +184,82 @@ class FemEoc
     static FemEoc instance_;
     return instance_;
   }
+  //! open file path/name and write a description string into tex file
   static void initialize(const std::string& path, const std::string& name, const std::string& descript) {
     instance().init(path,name,descript);
   }
+  //! open file name and write description string into tex file
   static void initialize(const std::string& name, const std::string& descript) {
     instance().init(name,descript);
   }
+  /** \brief add a vector of new eoc values  
+   *
+   *  \tparam  StrVectorType a vector type with operator[] 
+   *           returning a string (a C style array can be used)
+   *           the size of the vector is given as parameter
+   *  \return  a unique index used to add the error values 
+   */
   template <class StrVectorType>
   static size_t addEntry(const StrVectorType& descript,size_t size) {
     return instance().addentry(descript,size);
   }
+  /** \brief add a vector of new eoc values  
+   *
+   *  \tparam  StrVectorType a vector type with size() and operator[]
+   *           returning a string
+   *  \return  a unique index used to add the error values 
+   */
   template <class StrVectorType>
   static size_t addEntry(const StrVectorType& descript) {
     return instance().addentry(descript,descript.size());
   }
+  /** \brief add a single new eoc output  
+   *
+   *  \return  a unique index used to add the error values 
+   */
   static size_t addEntry(const std::string& descript) {
     return instance().addentry(descript);
   }
+  /** \brief add a single new eoc output  
+   *
+   *  \return  a unique index used to add the error values 
+   */
   static size_t addEntry(const char* descript) {
     return addEntry(std::string(descript));
   }
+  /** \brief add a vector of error values for the given id (returned by
+   *         addEntry)
+   *  \tparam  VectorType a vector type with an operator[] 
+   *           returning a double (C style array can be used)
+   */
   template <class VectorType>
   static void setErrors(size_t id,const VectorType& err,int size) {
     instance().seterrors(id,err,size);
   }
+  /** \brief add a vector of error values for the given id (returned by
+   *         addEntry)
+   *  \tparam  VectorType a vector type with a size() and an operator[] 
+   *           returning a double 
+   */
   template <class VectorType>
   static void setErrors(size_t id,const VectorType& err) {
     instance().seterrors(id,err,err.size());
   }
+  /** \brief add a vector in a FieldVector of error values for the given id (returned by
+   *         addEntry)
+   */
   template <int SIZE>
   static void setErrors(size_t id,const FieldVector<double,SIZE>& err) {
     instance().seterrors(id,err);
   }
+  /** \brief add a single error value for the given id (returned by
+   *         addEntry)
+   */
   static void setErrors(size_t id,const double& err) {
     instance().seterrors(id,err);
   }
+  /** \brief commit a line to the eoc file 
+   */
   static void write(double h,double size,double time,int counter) {
     instance().writeerr(h,size,time,counter);
   }
