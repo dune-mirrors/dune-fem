13894e08b88a1e0287cd714dc23ac0da6d6fcf77
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2009-01-26 15:00:59 +0000

    copy from release
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@4401 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/space/test/l2projection.cc
+++ fem/space/test/l2projection.cc
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <config.h>
+#include <string>
+#include <sstream>
 
 #include <dune/grid/io/file/dgfparser/dgfgridtype.hh>
 static const int dimw = dimworld;
@@ -13,14 +15,33 @@ static const int dimw = dimworld;
 
 #include <dune/fem/gridpart/gridpart.hh>
 #include <dune/fem/gridpart/adaptiveleafgridpart.hh> 
+#include <dune/fem/space/dgspace/dgadaptiveleafgridpart.hh> 
 #include <dune/fem/space/common/adaptmanager.hh>
 
 #include <dune/grid/common/referenceelements.hh>
+#include <dune/fem/operator/projection/l2projection.hh>
+#include <dune/fem/misc/l2error.hh>
+
 #include <dune/fem/misc/double.hh>
 
-#if HAVE_GRAPE
-#include <dune/grid/io/visual/grapedatadisplay.hh>
+// to use grape, set to WANT_GRAPE to 1
+#ifndef WANT_GRAPE
+#define WANT_GRAPE 0
 #endif
+
+#if HAVE_GRAPE
+  #define USE_GRAPE WANT_GRAPE
+#else
+  #define USE_GRAPE 0
+  #if WANT_GRAPE
+    #warning "Grape was not found by configure."
+  #endif
+#endif
+
+#if USE_GRAPE 
+  #include <dune/grid/io/visual/grapedatadisplay.hh>
+#endif
+
 using namespace Dune;
 
 // polynom approximation order of quadratures, 
@@ -44,7 +65,9 @@ const int polOrd = POLORDER;
 //***********************************************************************
 
 //! the index set we are using 
-typedef HierarchicGridPart<GridType> GridPartType;
+//typedef HierarchicGridPart<GridType> GridPartType;
+//typedef DGAdaptiveLeafGridPart<GridType> GridPartType;
+typedef AdaptiveLeafGridPart<GridType> GridPartType;
 
 //! define the function space, \f[ \R^2 \rightarrow \R \f]
 // see dune/common/functionspace.hh
@@ -72,7 +95,7 @@ class ExactSolution : public Function < FuncSpace , ExactSolution >
   typedef FuncSpace::RangeFieldType RangeFieldType;
   typedef FuncSpace::DomainType DomainType;
 public:
-  ExactSolution (FuncSpace &f) : Function < FuncSpace , ExactSolution > ( f ) {}
+  ExactSolution (const FuncSpace &f) : Function < FuncSpace , ExactSolution > ( f ) {}
  
   //! f(x,y) = x*(1-x)*y*(1-y)
   void evaluate (const DomainType & x , RangeType & ret)  const
@@ -89,136 +112,16 @@ public:
 };
  
 // ********************************************************************
-template <class DiscreteFunctionType>
-class L2Projection
+double algorithm (GridType& grid, DiscreteFunctionType& solution, bool display)
 {
-  typedef typename DiscreteFunctionType::FunctionSpaceType DiscreteFunctionSpaceType;
+   // create exact solution for error evaluation 
+   ExactSolution f ( solution.space() ); 
 
- public:
-  template <class FunctionType>
-  static void project (const FunctionType &f, DiscreteFunctionType &discFunc, int polOrd) 
-  {
-    typedef typename DiscreteFunctionSpaceType::Traits::GridPartType GridPartType;
-    typedef typename DiscreteFunctionSpaceType::Traits::IteratorType Iterator;
-
-    const DiscreteFunctionSpaceType& space =  discFunc.space();
-
-    discFunc.clear();
-
-    typedef typename DiscreteFunctionType::LocalFunctionType LocalFuncType;
-
-    typename DiscreteFunctionSpaceType::RangeType ret (0.0);
-    typename DiscreteFunctionSpaceType::RangeType phi (0.0);
-
-    Iterator endit = space.end();
-    for(Iterator it = space.begin(); it != endit ; ++it) 
-    {
-      // Get quadrature rule
-      CachingQuadrature<GridPartType,0> quad(*it, polOrd);
-
-      LocalFuncType lf = discFunc.localFunction(*it);
-
-      //! Note: BaseFunctions must be ortho-normal!!!!
-      typedef typename DiscreteFunctionSpaceType::BaseFunctionSetType BaseFunctionSetType ; 
-      const BaseFunctionSetType & baseset =
-        lf.baseFunctionSet();
-
-      const typename GridType::template Codim<0>::Entity::Geometry& 
-        itGeom = (*it).geometry();
-     
-      const int quadNop = quad.nop();
-      const int numDofs = lf.numDofs();
-      for(int qP = 0; qP < quadNop ; ++qP) 
-      {
-        f.evaluate(itGeom.global(quad.point(qP)), ret);
-        for(int i=0; i<numDofs; ++i) {
-          baseset.evaluate( i, quad[qP], phi );
-          lf[i] += quad.weight(qP) * (ret * phi) ;
-        }
-      }
-    }
-  }
-  
-  template <class FunctionType>
-  static void project (const FunctionType &f, DiscreteFunctionType &discFunc) 
-  {
-    const DiscreteFunctionSpaceType& space =  discFunc.space();
-    int polOrd = 2 * space.order();
-    project(f,discFunc,polOrd);
-  }
-};
-
-
-// calculates || u-u_h ||_L2
-template <class DiscreteFunctionType>
-class L2Error
-{
-  typedef typename DiscreteFunctionType::FunctionSpaceType DiscreteFunctionSpaceType;
-  typedef typename DiscreteFunctionSpaceType :: RangeType RangeType;
-
-public:
-  template <class FunctionType>
-  RangeType norm (const FunctionType &f, DiscreteFunctionType &discFunc,
-      double time, int polOrd) const
-  {
-    const DiscreteFunctionSpaceType & space = discFunc.space();
-
-    typedef typename DiscreteFunctionSpaceType::GridPartType GridPartType;
-    typedef typename DiscreteFunctionSpaceType::IteratorType IteratorType;
-    typedef typename DiscreteFunctionType::LocalFunctionType LocalFuncType;
-
-    RangeType ret (0.0);
-    RangeType phi (0.0);
-
-    RangeType error(0.0);
-
-    enum { dimRange = DiscreteFunctionSpaceType :: DimRange };
-
-    IteratorType endit = space.end();
-    for(IteratorType it = space.begin(); it != endit ; ++it)
-    {
-      CachingQuadrature<GridPartType,0> quad(*it, polOrd);
-      LocalFuncType lf = discFunc.localFunction(*it);
-      const int quadNop = quad.nop();
-      for(int qP = 0; qP < quadNop; ++qP)
-      {
-        double weight = quad.weight(qP) * (*it).geometry().integrationElement(quad.point(qP));
-        f.evaluate((*it).geometry().global(quad.point(qP)),time, ret);
-        lf.evaluate(quad[qP],phi);
-
-        for(int i=0; i< dimRange; ++i)
-          error[i] += weight * SQR(ret[i] - phi[i]);
-      }
-    }
-    
-    for(int i=0; i< dimRange; ++i) 
-    {
-      error[i] = sqrt(error[i]);
-    }
-    
-    return error;
-  }
-
-  template <class FunctionType>
-  RangeType norm (const FunctionType &f, DiscreteFunctionType &discFunc,
-      double time) const
-  {
-    const DiscreteFunctionSpaceType & space = discFunc.space();
-    int polOrd = 2 * space.order() + 2;
-    return norm(f,discFunc,time,polOrd);
-  }
-};
-// ********************************************************************
-double algorithm (GridType& grid, DiscreteFunctionType& solution  , int turn )
-{
-   GridPartType part ( grid );
-   DiscreteFunctionSpaceType linFuncSpace ( part );
-   ExactSolution f ( linFuncSpace ); 
+   // L2 error class 
    L2Error < DiscreteFunctionType > l2err;
        
    //! perform l2-projection
-   L2Projection<DiscreteFunctionType>::
-     project(f, solution);
+   L2ProjectionImpl::project(f, solution);
 
    // calculation L2 error 
    // pol ord for calculation the error chould by higher than 
@@ -228,11 +131,11 @@ double algorithm (GridType& grid, DiscreteFunctionType& solution  , int turn )
    for(int i=0; i<RangeType::dimension; ++i)
      std::cout << "\nL2 Error["<<i<<"] : " << error[i] << "\n\n";
 
-#if HAVE_GRAPE
+#if USE_GRAPE
    // if Grape was found, then display last solution 
-   if(0 && turn > 0)
+   if( display )
    {
-     GrapeDataDisplay < GridType > grape(part); 
+     GrapeDataDisplay < GridType > grape( solution.space().gridPart() ); 
      grape.dataDisplay( solution );
    }
 #endif
@@ -258,9 +161,10 @@ int main (int argc, char **argv)
     exit(1);
   }
   int ml = atoi( argv[1] );
-  double* error = new double[ml];
-  char tmp[16]; sprintf(tmp,"%d",dimw);
-  std::string macroGridName (tmp); 
+  std::vector< double> error(ml);
+  std::stringstream tmp; 
+  tmp << dimw;
+  std::string macroGridName (tmp.str()); 
   macroGridName += "dgrid.dgf";
 
   GridPtr<GridType> gridptr(macroGridName);
@@ -277,6 +181,7 @@ int main (int argc, char **argv)
     // grid.globalRefine(step);
     // DofManagerType& dm = DofManagerFactoryType :: getDofManager( grid );
     // dm.resize();
+    // dm.compress();
     AdaptationManager<GridType>::globalRefine(grid,step);
     error[i] = algorithm ( grid , solution , i==ml-1);
     if (i>0) 
@@ -285,7 +190,6 @@ int main (int argc, char **argv)
       std::cout << "EOC = " << eoc << " \n";
     }
   }
-  delete [] error;
   return 0;
   }
   catch( Exception e )
