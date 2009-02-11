bab6db6257a39d5a7cce0286ea160cbb5a71ca83
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2009-02-11 13:40:46 +0000

    L2ProjectionImpl --> DGL2ProjectionImpl
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@4441 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

---
+++ fem/operator/projection/dgl2projection.hh
@@ -0,0 +1,159 @@
+#ifndef DUNE_DGL2PROJECTION_HH
+#define DUNE_DGL2PROJECTION_HH
+
+#include <dune/fem/quadrature/cachequad.hh>
+#include <dune/fem/operator/common/operator.hh>
+#include <dune/fem/function/common/discretefunction.hh>
+#include <dune/fem/function/common/discretefunctionadapter.hh>
+#include <dune/fem/operator/1order/localmassmatrix.hh>
+
+namespace Dune 
+{
+
+// implementation of L2 projection for discontinuous spaces 
+class DGL2ProjectionImpl
+{
+  template <int dummy, bool isDiscreteFunction> 
+  struct ProjectChooser
+  {
+    template <class FunctionImp, class FunctionSpace> 
+    class FunctionAdapter
+    { 
+      const FunctionImp& function_;
+    public:  
+      typedef FunctionSpace FunctionSpaceType;
+      typedef typename FunctionSpaceType :: RangeType RangeType;
+      typedef typename FunctionSpaceType :: DomainType DomainType;
+      FunctionAdapter(const FunctionImp& f) : function_(f) {}
+
+      void evaluate(const DomainType& local,
+                    RangeType& ret) const 
+      {
+        function_.evaluate( local , ret );
+      }
+    };
+    
+    template <class FunctionImp, class DiscreteFunctionImp>
+    static void project(const FunctionImp& f, 
+                        DiscreteFunctionImp& discFunc,
+                        int polOrd) 
+    {
+      // some typedefs 
+      typedef typename DiscreteFunctionImp :: DiscreteFunctionSpaceType DiscreteFunctionSpaceType;
+      typedef typename DiscreteFunctionSpaceType :: GridPartType GridPartType;
+      typedef FunctionAdapter<FunctionImp, typename  DiscreteFunctionSpaceType :: FunctionSpaceType> FunctionAdapterType;
+      // create function adapter in case of incorrect implementation 
+      FunctionAdapterType af( f );
+      // create discrete function adapter 
+      DiscreteFunctionAdapter< FunctionAdapterType, GridPartType> adapter(
+          "DGL2projection::adapter" , f , discFunc.space().gridPart());
+      
+      DGL2ProjectionImpl::projectFunction(adapter, discFunc, polOrd);
+    }
+  };
+
+  template <int dummy> 
+  struct ProjectChooser<dummy,true>
+  {
+    template <class FunctionImp, class DiscreteFunctionImp>
+    static void project(const FunctionImp& f, 
+                        DiscreteFunctionImp& discFunc,
+                        int polOrd) 
+    {
+      DGL2ProjectionImpl::projectFunction(f, discFunc, polOrd);
+    }
+  };
+
+public:  
+  //! project function onto discrete function space  
+  template <class FunctionImp, class DiscreteFunctionImp>
+  static void project(const FunctionImp& f, DiscreteFunctionImp& discFunc, int polOrd = -1) 
+  {
+    ProjectChooser<0, Conversion<FunctionImp, IsDiscreteFunction> ::exists > :: project(f,discFunc,polOrd);
+  }
+
+protected:  
+  template <class FunctionImp, class DiscreteFunctionImp>
+  static void projectFunction(const FunctionImp& func, 
+                              DiscreteFunctionImp& discFunc, 
+                              int polOrd = -1) 
+  {
+    typedef typename DiscreteFunctionImp::DiscreteFunctionSpaceType DiscreteFunctionSpaceType;
+    typedef typename DiscreteFunctionImp::LocalFunctionType LocalFuncType;
+    typedef typename DiscreteFunctionSpaceType::Traits::GridPartType GridPartType;
+    typedef typename DiscreteFunctionSpaceType::Traits::IteratorType Iterator;
+    typedef typename DiscreteFunctionSpaceType::BaseFunctionSetType BaseFunctionSetType ; 
+    typedef typename GridPartType::GridType GridType;
+
+    typedef typename FunctionImp::LocalFunctionType LocalFType;
+    
+    typename DiscreteFunctionSpaceType::RangeType ret (0.0);
+    typename DiscreteFunctionSpaceType::RangeType phi (0.0);
+    const DiscreteFunctionSpaceType& space =  discFunc.space();
+
+    // type of quadrature 
+    typedef CachingQuadrature<GridPartType,0> QuadratureType; 
+    // type of local mass matrix 
+    typedef LocalDGMassMatrix< DiscreteFunctionSpaceType, QuadratureType > LocalMassMatrixType;
+
+    const int quadOrd = (polOrd == -1) ? (2 * space.order()) : polOrd;
+    
+    // create local mass matrix object
+    LocalMassMatrixType massMatrix( space, quadOrd );
+
+    // check whether geometry mappings are affine or not 
+    const bool affineMapping = massMatrix.affine();
+
+    // clear destination
+    discFunc.clear();
+
+    const Iterator endit = space.end();
+    for(Iterator it = space.begin(); it != endit ; ++it) 
+    {
+      // get entity 
+      const typename GridType::template Codim<0>::Entity& en = *it; 
+      // get geometry 
+      const typename GridType::template Codim<0>::Geometry& geo = en.geometry(); 
+      
+      // get quadrature 
+      QuadratureType quad(en, quadOrd);
+      
+      // get local function of destination 
+      LocalFuncType lf = discFunc.localFunction(en);
+      // get local function of argument 
+      const LocalFType f = func.localFunction(en);
+
+      // get base function set 
+      const BaseFunctionSetType & baseset = lf.baseFunctionSet();
+
+      const int quadNop = quad.nop();
+      const int numDofs = lf.numDofs();
+
+      for(int qP = 0; qP < quadNop ; ++qP) 
+      {
+        const double intel = (affineMapping) ? 
+             quad.weight(qP) : // affine case 
+             quad.weight(qP) * geo.integrationElement( quad.point(qP) ); // general case 
+
+        // evaluate function 
+        f.evaluate(quad[qP], ret);
+        
+        // do projection 
+        for(int i=0; i<numDofs; ++i) 
+        {
+          baseset.evaluate(i, quad[qP], phi);
+          lf[i] += intel * (ret * phi) ;
+        }
+      }
+
+      // in case of non-linear mapping apply inverse 
+      if ( ! affineMapping ) 
+      {
+        massMatrix.applyInverse( en, lf );
+      } 
+    }
+  }
+};
+
+} // end namespace Dune 
+#endif
--- fem/operator/projection/l2projection.hh
+++ fem/operator/projection/l2projection.hh
@@ -7,148 +7,19 @@
 #include <dune/fem/function/common/discretefunctionadapter.hh>
 #include <dune/fem/operator/1order/localmassmatrix.hh>
 
+#include "dgl2projection.hh"
+
 namespace Dune 
 {
 
-struct L2ProjectionImpl
+// deprecated class (use DGL2Projection instead)
+struct L2ProjectionImpl : public DGL2ProjectionImpl
 {
-  template <int dummy, bool isDiscreteFunction> 
-  struct ProjectChooser
-  {
-    template <class FunctionImp, class FunctionSpace> 
-    class FunctionAdapter
-    { 
-      const FunctionImp& function_;
-    public:  
-      typedef FunctionSpace FunctionSpaceType;
-      typedef typename FunctionSpaceType :: RangeType RangeType;
-      typedef typename FunctionSpaceType :: DomainType DomainType;
-      FunctionAdapter(const FunctionImp& f) : function_(f) {}
-
-      void evaluate(const DomainType& local,
-                    RangeType& ret) const 
-      {
-        function_.evaluate( local , ret );
-      }
-    };
-    
-    template <class FunctionImp, class DiscreteFunctionImp>
-    static void project(const FunctionImp& f, 
-                        DiscreteFunctionImp& discFunc,
-                        int polOrd) 
-    {
-      // some typedefs 
-      typedef typename DiscreteFunctionImp :: DiscreteFunctionSpaceType DiscreteFunctionSpaceType;
-      typedef typename DiscreteFunctionSpaceType :: GridPartType GridPartType;
-      typedef FunctionAdapter<FunctionImp, typename  DiscreteFunctionSpaceType :: FunctionSpaceType> FunctionAdapterType;
-      // create function adapter in case of incorrect implementation 
-      FunctionAdapterType af( f );
-      // create discrete function adapter 
-      DiscreteFunctionAdapter< FunctionAdapterType, GridPartType> adapter(
-          "L2projection::adapter" , f , discFunc.space().gridPart());
-      
-      L2ProjectionImpl::projectFunction(adapter, discFunc, polOrd);
-    }
-  };
-
-  template <int dummy> 
-  struct ProjectChooser<dummy,true>
-  {
-    template <class FunctionImp, class DiscreteFunctionImp>
-    static void project(const FunctionImp& f, 
-                        DiscreteFunctionImp& discFunc,
-                        int polOrd) 
-    {
-      L2ProjectionImpl::projectFunction(f, discFunc, polOrd);
-    }
-  };
-
+  //! project function onto discrete function space  
   template <class FunctionImp, class DiscreteFunctionImp>
-  static void project(const FunctionImp& f, DiscreteFunctionImp& discFunc, int polOrd_ = -1) 
+  DUNE_DEPRECATED static void project(const FunctionImp& f, DiscreteFunctionImp& discFunc, int polOrd = -1)
   {
-    ProjectChooser<0, Conversion<FunctionImp, IsDiscreteFunction> ::exists > :: project(f,discFunc,polOrd_);
-  }
-
-protected:  
-  template <class FunctionImp, class DiscreteFunctionImp>
-  static void projectFunction(const FunctionImp& func, 
-                              DiscreteFunctionImp& discFunc, 
-                              int polOrd_ = -1) 
-  {
-    typedef typename DiscreteFunctionImp::DiscreteFunctionSpaceType DiscreteFunctionSpaceType;
-    typedef typename DiscreteFunctionImp::LocalFunctionType LocalFuncType;
-    typedef typename DiscreteFunctionSpaceType::Traits::GridPartType GridPartType;
-    typedef typename DiscreteFunctionSpaceType::Traits::IteratorType Iterator;
-    typedef typename DiscreteFunctionSpaceType::BaseFunctionSetType BaseFunctionSetType ; 
-    typedef typename GridPartType::GridType GridType;
-
-    typedef typename FunctionImp::LocalFunctionType LocalFType;
-    
-    typename DiscreteFunctionSpaceType::RangeType ret (0.0);
-    typename DiscreteFunctionSpaceType::RangeType phi (0.0);
-    const DiscreteFunctionSpaceType& space =  discFunc.space();
-
-    // type of quadrature 
-    typedef CachingQuadrature<GridPartType,0> QuadratureType; 
-    // type of local mass matrix 
-    typedef LocalDGMassMatrix< DiscreteFunctionSpaceType, QuadratureType > LocalMassMatrixType;
-
-    const int quadOrd = (polOrd_ == -1) ? (2 * space.order()) : polOrd_;
-    
-    // create local mass matrix object
-    LocalMassMatrixType massMatrix( space, quadOrd );
-
-    // check whether geometry mappings are affine or not 
-    const bool affineMapping = massMatrix.affine();
-
-    // clear destination
-    discFunc.clear();
-
-    const Iterator endit = space.end();
-    for(Iterator it = space.begin(); it != endit ; ++it) 
-    {
-      // get entity 
-      const typename GridType::template Codim<0>::Entity& en = *it; 
-      // get geometry 
-      const typename GridType::template Codim<0>::Geometry& geo = en.geometry(); 
-      
-      // get quadrature 
-      QuadratureType quad(en, quadOrd);
-      
-      // get local function of destination 
-      LocalFuncType lf = discFunc.localFunction(en);
-      // get local function of argument 
-      const LocalFType f = func.localFunction(en);
-
-      // get base function set 
-      const BaseFunctionSetType & baseset = lf.baseFunctionSet();
-
-      const int quadNop = quad.nop();
-      const int numDofs = lf.numDofs();
-
-      for(int qP = 0; qP < quadNop ; ++qP) 
-      {
-        const double intel = (affineMapping) ? 
-             quad.weight(qP) : // affine case 
-             quad.weight(qP) * geo.integrationElement( quad.point(qP) ); // general case 
-
-        // evaluate function 
-        f.evaluate(quad[qP], ret);
-        
-        // do projection 
-        for(int i=0; i<numDofs; ++i) 
-        {
-          baseset.evaluate(i, quad[qP], phi);
-          lf[i] += intel * (ret * phi) ;
-        }
-      }
-
-      // in case of non-linear mapping apply inverse 
-      if ( ! affineMapping ) 
-      {
-        massMatrix.applyInverse( en, lf );
-      } 
-    }
+    DGL2ProjectionImpl :: project( f, discFunc, polOrd );
   }
 };
 
@@ -177,10 +48,13 @@ class L2Projection : public Operator<DFieldType, RFieldType,DType , RType> {
   //! apply L2 projection
   virtual void operator() (const DomainType& f, RangeType& discFunc) const 
   {
-    L2ProjectionImpl::project(f,discFunc,polOrd_);
+    if( discFunc.space().continuous() )
+      DUNE_THROW(NotImplemented,"L2-Projection not implemented for contiuous spaces!"); 
+    else 
+      DGL2ProjectionImpl::project(f,discFunc,polOrd_);
   }
 
-private:
+protected:
   const int polOrd_;
 };
 
