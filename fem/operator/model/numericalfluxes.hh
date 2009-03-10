da8ee0370bb768c4881348761ab0eb7604665df5
Nina Shokina <shokina@mathematik.uni-freiburg.de>
2009-03-10 16:31:39 +0000

    full wetting-drying treatment is implemented in numerical flux evaluation connected to revisions 141-142 of dune-swe
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@4533 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/operator/model/numericalfluxes.hh
+++ fem/operator/model/numericalfluxes.hh
@@ -107,13 +107,18 @@ namespace Dune {
       typename Traits::RangeType visc;
       typename Traits::FluxRangeType anaflux;
 
+      //std::cout << " reflectionLeft is " << reflectionLeft << std::endl;
+      //std::cout << " reflectionRight is " << reflectionRight << std::endl;
+
       gLeft = 0;
       gRight = 0;
+      RangeType uLeftRef;
+      RangeType uRightRef;
 
       if (reflectionRight == false) // (no reflection) or (reflection in entity)
       {
         // anaflux = F( uLeft )
-        std::cout << " reflectionRight = false, compute g(uLeft) .. "  << std::endl;
+        //std::cout << " reflectionRight = false, compute g(uLeft) .. "  << std::endl;
         model_.analyticalFlux( *(it->inside()), time,
                                it->intersectionSelfLocal().global(x),
                                uLeft, anaflux);
@@ -122,7 +127,7 @@ namespace Dune {
       else // (reflection in neighbor)
       {
         // anaflux = F( uRight )
-        std::cout << " reflectionRight = true, compute g(uRight) .. "  << std::endl;
+        //std::cout << " reflectionRight = true, compute g(uRight) .. "  << std::endl;
         model_.analyticalFlux( *(it->outside()), time,
                                it->intersectionNeighborLocal().global(x),
                                uRight, anaflux);
@@ -132,8 +137,7 @@ namespace Dune {
       if (reflectionLeft == true) // (reflection in entity)
       {
         // anaflux = F( uLeftRef )
-        std::cout << " reflectionLeft = true, compute g(uLeftRef) .. "  << std::endl;
-        RangeType uLeftRef;
+        //std::cout << " reflectionLeft = true, compute g(uLeftRef) .. "  << std::endl;
         model_.analyticalFlux( *(it->inside()), time,
                                it->intersectionSelfLocal().global(x),
                                model_.reflectU( uLeft, normal, uLeftRef ), anaflux);
@@ -155,8 +159,7 @@ namespace Dune {
         if (reflectionRight == true) // (reflection in neigbor)
         {
           // anaflux = F( uRightRef )
-          std::cout << " reflectionRight = true, compute g(uRightRef) ..  "  << std::endl;
-          RangeType uRightRef;
+          //std::cout << " reflectionRight = true, compute g(uRightRef) ..  "  << std::endl;
           model_.analyticalFlux( *(it->outside()), time,
                                  it->intersectionNeighborLocal().global(x),
                                  model_.reflectU( uRight, normal, uRightRef ), anaflux);
@@ -164,7 +167,7 @@ namespace Dune {
         }
         else // (no reflection)
         {
-          std::cout << " reflectionLeft = false, reflectionRight = false, compute g(uRight) ..  "  << std::endl;
+          //std::cout << " reflectionLeft = false, reflectionRight = false, compute g(uRight) ..  "  << std::endl;
           if (it->neighbor())
              model_.analyticalFlux( *(it->outside()), time,
                                     it->intersectionNeighborLocal().global(x),
@@ -176,6 +179,41 @@ namespace Dune {
                                     uRight, anaflux );
           }
           anaflux.umv( normal,gLeft );
+        }
+      }
+
+      double maxspeedl, maxspeedr, maxspeed;
+      double viscparal, viscparar, viscpara;
+
+// the version of the method maxSpeed is used with the additional argument for wetting-drying treatment
+
+      if (reflectionRight == false) // (no reflection) or (reflection in entity)
+      {
+        model_.maxSpeed( normal, time, it->intersectionGlobal().global(x), *(it->inside()),
+                         uLeft, viscparal, maxspeedl);
+      }
+      else // (reflection in neighbor)
+      {
+        model_.maxSpeed( normal, time, it->intersectionGlobal().global(x), *(it->outside()),
+                         uRight, viscparar, maxspeedr);
+      }
+
+      if (reflectionLeft == true) // (reflection in entity)
+      {
+        model_.maxSpeed( normal, time, it->intersectionGlobal().global(x), *(it->inside()),
+                         uLeftRef, viscparar, maxspeedr);
+      }
+      else
+      {
+        if (reflectionRight == true) // (reflection in neigbor)
+        {
+          model_.maxSpeed( normal, time, it->intersectionGlobal().global(x), *(it->outside()),
+                           uRightRef, viscparal, maxspeedl );
+        }
+        else // (no reflection)
+        {
+          model_.maxSpeed( normal, time, it->intersectionGlobal().global(x), *(it->outside()),
+                           uRight, viscparar, maxspeedr );
         }
       }
 
@@ -192,28 +230,46 @@ namespace Dune {
          model_.analyticalFlux( *(it->inside()), time,
                                 it->intersectionSelfLocal().global(x),
                                 uRight, anaflux );
-      anaflux.umv( normal,gLeft ); */
+      anaflux.umv( normal,gLeft );
 
-      double maxspeedl, maxspeedr, maxspeed;
-      double viscparal, viscparar, viscpara;
-
-// nsh: the version of the method maxSpeed is used with the additional argument for wetting-drying treatment
       model_.maxSpeed( normal, time, it->intersectionGlobal().global(x), *(it->inside()),
                        uLeft, viscparal, maxspeedl);
+
       model_.maxSpeed( normal, time, it->intersectionGlobal().global(x), *(it->outside()),
-                       uRight, viscparar, maxspeedr);
+                       uRight, viscparar, maxspeedr); */
 
       maxspeed = (maxspeedl>maxspeedr) ? maxspeedl : maxspeedr;
       viscpara = (viscparal>viscparar) ? viscparal : viscparar;
 
-      visc  = uRight;
-      visc -= uLeft;
-      visc *= 2.*viscpara;
-      gLeft -= visc;
-      
-      gLeft *= 0.5;
+      //std::cout << " reflectionRight = true, compute g(uRightRef) ..  "  << std::endl;
 
-      gRight = gLeft;
+      if (reflectionRight == true)
+      {
+        visc  = uRightRef - uRight;  
+      }
+      if (reflectionLeft == true)
+      {
+        visc  = uLeftRef - uLeft;  
+      }
+      else
+      {
+        visc  = uRight - uLeft;
+      }
+
+      visc *= 2.*viscpara;
+
+      if (reflectionRight == true)
+      {
+        gRight -= visc;
+        gRight *= 0.5;        
+        gLeft = gRight;  
+      }
+      else
+      {
+        gLeft -= visc;
+        gLeft *= 0.5;
+        gRight = gLeft;
+      }
 
       return maxspeed;
     }
