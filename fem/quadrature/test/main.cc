d022afdaaba9769b5d55fb8f567b336f5824fd7f
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2009-05-04 12:31:42 +0000

    initialize MPI
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@4629 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/quadrature/test/main.cc
+++ fem/quadrature/test/main.cc
@@ -2,14 +2,18 @@
 
 #include <config.h>
 
+#include <dune/fem/misc/mpimanager.hh>
+
 #include "../../misc/test.hh"
 #include "../../misc/suite.hh"
 
 #include "cachequad_test.hh"
 #include "quad_test.hh"
 
-int main()
+int main ( int argc, char **argv )
 {
+  Dune::MPIManager::initialize( argc, argv );
+
   std::stringstream albertaGridFile;
 #if HAVE_ALBERTA
   albertaGridFile << "../../../macrogrids/AlbertaGrid/" << ALBERTA_DIM << "dgrid.al";
