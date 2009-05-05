d24627f35f7a5f93c0d434727a846287a9d32f82
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2009-05-05 18:10:16 +0000

    localFaceIndex leads to wrong results.
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@4639 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/quadrature/cachepointlist.hh
+++ fem/quadrature/cachepointlist.hh
@@ -166,7 +166,7 @@ namespace Dune
       BaseType;
 
  protected:
-    using BaseType :: localFaceIndex;
+    using BaseType :: faceNumber;
     using BaseType :: quadImp;
 
   public:
@@ -225,7 +225,7 @@ namespace Dune
                        typename BaseType :: Side side)
       : BaseType( gridPart, intersection, order, side ),
         mapper_( CacheProvider< GridType, codimension > :: getMapper
-          ( quadImp(), elementGeometry(), localFaceIndex(),
+          ( quadImp(), elementGeometry(), faceNumber(),
             (side == BaseType :: INSIDE)
               ? TwistUtilityType :: twistInSelf( gridPart.grid(), intersection )
               : TwistUtilityType :: twistInNeighbor( gridPart.grid(), intersection )
