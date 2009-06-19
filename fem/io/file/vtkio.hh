477a790b916a6b79817e6c37596cbf886eeed396
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2009-06-19 14:21:27 +0000

    added support for SubsamplingVTKIO
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@4696 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/io/file/vtkio.hh
+++ fem/io/file/vtkio.hh
@@ -2,36 +2,49 @@
 #define VTKIO_HH
 
 #include <dune/grid/io/file/vtk/vtkwriter.hh>
-#include <dune/fem/version.hh>
+#include <dune/grid/io/file/vtk/subsamplingvtkwriter.hh>
 #include <dune/fem/gridpart/gridpartview.hh>
 
 namespace Dune
 {
 
-  template <class DF>
+  template< class DF >
   class VTKFunctionWrapper
-  : public VTKWriter< typename DF :: DiscreteFunctionSpaceType :: GridPartType :: GridViewType > :: VTKFunction
+  : public VTKWriter< typename DF::DiscreteFunctionSpaceType::GridPartType::GridViewType >::VTKFunction
   {
+    typedef VTKFunctionWrapper< DF > ThisType;
+
   public:
     typedef DF DiscreteFunctionType;
-    typedef typename DF::LocalFunctionType LocalFunctionType;
-    typedef typename DF::FunctionSpaceType FunctionSpaceType;
+
+    typedef typename DiscreteFunctionType::LocalFunctionType LocalFunctionType;
+    typedef typename DiscreteFunctionType::FunctionSpaceType FunctionSpaceType;
     typedef typename FunctionSpaceType::DomainType DomainType;
     typedef typename FunctionSpaceType::RangeType RangeType;
     typedef typename FunctionSpaceType::GridPartType GridPartType;
     typedef typename FunctionSpaceType::IteratorType::Entity EntityType;
-    enum {dimRange=FunctionSpaceType::dimRange,
-          dimDomain=FunctionSpaceType::dimDomain};
+
+    static const int dimRange = FunctionSpaceType::dimRange;
+    static const int dimDomain = FunctionSpaceType::dimDomain;
+
     //! constructor taking discrete function 
-    VTKFunctionWrapper(const DiscreteFunctionType& df,
-                       int component,bool vector)
-      : discFunc_(df),
-        vector_(vector),
-        component_(component) {}
+    VTKFunctionWrapper ( const DiscreteFunctionType& df,
+                         int component, bool vector )
+    : discFunc_( df ),
+      vector_( vector ),
+      component_( component )
+    {}
+
+    //! virtual destructor
+    virtual ~VTKFunctionWrapper ()
+    {}
+
     //! return number of components
-    virtual int ncomps () const {
+    virtual int ncomps () const
+    {
       return (!vector_) ? 1 : 3; // dimDomain;
     }
+
     //! evaluate single component comp in
     //! the entity
     virtual double evaluate (int comp, const EntityType& e, const DomainType& xi) const {
@@ -47,8 +60,10 @@ namespace Dune
       else 
         return val[component_];
     }
+
     //! get name
-    virtual std::string name () const {
+    virtual std::string name () const
+    {
       if (vector_) {
         std::stringstream ret_vec;
         ret_vec << discFunc_.name() << "-vec-" << component_;
@@ -58,38 +73,47 @@ namespace Dune
       ret << discFunc_.name() << "-" << component_;
       return ret.str();
     }
-    //! virtual destructor
-    virtual ~VTKFunctionWrapper () {}
+
   private:
-    const DF& discFunc_;
+    const DiscreteFunctionType &discFunc_;
     const bool vector_;
     const int component_;
   };
 
+
+
   //! /brief Output using VTK
-  template< class GridPartImp >
+  template< class GridPart >
   class VTKIO
-  : public VTKWriter< typename GridPartImp :: GridViewType >
   {
-    typedef VTKIO< GridPartImp > ThisType;
-    typedef VTKWriter< typename GridPartImp :: GridViewType > BaseType;
+    typedef VTKIO< GridPart > ThisType;
+
+    typedef VTKWriter< typename GridPart::GridViewType > VTKWriterType;
 
   public:
-    typedef GridPartImp GridPartType;
+    typedef GridPart GridPartType;
 
-    typedef typename GridPartType :: GridType GridType;
-    typedef typename GridPartType :: IndexSetType IndexSetType;
+    typedef typename GridPartType::GridType GridType;
+    typedef typename GridPartType::IndexSetType IndexSetType;
 
-  private:
-    const GridPartType& gridPart_;
+  protected:
+    VTKIO ( const GridPartType &gridPart, VTKWriterType *vtkWriter )
+    : gridPart_( gridPart ),
+      vtkWriter_( vtkWriter )
+    {}
 
   public:
     //! constructor  
     explicit VTKIO ( const GridPartType &gridPart,
-                     VTKOptions :: DataMode dm = VTKOptions :: conforming )
-    : BaseType( gridPart.gridView(), dm ),
-      gridPart_( gridPart )
+                     VTKOptions::DataMode dm = VTKOptions::conforming )
+    : gridPart_( gridPart ),
+      vtkWriter_( new VTKWriterType( gridPart.gridView(), dm ) )
     {}
+
+    ~VTKIO ()
+    {
+      delete vtkWriter_;
+    }
 
     //! return grid part 
     const GridPartType &gridPart () const
@@ -104,7 +128,7 @@ namespace Dune
       enum {dimRange=FunctionSpaceType::dimRange};
       enum {dimDomain=FunctionSpaceType::dimDomain};
       for (int i=0;i<dimRange;++i) 
-         BaseType::addCellData(new VTKFunctionWrapper<DF>(df,i,false)); 
+         vtkWriter_->addCellData(new VTKFunctionWrapper<DF>(df,i,false)); 
     }
 
     template< class DF >
@@ -114,7 +138,7 @@ namespace Dune
       typedef typename DF::FunctionSpaceType FunctionSpaceType;
       enum {dimRange=FunctionSpaceType::dimRange};
       enum {dimDomain=FunctionSpaceType::dimDomain};
-      BaseType::addCellData(new VTKFunctionWrapper<DF>(df,startPoint,true)); 
+      vtkWriter_->addCellData(new VTKFunctionWrapper<DF>(df,startPoint,true)); 
     }
 
     template< class DF >
@@ -124,21 +148,61 @@ namespace Dune
       enum {dimRange=FunctionSpaceType::dimRange};
       enum {dimDomain=FunctionSpaceType::dimDomain};
       for (int i=0;i<dimRange;++i) 
-        BaseType::addVertexData(new VTKFunctionWrapper<DF>(df,i,false)); 
+        vtkWriter_->addVertexData(new VTKFunctionWrapper<DF>(df,i,false)); 
     }
 
     template< class DF >
-    void addVectorVertexData( DF &df,
-                      int startPoint = 0 )
+    void addVectorVertexData( DF &df, int startPoint = 0 )
     {
       typedef typename DF::FunctionSpaceType FunctionSpaceType;
       enum {dimRange=FunctionSpaceType::dimRange};
       enum {dimDomain=FunctionSpaceType::dimDomain};
-      BaseType::addVertexData(new VTKFunctionWrapper<DF>(df,startPoint,true)); 
+      vtkWriter_->addVertexData(new VTKFunctionWrapper<DF>(df,startPoint,true)); 
     }
 
+    void clear ()
+    {
+      vtkWriter_->clear();
+    }
+
+    std::string write ( const std::string &name,
+                        VTKOptions::OutputType type = VTKOptions::ascii )
+    {
+      return vtkWriter_->write( name, type );
+    }
+
+    std::string pwrite ( const char *name, const char *path, const char *extendpath,
+                         VTKOptions::OutputType type = VTKOptions::ascii )
+    {
+      return vtkWriter_->pwrite( name, path, extendpath, type );
+    }
+
+  private:
+    const GridPartType& gridPart_;
+    VTKWriterType *vtkWriter_;
+  };
+
+
+
+  template< class GridPart >
+  class SubsamplingVTKIO
+  : public VTKIO< GridPart >
+  {
+    typedef SubsamplingVTKIO< GridPart > ThisType;
+    typedef VTKIO< GridPart > BaseType;
+
+    typedef SubsamplingVTKWriter< typename GridPart::GridViewType > VTKWriterType;
+
+  public:
+    typedef GridPart GridPartType;
+
+    explicit SubsamplingVTKIO ( const GridPartType &gridPart,
+                                unsigned int level = 0,
+                                bool coerceToSimplex = false )
+    : BaseType( gridPart, new VTKWriterType( gridPart.gridView(), level, coerceToSimplex ) )
+    {}
   };
   
 }
 
-#endif
+#endif // #ifndef VTKIO_HH
