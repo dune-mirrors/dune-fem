bda2adc82c8ec33a363d3173b0960950bd08a328
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2009-07-02 09:04:59 +0000

    ForLoop::apply can now take up to 8 arguments
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@4725 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/misc/forloop.hh
+++ fem/misc/forloop.hh
@@ -11,36 +11,64 @@ namespace Dune
   {
     static void apply ()
     {
-      Operation< first > :: apply();
-      ForLoop< Operation, first+1, last > :: apply();
+      Operation< first >::apply();
+      ForLoop< Operation, first+1, last >::apply();
     }
 
     template< class T1 >
     static void apply ( T1 &p1 )
     {
-      Operation< first > :: apply( p1 );
-      ForLoop< Operation, first+1, last > :: apply( p1 );
+      Operation< first >::apply( p1 );
+      ForLoop< Operation, first+1, last >::apply( p1 );
     }
     
     template< class T1, class T2 >
     static void apply ( T1 &p1, T2 &p2 )
     {
-      Operation< first > :: apply( p1, p2 );
-      ForLoop< Operation, first+1, last > :: apply( p1, p2 );
+      Operation< first >::apply( p1, p2 );
+      ForLoop< Operation, first+1, last >::apply( p1, p2 );
     }
 
     template< class T1, class T2, class T3 >
     static void apply ( T1 &p1, T2 &p2, T3 &p3 )
     {
-      Operation< first > :: apply( p1, p2, p3 );
-      ForLoop< Operation, first+1, last > :: apply( p1, p2, p3 );
+      Operation< first >::apply( p1, p2, p3 );
+      ForLoop< Operation, first+1, last >::apply( p1, p2, p3 );
     }
 
     template< class T1, class T2, class T3, class T4 >
     static void apply ( T1 &p1, T2 &p2, T3 &p3, T4 &p4 )
     {
-      Operation< first > :: apply( p1, p2, p3, p4 );
-      ForLoop< Operation, first+1, last > :: apply( p1, p2, p3, p4 );
+      Operation< first >::apply( p1, p2, p3, p4 );
+      ForLoop< Operation, first+1, last >::apply( p1, p2, p3, p4 );
+    }
+
+    template< class T1, class T2, class T3, class T4, class T5 >
+    static void apply ( T1 &p1, T2 &p2, T3 &p3, T4 &p4, T5 &p5 )
+    {
+      Operation< first >::apply( p1, p2, p3, p4, p5 );
+      ForLoop< Operation, first+1, last >::apply( p1, p2, p3, p4, p5 );
+    }
+
+    template< class T1, class T2, class T3, class T4, class T5, class T6 >
+    static void apply ( T1 &p1, T2 &p2, T3 &p3, T4 &p4, T5 &p5, T6 &p6 )
+    {
+      Operation< first >::apply( p1, p2, p3, p4, p5, p6 );
+      ForLoop< Operation, first+1, last >::apply( p1, p2, p3, p4, p5, p6 );
+    }
+
+    template< class T1, class T2, class T3, class T4, class T5, class T6, class T7 >
+    static void apply ( T1 &p1, T2 &p2, T3 &p3, T4 &p4, T5 &p5, T6 &p6, T7 &p7 )
+    {
+      Operation< first >::apply( p1, p2, p3, p4, p5, p6, p7 );
+      ForLoop< Operation, first+1, last >::apply( p1, p2, p3, p4, p5, p6, p7 );
+    }
+
+    template< class T1, class T2, class T3, class T4, class T5, class T6, class T7, class T8 >
+    static void apply ( T1 &p1, T2 &p2, T3 &p3, T4 &p4, T5 &p5, T6 &p6, T7 &p7, T8 &p8 )
+    {
+      Operation< first >::apply( p1, p2, p3, p4, p5, p6, p7, p8 );
+      ForLoop< Operation, first+1, last >::apply( p1, p2, p3, p4, p5, p6, p7, p8 );
     }
 
   private:
@@ -53,31 +81,55 @@ namespace Dune
   {
     static void apply ()
     {
-      Operation< last > :: apply();
+      Operation< last >::apply();
     }
 
     template< class T1 >
     static void apply ( T1 &p1 )
     {
-      Operation< last > :: apply( p1 );
+      Operation< last >::apply( p1 );
     }
     
     template< class T1, class T2 >
     static void apply ( T1 &p1, T2 &p2 )
     {
-      Operation< last > :: apply( p1, p2 );
+      Operation< last >::apply( p1, p2 );
     }
 
     template< class T1, class T2, class T3 >
     static void apply ( T1 &p1, T2 &p2,  T3 &p3 )
     {
-      Operation< last > :: apply( p1, p2, p3 );
+      Operation< last >::apply( p1, p2, p3 );
     }
 
     template< class T1, class T2, class T3, class T4 >
     static void apply ( T1 &p1, T2 &p2,  T3 &p3, T4 &p4 )
     {
-      Operation< last > :: apply( p1, p2, p3, p4 );
+      Operation< last >::apply( p1, p2, p3, p4 );
+    }
+
+    template< class T1, class T2, class T3, class T4, class T5 >
+    static void apply ( T1 &p1, T2 &p2,  T3 &p3, T4 &p4, T5 &p5 )
+    {
+      Operation< last >::apply( p1, p2, p3, p4, p5 );
+    }
+
+    template< class T1, class T2, class T3, class T4, class T5, class T6 >
+    static void apply ( T1 &p1, T2 &p2,  T3 &p3, T4 &p4, T5 &p5, T6 &p6 )
+    {
+      Operation< last >::apply( p1, p2, p3, p4, p5, p6 );
+    }
+
+    template< class T1, class T2, class T3, class T4, class T5, class T6, class T7 >
+    static void apply ( T1 &p1, T2 &p2,  T3 &p3, T4 &p4, T5 &p5, T6 &p6, T7 &p7 )
+    {
+      Operation< last >::apply( p1, p2, p3, p4, p5, p6, p7 );
+    }
+
+    template< class T1, class T2, class T3, class T4, class T5, class T6, class T7, class T8 >
+    static void apply ( T1 &p1, T2 &p2,  T3 &p3, T4 &p4, T5 &p5, T6 &p6, T7 &p7, T8 &p8 )
+    {
+      Operation< last >::apply( p1, p2, p3, p4, p5, p6, p7, p8 );
     }
   };
 
