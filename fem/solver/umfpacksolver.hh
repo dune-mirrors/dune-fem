1f005524d2ed47cff1f29290eb8c4b60de971c4f
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2009-09-03 13:50:07 +0000

    * correct indentation * add constructor without maxIter and verbose argument * make operator const (real matrix is mutable anyway)
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@4778 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/solver/umfpacksolver.hh
+++ fem/solver/umfpacksolver.hh
@@ -1,13 +1,17 @@
 #ifndef DUNE_UMFPACKSOLVER_HH
 #define DUNE_UMFPACKSOLVER_HH
 
+#include <limits>
+
 #include <dune/fem/operator/common/operator.hh>
+#include <dune/fem/io/parameter.hh>
 
 #ifdef ENABLE_UMFPACK 
 #include <umfpack.h>
 #endif
 
-namespace Dune {
+namespace Dune
+{
 
   /** @addtogroup DirectSolver  
       
@@ -16,97 +20,108 @@ namespace Dune {
       \f$A x = b\f$, where \f$A\f$ is a Mapping or
       Operator and \f$x\f$ and \f$b\f$ are discrete functions 
       (see DiscreteFunctionInterface) can be found. 
-      
-      @{
    **/
 
-/** \brief UMFPACK direct solver */
-template <class DiscreteFunctionType, class OperatorType>
-class UMFPACKOp : public Operator<
-      typename DiscreteFunctionType::DomainFieldType,
-      typename DiscreteFunctionType::RangeFieldType,
-            DiscreteFunctionType,DiscreteFunctionType> 
-{
 
-private:
-  // no const reference, we make const later 
-  OperatorType &op_;
-  typename DiscreteFunctionType::RangeFieldType epsilon_;
-  int maxIter_;
-  bool verbose_ ;
-
-public:
-  /** \brief constructor of UMFPACKOp
-      \param[in] op Operator to invert 
-      \param[in] redEps realative tolerance for residual 
-      \param[in] absLimit absolut solving tolerance for residual 
-      \param[in] maxIter maximal number of iterations performed 
-      \param[in] verbose verbosity 
-  */
-  UMFPACKOp(OperatorType & op, 
-            double  redEps, 
-            double absLimit, 
-            int maxIter, 
-            bool verbose ) :
-        op_(op), epsilon_ ( absLimit ) ,
-        maxIter_ (maxIter ) , verbose_ ( verbose ) {
-  }
-
-  void prepare (const DiscreteFunctionType& Arg, DiscreteFunctionType& Dest) const
+  /** \class UMFPACKOp
+   *  \ingroup DirectSolver
+   *  \brief UMFPACK direct solver
+   */
+  template< class DF, class Op >
+  struct UMFPACKOp
+  : public Operator< typename DF::DomainFieldType, typename DF::RangeFieldType, DF, DF >
   {
-  }
+    typedef DF DiscreteFunctionType;
+    typedef Op OperatorType;
 
-  void finalize () const
-  {
-  }
+    /** \brief constructor of UMFPACKOp
+        \param[in] op Operator to invert 
+        \param[in] redEps realative tolerance for residual 
+        \param[in] absLimit absolut solving tolerance for residual 
+        \param[in] maxIter maximal number of iterations performed 
+        \param[in] verbose verbosity 
+    */
+    UMFPACKOp ( const OperatorType &op, 
+                double redEps,
+                double absLimit,
+                int maxIter,
+                bool verbose )
+    : op_( op ),
+      epsilon_( absLimit ),
+      maxIter_( maxIter ),
+      verbose_( verbose )
+    {}
 
-  /** \brief solve the system 
-      \param[in] arg right hand side 
-      \param[out] dest solution 
-  */
-  void apply( const DiscreteFunctionType& arg, DiscreteFunctionType& dest ) const
-  {
-    // prepare operator 
-    prepare ( arg, dest );
+    UMFPACKOp ( const OperatorType &op,
+                double redEps,
+                double absLimit,
+                int maxIter = std::numeric_limits< int >::max() )
+    : op_( op ),
+      epsilon_( absLimit ),
+      maxIter_( maxIter ),
+      verbose_( Parameter::getValue< bool >( "fem.solver.verbose", false ) )
+    {}
+                
+
+    void prepare ( const DiscreteFunctionType &, DiscreteFunctionType & ) const
+    {}
+
+    void finalize () const
+    {}
+
+    /** \brief solve the system 
+        \param[in] arg right hand side 
+        \param[out] dest solution 
+    */
+    void apply ( const DiscreteFunctionType &arg, DiscreteFunctionType &dest ) const
+    {
+      // prepare operator 
+      prepare( arg, dest );
 
 #ifdef ENABLE_UMFPACK 
-    // call UMF solve method on SparseRowMatrix
-    op_.systemMatrix().solveUMF( arg, dest );
+      // call UMF solve method on SparseRowMatrix
+      op_.systemMatrix().solveUMF( arg, dest );
 #else 
-    DUNE_THROW(InvalidStateException,"UMFPACK was not found, reconfigure or use other solver!");
+      DUNE_THROW( InvalidStateException, "UMFPACK was not found, reconfigure or use other solver!" );
 #endif
-    // finalize operator  
-    finalize ();
-  }
 
-  /** \brief solve the system 
-      \param[in] arg right hand side 
-      \param[out] dest solution 
-  */
-  void operator ()( const DiscreteFunctionType& arg, DiscreteFunctionType& dest ) const
-  {
-    apply(arg,dest);
-  }
+      // finalize operator  
+      finalize ();
+    }
 
-  void printTexInfo(std::ostream& out) const
-  {
-    out << "Solver: UMFPACK direct solver ";
-    out  << "\\\\ \n";
-  }
+    /** \brief solve the system 
+        \param[in] arg right hand side 
+        \param[out] dest solution 
+    */
+    void operator() ( const DiscreteFunctionType &arg, DiscreteFunctionType &dest ) const
+    {
+      apply( arg, dest );
+    }
 
-  double averageCommTime() const 
-  {
-    return 0.0;
-  }
+    void printTexInfo(std::ostream& out) const
+    {
+      out << "Solver: UMFPACK direct solver ";
+      out  << "\\\\ \n";
+    }
 
-  int iterations() const 
-  {
-    return 0;
-  }
-};
+    double averageCommTime() const 
+    {
+      return 0.0;
+    }
 
-/**
-   @}
-**/
-} // end namespace Dune 
-#endif
+    int iterations() const 
+    {
+      return 0;
+    }
+
+  private:
+    // note: the matrix is changed by this operator!
+    const OperatorType &op_;
+    typename DiscreteFunctionType::RangeFieldType epsilon_;
+    int maxIter_;
+    bool verbose_ ;
+  };
+
+}
+
+#endif // #ifndef DUNE_UMFPACKSOLVER_HH
