9134491a35b97ad8c17cde96c61756da6dac7bd7
Andreas Dedner <A.S.Dedner@warwick.ac.uk>
2009-09-23 19:36:30 +0000

    added some more of the grid/common classes
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@4815 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/gridpart/defaultindexsets.hh
+++ fem/gridpart/defaultindexsets.hh
@@ -10,7 +10,7 @@
 #include <dune/grid/alugrid/interfaces.hh>
 #include <dune/grid/common/grid.hh>
 #include <dune/grid/common/adaptcallback.hh> // for compatibility only
-#include "../defaultindexsets.hh"
+#include <dune/grid/alugrid/defaultindexsets.hh>
 
 /** @file
  @author Robert Kloefkorn
@@ -19,6 +19,136 @@
 */
 
 namespace Dune {
+
+//! Wraps the interface methods of indexsets and adds the addiotnal needed
+//! functions 
+template <class IndexSetImp> 
+class IndexSetWrapper : public DefaultEmptyIndexSet 
+{
+public:
+  //! The types of the iterator 
+  template<int cd>
+  struct Codim
+  {
+    template<PartitionIteratorType pitype>
+    struct Partition
+    {
+      typedef typename IndexSetImp::template Codim<cd>::
+        template Partition<pitype>::Iterator  Iterator;
+    };
+  };
+
+  //! store const reference to set 
+  IndexSetWrapper(const IndexSetImp & set, bool adaptive = false) 
+    : DefaultEmptyIndexSet(adaptive)
+    , set_(set) 
+  {}
+  
+  //! store const reference to set 
+  IndexSetWrapper(const IndexSetWrapper<IndexSetImp> & s) 
+    : DefaultEmptyIndexSet(s.adaptive_), set_(s.set_) {}
+ 
+  //! return size of set for codim  
+  int size ( GeometryType type ) const   
+  {
+    return set_.size(type);
+  }
+
+  //! return size of grid entities per level and codim 
+  int size ( int codim ) const   
+  {
+    return set_.size(codim);
+  }
+
+  //! return index of en 
+  template <class EntityType> 
+  int index (const EntityType & en) const
+  {
+    return set_.index(en); 
+  }
+
+  //! return sub index of given entities sub entity with codim and number 
+  template< int codim, class EntityType >
+  int DUNE_DEPRECATED subIndex ( const EntityType &entity, int num ) const
+  {
+    return set_.template subIndex< codim >( entity, num );
+  }
+
+  template< class Entity >
+  int subIndex ( const Entity &entity, int num, unsigned int codim ) const
+  {
+    return set_.subIndex( entity, num, codim );
+  }
+
+  //! wrap geomTypes method of set 
+  const std::vector< GeometryType > & geomTypes (int codim) const 
+  {
+    return set_.geomTypes(codim); 
+  }
+
+  //! returns true if this set provides an index for given entity
+  template<class EntityType>
+  bool contains (const EntityType& en) const
+  {
+    return set_.contains(en); 
+  }
+
+  //! return index 
+  template <int codim, class EntityType> 
+  int index (const EntityType & en, int num) const
+  {
+    enum { enCodim = EntityType::codimension };
+    return IndexWrapper<IndexSetImp,EntityType,enCodim,codim>::index(set_,en,num);
+  }
+
+  /** @brief Iterator to first entity of given codimension and partition type.
+   */
+  template<int cd, PartitionIteratorType pitype>
+  typename Codim<cd>::template Partition<pitype>::Iterator begin () const
+  { 
+    return set_.template begin<cd,pitype> ();
+  }
+
+  /** @brief Iterator to one past the last entity of given codim for partition type
+   */
+  template<int cd, PartitionIteratorType pitype>
+  typename Codim<cd>::template Partition<pitype>::Iterator end () const
+  { 
+    return set_.template end<cd,pitype> ();
+  }
+
+private: 
+  const IndexSetImp & set_; 
+};
+//! Wraps LevelIndexSet for use with LagrangeFunctionSpace 
+template <class GridType>
+class WrappedLevelIndexSet 
+: public IndexSetWrapper< typename GridType :: Traits:: LevelIndexSet > 
+{
+  // my type, to be revised 
+  enum { myType = 1 };
+
+  typedef typename GridType :: Traits :: LevelIndexSet LevelIndexSetType;
+  typedef WrappedLevelIndexSet < GridType >  ThisType;
+public:
+  
+  //! number of codimensions 
+  enum { ncodim = GridType::dimension + 1 };
+
+  //! Constructor getting grid and level for Index Set 
+  WrappedLevelIndexSet (const GridType & grid , const int level ) 
+    : IndexSetWrapper<  LevelIndexSetType > (grid.levelIndexSet(level)) {}
+ 
+  //! return type of index set (for input/output)
+  static int type() { return myType; }
+  //! returns reference to singleton 
+  static ThisType & instance (const GridType & grid)
+  { 
+    static ThisType set(grid,grid.maxLevel());
+    return set;
+  }
+
+};
 
 //! compile time chooser for hierarchic or leaf index set
 /** \deprecated */
