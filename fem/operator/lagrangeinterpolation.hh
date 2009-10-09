4c1e324fc833a765cbd50d8d9b0419237c0f28c7
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2009-10-09 08:52:39 +0000

    reduce number of files
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@4887 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/operator/lagrangeinterpolation.hh
+++ fem/operator/lagrangeinterpolation.hh
@@ -18,24 +18,24 @@ namespace Dune
     typedef DiscreteFunctionImp DiscreteFunctionType;
 
     //! type of discrete function space
-    typedef typename DiscreteFunctionType :: DiscreteFunctionSpaceType
+    typedef typename DiscreteFunctionType::DiscreteFunctionSpaceType
       DiscreteFunctionSpaceType;
     //! type of local functions
-    typedef typename DiscreteFunctionType :: LocalFunctionType
+    typedef typename DiscreteFunctionType::LocalFunctionType
       LocalFunctionType;
 
     //! type of grid partition
-    typedef typename DiscreteFunctionSpaceType :: GridPartType GridPartType;
+    typedef typename DiscreteFunctionSpaceType::GridPartType GridPartType;
     //! type of grid
-    typedef typename DiscreteFunctionSpaceType :: GridType GridType;
+    typedef typename DiscreteFunctionSpaceType::GridType GridType;
 
     //! type of Lagrange point set
-    typedef typename DiscreteFunctionSpaceType :: LagrangePointSetType
+    typedef typename DiscreteFunctionSpaceType::LagrangePointSetType
       LagrangePointSetType;
     //! type of vectors in function's domain
-    typedef typename DiscreteFunctionSpaceType :: DomainType DomainType;
+    typedef typename DiscreteFunctionSpaceType::DomainType DomainType;
     //! type of vectors in function's range
-    typedef typename DiscreteFunctionSpaceType :: RangeType RangeType;
+    typedef typename DiscreteFunctionSpaceType::RangeType RangeType;
 
   public:
     /** interpolate an analytical function into a Lagrange discrete function
@@ -50,12 +50,12 @@ namespace Dune
      *              interpolation
      */
     template< class FunctionType >
-    inline static void interpolateFunction ( const FunctionType &function,
-                                             DiscreteFunctionType &discreteFunction );
+    static void interpolateFunction ( const FunctionType &function,
+                                      DiscreteFunctionType &discreteFunction );
 
   private:
     template< class FunctionType, bool hasLocalFunction >
-    struct callInterpolateDiscreteFunction;
+    struct CallInterpolateDiscreteFunction;
 
   protected:
     /** interpolate a discrete function into a Lagrange discrete function
@@ -70,13 +70,92 @@ namespace Dune
      *              interpolation
      */
     template< class FunctionType >
-    inline static void interpolateDiscreteFunction
-      ( const FunctionType &function,
-        DiscreteFunctionType &discreteFunction );
+    static void
+    interpolateDiscreteFunction ( const FunctionType &function,
+                                  DiscreteFunctionType &discreteFunction );
   };
+
+
+
+  template< class DiscreteFunctionImp >    
+  template< class FunctionType >
+  inline void LagrangeInterpolation< DiscreteFunctionImp >
+    :: interpolateFunction ( const FunctionType &function,
+                             DiscreteFunctionType &discreteFunction )
+  {
+    const bool hasLocalFunction = Conversion< FunctionType, HasLocalFunction >::exists;
+    CallInterpolateDiscreteFunction< FunctionType, hasLocalFunction >::call( function, discreteFunction );
+  }
+
+
+
+  template< class DiscreteFunctionType >
+  template< class FunctionType >
+  struct LagrangeInterpolation< DiscreteFunctionType >
+    ::CallInterpolateDiscreteFunction< FunctionType, true >
+  {
+    static void call( const FunctionType &function,
+                      DiscreteFunctionType &discreteFunction )
+    {
+      interpolateDiscreteFunction( function, discreteFunction );
+    }
+  };
+
+  template< class DiscreteFunctionType >
+  template< class FunctionType >
+  struct LagrangeInterpolation< DiscreteFunctionType >
+    ::CallInterpolateDiscreteFunction< FunctionType, false >
+  {
+    static void call( const FunctionType &function,
+                      DiscreteFunctionType &discreteFunction )
+    {
+      typedef typename DiscreteFunctionType::DiscreteFunctionSpaceType
+        DiscreteFunctionSpaceType;
+      typedef typename DiscreteFunctionSpaceType::GridPartType GridPartType;
+      typedef DiscreteFunctionAdapter< FunctionType, GridPartType >
+        DiscreteFunctionAdapterType;
+
+      const DiscreteFunctionSpaceType &dfSpace = discreteFunction.space();
+      DiscreteFunctionAdapterType dfAdapter( "function", function, dfSpace.gridPart() );
+      interpolateDiscreteFunction( dfAdapter, discreteFunction );
+    }
+  };
+
+
+  
+  template< class DiscreteFunctionImp >
+  template< class FunctionType >
+  inline void LagrangeInterpolation< DiscreteFunctionImp >
+    ::interpolateDiscreteFunction ( const FunctionType &function,
+                                    DiscreteFunctionType &discreteFunction )
+  {
+    typedef typename DiscreteFunctionSpaceType::IteratorType IteratorType;
+    static const int dimRange = DiscreteFunctionSpaceType::dimRange;
+
+    typedef typename FunctionType::LocalFunctionType FunctionLocalFunctionType;
+
+    const DiscreteFunctionSpaceType &dfSpace = discreteFunction.space();
+
+    IteratorType endit = dfSpace.end();
+    for( IteratorType it = dfSpace.begin(); it != endit; ++it )
+    {
+      const LagrangePointSetType &lagrangePointSet
+        = dfSpace.lagrangePointSet( *it );
+
+      FunctionLocalFunctionType f_local = function.localFunction( *it );
+      LocalFunctionType df_local = discreteFunction.localFunction( *it );
+      
+      // assume point based local dofs 
+      const int numDofs = df_local.numDofs();
+      for( int i = 0; i < numDofs; ++i )
+      {
+        RangeType phi;
+        f_local.evaluate( lagrangePointSet[ i/dimRange ], phi );
+        df_local[ i ] = phi[ i % dimRange ];
+      }
+    }
+  }
 
 }
 
-#include "lagrangeinterpolation_inline.hh"
-
-#endif
+#endif // #ifndef DUNE_FEM_LAGRANGEINTERPOLATION_HH
