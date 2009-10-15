2466484054a43aa73c5c156d4d8f852493d7376a
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2009-10-15 17:48:11 +0000

    * don't use CompileTimeChecker * some code beautification
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@4901 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- fem/function/vectorfunction/vectorfunction.hh
+++ fem/function/vectorfunction/vectorfunction.hh
@@ -1,84 +1,28 @@
 #ifndef DUNE_FEM_VECTORFUNCTION_HH
 #define DUNE_FEM_VECTORFUNCTION_HH
 
+#include <dune/common/static_assert.hh>
 #include <dune/common/typetraits.hh>
 
 #include <dune/fem/storage/vector.hh>
 #include <dune/fem/storage/envelope.hh>
 #include <dune/fem/function/common/dofblock.hh>
-//#include <dune/fem/space/common/dofmanager.hh>
 #include <dune/fem/function/common/discretefunction.hh>
 #include <dune/fem/function/localfunction/standardlocalfunction.hh>
 
 namespace Dune
 {
 
-#if 0
-  namespace VectorDiscreteFunctionHelper
-  {
-    template< class DofVector, class Dof, unsigned int Size >
-    class DofBlockProxy
-    {
-      friend class Envelope< DofBlockProxy >;
-
-    public:
-      typedef DofVector DofVectorType;
-
-      typedef Dof DofType;
-      
-      enum { size = Size };
-      
-      typedef unsigned int size_type;
-
-      typedef std :: pair< DofVectorType*, size_type > KeyType;
-
-    protected:
-      DofVectorType &dofVector_;
-      const size_type first_;
-
-    protected:
-      inline DofBlockProxy ( const KeyType key )
-      : dofVector_( *(key.first) ),
-        first_( size * key.second )
-      {}
-
-      inline DofBlockProxy ( const DofBlockProxy &other )
-      : dofVector_( other.dofVector_ ),
-        first_( other.first_ )
-      {}
-
-    public:
-      inline DofBlockProxy &operator= ( const DofBlockProxy &other )
-      {
-        for( size_type i = 0; i < size; ++i )
-          (*this)[ i ] = other[ i ];
-        return *this;
-      }
-      
-      inline const DofType &operator[] ( size_type index ) const
-      {
-        return dofVector_[ first_ + index ];
-      }
-      
-      inline DofType &operator[] ( size_type index )
-      {
-        return dofVector_[ first_ + index ];
-      }
-     
-      inline size_type dim () const
-      {
-        return size;
-      }
-    };
-  }
-#endif
-
-
+  // Internal Forward Declarations
+  // -----------------------------
 
   template< class DiscreteFunctionSpace, class DofVector >
   class VectorDiscreteFunction;
 
 
+
+  // VectorDiscreteFunctionTraits
+  // ----------------------------
 
   template< class DiscreteFunctionSpace, class DofVector >
   struct VectorDiscreteFunctionTraits
@@ -113,7 +57,6 @@ namespace Dune
       JacobianRangeType;
 
     typedef typename DiscreteFunctionSpaceType :: MapperType MapperType;
-    //typedef typename DiscreteFunctionSpaceType :: GridType GridType;
 
     typedef typename DofVectorType :: FieldType DofType;
     typedef DofVectorType DofStorageType;
@@ -123,14 +66,6 @@ namespace Dune
 
     enum { blockSize = DiscreteFunctionSpaceType :: localBlockSize };
 
-#if 0
-    typedef VectorDiscreteFunctionHelper :: DofBlockProxy
-      < DofVectorType, DofType, blockSize >
-      DofBlockType;
-    typedef VectorDiscreteFunctionHelper :: DofBlockProxy
-      < DofVectorType, const DofType, blockSize >
-      ConstDofBlockType;
-#endif
     typedef DofBlockProxy< DiscreteFunctionType, DofType, blockSize >
       DofBlockType;
     typedef DofBlockProxy
@@ -142,73 +77,60 @@ namespace Dune
 
 
 
+  // VectorDiscreteFunction
+  // ----------------------
+
   template< class DiscreteFunctionSpace, class DofVector >
   class VectorDiscreteFunction
-  : public DiscreteFunctionDefault
-    < VectorDiscreteFunctionTraits< DiscreteFunctionSpace, DofVector > >
+  : public DiscreteFunctionDefault< VectorDiscreteFunctionTraits< DiscreteFunctionSpace, DofVector > >
   {
+    typedef VectorDiscreteFunction< DiscreteFunctionSpace, DofVector > ThisType;
+    typedef DiscreteFunctionDefault< VectorDiscreteFunctionTraits< DiscreteFunctionSpace, DofVector > > BaseType;
+
+    dune_static_assert( SupportsVectorInterface< DofVector >::v, "DofVector must support VectorInterface." );
+
   public:
     //! type of this class's traits
-    typedef VectorDiscreteFunctionTraits< DiscreteFunctionSpace, DofVector >
-      Traits;
+    typedef VectorDiscreteFunctionTraits< DiscreteFunctionSpace, DofVector > Traits;
 
     //! type of the associated discrete function space
-    typedef typename Traits :: DiscreteFunctionSpaceType
-      DiscreteFunctionSpaceType;
+    typedef typename Traits::DiscreteFunctionSpaceType DiscreteFunctionSpaceType;
     //! type of the DoF storage array
-    typedef typename Traits :: DofVectorType DofVectorType;
+    typedef typename Traits::DofVectorType DofVectorType;
+
+    typedef typename Traits::DiscreteFunctionType DiscreteFunctionType;
+
+    typedef typename Traits::LocalFunctionType LocalFunctionType;
+    typedef typename Traits::LocalFunctionFactoryType  LocalFunctionFactoryType;
+    typedef typename Traits::LocalFunctionStorageType LocalFunctionStorageType;
+
+    typedef typename Traits::DomainType DomainType;
+    typedef typename Traits::RangeType RangeType;
+
+    typedef typename Traits::DomainFieldType DomainFieldType;
+    typedef typename Traits::RangeFieldType RangeFieldType;
+
+    typedef typename Traits::JacobianRangeType JacobianRangeType;
+
+    typedef typename Traits::DofType DofType;
+    typedef typename Traits::DofStorageType DofStorageType;
+
+    typedef typename Traits::ConstDofIteratorType ConstDofIteratorType;
+    typedef typename Traits::DofIteratorType DofIteratorType;
+
+    typedef typename Traits::DofBlockType DofBlockType;
+    typedef typename Traits::ConstDofBlockType ConstDofBlockType;
+    typedef typename Traits::DofBlockPtrType DofBlockPtrType;
+    typedef typename Traits::ConstDofBlockPtrType ConstDofBlockPtrType;
 
   private:
-    typedef VectorDiscreteFunction
-      < DiscreteFunctionSpaceType, DofVectorType > ThisType;
-    typedef DiscreteFunctionDefault< Traits > BaseType;
-
-  public:
-    typedef typename Traits :: DiscreteFunctionType DiscreteFunctionType;
-
-    typedef typename Traits :: LocalFunctionType LocalFunctionType;
-    typedef typename Traits :: LocalFunctionFactoryType
-      LocalFunctionFactoryType;
-    typedef typename Traits :: LocalFunctionStorageType
-      LocalFunctionStorageType;
-
-    typedef typename Traits :: DomainType DomainType;
-    typedef typename Traits :: RangeType RangeType;
-
-    typedef typename Traits :: DomainFieldType DomainFieldType;
-    typedef typename Traits :: RangeFieldType RangeFieldType;
-
-    typedef typename Traits :: JacobianRangeType JacobianRangeType;
-
-    typedef typename Traits :: DofType DofType;
-    typedef typename Traits :: DofStorageType DofStorageType;
-
-    typedef typename Traits :: ConstDofIteratorType ConstDofIteratorType;
-    typedef typename Traits :: DofIteratorType DofIteratorType;
-
-    typedef typename Traits :: DofBlockType DofBlockType;
-    typedef typename Traits :: ConstDofBlockType ConstDofBlockType;
-    typedef typename Traits :: DofBlockPtrType DofBlockPtrType;
-    typedef typename Traits :: ConstDofBlockPtrType ConstDofBlockPtrType;
-
-  private:
-    typedef CheckVectorInterface< DofVectorType > CheckDofVectorType;
-
-    typedef CompileTimeChecker
-      < Conversion< RangeFieldType, DofType > :: sameType >
-      Check_RangeFieldType_and_DofType_are_Equal;
-
-  private:
-    const LocalFunctionFactoryType lfFactory_;
-
-    DofVectorType *const dofVector_;
-    const bool freeDofVector_;
+    dune_static_assert( (Conversion< RangeFieldType, DofType >::sameType), "RangeFieldType and DofType must equal." );
 
   public:
     //! Constructor
-    inline VectorDiscreteFunction ( const std :: string &name,
-                                    const DiscreteFunctionSpaceType &dfSpace,
-                                    DofVectorType &dofVector )
+    VectorDiscreteFunction ( const std::string &name,
+                             const DiscreteFunctionSpaceType &dfSpace,
+                             DofVectorType &dofVector )
     : BaseType( name, dfSpace, lfFactory_ ),
       lfFactory_( *this ),
       dofVector_( &dofVector ),
@@ -217,14 +139,14 @@ namespace Dune
       assert( dofVector_->size() == (unsigned int)dfSpace.size() );
     }
 
-    inline VectorDiscreteFunction ( const ThisType &other )
+    VectorDiscreteFunction ( const ThisType &other )
     : BaseType( other.name(), other.space(), lfFactory_ ),
       lfFactory_( *this ),
       dofVector_( new DofVectorType( other.dofVector() ) ),
       freeDofVector_( true )
     {}
 
-    inline ~VectorDiscreteFunction ()
+    ~VectorDiscreteFunction ()
     {
       if( freeDofVector_ )
         delete dofVector_;
@@ -235,106 +157,96 @@ namespace Dune
     ThisType &operator= ( const ThisType & );
 
   public:
-    inline ThisType &operator+= ( const ThisType &u )
+    ThisType &operator+= ( const ThisType &u )
     {
       dofVector() += u.dofVector();
       return *this;
     }
 
-    inline ThisType &operator-= ( const ThisType &u )
+    ThisType &operator-= ( const ThisType &u )
     {
       dofVector() -= u.dofVector();
       return *this;
     }
 
-    inline void addScaled ( const RangeFieldType &s,
-                            const ThisType &u )
+    void addScaled ( const RangeFieldType &s, const ThisType &u )
     {
       dofVector().addScaled( s, u.dofVector() );
     }
 
-    inline void addScaled ( const ThisType &u,
-                            const RangeFieldType &s )
+    void addScaled ( const ThisType &u, const RangeFieldType &s )
     {
       addScaled( s, u );
     }
 
-    inline void assign ( const ThisType &u )
+    void assign ( const ThisType &u )
     {
       dofVector().assign( u.dofVector() );
     }
 
-    inline void clear ()
+    void clear ()
     {
       dofVector().assign( 0 );
     }
 
-    inline ConstDofIteratorType dbegin () const
+    ConstDofIteratorType dbegin () const
     {
       return dofVector().begin();
     }
 
-    inline DofIteratorType dbegin ()
+    DofIteratorType dbegin ()
     {
       return dofVector().begin();
     }
 
-    inline ConstDofIteratorType dend () const
+    ConstDofIteratorType dend () const
     {
       return dofVector().end();
     }
 
-    inline DofIteratorType dend ()
+    DofIteratorType dend ()
     {
       return dofVector().end();
     }
 
-    inline ConstDofBlockPtrType block ( unsigned int index ) const
+    ConstDofBlockPtrType block ( unsigned int index ) const
     {
-#if 0
-      typename ConstDofBlockType :: KeyType key( &(dofVector()), index );
-      return ConstDofBlockPtrType( key );
-#endif
-      typename ConstDofBlockType :: KeyType key( this, index );
+      typename ConstDofBlockType::KeyType key( this, index );
       return ConstDofBlockPtrType( key );
     }
 
-    inline DofBlockPtrType block ( unsigned int index )
+    DofBlockPtrType block ( unsigned int index )
     {
-#if 0
-      typename DofBlockType :: KeyType key( &(dofVector()), index );
-      return DofBlockPtrType( key );
-#endif
-      typename DofBlockType :: KeyType key( this, index );
+      typename DofBlockType::KeyType key( this, index );
       return DofBlockPtrType( key );
     }
 
-    inline const RangeFieldType &dof ( unsigned int index ) const
+    const RangeFieldType &dof ( unsigned int index ) const
     {
       return dofVector()[ index ];
     }
 
-    inline RangeFieldType &dof ( unsigned int index )
+    RangeFieldType &dof ( unsigned int index )
     {
       return dofVector()[ index ];
     }
 
-    inline const RangeFieldType *leakPointer () const
+    const RangeFieldType *leakPointer () const
     {
       return dofVector().leakPointer();
     }
 
-    inline RangeFieldType *leakPointer ()
+    RangeFieldType *leakPointer ()
     {
       return dofVector().leakPointer();
     }
 
-    inline RangeFieldType scalarProductDofs ( const ThisType &u ) const
+    RangeFieldType scalarProductDofs ( const ThisType &u ) const
     {
       return dofVector() * u.dofVector();
     }
 
-    inline int size () const
+    int size () const
     {
       return dofVector().size();
     }
@@ -348,6 +260,12 @@ namespace Dune
     {
       return *dofVector_;
     }
+
+  private:
+    const LocalFunctionFactoryType lfFactory_;
+
+    DofVectorType *const dofVector_;
+    const bool freeDofVector_;
   };
 
 
