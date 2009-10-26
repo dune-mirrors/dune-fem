52355cd9f8c3451866a5ac755e169eedc865a58f
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2009-10-26 16:13:21 +0000

    add a pk2dlagrangespace (this one works with twisted grids)
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@4972 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

---
+++ dune/fem/space/pqlagrangespace/pk2dlagrangespace.hh
@@ -0,0 +1,276 @@
+#ifndef  DUNE_FEM_PK2DLAGRANGESPACE_HH
+#define  DUNE_FEM_PK2DLAGRANGESPACE_HH
+
+#if HAVE_DUNE_LOCALFUNCTIONS
+
+#include <dune/common/misc.hh>
+#include <dune/grid/common/grid.hh>
+
+#include <dune/fem/space/common/dofmanager.hh>
+#include <dune/fem/space/common/discretefunctionspace.hh>
+#include <dune/fem/space/common/genericdofmapper.hh>
+#include <dune/fem/space/basefunctions/genericbasefunctionsets.hh>
+#include <dune/fem/space/basefunctions/basefunctionproxy.hh>
+#include <dune/fem/space/lagrangespace/lagrangedatahandle.hh>
+
+#include  <dune/finiteelements/pk2d.hh>
+
+namespace Dune
+{
+
+  template< class FunctionSpace, class GridPart, int polOrder = 1 >
+  class Pk2DLagrangeSpace;
+
+
+
+  // Pk2DLocalCoefficientsMap
+  // ------------------------
+
+  template< class IndexSet, class LocalFiniteElement >
+  struct Pk2DLocalCoefficientsMap
+  {
+    typedef typename LocalFiniteElement::Traits::LocalCoefficientsType
+      LocalCoefficientsType;
+
+    typedef GenericBaseFunctionSet< typename LocalFiniteElement::Traits::LocalBasisType >
+      BaseFunctionSetType;
+
+    Pk2DLocalCoefficientsMap ( const IndexSet &indexSet )
+    : indexSet_( indexSet )
+    {
+      for( int i = 0; i < 8; ++i )
+      {
+        fem_[ i ] = LocalFiniteElement( i );
+        baseFunctionSet_[ i ] = new BaseFunctionSetType( fem_[ i ].localBasis(), fem_[ i ].type() );
+      }
+    }
+
+    template< class Entity >
+    unsigned int operator () ( const Entity &entity ) const
+    {
+      assert( entity.type().isSimplex() );
+      const unsigned int n0 = indexSet_.subIndex( entity, 0, 2 );
+      const unsigned int n1 = indexSet_.subIndex( entity, 1, 2 );
+      const unsigned int n2 = indexSet_.subIndex( entity, 2, 2 );
+      return int( n0 > n1 ) + 2*int( n0 > n2 ) + 4*int( n1 > n2 );
+    }
+
+    template< class Entity >
+    const BaseFunctionSetType &baseFunctionSet ( const Entity &entity ) const
+    {
+      return *baseFunctionSet_[ (*this)( entity ) ];
+    }
+
+    template< class Topology >
+    unsigned int size () const
+    {
+      return GenericGeometry::IsSimplex< Topology >::value ? 8 : 0;
+    }
+
+    template< class Topology >
+    const LocalCoefficientsType &localCoefficients ( const unsigned int i ) const
+    {
+      assert( i < size< Topology >() );
+      return fem_[ i ].localCoefficients();
+    }
+
+  private:
+    const IndexSet &indexSet_;
+    LocalFiniteElement fem_[ 8 ];
+    const BaseFunctionSetType *baseFunctionSet_[ 8 ];
+  };
+
+
+
+  // Pk2DLagrangeSpaceTraits
+  // -----------------------
+
+  template< class FunctionSpace, class GridPart, int polOrder >
+  struct Pk2DLagrangeSpaceTraits
+  {
+    typedef FunctionSpace FunctionSpaceType;
+    typedef GridPart GridPartType;
+
+    typedef typename FunctionSpaceType::DomainFieldType DomainFieldType;
+    typedef typename FunctionSpaceType::DomainType DomainType;
+    typedef typename FunctionSpaceType::RangeFieldType RangeFieldType;
+    typedef typename FunctionSpaceType::RangeType RangeType;
+    typedef typename FunctionSpaceType::JacobianRangeType JacobianRangeType;
+    typedef typename FunctionSpaceType::ScalarFunctionSpaceType ScalarFunctionSpaceType;
+
+    static const int dimRange  = FunctionSpaceType::dimRange;
+    static const int dimDomain = FunctionSpaceType::dimDomain;
+    dune_static_assert( (dimRange == 1), "Pk2DLagrangeSpace expects dimRange == 1" );
+    dune_static_assert( (dimDomain == 2), "Pk2DLagrangeSpace expects dimDomain == 2" );
+    
+    typedef typename GridPartType::GridType GridType;
+    typedef typename GridPartType::IndexSetType IndexSetType;
+    typedef typename GridPartType::template Codim< 0 >::IteratorType IteratorType;
+
+    static const int polynomialOrder = polOrder;
+
+    typedef Pk2DLocalFiniteElement< DomainFieldType, RangeFieldType, polynomialOrder > LocalFiniteElementType;
+
+    typedef Pk2DLocalCoefficientsMap< IndexSetType, LocalFiniteElementType > LocalCoefficientsMapType;
+    typedef GenericDofMapper< GridPartType, LocalCoefficientsMapType > MapperType;
+    typedef MapperType BlockMapperType;
+
+    typedef GenericBaseFunctionSet< typename LocalFiniteElementType::Traits::LocalBasisType > BaseFunctionSetImp;
+    typedef SimpleBaseFunctionProxy< BaseFunctionSetImp > BaseFunctionSetType;
+
+    static const int localBlockSize = dimRange;
+
+    template< class DiscreteFunction, class Operation = DFCommunicationOperation::Add >
+    struct CommDataHandle
+    {
+      typedef LagrangeCommunicationHandler< DiscreteFunction, Operation > Type;
+      typedef Operation OperationType;
+    };
+
+    typedef Pk2DLagrangeSpace< FunctionSpace, GridPart, polOrder > DiscreteFunctionSpaceType;
+  };
+
+
+
+  // Pk2DLagrangeSpace
+  // -----------------
+
+  template< class FunctionSpace, class GridPart, int polOrder >
+  class Pk2DLagrangeSpace
+  : public DiscreteFunctionSpaceDefault< Pk2DLagrangeSpaceTraits< FunctionSpace, GridPart, polOrder > >,
+    public GenericDiscreteFunctionSpace
+  {
+    typedef Pk2DLagrangeSpace< FunctionSpace, GridPart, polOrder > ThisType;
+    typedef DiscreteFunctionSpaceDefault< Pk2DLagrangeSpaceTraits< FunctionSpace, GridPart, polOrder > > BaseType;
+
+  public:
+    typedef typename BaseType::Traits Traits;
+
+    typedef typename Traits :: GridPartType                          GridPartType;
+    typedef typename Traits :: GridType                              GridType;
+    typedef typename Traits :: IndexSetType                          IndexSetType;
+    typedef typename Traits :: IteratorType                          IteratorType;
+    //! dimension of the grid (not the world)
+    enum { dimension = GridType :: dimension };
+
+    typedef typename Traits :: FunctionSpaceType                     FunctionSpaceType;
+    //! field type for function space's domain
+    typedef typename Traits :: DomainFieldType                       DomainFieldType;
+    //! type for function space's domain
+    typedef typename Traits :: DomainType                            DomainType;
+    //! field type for function space's range
+    typedef typename Traits :: RangeFieldType                        RangeFieldType;
+    //! type for function space's range
+    typedef typename Traits :: RangeType                             RangeType;
+    //! dimension of function space's range
+    enum { dimRange = FunctionSpaceType :: dimRange };
+    //! type of scalar function space
+    typedef typename Traits :: ScalarFunctionSpaceType               ScalarFunctionSpaceType;
+   
+    //! maximum polynomial order of functions in this space
+    enum { polynomialOrder = Traits :: polynomialOrder };
+    
+    //! type of the base function set(s)
+    typedef typename Traits :: BaseFunctionSetImp                    BaseFunctionSetImp;
+    
+    typedef typename Traits :: BaseFunctionSetType                   BaseFunctionSetType;
+
+    typedef typename Traits::LocalCoefficientsMapType LocalCoefficientsMapType;
+
+    //! mapper used to implement mapToGlobal
+    typedef typename Traits :: MapperType                            MapperType;
+
+    //! local finite element type
+    typedef typename Traits :: LocalFiniteElementType                LocalFiniteElementType;
+
+    //! type for DoF
+    typedef RangeFieldType DofType;
+
+    //! dimension of a value
+    enum { dimVal = 1 };
+
+    //! type of DoF manager
+    typedef DofManager< GridType > DofManagerType;
+
+  public:
+    //! type of identifier for this discrete function space
+    typedef int IdentifierType;
+    //! identifier of this discrete function space
+    static const IdentifierType id = 664;
+
+  public:
+    using BaseType::gridPart;
+
+  public:
+    /** \brief constructor
+     *
+     *  \param[in]  gridPart  grid part for the Lagrange space
+     */
+    explicit Pk2DLagrangeSpace ( GridPartType &gridPart )
+    : BaseType( gridPart ),
+      localCoefficientsMap_( gridPart.indexSet() ),
+      mapper_( gridPart, localCoefficientsMap_ )
+    {}
+
+  private:
+    // forbid the copy constructor
+    Pk2DLagrangeSpace ( const ThisType & );
+
+  public:
+    /** \copydoc Dune::DiscreteFunctionSpaceInterface::contains */
+    bool contains ( const int codim ) const
+    {
+      return mapper().contains( codim );
+    }
+
+    /** \copydoc Dune::DiscreteFunctionSpaceInterface::continuous */
+    bool continuous () const
+    {
+      return true;
+    }
+
+    /** \brief get the type of this discrete function space 
+        \return DFSpaceIdentifier
+    **/
+    DFSpaceIdentifier type () const
+    {
+      return GenericSpace_id;
+    }
+
+    /** \copydoc Dune::DiscreteFunctionSpaceInterface::order */
+    int order () const
+    {
+      return polynomialOrder;
+    }
+
+    template< class Entity >
+    BaseFunctionSetType baseFunctionSet ( const Entity &entity ) const
+    {
+      return BaseFunctionSetType( &localCoefficientsMap_.baseFunctionSet( entity ) );
+    }
+
+    int dimensionOfValue () const
+    {
+      return dimVal;
+    }
+
+    MapperType &mapper () const
+    {
+      return mapper_;
+    }
+
+    MapperType &blockMapper () const
+    {
+      return mapper_;
+    }
+
+  private:
+    LocalCoefficientsMapType localCoefficientsMap_;
+    mutable MapperType mapper_;
+  };
+
+}
+
+#endif // #if HAVE_DUNE_LOCALFUNCTIONS
+
+#endif // #ifndef DUNE_FEM_PK2DLAGRANGESPACE_HH
---
+++ dune/fem/space/pqlagrangespace/test/test-pk2d.cc
@@ -0,0 +1,172 @@
+// include configurations options
+#include <config.h>
+
+
+// Includes from the IOStream Library
+// ----------------------------------
+
+#include <iostream>
+#include <sstream>
+
+
+// Includes from Core Modules
+// --------------------------
+
+#include <dune/grid/albertagrid.hh>
+#include <dune/grid/albertagrid/dgfparser.hh>
+
+typedef Dune::AlbertaGrid< 2 > GridType;
+
+
+
+// Includes from DUNE-FEM
+// ----------------------
+
+// include support for versioning
+#include <dune/fem/version.hh>
+
+// include parameter handling
+#include <dune/fem/io/parameter.hh>
+
+// include basic grid parts
+#include <dune/fem/gridpart/gridpart.hh>
+
+// include Lagrange discrete function space
+#include <dune/fem/space/pqlagrangespace/pk2dlagrangespace.hh>
+
+#include <dune/fem/function/adaptivefunction.hh>
+
+#include <dune/fem/solver/inverseoperators.hh>
+
+#include <dune/fem/misc/l2error.hh>
+
+#include <dune/fem/io/file/vtkio.hh>
+
+#include "massoperator.hh"
+
+
+
+// Global Type Definitions
+// -----------------------
+
+#if defined POLORDER
+const int polOrder = POLORDER;
+#else
+const int polOrder = 1;
+#endif
+
+// select grid part to use
+typedef Dune::LeafGridPart< GridType > GridPartType;
+
+typedef Dune::FunctionSpace< double, double, GridType::dimension, 1 > FunctionSpaceType;
+
+typedef Dune::Pk2DLagrangeSpace< FunctionSpaceType, GridPartType, polOrder >
+  DiscreteFunctionSpaceType;
+
+typedef Dune::AdaptiveDiscreteFunction< DiscreteFunctionSpaceType > DiscreteFunctionType;
+
+
+
+template< class FunctionSpace >
+struct Function
+{
+  typedef typename FunctionSpace::DomainType DomainType;
+  typedef typename FunctionSpace::RangeType RangeType;
+
+  void evaluate ( const DomainType &x, RangeType &value ) const
+  {
+    value = 1.;
+    for( int i = 0; i < FunctionSpace::dimDomain; ++i )
+      value *= sin( M_PI * x[ i ] );
+  }
+
+  void evaluate ( const DomainType &x, const double &t, RangeType &value ) const
+  {
+    evaluate( x, value );
+  }
+};
+
+
+
+// Algorithm
+// ---------
+
+template< class Function >
+double algorithm ( const Function &function, GridType &grid, int repeat )
+{
+  typedef Dune::MassOperator< DiscreteFunctionType > MassOperatorType;
+  typedef Dune::CGInverseOp< DiscreteFunctionType, MassOperatorType > InverseOperator;
+
+  GridPartType gridPart( grid );
+  DiscreteFunctionSpaceType dfSpace( gridPart );
+
+  MassOperatorType massOperator( dfSpace );
+  DiscreteFunctionType functional( "functional", dfSpace );
+  massOperator( function, functional );
+
+  DiscreteFunctionType solution( "solution", dfSpace );
+  solution.clear();
+  InverseOperator inverseOperator( massOperator, 1e-10, 1e-10, 50000, 0 );
+  inverseOperator( functional, solution );
+
+  if( Dune::Parameter::getValue< bool >( "l2projection.vtkout", false ) )
+  {
+    Dune::SubsamplingVTKIO< GridPartType > vtkWriter( gridPart, 4 );
+    vtkWriter.addVertexData( solution );
+    std::ostringstream name;
+    name << "l2projection-" << repeat;
+    std::cerr << "Writing solution to " << name.str() << "..." << std::endl;
+    vtkWriter.write( name.str() );
+  }
+  
+  Dune::L2Error< DiscreteFunctionType > l2error;
+  return l2error.norm( function, solution );
+}
+
+
+
+// Main Program
+// ------------
+
+int main ( int argc, char **argv )
+try
+{
+  // initialize MPI manager
+  Dune::MPIManager::initialize( argc, argv );
+  // add command line parameters to global parameter table
+  Dune::Parameter::append( argc, argv );
+
+  // get name of grid file
+  std::string gridfile;
+  Dune::Parameter::get( "l2projection.gridfile", gridfile );
+
+  const int level = Dune::Parameter::getValue< int >( "l2projection.level" );
+  const int repeats = Dune::Parameter::getValue< int >( "l2projection.repeats", 0 );
+
+  // use DGF parser to create grid
+  Dune::GridPtr< GridType > gridptr( gridfile );
+  // distribute the grid to all processes
+  gridptr->loadBalance();
+  gridptr->globalRefine( level );
+
+  Function< FunctionSpaceType > function;
+
+  double error = algorithm( function, *gridptr, 0 );
+  std::cout << error << std::endl;
+  for( int i = 0; i < repeats; ++i )
+  {
+    const double prevError = error;
+    gridptr->globalRefine( Dune::DGFGridInfo< GridType >::refineStepsForHalf() );
+    error = algorithm( function, *gridptr, i+1 );
+    const double eoc = log( prevError / error ) / M_LN2;
+    std::cout << error << "  " << eoc << std::endl;
+  }
+
+  return 0;
+}
+catch( Dune::Exception &exception )
+{
+  // display the exception message on the console
+  std::cerr << exception.what() << std::endl;
+  return 1;
+}
