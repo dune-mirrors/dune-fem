9dd670c82a8ff7c7ff63c1396273037d5a1fae21
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2010-03-09 12:00:09 +0000

    some testing
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@5210 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- dune/fem/storage/ssematrixdynamic.hh
+++ dune/fem/storage/ssematrixdynamic.hh
@@ -61,6 +61,7 @@ public:
 template < class K > 
 class SSEMatrix 
 {
+  SSEMatrix(const SSEMatrix& other) ;
 public:
   //typedef K  Field __attribute__((aligned (16)));
   typedef K  Field ;
@@ -69,43 +70,120 @@ public:
 
   typedef MatrixType SSEType;
 protected:  
+  typedef void multiplySSE_t(const size_t I, const size_t J, 
+                             K** const &A, 
+                             K*  const &X, 
+                             K*  &Y);
+
+  template <int n, int m> 
+  struct Multiply
+  {
+    static multiplySSE_t* mv(const int rows, const int cols) 
+    {
+      if( n == rows && m == cols )
+      {
+        return multSSE;
+      }
+      else if( n == rows ) 
+      {
+        return Multiply< n, m - 1>::mv( rows, cols );
+      }
+      else 
+      {
+        return Multiply<n-1, m> ::mv( rows, cols );
+      }
+    }
+
+    static multiplySSE_t* umtv(const int rows, const int cols) 
+    {
+      if( n == rows && m == cols )
+      {
+        return multUmtvSSE;
+      }
+      else if( n == rows ) 
+      {
+        return Multiply< n, m - 1>::umtv( rows, cols );
+      }
+      else 
+      {
+        return Multiply<n-1, m> ::umtv( rows, cols );
+      }
+    }
+    static inline 
+    void multSSE(const size_t I, const size_t J, 
+                   double** const &A, 
+                   double*  const &X, 
+                   double*  &Y)
+    {
+      assert( n == I );
+      assert( m == J );
+      SSEMatrix< K > :: multiplySSE(n, m, A, X , Y);
+    }
+
+    static inline 
+    void multUmtvSSE(const size_t I, const size_t J, 
+                     double** const &A, 
+                     double*  const &X, 
+                     double*  &Y)
+    {
+      assert( n == I );
+      assert( m == J );
+      SSEMatrix< K > :: umtv(n, m, A, X , Y);
+    }
+  };
+
+  template <int m> 
+  struct Multiply<0, m> 
+  {
+    static multiplySSE_t* mv(const int rows, const int cols) 
+    {
+      return 0; 
+    }
+    static multiplySSE_t* umtv(const int rows, const int cols) 
+    {
+      return 0; 
+    }
+  };
+
+  template <int n> 
+  struct Multiply<n, 0> 
+  {
+    static multiplySSE_t* mv(const int rows, const int cols) 
+    {
+      return 0; 
+    }
+    static multiplySSE_t* umtv(const int rows, const int cols) 
+    {
+      return 0; 
+    }
+  };
+
   MatrixType sseMat_;
-  mutable SSEVector< K >& xTmp_;
-  mutable SSEVector< K >& yTmp_;
+  mutable SSEVector< K > xTmp_;
+  mutable SSEVector< K > yTmp_;
 
   mutable MatrixType quadMat_;
 
+  multiplySSE_t* mvSSE_;
+  multiplySSE_t* umtvSSE_;
+
   const size_t rows_;
   const size_t cols_;
-
-  mutable size_t qSize_;
-
-  template < class QuadratureType, bool caching >
-  struct ApplyCaching
-  {
-    static int apply(const QuadratureType& quad, const int i )
-    {
-      return i;
-    }
-  };
-
-  template < class QuadratureType >
-  struct ApplyCaching< QuadratureType, true >
-  {
-    static int apply(const QuadratureType& quad, const int i )
-    {
-      return quad.cachingPoint( i );
-    }
-  };
+  const size_t realRows_;
 
 public:
-
-  SSEMatrix(const size_t n, const size_t m ) 
-    : xTmp_(*(new SSEVector< K >( m ))) 
-    , yTmp_(*(new SSEVector< K >( n )))
-    , quadMat_( 0 ), rows_( n ), cols_ ( m ), qSize_( 0 )
+  SSEMatrix(const size_t n, const size_t m ,
+            const size_t realRows ) 
+    : xTmp_(  m ) 
+    , yTmp_(  n )
+    , quadMat_( 0 )
+    , mvSSE_( Multiply<100, 100>::mv( realRows, m ) )
+    , umtvSSE_( Multiply<100, 100>::umtv( realRows, m ) )
+    , rows_( n ), cols_ ( m ), realRows_( realRows )
   {
-    sseMat_ = new RowType [ n ];
+    assert( mvSSE_ );
+    sseMat_  = new RowType [ n ];
+    quadMat_ = new RowType [ n ];
     assert( sseMat_ );
     const size_t SIZE_J = (2*(( m + 1 ) / 2));
     for( size_t i = 0; i < rows_; ++i) 
@@ -115,44 +193,8 @@ public:
     }
   } 
 
-  SSEMatrix(const size_t n, const size_t m,
-            MatrixType sseMat, 
-            SSEVector< K >& xTmp, 
-            SSEVector< K >& yTmp)
-    : sseMat_( sseMat ) 
-    , xTmp_( xTmp ), yTmp_( yTmp )
-    , rows_( n ), cols_ ( m ), quadMat_( 0 ), qSize_ ( 0 )
-  {
-  }
-
-  SSEMatrix(const SSEMatrix& other) 
-    : sseMat_( other.sseMat_ )
-    , xTmp_( other.xTmp_ )
-    , yTmp_( other.yTmp_ )
-    , quadMat_( 0 )
-    , rows_( other.rows_ )
-    , cols_( other.cols_ )
-    , qSize_( 0 )
-  {}
-
+  // dummy method for fillRangeCache
   void resize( const size_t ) {}
-
-  template <class quad_t>
-  const SSEMatrix<K> getQuadMat( const quad_t& quad ) const 
-  {
-    if( ! quadMat_ ) 
-    { 
-      qSize_ = quad.nop(); 
-      quadMat_ = new RowType [ qSize_ ];
-    }
-
-    assert( quadMat_ ); 
-    for( size_t i = 0; i < qSize_; ++i) 
-    {
-      quadMat_[ i ]  = sseMat_[ quad.cachingPoint( i ) ];
-    }
-    return SSEMatrix<K>(qSize_, cols(), quadMat_, xTmp_, yTmp_);
-  }
 
   ~SSEMatrix() 
   {
@@ -164,9 +206,6 @@ public:
       }
       delete [] sseMat_;
       delete [] quadMat_;
-
-      delete & xTmp_; 
-      delete & yTmp_;
     }
     sseMat_ = 0;
     quadMat_ = 0;
@@ -187,32 +226,59 @@ public:
   size_t rows () const { return rows_; }
   size_t cols () const { return cols_; }
 
-#if 1
   template <class X, class Y> 
   void mv(const X& x, Y& y, const size_t offset ) const 
+  {
+    mv( sseMat_, rows(), x, y, offset );
+  }
+
+  template <class X, class Y, class quad_t > 
+  void mv(const X& x, Y& y, 
+          const quad_t& quad,
+          const size_t offset ) const 
+  {
+    assert( realRows_ == quad.nop() );
+    for( size_t i = 0; i < realRows_; ++i)
+    {
+      quadMat_[ i ] = sseMat_[ quad.cachingPoint( i ) ];
+    }
+    mv( quadMat_, realRows_, x, y, offset );
+  }
+
+#if 1
+  template <class X, class Y> 
+  void 
+  mv(const MatrixType& mat, 
+     const size_t realRows,
+     const X& x, Y& y, 
+     const size_t offset ) const 
   {
     for(size_t r = 0; r < offset ; ++r )
     {
       for(size_t c = 0; c < cols(); ++c ) xTmp_[ c ] = x[ c * offset + r ];
-      multiplySSE( rows(), cols(), raw(), xTmp_.raw(), yTmp_.raw() );
-      for(size_t i = 0; i < rows(); ++i ) y[ i ][ r ] = yTmp_[ i ];
+      //multiplySSE( realRows, cols(), mat, xTmp_.raw(), yTmp_.raw() );
+      mvSSE_( realRows, cols(), mat, xTmp_.raw(), yTmp_.raw() );
+      for(size_t i = 0; i < realRows; ++i ) y[ i ][ r ] = yTmp_[ i ];
     } 
   }
-
 #else
-  template <class X, class Y> 
-  void mv(const X& x, Y& y, const size_t offset ) const 
+  template <int n, int m, class X, class Y> 
+  void 
+  mv(const MatrixType& mat, 
+     const size_t realRows,
+     const X& x, Y& y, 
+     const size_t offset ) const 
   {
-    for (size_t i = 0; i < rows(); ++i)
+    for (size_t i = 0; i < realRows; ++i)
     {
-      const RowType& matRow = sseMat_[ i ];
+      const RowType& matRow = mat[ i ];
 
       for(size_t r = 0; r < offset; ++r ) 
       {
         y[ i ][ r ] = 0;
       }
 
-      for (size_t j = 0; j < cols(); ++j)
+      for (size_t j = 0; j < cols_; ++j)
       {
         for(size_t r = 0; r < offset; ++r ) 
         {
@@ -223,21 +289,76 @@ public:
   }
 #endif
 
+  template <class X, class Y, class quad_t > 
+  void umtv(const X& x, Y& y, const quad_t& quad, 
+            const size_t offset ) const 
+  {
+    const size_t quadSize = quad.nop();
+    for( size_t i = 0; i < quadSize; ++i) 
+    {
+      quadMat_[ i ] = sseMat_[ quad.cachingPoint( i ) ];
+    }
+    umtv( quadMat_, quadSize, x, y, offset );
+  }
+
   template <class X, class Y> 
   void umtv(const X& x, Y& y, const size_t offset ) const 
   {
-    for( size_t row = 0; row < rows() ; ++row )
+    umtv( sseMat_, rows(), x, y, offset );
+  }
+
+#if 1
+  template <class X, class Y> 
+  void umtv(const MatrixType& mat, const size_t realRows,
+            const X& x, Y& y, const size_t offset ) const 
+  {
+    for(size_t r = 0; r < offset ; ++r )
     {
-      for( size_t col = 0; col < cols(); ++col ) 
+      for(size_t i = 0; i < realRows; ++i ) yTmp_[ i ] = x[ i ][ r ];
+      //multiplySSE( realRows, cols(), mat, xTmp_.raw(), yTmp_.raw() );
+      umtvSSE_( realRows, cols(), mat, yTmp_.raw(), xTmp_.raw() );
+      for(size_t c = 0; c < cols(); ++c ) y[ c * offset + r ] += xTmp_[ c ];
+    } 
+  }
+
+  static inline 
+  void umtv(const size_t rows,
+            const size_t cols,
+            double** const &A, 
+            double*  const &X, 
+            double*  &Y)
+  {
+    for( size_t j = 0; j < cols; ++j ) 
+    {
+      Y[ j ] = 0;
+    }
+    for( size_t i = 0; i < rows; ++i )
+    {
+      for( size_t j = 0; j < cols; ++j ) 
       {
-        const Field& matvalue = sseMat_[ row ][ col ];
-        for( int r = 0; r < offset ; ++r ) 
+        Y[ j ] += A[ i ][ j ] * X[ i ];
+      }
+    }
+  }
+#else 
+  template <class X, class Y> 
+  void umtv(K** const & mat, const size_t realRows,
+            const X& x, Y& y, const size_t offset ) const 
+  {
+    for( size_t row = 0; row < realRows ; ++row )
+    {
+      for( size_t col = 0; col < cols_; ++col ) 
+      {
+        const Field& matvalue = mat[ row ][ col ];
+        size_t colR = col * offset;
+        for( int r = 0; r < offset ; ++r, ++ colR ) 
         {
-          y[ col * offset + r ] += matvalue * x[ row ][ r ];
+          y[ colR ] += matvalue * x[ row ][ r ];
         }
       }
     }
   }
+#endif
 
   template <class X, class Y> 
   void mv(const X& x, Y& y) const 
@@ -256,13 +377,64 @@ public:
 
   size_t size() const { return rows() * cols(); }
 
-protected:
-  template<class RowVector, class ColVector>  
-  inline 
+  static inline 
+  void multiplySSE(const size_t I, const size_t J, 
+                   double** const &A, 
+                   double*  const &X, 
+                   double*  &Y)
+  {
+#if 1
+    for(size_t i = 0; i < I; ++i ) 
+    {
+      Y[ i ] = 0; 
+      for( size_t j = 0; j < J; ++j ) 
+      {
+        Y[ i ] += A[ i ][ j ] * X[ j ]; 
+      }
+    }
+#else 
+    const size_t N = 3;
+    const size_t FI = ((N)*(( I )/(N)));  
+    const size_t FJ = (2*( J )/2);
+
+    for (size_t i = 0; i < FI; i += N) 
+    {
+      __m128d tmp[N];
+      for (size_t n = 0; n < N; ++n)
+        tmp[n] = _mm_set1_pd(0);
+
+      for (size_t j = 0; j < FJ; j += 2)
+        for (size_t n = 0; n < N; ++n)
+          tmp[n] = _mm_add_pd(tmp[n], _mm_mul_pd(_mm_load_pd(A[i+n]+j), _mm_load_pd(X+j)));
+
+      for (size_t n = 0; n < N; ++n)
+        _mm_store_sd(Y+i+n, _mm_add_sd(tmp[n], _mm_shuffle_pd(tmp[n], tmp[n], 1)));
+
+      for (size_t n = 0; n < N; ++n)
+        for (size_t j = FJ; j < J; ++j)
+          Y[i+n] += A[i+n][j] * X[j];
+    }
+
+    for (size_t i = FI; i < I; ++i) 
+    {
+      __m128d tmp = _mm_set1_pd(0);
+
+      for (size_t j = 0; j < FJ; j += 2)
+        tmp = _mm_add_pd(tmp, _mm_mul_pd(_mm_load_pd(A[i]+j), _mm_load_pd(X+j)));
+
+      _mm_store_sd(Y+i, _mm_add_sd(tmp, _mm_shuffle_pd(tmp, tmp, 1)));
+
+      for (size_t j = FJ; j < J; ++j)
+        Y[i] += A[i][j] * X[j];
+    }
+#endif
+  }
+
+  static inline 
   void multiplySSE(const size_t I, const size_t J, 
                    float** const &A, 
-                   const RowVector& X, 
-                   ColVector& Y)
+                   float* const &X, 
+                   float* Y)
   {
     const size_t N = 3;
 
@@ -299,49 +471,6 @@ protected:
       _mm_store_ss(Y+i, _mm_add_ss(_mm_add_ss(_mm_add_ss(tmp, _mm_shuffle_ps(tmp, tmp, 1)), _mm_shuffle_ps(tmp, tmp, 2)), _mm_shuffle_ps(tmp, tmp, 3)));
 
       for (size_t j = FJ; j < J; j++)
-        Y[i] += A[i][j] * X[j];
-    }
-  }
-
-  template<class RowVector, class ColVector>  
-  inline 
-  void multiplySSE(const size_t I, const size_t J, 
-                   double** const &A, 
-                   const RowVector&X, 
-                   ColVector& Y)
-  {
-    const size_t N = 3;
-    const size_t FI = ((N)*(( I )/(N)));  
-    const size_t FJ = (2*( J )/2);
-
-    for (size_t i = 0; i < FI; i += N) 
-    {
-      __m128d tmp[N];
-      for (size_t n = 0; n < N; ++n)
-        tmp[n] = _mm_set1_pd(0);
-
-      for (size_t j = 0; j < FJ; j += 2)
-        for (size_t n = 0; n < N; ++n)
-          tmp[n] = _mm_add_pd(tmp[n], _mm_mul_pd(_mm_load_pd(A[i+n]+j), _mm_load_pd(X+j)));
-
-      for (size_t n = 0; n < N; ++n)
-        _mm_store_sd(Y+i+n, _mm_add_sd(tmp[n], _mm_shuffle_pd(tmp[n], tmp[n], 1)));
-
-      for (size_t n = 0; n < N; ++n)
-        for (size_t j = FJ; j < J; ++j)
-          Y[i+n] += A[i+n][j] * X[j];
-    }
-
-    for (size_t i = FI; i < I; ++i) 
-    {
-      __m128d tmp = _mm_set1_pd(0);
-
-      for (size_t j = 0; j < FJ; j += 2)
-        tmp = _mm_add_pd(tmp, _mm_mul_pd(_mm_load_pd(A[i]+j), _mm_load_pd(X+j)));
-
-      _mm_store_sd(Y+i, _mm_add_sd(tmp, _mm_shuffle_pd(tmp, tmp, 1)));
-
-      for (size_t j = FJ; j < J; ++j)
         Y[i] += A[i][j] * X[j];
     }
   }
