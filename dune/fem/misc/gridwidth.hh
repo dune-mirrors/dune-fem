fdc2bf5376c1a2c7f846ca49e9512aeb7c2ce186
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2010-03-24 15:01:49 +0000

    cleanup of gridwidth calculator.
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@5226 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- dune/fem/misc/gridwidth.hh
+++ dune/fem/misc/gridwidth.hh
@@ -18,15 +18,73 @@ template <class GridPartImp>
 class GridWidthProvider;
 
 //! utility functions for calculating the maximum grid width 
-struct GridWidth 
+class GridWidth 
 {
-  // for convenience  
-  template< class GridPartType, class EntityType >
+protected:  
+  template< class GridPartType, class EntityType, 
+            class ElemGeoInfoType, class FaceGeoInfoType >
   static inline double
-  maxEdgeWidth ( const GridPartType &gridPart,
-                 const EntityType &en )
+  maxEdgeWidth ( const GridPartType & gridPart,
+                 const EntityType& entity,
+                 const ElemGeoInfoType &geoInfo,
+                 const FaceGeoInfoType &faceGeoInfo )
   {
+    typedef typename GridPartType :: IntersectionIteratorType IntersectionIteratorType;
+    double faceVol = std::numeric_limits<double>::max() ;
+    int indexInInside = -1;
+    double currVol = std::numeric_limits<double>::min() ;
+    double refFaceVol = std::numeric_limits<double>::min() ;
+    
+    const IntersectionIteratorType endit = gridPart.iend( entity );
+    for( IntersectionIteratorType it = gridPart.ibegin( entity );
+         it != endit; ++it)
+    {
+      typedef typename IntersectionIteratorType::Intersection Intersection;
+      const Intersection &intersection = *it;
+      
+      typedef typename Intersection::Geometry LocalGeometryType;
+      const LocalGeometryType &interGeo = intersection.geometry();
+
+      const int localIndex = intersection.indexInInside();
+      // calculate face Volume also for non-conforming intersections  
+      if( indexInInside != localIndex )
+      {
+        if( indexInInside >= 0 )
+          faceVol = std::min( faceVol, currVol*refFaceVol );
+        
+        refFaceVol = faceGeoInfo.referenceVolume( intersection.type() );
+        indexInInside = localIndex;
+        currVol = 0.0;
+      }
+
+      currVol += interGeo.volume();
+    }
+
+    // do check for last set of intersections  
+    if( indexInInside >= 0 )
+      faceVol = std::min( faceVol, currVol*refFaceVol );
+
+    const double elemVol = entity.geometry().volume() 
+                         * geoInfo.referenceVolume( entity.type() );
+    return elemVol / faceVol;
+  }
+
+public:  
+  /** \brief calculate minimal grid width as 
+      \f$h_{E} :=\frac{|E|}{h^{E}_{m}}\f$ with \f$h^{E}_{m} := \min_{e \in \mathcal{I}_{E}} |e| \f$.
+
+      \param gridPart set of element the minmal with is calculated for 
+      \param communicate do global communication to get minimal width for all processes
+      (default = true)
+  */
+  template <class GridPartType> 
+  static inline 
+  double calcGridWidth (const GridPartType & gridPart, 
+                        const bool communicate = true )
+  {     
     typedef typename GridPartType :: GridType GridType;
+    typedef typename GridPartType :: template Codim<0> :: IteratorType IteratorType; 
+    typedef typename IteratorType :: Entity EntityType;
 
     // get geo infor for elements 
     typedef AllGeomTypes< typename GridPartType :: IndexSetType, GridType > GeomInfoType;
@@ -36,72 +94,21 @@ struct GridWidth
     typedef GeometryInformation< GridType , 1 > FaceGeometryInformationType;
     FaceGeometryInformationType faceGeoInfo( geoInfo.geomTypes(1) );
 
-    typedef typename GridPartType :: IntersectionIteratorType IntersectionIteratorType;
-    IntersectionIteratorType begin = gridPart.ibegin(en);
-    const IntersectionIteratorType end = gridPart.iend(en);
-
-    return maxEdgeWidth( begin, end, geoInfo, faceGeoInfo, en );
-  }
-  
-  template< class IntersectionIteratorType, class ElemGeoInfoType, class FaceGeoInfoType, class EntityType >
-  static inline double
-  maxEdgeWidth ( IntersectionIteratorType &it, 
-                 const IntersectionIteratorType &endit,
-                 const ElemGeoInfoType &geoInfo,
-                 const FaceGeoInfoType &faceGeoInfo, 
-                 const EntityType &entity )
-  {
-    typedef typename EntityType::Geometry Geometry;
-    enum { dim = EntityType::dimension };
-    
-    double faceVol = std::numeric_limits<double>::max() ;
-    int indexInInside = -1;
-    double currVol = std::numeric_limits<double>::min() ;
-    double refFaceVol = std::numeric_limits<double>::min() ;
-    
-    for( ; it != endit; ++it)
-    {
-      typedef typename IntersectionIteratorType::Intersection Intersection;
-      const Intersection &intersection = *it;
-      
-      typedef typename Intersection::Geometry LocalGeometryType;
-      const LocalGeometryType &interGeo = intersection.geometry();
-
-      // calculate face Volume also for non-conforming intersections  
-      if( indexInInside != intersection.indexInInside() )
-      {
-        if( indexInInside >= 0 )
-          faceVol = std::min( faceVol, currVol*refFaceVol );
-        
-        refFaceVol = faceGeoInfo.referenceVolume( intersection.type() );
-        indexInInside = intersection.indexInInside();
-        currVol = 0.0;
-      }
-
-      currVol += interGeo.volume();
-    }
-
-    const Geometry &geo = entity.geometry();
-    const double elemVol = geo.volume() * geoInfo.referenceVolume( geo.type() );
-    return elemVol / faceVol;
-  }
-
-  template <class GridPartType> 
-  static inline double calcGridWidth (const GridPartType & gridPart)
-  {     
-    double maxwidth = 0.0;
-    typedef typename GridPartType :: GridType GridType;
-    typedef typename GridPartType :: template Codim<0> :: IteratorType IteratorType; 
+    // initialize with big value 
+    double width = std::numeric_limits<double>::max() ;
 
     // unstructured case 
     if( Capabilities::IsUnstructured<GridType>::v )
     {
-      IteratorType endit = gridPart.template end<0> (); 
+      const IteratorType endit = gridPart.template end<0> (); 
       for(IteratorType it = gridPart.template begin<0> (); 
           it != endit; ++it )
       {
-        const double w = maxEdgeWidth(gridPart, *it);
-        if(w > maxwidth) maxwidth = w;
+        width = std::min( maxEdgeWidth(gridPart, 
+                                       *it, 
+                                       geoInfo,
+                                       faceGeoInfo ),
+                          width );
       }
     }
     else 
@@ -110,55 +117,18 @@ struct GridWidth
       IteratorType it = gridPart.template begin<0> (); 
       if( it != gridPart.template end<0> () )
       {
-        const double w = maxEdgeWidth(gridPart, *it);
-        if(w > maxwidth) maxwidth = w;
+        width = std::min( maxEdgeWidth(gridPart, 
+                                       *it, 
+                                       geoInfo,
+                                       faceGeoInfo ),
+                          width );
       }
     }
-    return maxwidth;
-  }   
 
-  template <class EntityType, class GeomInfoType, class FaceGeomInfoType> 
-  static inline double  
-  leafEdgeWidth(const EntityType &en,
-                const GeomInfoType& geoInfo,
-                const FaceGeomInfoType& faceGeoInfo)
-  {
-    typedef typename EntityType :: LeafIntersectionIterator IntersectionIteratorType;
-    const IntersectionIteratorType endit = en.ileafend();
-    IntersectionIteratorType it = en.ileafbegin();
+    // calculate global minimum 
+    if( communicate )
+      width = gridPart.grid().comm().min( width );
 
-    return maxEdgeWidth(it, endit, geoInfo, faceGeoInfo, en);
-  }
-
-  template <class GridType, class GeomInfoType, class FaceGeomInfoType> 
-  static inline double calcLeafWidth (
-      const GridType & grid, 
-      const GeomInfoType& geoInfo,
-      const FaceGeomInfoType& faceGeoInfo)
-  {     
-    double width = std::numeric_limits<double>::max() ;
-    typedef typename GridType::template Codim<0> :: 
-      template Partition<Interior_Partition> ::  LeafIterator IteratorType; 
-    
-    // unstructured case 
-    if( Capabilities::IsUnstructured<GridType>::v )
-    {
-      IteratorType endit = grid.template leafend<0, Interior_Partition> (); 
-      for(IteratorType it = grid.template leafbegin<0, Interior_Partition> (); 
-          it != endit; ++it )
-      {
-        width = std::min( width , leafEdgeWidth(*it, geoInfo, faceGeoInfo) );
-      }
-    }
-    else 
-    {
-      // here we only need to check one element 
-      IteratorType it = grid.template leafbegin<0, Interior_Partition> (); 
-      if( it != grid.template leafend<0, Interior_Partition> () )
-      {
-        width = std::min( width , leafEdgeWidth(*it, geoInfo, faceGeoInfo) );
-      }
-    }
     return width;
   }   
 };
@@ -177,20 +147,11 @@ protected:
 
   typedef HierarchicGridPart< GridType > GridPartType; 
   
-  // get geo infor for elements 
-  typedef AllGeomTypes< typename GridPartType :: IndexSetType, GridType > GeomInfoType;
-
-  // get geo infor for faces  
-  typedef GeometryInformation< GridType , 1 > FaceGeometryInformationType;
-
   const GridType& grid_;
   const DofManagerType& dm_;
 
   GridPartType gridPart_;
 
-  const GeomInfoType geoInfo_;
-  const FaceGeometryInformationType faceGeoInfo_;
-  
   mutable double width_;
   mutable int sequence_;
 
@@ -201,8 +162,6 @@ public:
     : grid_( *grid )
     , dm_( DofManagerType :: instance( grid_ ))
     , gridPart_( const_cast<GridType& > (grid_) )
-    , geoInfo_( gridPart_.indexSet() )
-    , faceGeoInfo_( geoInfo_.geomTypes(1) ) 
     , width_(-1.0)
     , sequence_(-1)
   {
@@ -213,18 +172,6 @@ public:
   {
     calcWidths();
     return width_;
-  }
-  
-  //! return characteristic grid width 
-  double minGridWidth () const DUNE_DEPRECATED
-  {
-    return gridWidth();
-  }
-  
-  //! return characteristic grid width 
-  double maxGridWidth () const DUNE_DEPRECATED
-  {
-    return gridWidth();
   }
 
 protected:  
@@ -239,10 +186,8 @@ protected:
 
     if( dm_.sequence() != sequence_ )
     {
-      double width = GridWidth :: calcLeafWidth( grid_ , geoInfo_, faceGeoInfo_ );
-
-      // take global minmum 
-      width_ = grid_.comm().min( width );
+      // calculate grid width 
+      width_ = GridWidth :: calcGridWidth( gridPart_ , true );
 
       assert( width_ > 0 );
       sequence_ = dm_.sequence();
