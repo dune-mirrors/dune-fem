d5a04e9565a37a1f9a5943775193b2dfa1c57f94
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2010-03-25 17:14:08 +0000

    small change
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@5237 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- dune/fem/io/file/binarydataio.hh
+++ dune/fem/io/file/binarydataio.hh
@@ -7,13 +7,9 @@
 #include <dune/common/misc.hh>
 #include <dune/grid/common/capabilities.hh>
 #include <dune/grid/common/grid.hh>
-// #include <dune/grid/common/defaultindexsets.hh>
 #include <dune/grid/utility/grapedataioformattypes.hh>
 
 #include <dune/fem/space/common/dofmanager.hh>
-
-#include <dune/grid/io/file/dgfparser/dgfparser.hh> 
-
 #include <dune/fem/misc/gridname.hh> 
 
 //- Local includes 
@@ -232,7 +228,7 @@ class BinaryDataIO
 public:
    typedef GridImp GridType;
 
-   BinaryDataIO () {};
+   BinaryDataIO () {}
 
    /** Write Grid with GridType file filename and time 
    *
