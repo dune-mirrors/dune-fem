7eb92daece3f1c412bfa2ac163a7c88b794b52c5
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2010-04-09 12:59:29 +0000

    remove unused file
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@5295 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- dune/fem/gridpart/gridpartutility.hh
+++
@@ -1,7 +0,0 @@
-#ifndef DUNE_GRIDPARTUTILITY_HH
-#define DUNE_GRIDPARTUTILITY_HH
-
-#warning "dune/gem/gridpart/gridpartutility.hh is not required anymore."
-#warning "GridParts now export all iterators. Please adapt your code accordingly-" 
-
-#endif
