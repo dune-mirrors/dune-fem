06f255d06d7313367305a3930e2a33bd32ed5e97
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2010-04-12 06:32:29 +0000

    suggestion for a simpler Operator interface (called Dune::Fem::Operator) (the old thick interface is now derived from this one)
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@5314 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- dune/fem/operator/common/operator.hh
+++ dune/fem/operator/common/operator.hh
@@ -1,10 +1,52 @@
-#ifndef DUNE_OPERATOR_HH
-#define DUNE_OPERATOR_HH
+#ifndef DUNE_FEM_OPERATOR_HH
+#define DUNE_FEM_OPERATOR_HH
 
-#include "mapping.hh"
+#include <dune/fem/operator/common/mapping.hh>
 
 namespace Dune
 {
+
+  namespace Fem
+  {
+
+    /** \class Operator
+     *  \brief abstract operator
+     *
+     *  Operators map a discrete function onto another discrete function.
+     *  Their interface is described by the abstract class Operator.
+     *
+     *  \tparam  DomainFunction  type of discrete function for the domain
+     *  \tparam  RangeFunction   type of discrete function for the range
+     *
+     *  \interfaceclass
+     */
+    template< class DomainFunction, class RangeFunction >
+    struct Operator
+    {
+      /** \brief type of discrete function in the operator's domain */
+      typedef DomainFunction DomainFunctionType;
+      /** \brief type of discrete function in the operator's range */
+      typedef RangeFunction RangeFunctionType;
+
+      /** \brief field type of the operator's domain */
+      typedef typename DomainFunctionType::RangeFieldType DomainFieldType;
+      /** \brief field type of the operator's range */
+      typedef typename RangeFunctionType::RangeFieldType RangeFieldType;
+
+      /** \brief application operator
+       *
+       *  \param[in]   u  argument discrete function
+       *  \param[out]  w  destination discrete function
+       *
+       *  \note This method has to be implemented by all derived classes.
+       */
+      virtual void operator() ( const DomainFunctionType &u, RangeFunctionType &w ) const = 0;
+    };
+
+  } // end namespace Dune::Fem
+
+
+
 /** @addtogroup OperatorCommon 
     Operators are mappings from function spaces into function spaces.
 
@@ -25,8 +67,12 @@ namespace Dune
 */
 template <typename DFieldType, typename RFieldType,
           typename DType , typename RType>
-class Operator : public Mapping <DFieldType,RFieldType,DType,RType>
+class Operator
+: public Mapping <DFieldType,RFieldType,DType,RType>,
+  public Fem::Operator< DType, RType >
 {
+  typedef Fem::Operator< DType, RType > BaseType;
+
 protected: 
   //! \brief type of mapping base class 
   typedef Mapping <DFieldType,RFieldType,DType,RType> MappingType;
@@ -38,12 +84,16 @@ public:
   typedef DFieldType DomainFieldType;
   typedef RFieldType RangeFieldType;
 
+  using BaseType::operator();
+
+#if 0
   /** \brief Application operator 
       \param[in] arg argument 
       \param[out] dest destination 
       \note This method has to be implemented by all derived classes. 
   */
   virtual void operator() (const DomainType& arg, RangeType& dest) const = 0;
+#endif
  
 protected:
   /** \brief The method apply calls the application operator. The method 
@@ -59,5 +109,7 @@ protected:
 }; // end class Operator 
 
 ///@}
+
 } // end namespace Dune 
-#endif
+
+#endif // #ifndef DUNE_FEM_OPERATOR_HH
