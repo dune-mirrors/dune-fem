0259628f8e82c043ae25a00d79291564b4198f54
Tobias Malkmus <tomalk@mathematik.uni-freiburg.de>
2010-04-27 07:47:38 +0000

    bug in the mapper with with Polorder >1 and dimRange = 2, test problem
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@5396 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

---
+++ dune/fem/space/lagrangespace/test/testnonscalarmapper.cc
@@ -0,0 +1,180 @@
+#include <config.h>
+
+//#define USE_TWISTFREE_MAPPER
+
+#include <dune/common/fvector.hh>
+
+// DGF gridtype 
+#include <dgfgridtype.hh>
+#include <dune/fem/misc/mpimanager.hh>
+#include <dune/fem/gridpart/adaptiveleafgridpart.hh>
+// adaptation classes 
+#include <dune/fem/space/common/adaptmanager.hh>
+// lagrange space 
+#include <dune/fem/space/lagrangespace.hh>
+#include <dune/fem/function/adaptivefunction.hh>
+#include <dune/fem/io/file/vtkio.hh>
+
+#ifndef POLORDER 
+const int PolynomialOrder = 2;
+#else 
+const int PolynomialOrder = POLORDER;
+#endif
+
+#if SCALAR
+const int dimRange = 1;
+#else 
+const int dimRange = GridType :: dimensionworld;
+#endif
+
+
+using namespace Dune;
+
+// Main Program
+// ------------
+int main( int argc, char **argv )
+{
+  // initialize MPI 
+  MPIManager :: initialize ( argc, argv );
+  const int rank = MPIManager :: rank ();
+  
+  try
+  {
+    if(argc < 2)
+    {
+      std::cout<<"usuage: "<<argv[0]<<" startLevel times"<<std::endl;
+      std::cout<<" with startLevel > 0 and times >0 "<<std::endl;
+      abort();
+    }
+    // generate GridPointer holding grid instance
+    GridPtr< GridType > gridptr ("2dgrid.dgf");
+
+    // get grid reference 
+    GridType& grid = *gridptr ;
+
+    const int minLevel = atoi(argv[1]);
+    const int times = atoi(argv[2]);
+
+    GlobalRefine::apply(grid, minLevel);
+//    grid.globalRefine(minLevel);
+                  
+
+    typedef AdaptiveLeafGridPart<GridType> GridPartType;
+    GridPartType gridPart (grid);
+
+    typedef FunctionSpace< double, double, GridType::dimensionworld, dimRange  > FunctionSpace;
+
+    typedef FunctionSpace::DomainType DomainType;
+    typedef FunctionSpace::RangeType RangeType;
+    typedef FunctionSpace::RangeFieldType RangeFieldType;
+
+
+    typedef LagrangeDiscreteFunctionSpace < FunctionSpace, GridPartType, PolynomialOrder, CachingStorage > DiscreteFunctionSpace;
+    typedef AdaptiveDiscreteFunction< DiscreteFunctionSpace > DiscreteFunction;
+
+    DiscreteFunctionSpace dFspace( gridPart );
+    DiscreteFunction solution("testSolution", dFspace);
+
+    typedef DiscreteFunctionSpace :: IteratorType IteratorType;
+    typedef IteratorType :: Entity EntityType;
+
+    typedef DiscreteFunctionSpace :: LagrangePointSetType LagrangePointSetType;
+
+    const int faceCodim = 0;
+    typedef LagrangePointSetType :: Codim< faceCodim > :: SubEntityIteratorType
+      FaceDofIteratorType;
+
+    typedef GridPartType :: IntersectionIteratorType IntersectionIteratorType;
+    typedef IntersectionIteratorType::Intersection IntersectionType; 
+        
+#if SCALAR
+    DomainType v;
+    v[0] = M_PI;
+    v[1] =-std::sqrt(2);
+    v/= v.two_norm();
+
+    Dune :: Parameter :: get( "test.velocity", v, v);
+#endif
+
+
+
+    for(int i= 0;i<times;++i)
+    {
+      if(i>0)
+      {
+        std::cout<<"refining ... "<<std::endl;
+        GlobalRefine::apply(grid,DGFGridInfo<GridType>::refineStepsForHalf());
+      }
+
+      solution.clear();
+
+      const IteratorType endit = dFspace.end();
+      for( IteratorType it = dFspace.begin(); it != endit; ++it )
+      {
+        const EntityType &entity = *it;
+        typedef DiscreteFunction :: LocalFunctionType LocalFunctionType;
+
+        LocalFunctionType solutionLocal = solution.localFunction( entity );      
+
+        typedef EntityType :: Geometry Geometry;
+        const Geometry& geo = entity.geometry();
+
+        const LagrangePointSetType &lagrangePointSet = dFspace.lagrangePointSet( entity );
+
+#if 0
+        IntersectionIteratorType iIt = gridPart.ibegin( entity );
+        const IntersectionIteratorType endiIt = gridPart.iend( entity );
+
+        for( ; iIt != endiIt; ++iIt )
+        {                  
+          const IntersectionType &intersection = *iIt;
+          const int face = intersection.indexInInside();
+#endif
+          const int face =0;
+
+
+          FaceDofIteratorType faceIt
+            = lagrangePointSet.beginSubEntity< faceCodim >( face );
+          const FaceDofIteratorType faceEndIt
+            = lagrangePointSet.endSubEntity< faceCodim >( face  );
+
+          for( ; faceIt != faceEndIt; ++faceIt )
+          {
+            const DomainType &local = lagrangePointSet.point( *faceIt );
+            const DomainType global = geo.global( local );
+
+            for(int r=0;r<dimRange;++r)
+            {
+              const int localDof = (*faceIt)*dimRange + r;
+              RangeType phi, phiLocal;
+#if SCALAR
+              phi = global * v;
+#else
+              phi = global;
+#endif
+
+              solutionLocal.baseFunctionSet().evaluate( localDof, local, phiLocal );
+              const double value = ( phiLocal * phi ) / phiLocal.two_norm();
+
+              solutionLocal[localDof] = value;
+            }
+          }
+//      }
+      }
+
+      SubsamplingVTKIO<GridPartType> vtkio(gridPart, 1);
+      vtkio.addVertexData(solution);
+      std::stringstream name;
+      name<<"testnonscalar_"<<i;
+      vtkio.write(name.str().c_str());               
+    }
+
+    return 0;
+  }
+  catch( const Exception &exception )                                     
+  {
+    if( rank == 0 )
+      std :: cerr << exception << std :: endl;
+    return 1;
+  }                                                                 
+}
