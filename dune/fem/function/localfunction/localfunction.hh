55b3682cffa4ce21cab6dd3ff461902003a1d410
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2010-05-06 11:17:46 +0000

    remove unused include
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@5475 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- dune/fem/function/localfunction/localfunction.hh
+++ dune/fem/function/localfunction/localfunction.hh
@@ -3,7 +3,6 @@
 
 #include <dune/fem/misc/engineconcept.hh>
 #include <dune/fem/space/common/dofstorage.hh>
-#include <dune/fem/misc/fieldmatrixhelper.hh>
 #include <dune/fem/function/common/function.hh>
 
 namespace Dune
