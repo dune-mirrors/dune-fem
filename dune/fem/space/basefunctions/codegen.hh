941ad8493c74f420526cfbfc995c330f7e23e850
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2010-07-29 11:48:45 +0000

    slight improvements.
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@5593 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- dune/fem/space/basefunctions/codegen.hh
+++ dune/fem/space/basefunctions/codegen.hh
@@ -12,7 +12,7 @@ namespace Fem {
           const int dimRange, const size_t numRows, const size_t numCols ) 
   {
     out << "template <class BaseFunctionSet>" << std::endl;
-    out << "struct EvaluateRanges<BaseFunctionSet, " << dimRange << ", " << numRows << ", " << numCols << ">" << std::endl;
+    out << "struct EvaluateRanges<BaseFunctionSet, Fem :: EmptyGeometry, " << dimRange << ", " << numRows << ", " << numCols << ">" << std::endl;
     out << "{" << std::endl;
     out << "  template< class QuadratureType,"<< std::endl;
     out << "            class RangeVectorType," << std::endl;
@@ -24,22 +24,19 @@ namespace Fem {
     out << "                    RangeFactorType &rangeVector)" << std::endl;
     out << "  {" << std::endl;
     out << "    typedef typename ScalarRangeType :: field_type field_type;" << std::endl;
-    //out << "    typedef typename RangeVectorType :: value_type value_type;" << std::endl; 
+    out << "    typedef typename RangeVectorType :: value_type value_type;" << std::endl; 
     for( size_t row=0; row< numRows; ++row ) 
     {
       out << "    {" << std::endl;
-      out << "      const size_t baseRow = quad.cachingPoint( " << row << " );" << std::endl;
+      out << "      const value_type& rangeStorageRow = rangeStorage[ quad.cachingPoint( " << row << " ) ];" << std::endl;
       out << "      RangeType& result = rangeVector[ " << row << " ];"  << std::endl;
       out << "      result = 0;" << std::endl;
       for( size_t col = 0, colR = 0; col < numCols; ++col ) 
       {
-        out << "      {" << std::endl;
-        out << "        const field_type& phi = rangeStorage[ baseRow ][ " << col << " ][ 0 ];" << std::endl;
         for( int r = 0; r < dimRange; ++r , ++colR ) 
         {
-          out << "        result[ " << r << " ] += dofs[ " << colR << " ] * phi;" << std::endl;
+          out << "      result[ " << r << " ]  +=  rangeStorageRow[ " << col << " ][ 0 ] * dofs[ " << colR << " ];" << std::endl;
         }
-        out << "      }" << std::endl;
       }
       out << "    }" << std::endl;
     }
@@ -51,7 +48,7 @@ namespace Fem {
           const int dimRange, const size_t numRows, const size_t numCols ) 
   {
     out << "template <class BaseFunctionSet>" << std::endl;
-    out << "struct AxpyRanges<BaseFunctionSet, " << dimRange << ", " << numRows << ", " << numCols << ">" << std::endl;
+    out << "struct AxpyRanges<BaseFunctionSet, Fem :: EmptyGeometry, " << dimRange << ", " << numRows << ", " << numCols << ">" << std::endl;
     out << "{" << std::endl;
     out << "  template< class QuadratureType,"<< std::endl;
     out << "            class RangeVectorType," << std::endl;
@@ -62,9 +59,17 @@ namespace Fem {
     out << "                    const RangeFactorType &rangeFactors," << std::endl;
     out << "                    LocalDofVectorType& dofs)" << std::endl;
     out << "  {" << std::endl;
+    out << "    typedef typename RangeVectorType :: value_type value_type;" << std::endl; 
+    out << "    const value_type* const rangeStorageTmp[ " << numRows << " ] = {" << std::endl;
+    for( size_t row = 0; row<numRows ; ++ row )
+    {
+      out << "       &( rangeStorage[ quad.cachingPoint( " << row << " ) ] )";
+      if( row < numRows - 1 ) out << " ," << std::endl;
+      else out << "  };" << std::endl;
+    }
+    out << std::endl;
     /*
     out << "    typedef typename ScalarRangeType :: field_type field_type;" << std::endl;
-    out << "    typedef typename RangeVectorType :: value_type value_type;" << std::endl; 
     for( int row=0; row< numRows; ++row ) 
     {
       out << "    {" << std::endl;
@@ -92,7 +97,8 @@ namespace Fem {
           out << "    dofs[ " << colR << " ] +=" << std::endl;
           for( size_t row=0; row< numRows; ++row ) 
           {
-            out << "      rangeStorage[ quad.cachingPoint( " << row << " ) ][ " << col << " ][ 0 ] * rangeFactors[ " << row << " ][ " << r << " ]"; 
+            //out << "      rangeStorage[ quad.cachingPoint( " << row << " ) ][ " << col << " ][ 0 ] * rangeFactors[ " << row << " ][ " << r << " ]"; 
+            out << "      (*(rangeStorageTmp[ " << row << " ]))[ " << col << " ][ 0 ] * rangeFactors[ " << row << " ][ " << r << " ]"; 
             if( row < numRows - 1 ) 
               out << " + " << std::endl;
             else 
