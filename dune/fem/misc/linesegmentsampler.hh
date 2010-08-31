b19b62b74263c9e9591c62b76698f139d4c66fd9
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2010-08-31 14:19:54 +0000

    add a line segment sampler (samples multidimensional discrete functions over a line)
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@5656 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

---
+++ dune/fem/misc/linesegmentsampler.hh
@@ -0,0 +1,143 @@
+#ifndef DUNE_FEM_LINESEGMENTSAMPLER_HH
+#define DUNE_FEM_LINESEGMENTSAMPLER_HH
+
+#include <limits>
+#include <vector>
+
+#include <dune/common/fvector.hh>
+
+#include <dune/grid/common/genericreferenceelements.hh>
+
+namespace Dune
+{
+
+  namespace Fem
+  {
+
+    // LineSegmentSampler
+    // ------------------
+
+    template< class GridPart >
+    struct LineSegmentSampler
+    {
+      typedef GridPart GridPartType;
+
+      typedef typename GridPartType::GridType::ctype DomainFieldType;
+
+      static const int dimDomain = GridPartType::GridType::dimensionworld;
+      static const int dimGrid = GridPartType::GridType::dimension;
+
+      typedef FieldVector< DomainFieldType, dimDomain  > DomainType;
+      typedef FieldVector< DomainFieldType, dimGrid > LocalDomainType;
+
+    private:
+      dune_static_assert( dimDomain == dimGrid, "LineSegmentSampler supports only flat grids." );
+
+      typedef GenericReferenceElement< DomainFieldType, dimGrid > ReferenceElement;
+      typedef GenericReferenceElements< DomainFieldType, dimGrid > ReferenceElements;
+
+    public:
+      LineSegmentSampler ( const GridPart &gridPart, const DomainType &left, const DomainType &right )
+      : gridPart_( gridPart ), left_( left ), right_( right )
+      {}
+
+      template< class GridFunction >
+      void operator() ( const GridFunction &f, std::vector< typename GridFunction::RangeType > &samples ) const;
+
+      const GridPart &gridPart () const { return gridPart_; }
+
+    private:
+      const GridPart &gridPart_;
+      DomainType left_, right_;
+    };
+
+
+
+    // Implementation of LineSegmentSampler
+    // ------------------------------------
+
+    template< class GridPart >
+    template< class GridFunction >
+    inline void LineSegmentSampler< GridPart >
+      ::operator() ( const GridFunction &f, std::vector< typename GridFunction::RangeType > &samples ) const
+    {
+      typedef typename GridPartType::template Codim< 0 >::IteratorType IteratorType;
+      typedef typename GridPartType::template Codim< 0 >::EntityType EntityType;
+
+      typedef typename GridFunction::LocalFunctionType LocalFunctionType;
+
+      const int numSamples = samples.size();
+      if( numSamples < 2 )
+        DUNE_THROW( InvalidStateException, "LineSegmentSampler cannot sample less than 2 points." );
+      DomainType ds = right_ - left_;
+      ds /= DomainFieldType( numSamples - 1 );
+
+      const typename GridFunction::RangeFieldType nan
+        = std::numeric_limits< typename GridFunction::RangeFieldType >::quiet_NaN();
+      for( int i = 0; i < numSamples; ++i )
+        samples[ i ] = typename GridFunction::RangeType( nan );
+
+      const IteratorType end = gridPart().template end< 0 >();
+      for( IteratorType it = gridPart().template begin< 0 >(); it != end; ++it )
+      {
+        const EntityType &entity = *it;
+        const typename EntityType::Geometry &geometry = entity.geometry();
+
+        DomainFieldType lower = std::numeric_limits< DomainFieldType >::min();
+        DomainFieldType upper = std::numeric_limits< DomainFieldType >::max();
+
+        const ReferenceElement &refElement = ReferenceElements::general( geometry.type() );
+        const int numFaces = refElement.size( 1 );
+        for( int face = 0; face < numFaces; ++face )
+        {
+          const LocalDomainType &center = refElement.position( face, 1 );
+
+          DomainType normal;
+          const LocalDomainType &refNormal = refElement.volumeOuterNormal( face );
+          geometry.jacobianInverseTransposed( center ).mv( refNormal, normal );
+
+          const DomainFieldType nds = normal * ds;
+          const DomainFieldType ncl = normal * (geometry.global( center ) - left_);
+          if( std::abs( nds ) > 1e-8 )
+          {
+            // ds is not parallel to the face
+            const DomainFieldType alpha = ncl / nds;
+            if( nds > 0 )
+              upper = std::min( upper, alpha );
+            else
+              lower = std::max( lower, alpha );
+          }
+          else if( ncl < -1e-8 )
+          {
+            // ds is parallel to the face and the line lies on the outside
+            lower = std::numeric_limits< DomainFieldType >::max();
+            upper = std::numeric_limits< DomainFieldType >::min();
+          }
+        }
+
+        if( lower <= upper )
+        {
+          const LocalFunctionType &lf = f.localFunction( entity );
+          const int i_upper = std::min( int( std::floor( upper + 1e-8 ) ), numSamples - 1 );
+          const int i_lower = std::max( int( std::ceil( lower - 1e-8 ) ), 0 );
+          for( int i = i_lower; i <= i_upper; ++i )
+          {
+            DomainType xi = left_;
+            xi.axpy( DomainFieldType( i ), ds );
+            lf.evaluate( geometry.local( xi ), samples[ i ] );
+          }
+        }
+      }
+
+      bool valid = true;
+      for( int i = 0; i < numSamples; ++i )
+        valid &= (samples[ i ] == samples[ i ]);
+      if( !valid )
+        DUNE_THROW( InvalidStateException, "LineSegmentSampler could not find all samples." );
+    }
+
+  }
+
+}
+
+#endif // #ifndef DUNE_FEM_LINESEGMENTSAMPLER_HH
