4cc688a31389ce63bda99638f5d32d80b06345ed
Slavko Brdar <slavko@mathematik.uni-freiburg.de>
2010-09-03 09:18:46 +0000

    - docu for LatexTableWriter
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@5663 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- dune/fem/io/file/latextablewriter.hh
+++ dune/fem/io/file/latextablewriter.hh
@@ -21,15 +21,27 @@ namespace Dune
     // AbstractColumnWriter
     // --------------------
 
+    /** \class AbstractColumnWriter
+     *  \brief Class representing column writer in general
+     *  
+     *  This class represents column writer.
+     *
+     * \tparam DataTuple Type of the data tuple
+     */
     template< class DataTuple >
     struct AbstractColumnWriter
     {
+      //! The alignment for the data in this column
       enum Alignment { AlignLeft, AlignCenter, AlignRight };
 
+      //! Destructor
       virtual ~AbstractColumnWriter () {}
 
+      //! \return alignment
       virtual Alignment alignment () const { return AlignCenter; }
+      //! \return data entry
       virtual std::string entry ( const DataTuple & ) const = 0;
+      //! \return column titles as latex row
       virtual std::string header () const = 0;
     };
 
@@ -38,6 +50,15 @@ namespace Dune
     // NumberColumnWriter
     // ------------------
 
+    /** \class NumberColumnWriter
+     *  \brief gets the @a N th element of a provided tuple assuming its a number
+     *
+     *  This class extracts the @a N th element of the tuple assuming
+     *  the @a N th element is a number
+     *
+     *  \tparam DataTuple The type of the data tuple
+     *  \tparam N Index of the entry in the data tuple tp be extracted
+     */
     template< class DataTuple, int N >
     class NumberColumnWriter
     : public AbstractColumnWriter< DataTuple >
@@ -45,24 +66,36 @@ namespace Dune
       typedef AbstractColumnWriter< DataTuple > BaseType;
 
     public:
+      /** Constructor
+       *  \param[in] header Column titles in latex row format
+       *  \param[in] decimals The precision of double for output to latex table
+       */
       explicit NumberColumnWriter ( const std::string &header, const int decimals = 6 )
       : header_( header ),
         decimals_( decimals )
       {}
 
+      //! set the aligment of the entries for this column in the latex table
       typename BaseType::Alignment alignment () const
       {
         return BaseType::AlignRight;
       }
 
+      /** \brief returns @a N the element from @a data tuple
+       *  \return String representing @a N th element of @a data
+       *
+       *  \note @a N th element of @a data is assumed to be number
+       */
       std::string entry ( const DataTuple &data ) const
       {
         return toString( ElementAccess< N >::get( data ) );
       }
 
+      //! return Column titles in latex row format
       std::string header () const { return header_; }
 
     protected:
+      //! converts @a number to std::string
       template< class Number >
       std::string toString ( const Number &number ) const
       {
@@ -82,6 +115,15 @@ namespace Dune
     // EOCColumnWriter
     // ---------------
 
+    /** \class EOCColumnWriter
+     *  \brief calculates EOC from the @a Nh th and @a Ne th element of tuple
+     *
+     *  This class extracts the @a N th element of the tuple assuming
+     *  the @a N th element is a number
+     *
+     *  \tparam DataTuple The type of the data tuple
+     *  \tparam N Index of the entry in the data tuple tp be extracted
+     */
     template< class DataTuple, int Nh, int Ne >
     class EOCColumnWriter
     : public NumberColumnWriter< DataTuple, Ne >
@@ -89,11 +131,25 @@ namespace Dune
       typedef NumberColumnWriter< DataTuple, Ne > BaseType;
 
     public:
+      /** Constructor
+       *  \param[in] header Column titles in latex row format
+       *  \param[in] decimal Decimal placese for this column. Default 2.
+       */
       explicit EOCColumnWriter ( const std::string &header, const int decimals = 2 )
       : BaseType( header, decimals ),
         hOld_( std::numeric_limits< double >::infinity() )
       {}
 
+      /** \brief returns latex table entry 
+       *
+       *  \param[in] data Data tuple
+       *  
+       *  \return String representing EOC calculated from @a Nh th
+       *          and @a Ne th elements of the @a data tuple
+       *
+       *  \note @a Ne th and @a Nh th elements of data are assumed to
+       *        be numbers
+       */
       std::string entry ( const DataTuple &data ) const
       {
         const double h = ElementAccess< Nh >::get( data );
@@ -120,24 +176,45 @@ namespace Dune
     // LatexTableWriter
     // ----------------
 
+    /** \class LatexTableWriter
+     *  \brief writes latex tables based on user-defined row structure
+     *
+     *  The class LatexTableWriter writes a latex table where each
+     *  row corresponds to user-provided row structure of the @a DataTuple type.
+     */
     template< class DataTuple >
     struct LatexTableWriter
     {
+      //! Abstract column type
       typedef AbstractColumnWriter< DataTuple > ColumnWriterType;
+      //! Abstract column vector type
       typedef std::vector< const ColumnWriterType * > ColumnWriterVectorType;
 
+      /** Constructor
+       *  \param[in] filename The name of the latex file
+       *  \param[in] columnWriter Abstract column writer
+       */
       LatexTableWriter ( const std::string &filename, const ColumnWriterVectorType &columnWriter );
+      /** Destructor
+       *  \brief writes "\end{tabular}" to the latex file and removes column vector
+       */
       ~LatexTableWriter ();
 
+      //! Write row to the table
       void writeRow ( const DataTuple &data );
+      //! Adds extra space between two columns in the latex table
       void writeSeparator ();
 
     private:
+      //! Type of iterator over column vector
       typedef typename ColumnWriterVectorType::const_iterator ColumnWriterIteratorType;
 
+      //! Remove all columns in column vector
       void cleanUp ();
 
+      //! The latex format of the table
       std::string preamble () const;
+      //! The column titles
       std::string header () const;
 
       ColumnWriterVectorType columnWriters_;
