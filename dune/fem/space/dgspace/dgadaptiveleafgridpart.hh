4453e1704e6e3b60305b627d5e97a51ee9b25327
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2010-10-29 11:35:24 +0000

    make headercheck compile.
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@5792 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- dune/fem/space/dgspace/dgadaptiveleafgridpart.hh
+++ dune/fem/space/dgspace/dgadaptiveleafgridpart.hh
@@ -1,4 +1,6 @@
 #ifndef DGADAPTIVELEAFGRIDPART_HH
 #define DGADAPTIVELEAFGRIDPART_HH
+#ifndef HEADERCHECK
 #warning "Header was removed, use <dune/fem/gridpart/adaptiveleafgridpart.hh> instread!"
+#endif
 #endif
--- dune/fem/space/dgspace/dgadaptiveleafindexset.hh
+++ dune/fem/space/dgspace/dgadaptiveleafindexset.hh
@@ -1,4 +1,6 @@
 #ifndef DUNE_DGADAPTIVELEAFINDEXSET_HH
 #define DUNE_DGADAPTIVELEAFINDEXSET_HH
+#ifndef HEADERCHECK
 #error "Header was removed, use <dune/fem/gridpart/adaptiveleafindexset.hh> instread!" 
+#endif
 #endif
