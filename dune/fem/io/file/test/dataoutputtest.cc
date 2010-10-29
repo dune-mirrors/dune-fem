ace5e3ac70dd9db4be1735ebf6a42180a03dd20f
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2010-10-29 11:27:37 +0000

    remove deprecated header.
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@5790 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- dune/fem/io/file/test/dataoutputtest.cc
+++ dune/fem/io/file/test/dataoutputtest.cc
@@ -15,7 +15,6 @@ static const int dimw = Dune::GridSelector::dimworld;
 
 #include <dune/fem/gridpart/gridpart.hh>
 #include <dune/fem/gridpart/adaptiveleafgridpart.hh> 
-#include <dune/fem/space/dgspace/dgadaptiveleafgridpart.hh> 
 #include <dune/fem/space/common/adaptmanager.hh>
 #include <dune/fem/function/common/discretefunctionadapter.hh>
 
