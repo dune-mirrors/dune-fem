fb72bf05ae81651107d777b93af9a873497d6575
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2010-11-25 10:41:15 +0000

    use topologyId in QuadratureKey
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@5839 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- dune/fem/quadrature/caching/pointmapper.hh
+++ dune/fem/quadrature/caching/pointmapper.hh
@@ -7,45 +7,42 @@
 //- Dune includes 
 #include <dune/fem/quadrature/quadrature.hh>
 
-namespace Dune {
+namespace Dune
+{
 
-  class QuadratureKey  
+  struct QuadratureKey  
   {
-  protected:
-    const size_t id_;
-    
-    static inline size_t quadId2MapperId(const GeometryType& elemGeo, 
-                                         const size_t quadId) 
+    QuadratureKey ( const GeometryType &geoType, const size_t id )
+    : id_( (topologyId( geoType ) << 16) + id )
     {
-      // in 1d or 0d everthing is simplex 
-      const int basicType = (elemGeo.dim() <= 1) ? 
-          GeometryType :: simplex : elemGeo.basicType();
-
-      // split between different geometry basic types 
-      assert( basicType >= 0 );
-      assert( quadId < 65536 );
-      return (basicType * 65536) + quadId;
+      assert( id < (1 << 16) );
     }
-  public:  
-    QuadratureKey(const GeometryType& geoType, const size_t id) 
-      : id_( quadId2MapperId(geoType, id)) 
-    {}
     
-    QuadratureKey(const QuadratureKey& other) 
-      : id_(other.id_)
-    {}
-    
-    bool operator < (const QuadratureKey& other) const 
+    bool operator< ( const QuadratureKey &other ) const
     {
       return (id_ < other.id_);
     }
     
-    bool operator == (const QuadratureKey& other) const 
+    bool operator== ( const QuadratureKey &other ) const
     {
-      return id_ == other.id_;
+      return (id_ == other.id_);
     }
+
+  protected:
+    static unsigned int topologyId ( const GeometryType &type )
+    {
+#if DUNE_VERSION_NEWER(DUNE_COMMON,2,1,0)
+      return type.id();
+#else
+      return GenericGeometry::topologyId( type );
+#endif
+    }
+
+    const size_t id_;
   };
-    
+
+
+
   template <class ct, int dim>
   struct CachingTraits {
     //! type of integration point list implementation, fix type here 
