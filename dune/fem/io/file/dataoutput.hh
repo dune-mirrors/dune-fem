1af2bf17df0dbf6994a1a6ff29d80f3b12f2485f
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2011-04-08 13:48:39 +0000

    bug fix when output is none.
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/branches/release-1.2@6106 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- dune/fem/io/file/dataoutput.hh
+++ dune/fem/io/file/dataoutput.hh
@@ -681,7 +681,7 @@ namespace Dune
     switch( outputFormat_ )
     {
     // if no output was chosen just return  
-    case none: return ;
+    case none: break ;
     case binary:   
       writeBinaryData( sequenceStamp );
       break;
@@ -700,22 +700,26 @@ namespace Dune
       DUNE_THROW(NotImplemented,"DataOutput::write: wrong output format");
     }
 
-    if (sequence_)
-      sequence_ << writeStep_ << " "
-                << filename << " "
-                << sequenceStamp
-                << outstring
-                << std::endl;
-
-    if( Parameter::verbose() )
+    // only write sequence when output is not none 
+    if( outputFormat_ != none ) 
     {
-      // only write info for proc 0, otherwise on large number of procs
-      // this is to much output 
-      std::cout << myClassName() << "[" << grid_.comm().rank() << "]::write data"
-                << " writestep=" << writeStep_
-                << " sequenceStamp=" << sequenceStamp
-                << outstring
-                << std::endl;
+      if (sequence_)
+        sequence_ << writeStep_ << " "
+                  << filename << " "
+                  << sequenceStamp
+                  << outstring
+                  << std::endl;
+
+      if( Parameter::verbose() )
+      {
+        // only write info for proc 0, otherwise on large number of procs
+        // this is to much output 
+        std::cout << myClassName() << "[" << grid_.comm().rank() << "]::write data"
+                  << " writestep=" << writeStep_
+                  << " sequenceStamp=" << sequenceStamp
+                  << outstring
+                  << std::endl;
+      }
     }
 
     saveTime_ += saveStep_;
