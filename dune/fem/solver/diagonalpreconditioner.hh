41f3e45b46d926baa9f8a3a23c8319cc7493409f
Mirko Kränkel <kraenkel@mathematik.uni-freiburg.de>
2011-05-31 19:16:04 +0000

    diagnalpreconditioner for use with inverseoperator
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@6131 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

---
+++ dune/fem/solver/diagonalpreconditioner.hh
@@ -0,0 +1,82 @@
+#ifndef DUNE_FEM_DIAGONALPRECONDITIONER_HH
+#define DUNE_FEM_DIAGONALPRECONDTIIONER_HH
+
+#include <dune/common/static_assert.hh>
+
+#include <dune/fem/function/common/discretefunction.hh>
+#include <dune/fem/operator/common/operator.hh>
+
+
+
+namespace Dune
+{
+
+
+// DiagonalPreconditioner
+//   // -----------------------
+//
+//     /** \class DiagonalPreconditioner
+//       *  \ingroup OEMSolver
+//       *  \brief   Precondtioner, multiplies with inverse of the diagonal
+//       *
+//       *  \param  DFImp type of the disctete function
+//       *  \param  MatrixImp type of the diagonal matrix
+//       //                    */
+//
+template<class DFImp,class MatrixImp>
+class DiagonalPreconditioner
+  :public Operator< typename DFImp::RangeFieldType,typename DFImp::RangeFieldType,DFImp,DFImp>
+{
+  typedef DFImp DiscreteFunctionType;
+  typedef MatrixImp MatrixType;
+  
+private:
+  const MatrixType& matrix_;
+
+public:
+  DiagonalPreconditioner(const MatrixType &matrix)
+    :matrix_(matrix)
+  {}
+
+ virtual  void operator()(const DFImp &u,DFImp &res) const
+  {
+    apply(u,res);
+  }
+
+//   void apply(const DFImp u,DFImp res)
+//       {
+//         typedef typename DFImp::DofIteratorType DofIteratorType;  
+//         typedef typename DFImp::ConstDofIteratorType ConstDofIteratorType;  
+     
+// 	DofIteratorType ret_it = res.dbegin(); 
+// 	ConstDofIteratorType f_it = u.dbegin();  
+// 	for(ret_it; ret_it!=res.dend(); ++ret_it)
+// 	  {
+// 	    int i=ret_it.index();
+// 	    ret_it=f_it;
+// 	    ret_it/=matrix_(i,i);
+// 	    ++f_it;    
+// 	  }
+//       } 
+
+protected:
+  void apply(const DFImp &u,DFImp &res) const
+  {
+    typedef typename DFImp::DofIteratorType DofIteratorType;  
+    typedef typename DFImp::ConstDofIteratorType ConstDofIteratorType;  
+    
+    DofIteratorType ret_it = res.dbegin(); 
+    ConstDofIteratorType f_it = u.dbegin();  
+    for(int i=0; ret_it!=res.dend(); ++ret_it)
+      {
+	(*ret_it)=(*f_it);
+        (*ret_it)/=matrix_(i,i);
+        ++f_it;    
+	++i;
+      }
+      }
+
+};
+ 
+
+}
