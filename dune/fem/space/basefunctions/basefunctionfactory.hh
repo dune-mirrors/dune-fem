97dc66ae0eb87bc5ae6f3a1e1f0eeecb02248ee3
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2011-08-25 20:17:43 +0000

    some minor cleanup
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@6319 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- dune/fem/space/basefunctions/basefunctionfactory.hh
+++ dune/fem/space/basefunctions/basefunctionfactory.hh
@@ -1,5 +1,5 @@
-#ifndef DUNE_BASEFUNCTIONFACTORY_HH
-#define DUNE_BASEFUNCTIONFACTORY_HH
+#ifndef DUNE_FEM_BASEFUNCTIONFACTORY_HH
+#define DUNE_FEM_BASEFUNCTIONFACTORY_HH
 
 #include <dune/common/geometrytype.hh>
 
@@ -11,30 +11,33 @@ namespace Dune
   //! \brief Interface class for the generation of base functions.
   //! For every concrete set of base functions, derive your own concrete
   //! base function factory.
-  template <class FunctionSpaceImp>
-  class BaseFunctionFactory 
+  template< class FunctionSpace >
+  struct BaseFunctionFactory 
   {
-  public:
-    typedef BaseFunctionInterface<FunctionSpaceImp> BaseFunctionType;
-  public:
+    typedef FunctionSpace FunctionSpaceType;
+
+    typedef BaseFunctionInterface< FunctionSpaceType > BaseFunctionType;
+
     //! constructor storing geometry type 
-    BaseFunctionFactory( const GeometryType &geo )
+    explicit BaseFunctionFactory ( const GeometryType &geo )
     : geo_( geo )
     {}
 
     virtual ~BaseFunctionFactory() {}
 
     //! return pointer to num-th base function 
-    virtual BaseFunctionType* baseFunction(int num) const = 0;
+    virtual BaseFunctionType *baseFunction ( int num ) const = 0;
 
     //! return number of base function 
-    virtual int numBaseFunctions() const = 0;
+    virtual int numBaseFunctions () const = 0;
 
     //! return geometry type to which base functions belong
-    GeometryType geometry() const { return geo_; }
+    const GeometryType &geometry () const { return geo_; }
+
   private:
     GeometryType geo_;
   };
 
-} // end namespace Dune
-#endif
+} // namespace Dune
+
+#endif // #ifndef DUNE_FEM_BASEFUNCTIONFACTORY_HH
