ee8269d77497c845e95fea8528a4a2c6c51f8348
Andreas Dedner <A.S.Dedner@warwick.ac.uk>
2011-09-16 09:32:34 +0000

    also test if index set is consecutive - if this was required
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@6342 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- dune/fem/gridpart/test/test-filteredgridpart.cc
+++ dune/fem/gridpart/test/test-filteredgridpart.cc
@@ -6,6 +6,7 @@
 
 // dune-common includes
 #include<dune/common/exceptions.hh>
+#include <dune/grid/common/genericreferenceelements.hh>
 
 // dune-fem includes
 #include <dune/fem/gridpart/adaptiveleafgridpart.hh>
@@ -19,42 +20,98 @@
 template< class GridPartType >
 void testGridPart( const GridPartType & gridPart )
 {
+  typedef typename GridPartType::IndexSetType IndexSetType;
+  typedef typename GridPartType::IndexSetType::IndexType IndexType;
+  const IndexSetType & indexSet = gridPart.indexSet();
+  IndexType maxIndex = 0;
   int count = 0;
+  std::vector< bool > isConsecutiveIndex(GridPartType::dimension+1,true);
+  std::vector< std::vector< bool > > consecutiveIndex(GridPartType::dimension+1);
+  for (int c=0;c<=GridPartType::dimension;++c)
+    consecutiveIndex[c].resize( indexSet.size(c), false );
+
   typedef typename GridPartType::template Codim< 0 >::IteratorType IteratorType;
   const IteratorType end = gridPart.template end< 0 >();
   for( IteratorType it = gridPart.template begin< 0 >(); it != end; ++it )
+  {
     ++count;
+    IndexType index = indexSet.index(*it);
+    maxIndex = std::max( index, maxIndex);
+
+    if (index >= consecutiveIndex[0].size()) isConsecutiveIndex[0] = false;
+    else consecutiveIndex[0][index] = true;
+    for (int c=0;c<=GridPartType::dimension;++c)
+    {
+      int nSubEn = Dune::GenericReferenceElements< typename GridPartType::GridType::ctype, GridPartType::dimension >::
+          general( it->type() ).size(c);
+      for (int i=0;i<nSubEn;++i)
+      {
+        IndexType index = indexSet.subIndex(*it,i,c);
+        if (index >= consecutiveIndex[c].size()) isConsecutiveIndex[c] = false;
+        else consecutiveIndex[c][index] = true;
+      }
+    }
+  }
 
   std::cout << "entities visited: " << count << std::endl;
   
-  typedef typename GridPartType::IndexSetType IndexSetType;
-  const IndexSetType & indexSet = gridPart.indexSet();
   std::cout << "entities in index set: " << indexSet.size( 0 ) << std::endl;
+  std::cout << "maximum value in index set: " << maxIndex << std::endl;
+
+  for (int c=0;c<=GridPartType::dimension;++c)
+  {
+    std::cout << "index set for codim " << c; 
+    if ( !isConsecutiveIndex[c] ) 
+    {
+      std::cout << " is not consecutive: too large index encountered" << std::endl;
+      continue;
+    }
+    for (unsigned int i=0;i<consecutiveIndex[c].size();++i)
+    {
+      if (!consecutiveIndex[c][i]) 
+      {
+        isConsecutiveIndex[c] = false;
+        break;
+      }
+    }
+    if ( !isConsecutiveIndex[c] ) 
+    {
+      std::cout << " is not consecutive: hole encountered" << std::endl;
+    }
+    else
+    {
+      std::cout << " is consecutive" << std::endl;
+    }
+  }
 }
 
 template< int codim, class GridPartType >
 void testSubEntities( const GridPartType & gridPart )
 {
+  typedef typename GridPartType::IndexSetType IndexSetType;
+  typedef typename GridPartType::IndexSetType::IndexType IndexType;
+  const IndexSetType & indexSet = gridPart.indexSet();
+  IndexType maxIndex = 0;
   int count = 0;
   typedef typename GridPartType::template Codim< codim >::IteratorType IteratorType;
   const IteratorType end = gridPart.template end< codim >();
   for( IteratorType it = gridPart.template begin< codim >(); it != end; ++it )
+  {
     ++count;
+    IndexType index = indexSet.index(*it);
+    maxIndex = std::max( index, maxIndex);
+  }
 
   std::cout << "codim " << codim << " subentities visited: " << count << std::endl;
 
-  typedef typename GridPartType::IndexSetType IndexSetType;
-  const IndexSetType & indexSet = gridPart.indexSet();
   std::cout << "entities in index set: " << indexSet.size( codim ) << std::endl;
-
+  std::cout << "maximum value in index set: " << maxIndex << std::endl;
 }
-
 
 typedef Dune::GridSelector::GridType GridType;
 typedef Dune::DGAdaptiveLeafGridPart< GridType > HostGridPartType;
 typedef Dune::Fem::RadialFilter< GridType::ctype, GridType::dimensionworld > BasicFilterType;
 typedef Dune::Fem::BasicFilterWrapper< HostGridPartType, BasicFilterType > FilterType;
-typedef Dune::Fem::FilteredGridPart< HostGridPartType, FilterType, true > GridPartType;
 
 int main ( int argc, char ** argv )
 {
@@ -73,11 +130,31 @@ int main ( int argc, char ** argv )
     BasicFilterType::GlobalCoordinateType center( 0 );
     BasicFilterType basicFilter( center, .25 );
     FilterType filter( hostGridPart, basicFilter );
-    GridPartType gridPart( hostGridPart, filter );
 
-    // run test
-    testGridPart( gridPart );
-    testSubEntities< GridType::dimension >( gridPart );
+    {
+      // allow non consecutive index set
+      typedef Dune::Fem::FilteredGridPart< HostGridPartType, FilterType, false > GridPartType;
+      GridPartType gridPart( hostGridPart, filter );
+      // run test
+      std::cout << "Non consecutive inex set: test using codim=0 iterator" << std::endl;
+      testGridPart( gridPart );
+      std::cout << std::endl;
+      std::cout << "Non consecutive inex set: test using codim=dimension iterator" << std::endl;
+      testSubEntities< GridType::dimension >( gridPart );
+      std::cout << std::endl << std::endl;
+    }
+    {
+      // force consecutive index set
+      typedef Dune::Fem::FilteredGridPart< HostGridPartType, FilterType, true > GridPartType;
+      GridPartType gridPart( hostGridPart, filter );
+      // run test
+      std::cout << "Consecutive inex set: test using codim=0 iterator" << std::endl;
+      testGridPart( gridPart );
+      std::cout << std::endl;
+      std::cout << "Consecutive inex set: test using codim=dimension iterator" << std::endl;
+      testSubEntities< GridType::dimension >( gridPart );
+      std::cout << std::endl << std::endl;
+    }
 
   } catch (Dune::Exception &e) {
     std::cerr << e << std::endl;
