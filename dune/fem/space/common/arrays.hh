1cf1ac0cd01d955cdd93f6394915e64046eaa8f5
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2011-09-27 09:11:39 +0000

    remove old version check
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@6394 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- dune/fem/space/common/arrays.hh
+++ dune/fem/space/common/arrays.hh
@@ -19,11 +19,8 @@
 // include BLAS for daxpy operation 
 #include <dune/fem/solver/oemsolver/cblas.h>
 #endif
-#if DUNE_VERSION_NEWER_REV( DUNE_GRID, 2, 1, 0 )
+
 #include <dune/grid/alugrid/common/interfaces.hh>
-#else
-#include <dune/grid/alugrid/interfaces.hh>
-#endif
 
 // include xdr wrapper 
 #include <dune/fem/io/streams/streams.hh>
