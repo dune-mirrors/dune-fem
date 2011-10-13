334b16ec5e8decfd4e487847179e0bd4cca24a4c
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2011-10-13 09:18:54 +0000

    make FieldMatrixConverter work seamlessly with DenseMatrix.
    Warning: You need to update dune-common for this to work
    
    @Tobi: Could you please check that, e.g., the frobenius_norm still works?
    
    
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@6407 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- dune/fem/misc/fmatrixconverter.hh
+++ dune/fem/misc/fmatrixconverter.hh
@@ -2,294 +2,327 @@
 #define DUNE_FIELDMATRIXCONVERTER_HH
 
 #include <cassert>
+
 #include <dune/common/fmatrix.hh>
 
-namespace Dune {
-
-template <class VectorType, class ConvertToType> 
-class FieldMatrixConverter;
-
-template <class VectorType, class ConvertToType> 
-struct DenseMatVecTraits< FieldMatrixConverter<VectorType,ConvertToType> >
+namespace Dune
 {
-  typedef FieldMatrixConverter<VectorType,ConvertToType> derived_type;
-  typedef DenseMatVecTraits< ConvertToType > Traits;
-  typedef typename Traits::container_type container_type;
-  typedef typename Traits::value_type value_type;
-  typedef typename Traits::size_type size_type;
-  typedef typename Traits::row_type row_type;
-};
 
-template< class K, int m > class FieldMatrixConverterRow;
-template< class K, int m >
-struct DenseMatVecTraits< FieldMatrixConverterRow<K,m> >
-{
-  typedef FieldMatrixConverterRow<K,m> derived_type;
-  typedef K* container_type;
-  typedef K value_type;
-  typedef size_t size_type;
-};
+  template< class VectorType, class ConvertToType >
+  class FieldMatrixConverter;
 
-// derive from dense vector to inherit functionality 
-template <class K, int m>  
-class FieldMatrixConverterRow : public DenseVector< FieldMatrixConverterRow< K , m > >
-{
-protected:  
-  K* ptr_;
+  template< class K, int m >
+  class FieldMatrixConverterRow;
 
-  typedef DenseVector< FieldMatrixConverterRow< K , m > >  BaseType ;
-public:  
-  typedef typename BaseType :: size_type size_type;
 
-  using BaseType :: operator = ;
-  using BaseType :: operator * ;
 
-  FieldMatrixConverterRow( K* ptr ) : ptr_( ptr ) 
+  template< typename K, int n, int m > 
+  struct DenseMatVecTraits< FieldMatrixConverter< FieldVector< K, n*m >, FieldMatrix< K, n, m > > >
   {
-    assert( ptr_ );
-  }
+    typedef FieldMatrixConverter< FieldVector< K, n*m >, FieldMatrix< K, n, m > > derived_type;
+    typedef DenseMatVecTraits< FieldMatrix< K, n, m > > Traits;
+    typedef typename Traits::container_type container_type;
+    typedef typename Traits::value_type value_type;
+    typedef typename Traits::size_type size_type;
+    typedef typename Traits::row_type row_type;
 
-  FieldMatrixConverterRow& operator = ( const FieldMatrixConverterRow& other )
-  {
-    for(size_t i=0; i<vec_size(); ++i ) 
-    {
-      vec_access( i ) = other[ i ];
-    }
-    return *this;
-  }
-
-  template <class Impl> 
-  FieldMatrixConverterRow& operator = ( const DenseVector< Impl >& other )
-  {
-    assert( other.size() == vec_size() );
-    for(size_t i=0; i<vec_size(); ++i ) 
-    {
-      vec_access( i ) = other[ i ];
-    }
-    return *this;
-  }
-
-  template <class V>
-  K operator* ( const DenseVector< V >& x ) const 
-  {
-    assert( vec_size() == x.size() );
-    K result( 0 );
-    for(size_type i=0; i<vec_size(); ++i )
-    {
-      result += vec_access( i ) * x[ i ];
-    }
-    return result;
-  }
-
-  // make this thing a vector
-  size_t vec_size() const { return m; }
-  K & vec_access(size_t i) { assert( i < vec_size() ); return ptr_[ i ]; }
-  const K & vec_access(size_t i) const { assert( i < vec_size() ); return ptr_[ i ]; }
-};
-
-
-//! convert a FieldVector with length n * m to a FieldMatrix with n rows and m cols 
-template <typename K, int n, int m> 
-class FieldMatrixConverter<FieldVector<K ,n * m> , FieldMatrix<K ,n, m> > 
-  : public DenseMatrix< FieldMatrixConverter<FieldVector<K ,n * m> , FieldMatrix<K ,n, m> > >
-{
-public:
-  //! internal storage of matrix 
-  typedef FieldVector<K ,n * m> InteralVectorType;
-
-  //! type of class return upon operator [] which behaves like a reference 
-  typedef FieldMatrixConverterRow<K , m> RowType;
-
-  //! type of class return upon operator [] which behaves like a reference 
-  typedef RowType row_type;
-
-  //! export the type representing the field
-  typedef K field_type;
-
-  //! export the type representing the components
-  typedef K block_type;
-
-  //! The type used for the index access and size operations.
-  typedef std::size_t size_type;
-
-  enum {
-    //! The number of rows.
-    rows = n,
-    //! The number of columns.
-    cols = m,
-    //! The total dimension 
-    dimension = n * m 
+    typedef FieldMatrixConverterRow< K, m > row_reference;
+    typedef FieldMatrixConverterRow< K, m > const_row_reference;
   };
 
-  FieldMatrixConverter(InteralVectorType& v) 
-    : vec_( v )
-#ifndef NDEBUG 
-    , mutableVec_( true )
-#endif
+  template< class K, int m >
+  struct DenseMatVecTraits< FieldMatrixConverterRow< K, m > >
   {
-  }
+    typedef FieldMatrixConverterRow< K, m > derived_type;
+    typedef K* container_type;
+    typedef K value_type;
+    typedef size_t size_type;
+  };
 
-  FieldMatrixConverter(const InteralVectorType& v) 
-    : vec_(const_cast<InteralVectorType&> (v) )
-#ifndef NDEBUG 
-    , mutableVec_( false )
-#endif
+  // derive from dense vector to inherit functionality 
+  template< class K, int m >
+  class FieldMatrixConverterRow
+  : public DenseVector< FieldMatrixConverterRow< K , m > >
   {
-  }
+    typedef DenseVector< FieldMatrixConverterRow< K , m > > Base;
 
-  FieldMatrixConverter(const FieldMatrixConverter& other) 
-    : vec_( other.vec_ )
-#ifndef NDEBUG 
-    , mutableVec_( other.mutableVec_ )
-#endif
-  {}
+  public:  
+    typedef typename Base::size_type size_type;
 
-  // return row 
-  RowType operator [] (const size_t row) 
-  {
-    assert( mutableVec_ );
-    return RowType( (&vec_[ row * cols ]) );
-  }
-  
-  // return row  
-  RowType operator [] (const size_t row) const 
-  {
-    return RowType( (&vec_[ row * cols ]) );
-  }
+    using Base::operator =;
+    using Base::operator *;
 
-  template<class X, class Y>
-  void mv(const X& x, Y& y) const 
-  {
-    for(size_type i=0; i<n; ++i) 
+    FieldMatrixConverterRow ( K *ptr )
+    : ptr_( ptr )
     {
-      y[ i ] = (*this)[ i ] * x;
+      assert( ptr_ );
     }
-  }
 
-  template<class X, class Y>
-  void umv(const X& x, Y& y) const 
-  {
-    for(size_type i=0; i<n; ++i) 
+    FieldMatrixConverterRow& operator = ( const FieldMatrixConverterRow& other )
     {
-      y[ i ] += (*this)[ i ] * x;
+      for(size_t i=0; i<vec_size(); ++i ) 
+      {
+        vec_access( i ) = other[ i ];
+      }
+      return *this;
     }
-  }
 
-  template<class X, class Y>
-  void mtv(const X& x, Y& y) const 
-  {
-    for( size_type i = 0; i < cols; ++i )
+    template <class Impl> 
+    FieldMatrixConverterRow& operator = ( const DenseVector< Impl >& other )
     {
-      y[ i ] = 0;
-      for( size_type j = 0; j < rows; ++j )
-        y[ i ] += (*this)[ j ][ i ] * x[ j ];
+      assert( other.size() == vec_size() );
+      for(size_t i=0; i<vec_size(); ++i ) 
+      {
+        vec_access( i ) = other[ i ];
+      }
+      return *this;
     }
-  }
 
-  template<class X, class Y>
-  void umtv(const X& x, Y& y) const 
-  {
-    for( size_type i = 0; i < cols; ++i )
+    template <class V>
+    K operator* ( const DenseVector< V >& x ) const 
     {
-      for( size_type j = 0; j < rows; ++j )
-        y[ i ] += (*this)[ j ][ i ] * x[ j ];
+      assert( vec_size() == x.size() );
+      K result( 0 );
+      for(size_type i=0; i<vec_size(); ++i )
+      {
+        result += vec_access( i ) * x[ i ];
+      }
+      return result;
     }
-  }
 
-  K frobenius_norm() const
+    // make this thing a vector
+    size_t vec_size() const { return m; }
+    K & vec_access(size_t i) { assert( i < vec_size() ); return ptr_[ i ]; }
+    const K & vec_access(size_t i) const { assert( i < vec_size() ); return ptr_[ i ]; }
+
+  private:
+    K *ptr_;
+  };
+
+
+
+
+  //! convert a FieldVector with length n * m to a FieldMatrix with n rows and m cols 
+  template< typename K, int n, int m > 
+  class FieldMatrixConverter< FieldVector< K, n*m >, FieldMatrix< K, n, m > >
+  : public DenseMatrix< FieldMatrixConverter< FieldVector< K , n*m >, FieldMatrix< K, n, m > > >
   {
-    K ret(0);
-    for( size_type i = 0; i < cols; ++i )
+    typedef DenseMatrix< FieldMatrixConverter< FieldVector< K , n*m >, FieldMatrix< K, n, m > > > Base;
+
+  public:
+    //! internal storage of matrix 
+    typedef FieldVector<K ,n * m> InteralVectorType;
+
+    //! type of class return upon operator [] which behaves like a reference
+    //typedef FieldMatrixConverterRow< K , m> RowType;
+
+    typedef typename Base::row_type row_type;
+    typedef typename Base::row_reference row_reference;
+    typedef typename Base::const_row_reference const_row_reference;
+
+    //! export the type representing the field
+    typedef K field_type;
+
+    //! export the type representing the components
+    typedef K block_type;
+
+    //! The type used for the index access and size operations.
+    typedef std::size_t size_type;
+
+    enum {
+      //! The number of rows.
+      rows = n,
+      //! The number of columns.
+      cols = m,
+      //! The total dimension 
+      dimension = n * m 
+    };
+
+    FieldMatrixConverter(InteralVectorType& v) 
+      : vec_( v )
+  #ifndef NDEBUG 
+      , mutableVec_( true )
+  #endif
     {
-      for( size_type j = 0; j < rows; ++j )
-        ret += (*this)[ i ][ j ] * (*this)[ i ][ j ];
     }
-    return std::sqrt(ret);
-  }
+
+    FieldMatrixConverter(const InteralVectorType& v) 
+      : vec_(const_cast<InteralVectorType&> (v) )
+  #ifndef NDEBUG 
+      , mutableVec_( false )
+  #endif
+    {
+    }
+
+    FieldMatrixConverter(const FieldMatrixConverter& other) 
+      : vec_( other.vec_ )
+  #ifndef NDEBUG 
+      , mutableVec_( other.mutableVec_ )
+  #endif
+    {}
+
+#if 0
+    // return row 
+    RowType operator [] (const size_t row) 
+    {
+      assert( mutableVec_ );
+      return RowType( (&vec_[ row * cols ]) );
+    }
     
-
-  FieldMatrixConverter& operator = (const FieldMatrixConverter& other) 
-  {
-    assert( mutableVec_ );
-    vec_ = other.vec_;
-#ifndef NDEBUG 
-    mutableVec_ = other.mutableVec_;
-#endif
-    return *this;
-  }
-
-  FieldMatrixConverter& operator = (const FieldMatrix<K, n, m>& matrix) 
-  {
-    assert( mutableVec_ );
-    for(size_t i=0; i< rows; ++i) 
+    // return row  
+    RowType operator [] (const size_t row) const 
     {
-      for(size_t j=0; j<cols; ++j)
+      return RowType( (&vec_[ row * cols ]) );
+    }
+
+    template<class X, class Y>
+    void mv(const X& x, Y& y) const 
+    {
+      for(size_type i=0; i<n; ++i) 
       {
-        vec_[ i * cols + j ] = matrix[ i ][ j ];
+        y[ i ] = (*this)[ i ] * x;
       }
     }
-    return *this;
-  }
-  FieldMatrixConverter& operator += (const FieldMatrix<K, n, m>& matrix) 
-  {
-    assert( mutableVec_ );
-    for(size_t i=0; i< rows; ++i) 
+
+    template<class X, class Y>
+    void umv(const X& x, Y& y) const 
     {
-      for(size_t j=0; j<cols; ++j)
+      for(size_type i=0; i<n; ++i) 
       {
-        vec_[ i * cols + j ] += matrix[ i ][ j ];
+        y[ i ] += (*this)[ i ] * x;
       }
     }
-    return *this;
-  }
 
-  /** \brief Sends the matrix to an output stream */
-  friend std::ostream& operator<< (std::ostream& s, 
-                                   const FieldMatrixConverter& a)
-  {
-    for (size_type i=0; i<n; ++i)
+    template<class X, class Y>
+    void mtv(const X& x, Y& y) const 
     {
-      for(size_type j=0; j<m; ++j)
-        s << a[ i ][ j ] << " "; 
-      s << std::endl;
+      for( size_type i = 0; i < cols; ++i )
+      {
+        y[ i ] = 0;
+        for( size_type j = 0; j < rows; ++j )
+          y[ i ] += (*this)[ j ][ i ] * x[ j ];
+      }
     }
-    return s;
-  }
 
-protected:
-  mutable InteralVectorType& vec_;
-#ifndef NDEBUG 
-  bool mutableVec_;
-#endif
-};
-
-// method that implements the operator= of a FieldMatrix taking a FieldMatrixConverter
-template<class K, int n, int m>
-inline void istl_assign_to_fmatrix(FieldMatrix<K,n,m>& A, 
-    const FieldMatrixConverter< FieldVector<K, n * m> , FieldMatrix<K,n,m> >& B)
-{
-  for(size_t i=0; i<n; ++i) 
-  {
-    for(size_t j=0; j<m; ++j)
+    template<class X, class Y>
+    void umtv(const X& x, Y& y) const 
     {
-      A[ i ][ j ] = B[ i ][ j ];
+      for( size_type i = 0; i < cols; ++i )
+      {
+        for( size_type j = 0; j < rows; ++j )
+          y[ i ] += (*this)[ j ][ i ] * x[ j ];
+      }
     }
-  }
-}
-template<class K, int n, int m>
-inline void istl_assign_to_fmatrix(DenseMatrix<FieldMatrix<K,n,m> >& A, 
-    const FieldMatrixConverter< FieldVector<K, n * m> , FieldMatrix<K,n,m> >& B)
-{
-  for(size_t i=0; i<n; ++i) 
-  {
-    for(size_t j=0; j<m; ++j)
+
+    K frobenius_norm() const
     {
-      A[ i ][ j ] = B[ i ][ j ];
+      K ret(0);
+      for( size_type i = 0; i < cols; ++i )
+      {
+        for( size_type j = 0; j < rows; ++j )
+          ret += (*this)[ i ][ j ] * (*this)[ i ][ j ];
+      }
+      return std::sqrt(ret);
+    }
+#endif 
+
+    FieldMatrixConverter& operator = (const FieldMatrixConverter& other) 
+    {
+      assert( mutableVec_ );
+      vec_ = other.vec_;
+  #ifndef NDEBUG 
+      mutableVec_ = other.mutableVec_;
+  #endif
+      return *this;
+    }
+
+    FieldMatrixConverter& operator = (const FieldMatrix<K, n, m>& matrix) 
+    {
+      assert( mutableVec_ );
+      for(size_t i=0; i< rows; ++i) 
+      {
+        for(size_t j=0; j<cols; ++j)
+        {
+          vec_[ i * cols + j ] = matrix[ i ][ j ];
+        }
+      }
+      return *this;
+    }
+    FieldMatrixConverter& operator += (const FieldMatrix<K, n, m>& matrix) 
+    {
+      assert( mutableVec_ );
+      for(size_t i=0; i< rows; ++i) 
+      {
+        for(size_t j=0; j<cols; ++j)
+        {
+          vec_[ i * cols + j ] += matrix[ i ][ j ];
+        }
+      }
+      return *this;
+    }
+
+    /** \brief Sends the matrix to an output stream */
+    friend std::ostream& operator<< (std::ostream& s, 
+                                     const FieldMatrixConverter& a)
+    {
+      for (size_type i=0; i<n; ++i)
+      {
+        for(size_type j=0; j<m; ++j)
+          s << a[ i ][ j ] << " "; 
+        s << std::endl;
+      }
+      return s;
+    }
+
+    // make this thing a matrix
+    size_type mat_rows() const { return rows; }
+    size_type mat_cols() const { return cols; }
+
+    row_reference mat_access ( size_type i )
+    {
+      assert( i < rows );
+      return row_reference( (&vec_[ i * cols ]) );
+    }
+
+    const_row_reference mat_access ( size_type i ) const
+    {
+      assert( i < rows );
+      return const_row_reference( (&vec_[ i * cols ]) );
+    }
+
+  protected:
+    mutable InteralVectorType& vec_;
+  #ifndef NDEBUG 
+    bool mutableVec_;
+  #endif
+  };
+
+  // method that implements the operator= of a FieldMatrix taking a FieldMatrixConverter
+  template<class K, int n, int m>
+  inline void istl_assign_to_fmatrix(FieldMatrix<K,n,m>& A, 
+      const FieldMatrixConverter< FieldVector<K, n * m> , FieldMatrix<K,n,m> >& B)
+  {
+    for(size_t i=0; i<n; ++i) 
+    {
+      for(size_t j=0; j<m; ++j)
+      {
+        A[ i ][ j ] = B[ i ][ j ];
+      }
     }
   }
-}
+  template<class K, int n, int m>
+  inline void istl_assign_to_fmatrix(DenseMatrix<FieldMatrix<K,n,m> >& A, 
+      const FieldMatrixConverter< FieldVector<K, n * m> , FieldMatrix<K,n,m> >& B)
+  {
+    for(size_t i=0; i<n; ++i) 
+    {
+      for(size_t j=0; j<m; ++j)
+      {
+        A[ i ][ j ] = B[ i ][ j ];
+      }
+    }
+  }
 
+} // namespace Dune
 
-
-} // end namespace Dune 
 #endif // end DUNE_FIELDMATRIXCONVERTER_HH
