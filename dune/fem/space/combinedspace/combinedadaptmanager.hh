4bc219f0c76ce389747424471257ac3bc1b7ddd8
Tobias Malkmus <tomalk@mathematik.uni-freiburg.de>
2011-12-02 14:45:47 +0000

    adaptaion to the new restr. prolong conzept
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@6567 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- dune/fem/space/combinedspace/combinedadaptmanager.hh
+++ dune/fem/space/combinedspace/combinedadaptmanager.hh
@@ -9,12 +9,6 @@
 //- local includes 
 #include "combinedspace.hh"
 
-//----------------------------------------------------------------------
-//-   1) Gewichte zwischen Vater/Sohn default Implementieren auf Gitter
-//-      (father.weight(son) oder so
-//-   2) Caching der Basisfunktionen fuer Vater-BasisFunktionen fuer
-//-      Kinderquadraturen
-//----------------------------------------------------------------------
 
 namespace Dune
 {
@@ -24,226 +18,30 @@ namespace Dune
 **/
 
 #ifdef USE_OLD_COMBINEDSPACE
-//***********************************************************************
-/** \brief This is a restriction/prolongation operator for combined DG data. 
- */
-template< class DiscreteFunctionImp, int polOrd >
-class RestrictProlongCombinedSpace
-: public RestrictProlongInterfaceDefault< RestrictProlongTraits
-  < RestrictProlongCombinedSpace< DiscreteFunctionImp,polOrd >,
-    typename DiscreteFunctionImp::DiscreteFunctionSpaceType::GridType::ctype > >
-{
-  typedef RestrictProlongInterfaceDefault< RestrictProlongTraits
-    < RestrictProlongCombinedSpace< DiscreteFunctionImp, polOrd >,
-      typename DiscreteFunctionImp::DiscreteFunctionSpaceType::GridType::ctype > >
-    BaseType;
-
-  typedef typename BaseType::DomainFieldType DomainFieldType;
-
-public:
-  typedef DiscreteFunctionImp DiscreteFunctionType;
-
-  typedef typename DiscreteFunctionType::DiscreteFunctionSpaceType
-    DiscreteFunctionSpaceType;
-  typedef typename DiscreteFunctionType::LocalFunctionType LocalFunctionType;
-
-  typedef typename DiscreteFunctionSpaceType::GridPartType GridPartType;
-  typedef typename DiscreteFunctionSpaceType::GridType GridType;
-
-  typedef typename DiscreteFunctionSpaceType::RangeType  RangeType;
-  typedef typename DiscreteFunctionSpaceType::DomainType DomainType;
-
-  typedef CachingQuadrature<GridPartType,0> QuadratureType;
-  typedef typename GridType::template Codim<0>::Entity::LocalGeometry LocalGeometry;
-
-  static const int dimRange = DiscreteFunctionSpaceType::dimRange;
-
-protected:
-  using BaseType :: calcWeight;
-  using BaseType :: entitiesAreCopies;
-
-public:  
-  //! Constructor
-  explicit RestrictProlongCombinedSpace( DiscreteFunctionType &df )
-  : df_( df ),
-    quadord_( 2*df_.space().order() ),
-    weight_( -1.0 )
-  {}
-
-  /** \brief explicit set volume ratio of son and father
-   *
-   *  \param[in]  weight  volume of son / volume of father
-   *
-   *  \note If this ratio is set, it is assume to be constant.
-   */
-  void setFatherChildWeight ( const DomainFieldType &weight ) const
+  /** \brief specialization of RestrictProlongDefault for
+      CombinedSpace.
+  */
+  template < class DiscreteFunctionSpaceImp, 
+             int N, 
+             DofStoragePolicy policy> 
+  struct DefaultLocalRestrictProlong< CombinedSpace<DiscreteFunctionSpaceImp,N,policy> > 
+  : public DiscontinuousGalerkinLocalRestrictProlong< CombinedSpace<DiscreteFunctionSpaceImp,N,policy> >
   {
-    weight_ = weight;
-  }
-
-  //! restrict data to father 
-  template< class EntityType >
-  void restrictLocal ( const EntityType &father, const EntityType &son, bool initialize ) const
-  {
-    assert( !df_.space().continuous() );
-
-    // if father and son are copies, do nothing 
-    if( entitiesAreCopies( df_.space().indexSet(), father, son ) )
-      return;
-    
-    typedef typename DiscreteFunctionSpaceType::DomainFieldType DomainFieldType;
-
-    assert( !father.isLeaf() );
-    const DomainFieldType weight = (weight_ < 0.0) ? calcWeight( father, son) : weight_;
-
-    LocalFunctionType vati = df_.localFunction( father);
-    LocalFunctionType sohn = df_.localFunction( son   );
-
-    if(initialize) 
+    DefaultLocalRestrictProlong( const CombinedSpace<DiscreteFunctionSpaceImp,N,policy> & space)
     {
-      vati.clear();
+      assert( !space.continuous() );
     }
-    
-    const LocalGeometry& geometryInFather = son.geometryInFather();
-
-    RangeType value ;
-
-    QuadratureType quad( son, quadord_);
-
-    const int nop = quad.nop();
-    for(int qP = 0; qP < nop; ++qP) 
-    {
-      sohn.evaluate( quad[ qP ], value );
-      // calculate factor 
-      const DomainFieldType intel = quad.weight(qP) * weight;
-
-      // apply weight 
-      value *= intel;
-
-      // apply axpy on father 
-      vati.axpy( geometryInFather.global(quad.point(qP)), value );
-    }
-  }
-
-  //! prolong data to children 
-  template <class EntityType>
-  void prolongLocal ( EntityType &father, EntityType &son, bool initialize ) const
-  {
-    assert( !df_.space().continuous() );
-
-    // if father and son are copies, do nothing 
-    if( this->entitiesAreCopies( df_.space().indexSet(), father, son ) ) return ; 
-    
-    // get local functions 
-    LocalFunctionType vati = df_.localFunction( father);
-    LocalFunctionType sohn = df_.localFunction( son   );
-
-    // set sohn to zero
-    sohn.clear();
-
-    // get quadrature 
-    QuadratureType quad( son, quadord_ );
-
-    // get geometry 
-    const LocalGeometry& geometryInFather = son.geometryInFather();
-    
-    RangeType value ;
-
-    // get number of points 
-    const int nop = quad.nop();
-    for(int qP = 0; qP < nop; ++qP) 
-    {
-      // evaluate father 
-      vati.evaluate( geometryInFather.global( quad.point(qP) ), value );
-
-      // apply weight 
-      value *= quad.weight(qP);
-
-      // add to son 
-      sohn.axpy( quad[ qP ], value );
-    }
-  }
-
-  //! add discrete function to communicator 
-  template <class CommunicatorImp>
-  void addToList(CommunicatorImp& comm)
-  {
-    comm.addToList(df_);
-  }
-
-private:
-  mutable DiscreteFunctionType & df_;
-  int quadord_;
-  mutable DomainFieldType weight_;
-};
-
-/** \brief This is a restriction/prolongation operator for 
-    combined DG data with polynomial order 0. 
-*/
-template <class DiscreteFunctionImp> 
-class RestrictProlongCombinedSpace<DiscreteFunctionImp,0>
-: public RestrictProlongPieceWiseConstantData<DiscreteFunctionImp> 
-{
-public:
-  typedef DiscreteFunctionImp DiscreteFunctionType;
-  typedef RestrictProlongPieceWiseConstantData<DiscreteFunctionImp>  BaseType; 
-public:  
-  //! Constructor
-  RestrictProlongCombinedSpace( DiscreteFunctionType & df ) :
-    BaseType(df)
-  {}
-};
-
-/** \brief specialization of RestrictProlongDefault for
-    CombinedSpace.
-*/
-template <class DiscFunc,
-          class DiscreteFunctionSpaceImp, 
-          int N, 
-          DofStoragePolicy policy> 
-class RestrictProlongDefaultImplementation< 
-  DiscFunc,CombinedSpace<DiscreteFunctionSpaceImp,N,policy> > 
-: public RestrictProlongDefault CombinedSpace<
-  DiscFunc,DiscreteFunctionSpaceImp :: polynomialOrder >
-{
-public:
-  //! type of discrete function 
-  typedef DiscFunc DiscreteFunctionType;
-  //! type of base class  
-  typedef RestrictProlongCombinedSpace<
-      DiscFunc, DiscreteFunctionSpaceImp :: polynomialOrder > BaseType;
-public:  
-  //! Constructor
-  RestrictProlongDefaultImplementation ( DiscreteFunctionType & df ) : 
-    BaseType(df) 
-  {
-  }
-};
+  };
 #else 
-template <class DiscFunc,
-          class DiscreteFunctionSpaceImp, 
-          int N, 
-          DofStoragePolicy policy> 
-class RestrictProlongDefaultImplementation< 
-  DiscFunc, CombinedSpace<DiscreteFunctionSpaceImp,N,policy> > 
-: public RestrictProlongDefaultImplementation< 
-      DiscFunc, 
-      typename CombinedSpace<DiscreteFunctionSpaceImp,N,policy> :: BaseType > 
-{
-public:
-  //! type of discrete function 
-  typedef DiscFunc DiscreteFunctionType;
-
-  typedef RestrictProlongDefaultImplementation< 
-          DiscFunc, 
-          typename CombinedSpace<DiscreteFunctionSpaceImp,N,policy> :: BaseType > BaseType;
-public:  
-  //! Constructor
-  RestrictProlongDefaultImplementation ( DiscreteFunctionType & df ) : 
-    BaseType(df) 
+  template < class DiscreteFunctionSpaceImp, 
+             int N, 
+             DofStoragePolicy policy> 
+  struct DefaultLocalRestrictProlong< CombinedSpace<DiscreteFunctionSpaceImp,N,policy> > 
+  : public EmptyLocalRestrictProlong< CombinedSpace<DiscreteFunctionSpaceImp,N,policy> >
   {
-  }
-};
+    DefaultLocalRestrictProlong( const CombinedSpace<DiscreteFunctionSpaceImp,N,policy> & )
+    {}
+  };
 #endif 
 
 ///@}
