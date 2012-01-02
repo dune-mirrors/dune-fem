62f3985c9784171074900270ad074acf9691f00e
Robert Klöfkorn <robertk@mathematik.uni-stuttgart.de>
2012-01-02 16:45:13 +0000

    make link with gcc-4.6.1
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/branches/release-1.2@6586 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- dune/fem/space/lagrangespace/basefunctions.cc
+++ dune/fem/space/lagrangespace/basefunctions.cc
@@ -5,44 +5,25 @@
 namespace Dune
 {
 
-  namespace Library
-  {
+  template class LagrangeBaseFunctionFactory< FunctionSpace< double, double, 1, 1 >, 1, 1 >;
+  template class LagrangeBaseFunctionFactory< FunctionSpace< double, double, 2, 1 >, 2, 1 >;
+  template class LagrangeBaseFunctionFactory< FunctionSpace< double, double, 3, 1 >, 3, 1 >;
+  template class LagrangeBaseFunctionFactory< FunctionSpace< float, float, 1, 1 >, 1, 1 >;
+  template class LagrangeBaseFunctionFactory< FunctionSpace< float, float, 2, 1 >, 2, 1 >;
+  template class LagrangeBaseFunctionFactory< FunctionSpace< float, float, 3, 1 >, 3, 1 >;
 
-    template< class Field, unsigned int dim, unsigned int pOrder >
-    void Instantiate_LagrangeBaseFunctionFactory ()
-    {
-      LagrangeBaseFunctionFactory
-        < FunctionSpace< Field, Field, dim, 1 >, dim, pOrder >
-        factory( GeometryType( GeometryType :: simplex, dim ) );
-      factory.numBaseFunctions();
-    }
+  template class LagrangeBaseFunctionFactory< FunctionSpace< double, double, 1, 1 >, 1, 2 >;
+  template class LagrangeBaseFunctionFactory< FunctionSpace< double, double, 2, 1 >, 2, 2 >;
+  template class LagrangeBaseFunctionFactory< FunctionSpace< double, double, 3, 1 >, 3, 2 >;
+  template class LagrangeBaseFunctionFactory< FunctionSpace< float, float, 1, 1 >, 1, 2 >;
+  template class LagrangeBaseFunctionFactory< FunctionSpace< float, float, 2, 1 >, 2, 2 >;
+  template class LagrangeBaseFunctionFactory< FunctionSpace< float, float, 3, 1 >, 3, 2 >;
 
-    void Instantiate_LagrangeSpace_Templates ()
-    {
-      Instantiate_LagrangeBaseFunctionFactory< double, 1, 1 >();
-      Instantiate_LagrangeBaseFunctionFactory< double, 2, 1 >();
-      Instantiate_LagrangeBaseFunctionFactory< double, 3, 1 >();
-
-      Instantiate_LagrangeBaseFunctionFactory< float, 1, 1 >();
-      Instantiate_LagrangeBaseFunctionFactory< float, 2, 1 >();
-      Instantiate_LagrangeBaseFunctionFactory< float, 3, 1 >();
-      
-      Instantiate_LagrangeBaseFunctionFactory< double, 1, 2 >();
-      Instantiate_LagrangeBaseFunctionFactory< double, 2, 2 >();
-      Instantiate_LagrangeBaseFunctionFactory< double, 3, 2 >();
-      
-      Instantiate_LagrangeBaseFunctionFactory< float, 1, 2 >();
-      Instantiate_LagrangeBaseFunctionFactory< float, 2, 2 >();
-      Instantiate_LagrangeBaseFunctionFactory< float, 3, 2 >();
-
-      Instantiate_LagrangeBaseFunctionFactory< double, 1, 3 >();
-      Instantiate_LagrangeBaseFunctionFactory< double, 2, 3 >();
-      Instantiate_LagrangeBaseFunctionFactory< double, 3, 3 >();
-      
-      Instantiate_LagrangeBaseFunctionFactory< float, 1, 3 >();
-      Instantiate_LagrangeBaseFunctionFactory< float, 2, 3 >();
-      Instantiate_LagrangeBaseFunctionFactory< float, 3, 3 >();
-    }
-  }
+  template class LagrangeBaseFunctionFactory< FunctionSpace< double, double, 1, 1 >, 1, 3 >;
+  template class LagrangeBaseFunctionFactory< FunctionSpace< double, double, 2, 1 >, 2, 3 >;
+  template class LagrangeBaseFunctionFactory< FunctionSpace< double, double, 3, 1 >, 3, 3 >;
+  template class LagrangeBaseFunctionFactory< FunctionSpace< float, float, 1, 1 >, 1, 3 >;
+  template class LagrangeBaseFunctionFactory< FunctionSpace< float, float, 2, 1 >, 2, 3 >;
+  template class LagrangeBaseFunctionFactory< FunctionSpace< float, float, 3, 1 >, 3, 3 >;
   
-}
+} // namespace Dune
