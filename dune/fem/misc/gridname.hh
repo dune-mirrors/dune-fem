f8787a8e2d3f5d9a0efea0c2cf7630f4a96380c5
Tobias Malkmus <tomalk@mathematik.uni-freiburg.de>
2012-07-16 09:31:04 +0000

    add new gridpart names patch
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/branches/release-1.3@7289 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- dune/fem/misc/gridname.hh
+++ dune/fem/misc/gridname.hh
@@ -55,6 +55,8 @@ namespace Dune
         knownGrids.push_back( "OneDGrid" );
         knownGrids.push_back( "GeometryGrid" );
         knownGrids.push_back( "CartesianGrid" );
+        knownGrids.push_back( "CacheItGrid" );
+        knownGrids.push_back( "ParallelGrid" );
         knownGrids.push_back( "ParallelSimplexGrid" );
         knownGrids.push_back( "SPGrid" );
         knownGrids.push_back( "P4estGrid" );
