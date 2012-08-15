32ec950b01d8211384bfd9544926ce8886ef6359
Tobias Malkmus <tomalk@mathematik.uni-freiburg.de>
2012-08-15 13:11:13 +0000

    intendending + namespace Fem adaptation
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@7465 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- dune/fem/operator/common/operator.hh
+++ dune/fem/operator/common/operator.hh
@@ -121,73 +121,73 @@ namespace Dune
       static const bool assembled DUNE_DEPRECATED_MSG("Please derive from Dune:Fem::AssembledOperator") = true;
     };
 	      
-  } // end namespace Dune::Fem
+  } // namespace Fem
 
 
 
-/** @addtogroup OperatorCommon 
-    Operators are mappings from function spaces into function spaces.
+  /** @addtogroup OperatorCommon 
+      Operators are mappings from function spaces into function spaces.
 
-    \remarks 
-    The most general interface for a mapping is defined by Mapping. From
-    Mapping the Function is derived and also Operator is derived. 
-    Operator defines the interface for operations on discrete functions
-    while Function is an interface for functions like sinus.
+      \remarks 
+      The most general interface for a mapping is defined by Mapping. From
+      Mapping the Function is derived and also Operator is derived. 
+      Operator defines the interface for operations on discrete functions
+      while Function is an interface for functions like sinus.
+      
+      @{
+   */
+
+  /** \brief An abstract operator
+   Interface class for Operators. Operators are applied to Functions and
+   the result is a Function again. 
+
+   \interfaceclass
+  */
+  template <typename DFieldType, typename RFieldType,
+            typename DType , typename RType>
+  class Operator
+  : public Fem::Mapping < DFieldType, RFieldType, DType, RType >,
+    public Fem::Operator< DType, RType >
+  {
+    typedef Fem::Operator< DType, RType > BaseType;
+
+  protected: 
+    //! \brief type of mapping base class 
+    typedef Fem::Mapping <DFieldType,RFieldType,DType,RType> MappingType;
     
-    @{
- */
+  public:
+    //- remember template parameters for derived classes  
+    typedef DType DomainType;
+    typedef RType  RangeType;
+    typedef DFieldType DomainFieldType;
+    typedef RFieldType RangeFieldType;
 
-/** \brief An abstract operator
- Interface class for Operators. Operators are applied to Functions and
- the result is a Function again. 
-
- \interfaceclass
-*/
-template <typename DFieldType, typename RFieldType,
-          typename DType , typename RType>
-class Operator
-: public Mapping <DFieldType,RFieldType,DType,RType>,
-  public Fem::Operator< DType, RType >
-{
-  typedef Fem::Operator< DType, RType > BaseType;
-
-protected: 
-  //! \brief type of mapping base class 
-  typedef Mapping <DFieldType,RFieldType,DType,RType> MappingType;
-  
-public:
-  //- remember template parameters for derived classes  
-  typedef DType DomainType;
-  typedef RType  RangeType;
-  typedef DFieldType DomainFieldType;
-  typedef RFieldType RangeFieldType;
-
-  using BaseType::operator();
+    using BaseType::operator();
 
 #if 0
-  /** \brief Application operator 
-      \param[in] arg argument 
-      \param[out] dest destination 
-      \note This method has to be implemented by all derived classes. 
-  */
-  virtual void operator() (const DomainType& arg, RangeType& dest) const = 0;
+    /** \brief Application operator 
+        \param[in] arg argument 
+        \param[out] dest destination 
+        \note This method has to be implemented by all derived classes. 
+    */
+    virtual void operator() (const DomainType& arg, RangeType& dest) const = 0;
 #endif
- 
-protected:
-  /** \brief The method apply calls the application operator. The method 
-      has to be implemented here, because this method called when a mapping list 
-      is evaluated. 
-      \param[in] arg argument 
-      \param[out] dest destination 
-  */
-  virtual void apply (const DomainType& arg, RangeType& dest) const 
-  {
-    this->operator() (arg, dest); 
-  }
-}; // end class Operator 
+   
+  protected:
+    /** \brief The method apply calls the application operator. The method 
+        has to be implemented here, because this method called when a mapping list 
+        is evaluated. 
+        \param[in] arg argument 
+        \param[out] dest destination 
+    */
+    virtual void apply (const DomainType& arg, RangeType& dest) const 
+    {
+      this->operator() (arg, dest); 
+    }
+  }; // class Operator 
 
-///@}
+  ///@}
 
-} // end namespace Dune 
+} // namespace Dune 
 
 #endif // #ifndef DUNE_FEM_OPERATOR_HH
