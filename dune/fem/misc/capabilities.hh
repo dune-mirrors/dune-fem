f9e800f410daee0a4c459c88608f37045d6c1512
Tobias Malkmus <tomalk@mathematik.uni-freiburg.de>
2012-08-20 07:40:20 +0000

    move "core capabilites" to namespace Dune to be consitent with the core modules
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@7498 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- dune/fem/misc/capabilities.hh
+++ dune/fem/misc/capabilities.hh
@@ -13,88 +13,97 @@
 namespace Dune
 {
 
+  // Core Capabilities
+  // -----------------
+
+  namespace Capabilities
+  {
+
+    // hasHierarchicIndexSet
+    // ---------------------
+
+    template< class Grid >
+    struct hasHierarchicIndexSet;
+
+    template< class Grid >
+    struct hasHierarchicIndexSet< const Grid >
+    {
+      static const bool v = hasHierarchicIndexSet< Grid >::v;
+    };
+
+    template< class Grid >
+    struct hasHierarchicIndexSet
+    {
+      static const bool v = Dune::Capabilities::hasHierarchicIndexSet< Grid >::v;
+    };
+
+    template< int dim, int dimw >
+    struct hasHierarchicIndexSet< ALUConformGrid< dim, dimw > >
+    {
+      static const bool v = true;
+    };
+
+    template< int dim, int dimw >
+    struct hasHierarchicIndexSet< ALUCubeGrid< dim, dimw > >
+    {
+      static const bool v = true;
+    };
+
+    template< int dim, int dimw >
+    struct hasHierarchicIndexSet< ALUSimplexGrid< dim, dimw > >
+    {
+      static const bool v = true;
+    };
+
+    template< int dim, int dimw, ALUGridElementType elType, ALUGridRefinementType refineType, class Comm >
+    struct hasHierarchicIndexSet< ALUGrid< dim, dimw, elType, refineType, Comm > >
+    {
+      static const bool v = true;
+    };
+
+    template<>
+    struct hasHierarchicIndexSet< OneDGrid >
+    {
+      static const bool v = false;
+    };
+
+    template< int dim, int dimw, class ctype >
+    struct hasHierarchicIndexSet< SGrid< dim, dimw, ctype > >
+    {
+      static const bool v = false;
+    };
+
+    template< int dim >
+    struct hasHierarchicIndexSet< UGGrid< dim > >
+    {
+      static const bool v = false;
+    };
+
+    template< int dim >
+    struct hasHierarchicIndexSet< YaspGrid< dim > >
+    {
+      static const bool v = false;
+    };
+
+
+    template< class HostGrid, class CoordFunction, class Allocator >
+    struct hasHierarchicIndexSet< GeometryGrid< HostGrid, CoordFunction, Allocator > >
+    {
+      //static const bool v = hasHierarchicIndexSet< HostGrid > :: v;
+      static const bool v = false ;
+    };
+
+  } // namespace Capabilities
+
+
   namespace Fem
   {
 
-    // Capabilities
-    // ------------
-
+    // Fem Capabilities
+    // ----------------
+ 
     namespace Capabilities
     {
-
-      // hasHierarchicIndexSet
-      // ---------------------
-
-      template< class Grid >
-      struct hasHierarchicIndexSet;
-
-      template< class Grid >
-      struct hasHierarchicIndexSet< const Grid >
-      {
-        static const bool v = hasHierarchicIndexSet< Grid >::v;
-      };
-
-      template< class Grid >
-      struct hasHierarchicIndexSet
-      {
-        static const bool v = Dune::Capabilities::hasHierarchicIndexSet< Grid >::v;
-      };
-
-      template< int dim, int dimw >
-      struct hasHierarchicIndexSet< ALUConformGrid< dim, dimw > >
-      {
-        static const bool v = true;
-      };
-
-      template< int dim, int dimw >
-      struct hasHierarchicIndexSet< ALUCubeGrid< dim, dimw > >
-      {
-        static const bool v = true;
-      };
-
-      template< int dim, int dimw >
-      struct hasHierarchicIndexSet< ALUSimplexGrid< dim, dimw > >
-      {
-        static const bool v = true;
-      };
-
-      template< int dim, int dimw, ALUGridElementType elType, ALUGridRefinementType refineType, class Comm >
-      struct hasHierarchicIndexSet< ALUGrid< dim, dimw, elType, refineType, Comm > >
-      {
-        static const bool v = true;
-      };
-
-      template<>
-      struct hasHierarchicIndexSet< OneDGrid >
-      {
-        static const bool v = false;
-      };
-
-      template< int dim, int dimw, class ctype >
-      struct hasHierarchicIndexSet< SGrid< dim, dimw, ctype > >
-      {
-        static const bool v = false;
-      };
-
-      template< int dim >
-      struct hasHierarchicIndexSet< UGGrid< dim > >
-      {
-        static const bool v = false;
-      };
-
-      template< int dim >
-      struct hasHierarchicIndexSet< YaspGrid< dim > >
-      {
-        static const bool v = false;
-      };
-
-
-      template< class HostGrid, class CoordFunction, class Allocator >
-      struct hasHierarchicIndexSet< GeometryGrid< HostGrid, CoordFunction, Allocator > >
-      {
-        //static const bool v = hasHierarchicIndexSet< HostGrid > :: v;
-        static const bool v = false ;
-      };
 
       // hasAllCodimEntities
       // -------------------
@@ -111,7 +120,6 @@ namespace Dune
         static const bool v = Loop< MetaAnd, Codim, Grid :: dimension > :: v;
         static const bool value = v;
       };
-
 
 
       // supportsCallbackAdaptation
@@ -172,7 +180,7 @@ namespace Dune
       };
 
       // isLocallyAdaptive
-      // --------------------------
+      // -----------------
 
       template< class Grid >
       struct isLocallyAdaptive
