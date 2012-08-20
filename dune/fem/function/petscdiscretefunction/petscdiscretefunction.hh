a36a01336829014765c40e1157c3da3fcec159cb
Tobias Malkmus <tomalk@mathematik.uni-freiburg.de>
2012-08-20 08:10:06 +0000

    move petsc stuff inplace
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@7500 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

---
+++ dune/fem/function/petscdiscretefunction/petscdiscretefunction.hh
@@ -0,0 +1,315 @@
+// vim: set expandtab ts=2 sw=2 sts=2:
+#ifndef DUNE_FEM_PETSCDISCRETEFUNCTION_HH
+#define DUNE_FEM_PETSCDISCRETEFUNCTION_HH
+
+#include <string>
+#include <algorithm>
+#include <map>
+#include <vector>
+#include <iterator>
+#include <utility>
+
+#if defined HAVE_PETSC
+
+#include <dune/fem/misc/petsc/petsccommon.hh>
+#include <dune/fem/misc/petsc/petscdofmappings.hh>
+#include <dune/fem/misc/petsc/petscvector.hh>
+#include <dune/fem/misc/petsc/petsclocalfunction.hh>
+
+#include <dune/fem/misc/petsc/petscslavedofprovider.hh>
+
+
+#include <dune/common/fvector.hh> 
+
+// If we don't define this, there is an error when headerchecking this file...
+#define WANT_CACHED_COMM_MANAGER 0
+
+#include <dune/fem/function/common/discretefunction.hh>
+#include <dune/fem/function/localfunction/localfunction.hh>
+//#include <dune/fem/function/localfunction/standardlocalfunction.hh>
+#include <dune/fem/function/blockvectordiscretefunction/blockvectordiscretefunction.hh>
+
+#include <dune/geometry/referenceelements.hh>
+
+#include <dune/common/shared_ptr.hh>
+
+namespace Dune 
+{
+
+  namespace Fem 
+  {
+
+
+    /* ==============================
+     * forward declarations
+     */
+    template< typename S > class PetscDiscreteFunction;
+
+
+    /* ========================================
+     * struct PetscDiscreteFunctionTraits
+     * =======================================
+     */
+    template< typename S >
+    struct PetscDiscreteFunctionTraits
+    {
+      typedef PetscDiscreteFunctionTraits< S >                          ThisType;
+      typedef S                                                         DiscreteFunctionSpaceType;
+      typedef typename DiscreteFunctionSpaceType::DomainType            DomainType;
+      typedef typename DiscreteFunctionSpaceType::RangeType             RangeType;
+      typedef typename DiscreteFunctionSpaceType::MapperType            MapperType;
+      typedef typename DiscreteFunctionSpaceType::BlockMapperType       BlockMapperType;
+      typedef typename DiscreteFunctionSpaceType::GridPartType          GridPartType;
+      typedef PetscDiscreteFunction< S >                                DiscreteFunctionType;
+    };
+
+    /* ========================================
+     * class PetscDiscreteFunction
+     * =======================================
+     */
+    template< typename S >
+    class PetscDiscreteFunction
+    : public HasLocalFunction,
+      public IsBlockVectorDiscreteFunction
+    {
+      typedef PetscDiscreteFunction< S > ThisType;
+      friend class PetscLocalFunction< ThisType >;
+
+    public:
+
+      /*
+       * types
+       */
+      typedef PetscDiscreteFunctionTraits< S > Traits;
+      typedef typename Traits::DiscreteFunctionSpaceType                DiscreteFunctionSpaceType;
+      typedef typename DiscreteFunctionSpaceType::FunctionSpaceType     FunctionSpaceType;
+
+      const static size_t localBlockSize = DiscreteFunctionSpaceType::localBlockSize;
+
+      typedef PetscVector< DiscreteFunctionSpaceType  >                 PetscVectorType;
+
+      typedef typename DiscreteFunctionSpaceType::RangeFieldType        RangeFieldType;
+      typedef typename DiscreteFunctionSpaceType::DomainFieldType       DomainFieldType;
+      typedef typename Traits::BlockMapperType                          BlockMapperType;
+      typedef typename Traits::GridPartType                             GridPartType;
+      typedef PetscLocalFunctionFactory< ThisType >                     LocalFunctionFactoryType;
+      typedef PetscLocalFunctionStack< ThisType >                       LocalFunctionStorageType;
+
+      typedef PetscLocalFunction< ThisType >                            LocalFunctionType;
+
+      typedef typename Traits::DomainType                               DomainType;
+      typedef typename Traits::RangeType                                RangeType;
+
+      typedef typename PetscVectorType::DofBlockType                    DofBlockType;
+      typedef typename PetscVectorType::ConstDofBlockType               ConstDofBlockType;
+      typedef typename PetscVectorType::DofIteratorType                 DofIteratorType;
+      typedef typename PetscVectorType::ConstDofIteratorType            ConstDofIteratorType;
+      typedef typename PetscVectorType::DofBlockPtrType                 DofBlockPtrType;
+      typedef typename PetscVectorType::ConstDofBlockPtrType            ConstDofBlockPtrType;
+
+    public:
+
+      /*
+       * methods
+       */
+      PetscDiscreteFunction ( const std::string &name,
+                              const DiscreteFunctionSpaceType &dfSpace )
+      : name_( name ),
+        dfSpace_( dfSpace ),
+        lfFactory_( *this ),
+        lfStorage_( lfFactory_ ),
+        petscVector_( dfSpace )
+      {}
+
+      PetscDiscreteFunction ( const ThisType &other )
+      : name_( "copy of " + other.name_ ),
+        dfSpace_( other.dfSpace_ ),
+        lfFactory_( other.lfFactory_ ),
+        lfStorage_( other.lfStorage_ ),
+        petscVector_( other.petscVector_ )
+      {}
+
+      ~PetscDiscreteFunction () 
+      {}
+
+      /** \copydoc Dune::Fem::DiscreteFunctionInterface::block( unsigned int index ) const */
+      ConstDofBlockPtrType block ( unsigned int index ) const
+      {
+        return petscVector().block( index );
+      }
+
+      /** \copydoc Dune::Fem::DiscreteFunctionInterface::block( unsigned int index ) const */
+      DofBlockPtrType block ( unsigned int index )
+      {
+        return petscVector().block( index );
+      }
+
+      /** \copydoc Dune::Fem::DiscreteFunctionInterface::size() const */
+      size_t size () const { return petscVector().size(); }
+
+      /** \copydoc Dune::Fem::DiscreteFunctionInterface::dbegin() const */
+      DofIteratorType dbegin () { return petscVector().dbegin(); }
+
+      /** \copydoc Dune::Fem::DiscreteFunctionInterface::dbegin() const */
+      ConstDofIteratorType dbegin () const { return petscVector().dbegin(); }
+
+      /** \copydoc Dune::Fem::DiscreteFunctionInterface::dend() const */
+      DofIteratorType dend() { return petscVector().dend(); }
+
+      /** \copydoc Dune::Fem::DiscreteFunctionInterface::dend() const */
+      ConstDofIteratorType dend() const { return petscVector().dend(); }
+
+      /** \copydoc Dune::Fem::DiscreteFunctionInterface::clear() */
+      void clear ()
+      {
+        petscVector().clear();
+      }
+
+        /** \copydoc Dune::Fem::DiscreteFunctionInterface::communicate() */
+      void communicate ()
+      {
+        petscVector().communicateNow();
+      }
+
+      /** \copydoc Dune::Fem::DiscreteFunctionInterface::scalarProductDofs */
+      PetscScalar scalarProductDofs ( const ThisType &other ) const
+      {
+        return petscVector()*other.petscVector();
+      }
+
+      /** \copydoc Dune::Fem::Function::evaluate(const FieldVector<int,diffOrder> &diffVariable,const DomainType &x,RangeType &ret) const */
+      template< int diffOrder >
+      void evaluate ( const ::Dune::FieldVector< int, diffOrder > &diffVariable,
+                      const DomainType &x,
+                      RangeType &ret ) const
+      {
+        typedef typename DiscreteFunctionSpaceType::IteratorType Iterator;
+        typedef typename Iterator::Entity Entity;
+        typedef typename Entity::Geometry Geometry;
+        typedef typename Geometry::LocalCoordinate LocalCoordinateType;
+
+        const int dimLocal = LocalCoordinateType::dimension;
+        
+        const Iterator end = space().end();
+        for( Iterator it = space().begin(); it != end; ++it )
+        {
+          const Entity &entity = *it;
+          const Geometry &geometry = entity.geometry();
+
+          const GenericReferenceElement< DomainFieldType, dimLocal > &refElement
+            = GenericReferenceElements< DomainFieldType, dimLocal >::general( geometry.type() );
+
+          const LocalCoordinateType xlocal = geometry.local( x );
+          if( refElement.checkInside( xlocal ) )
+          {
+            localFunction( entity ).evaluate( diffVariable, xlocal, ret );
+            return;
+          }
+        }
+        DUNE_THROW( RangeError, "PetscDiscreteFunction::evaluate: x is not within domain." );
+      }
+
+      /** \copydoc Dune::Fem::Function::evaluate(const DomainType &x,RangeType &ret) const */
+      void evaluate ( const DomainType &x, RangeType &ret ) const
+      {
+        Dune::FieldVector< int, 0 > diffVariable;
+        evaluate( diffVariable, x, ret );
+      }
+
+      /** \copydoc Dune::Fem::DiscreteFunctionInterface::operator+= */
+      const ThisType& operator+= ( const ThisType &other )
+      {
+        petscVector() += other.petscVector();
+        return *this;
+      }
+
+      /** \copydoc Dune::Fem::DiscreteFunctionInterface::operator-= */
+      const ThisType& operator-= ( const ThisType &other )
+      {
+        petscVector() -= other.petscVector();
+        return *this;
+      }
+
+      /** \copydoc Dune::Fem::DiscreteFunctionInterface::operator*= */
+      const ThisType& operator*= ( const PetscScalar scalar )
+      {
+        petscVector() *= scalar;
+        return *this;
+      }
+
+      /** \copydoc Dune::Fem::DiscreteFunctionInterface::operator/= */
+      const ThisType& operator/= ( const PetscScalar scalar )
+      {
+        petscVector() /= scalar;
+        return *this;
+      }
+
+      /** \copydoc Dune::Fem::DiscreteFunctionInterface::axpy */
+      void axpy ( const RangeFieldType &scalar, const ThisType &other )
+      {
+        petscVector().axpy( static_cast< const PetscScalar& >( scalar ), other.petscVector() );
+      }
+
+      /** \copydoc Dune::Fem::DiscreteFunctionInterface::space() const */
+      const DiscreteFunctionSpaceType& space () const { return dfSpace_; }
+
+      /** \brief obtain a constand pointer to the underlying PETSc Vec */
+      const Vec* petscVec () const { return petscVector().getVector(); }
+
+      /** \brief obtain a pointer to the underlying PETSc Vec */
+      Vec* petscVec () { return petscVector().getVector(); }
+
+      /** \copydoc Dune::Fem::DiscreteFunctionInterface::localFunction(const EntityType &entity) */
+      template< class EntityType >
+      LocalFunctionType localFunction ( const EntityType &entity )
+      {
+        return localFunctionStorage().localFunction( entity );
+      }
+
+      /** \copydoc Dune::Fem::DiscreteFunctionInterface::localFunction(const EntityType &entity) const */
+      template< class EntityType >
+      LocalFunctionType localFunction ( const EntityType &entity ) const
+      {
+        return localFunctionStorage().localFunction( entity );
+      }
+
+      /** \copydoc Dune::Fem::DiscreteFunctionInterface::name() const */
+      const std::string& name () const
+      {
+        return name_;
+      }
+
+    private:
+      PetscDiscreteFunction ();
+      ThisType& operator= ( const ThisType &other );
+
+      /*
+       * private methods
+       */
+      LocalFunctionStorageType& localFunctionStorage () const 
+      {
+        return lfStorage_;
+      }
+
+      PetscVectorType& petscVector () { return petscVector_; }
+
+      const PetscVectorType& petscVector () const { return petscVector_; }
+
+      /*
+       * data fields
+       */
+      std::string name_;
+      const DiscreteFunctionSpaceType &dfSpace_;
+      LocalFunctionFactoryType lfFactory_;
+      mutable LocalFunctionStorageType lfStorage_;
+      PetscVectorType petscVector_;
+    };
+
+
+  } // namespace Fem
+
+} // namespace Dune
+
+#endif // #if defined HAVE_PETSC
+
+#endif // #ifndef DUNE_FEM_PETSCDISCRETEFUNCTION_HH
---
+++ dune/fem/operator/common/petsclinearoperator.hh
@@ -0,0 +1,357 @@
+// vim: set expandtab ts=2 sw=2 sts=2:
+#ifndef DUNE_FEM_PETSCLINEAROPERATOR_HH
+#define DUNE_FEM_PETSCLINEAROPERATOR_HH
+
+#include <iostream>
+#include <vector>
+
+#include <dune/common/dynmatrix.hh>
+
+#include <dune/fem/misc/functor.hh>
+#include <dune/fem/operator/common/localmatrix.hh> 
+#include <dune/fem/operator/common/localmatrixwrapper.hh>
+
+#include <dune/fem/misc/petsc/petsccommon.hh>
+#include <dune/fem/function/petscdiscretefunction/petscdiscretefunction.hh>
+
+#if defined HAVE_PETSC
+
+#include "petscmat.h"
+
+
+namespace Dune 
+{
+  namespace Fem 
+  {
+
+    /* ========================================
+     * class PetscLinearOperator
+     */
+    template< typename DomainSpace, typename RangeSpace >
+    class PetscLinearOperator 
+    {
+      typedef PetscLinearOperator< DomainSpace, RangeSpace > ThisType;
+    public:
+
+      typedef DomainSpace DomainSpaceType;
+      typedef RangeSpace RangeSpaceType;
+      typedef typename RangeSpaceType::RangeFieldType RangeFieldType;
+      typedef PetscDiscreteFunction< DomainSpaceType > DomainFunctionType;
+      typedef PetscDiscreteFunction< RangeSpaceType > RangeFunctionType;
+      typedef typename DomainSpaceType::GridType::template Codim< 0 >::Entity ColEntityType;
+      typedef typename RangeSpaceType::GridType::template Codim< 0 >::Entity RowEntityType;
+
+      const static size_t domainLocalBlockSize = DomainSpaceType::localBlockSize;
+      const static size_t rangeLocalBlockSize = RangeSpaceType::localBlockSize;
+
+    private:
+      typedef PetscSlaveDofProvider< DomainSpaceType > ColPetscSlaveDofsType;
+      typedef PetscSlaveDofProvider< RangeSpaceType  > RowPetscSlaveDofsType;
+
+    public:
+      typedef typename ColPetscSlaveDofsType :: PetscDofMappingType   ColDofMappingType;
+      typedef typename RowPetscSlaveDofsType :: PetscDofMappingType   RowDofMappingType;
+
+      // the local matrix
+      class LocalMatrix;
+
+      struct LocalMatrixTraits
+      {
+        typedef DomainSpace DomainSpaceType;
+        typedef RangeSpace RangeSpaceType;
+        typedef LocalMatrix LocalMatrixType;
+        typedef typename RangeSpaceType::RangeFieldType RangeFieldType;
+
+        // copied this typedef from spmatrix.hh
+        typedef RangeFieldType LittleBlockType;
+      };
+
+      //! type of local matrix object
+      typedef LocalMatrix  ObjectType;
+      typedef ThisType     LocalMatrixFactoryType;
+      typedef ObjectStack< LocalMatrixFactoryType > LocalMatrixStackType;
+
+      //! type of local matrix using stacking mechanism
+      typedef LocalMatrixWrapper< LocalMatrixStackType > LocalMatrixType;
+
+      /*
+       * ctors, dtor, methods...
+       */
+      PetscLinearOperator ( const DomainSpaceType &domainSpace, const RangeSpaceType &rangeSpace )
+      : domainSpace_( domainSpace ),
+        rangeSpace_( rangeSpace ),
+        colSlaveDofs_( domainSpace_ ),
+        rowSlaveDofs_( rangeSpace_ ),
+        localMatrixStack_( *this )     
+      {
+        /*
+         * initialize the row and column petsc dof mappings
+         */
+        const PetscInt localRows = rowDofMapping().numOwnedDofBlocks();
+        const PetscInt localCols = colDofMapping().numOwnedDofBlocks();
+
+        assert( domainLocalBlockSize == rangeLocalBlockSize );
+        // create matrix 
+        ::Dune::Petsc::MatCreate( &petscMatrix_ );
+        
+        if( domainLocalBlockSize > 1 ) 
+        {
+          ::Dune::Petsc::MatSetType( petscMatrix_, MATBAIJ );
+          // set block size 
+          PetscInt bs = domainLocalBlockSize ;
+          ::Dune::Petsc::MatSetBlockSize( petscMatrix_, bs );
+        }
+        else 
+        {
+          ::Dune::Petsc::MatSetType( petscMatrix_, MATAIJ );
+        }
+
+        // set sizes of the matrix 
+        ::Dune::Petsc::MatSetSizes( petscMatrix_, localRows, localCols, PETSC_DETERMINE, PETSC_DETERMINE );
+      }
+
+      //! destructor deleting PETSc Mat object 
+      ~PetscLinearOperator ()
+      {
+        ::Dune::Petsc::MatDestroy( &petscMatrix_ );
+      }
+
+      // TODO: This is not an interface method. Remove it? But right now I need it.
+      void communicate ()
+      {
+        ::Dune::Petsc::MatAssemblyBegin( petscMatrix_, MAT_FINAL_ASSEMBLY );
+        ::Dune::Petsc::MatAssemblyEnd( petscMatrix_, MAT_FINAL_ASSEMBLY );
+      }
+
+      const DomainSpaceType& domainSpace () const { return domainSpace_; }
+      const RangeSpaceType& rangeSpace () const { return rangeSpace_; }
+
+      void apply ( const DomainFunctionType &arg, RangeFunctionType &dest ) const
+      {
+        ::Dune::Petsc::MatMult( petscMatrix_, *arg.petscVec() , *dest.petscVec() );
+      }
+
+      void reserve () {} 
+
+      void clear ()
+      {
+        ::Dune::Petsc::MatZeroEntries( petscMatrix_ );
+      }
+
+      //! interface method from LocalMatrixFactory 
+      ObjectType* newObject() const
+      {
+        return new ObjectType( *this, domainSpace_, rangeSpace_ );
+      }
+      
+      //! return local matrix representation 
+      LocalMatrixType localMatrix ( const RowEntityType &rowEntity, const ColEntityType &colEntity )
+      {
+        return LocalMatrixType(localMatrixStack_,rowEntity,colEntity);
+      }
+
+      // just here for debugging
+      #ifndef NDEBUG
+      void view () const 
+      {
+        ::Dune::Petsc::MatView( petscMatrix_, PETSC_VIEWER_STDOUT_WORLD );
+      }
+      #endif
+
+      // return reference to PETSc matrix object 
+      Mat& petscMatrix () const { return petscMatrix_; }
+
+      //! return reference to row global mapping 
+      const RowDofMappingType& rowDofMapping() const { return rowSlaveDofs_.dofMapping(); }
+      //! return reference to column global mapping 
+      const ColDofMappingType& colDofMapping() const { return colSlaveDofs_.dofMapping(); }
+
+    private:
+      PetscLinearOperator ();
+
+      /*
+       * data fields
+       */
+      const DomainSpaceType &domainSpace_;
+      const RangeSpaceType &rangeSpace_;
+      ColPetscSlaveDofsType colSlaveDofs_;
+      RowPetscSlaveDofsType rowSlaveDofs_;
+
+      mutable Mat petscMatrix_;
+
+      mutable LocalMatrixStackType localMatrixStack_;
+    };
+
+
+
+    /* ========================================
+     * class PetscLinearOperator::LocalMatrix
+     */
+    template< typename DomainSpace, typename RangeSpace >
+    class PetscLinearOperator< DomainSpace, RangeSpace >::LocalMatrix 
+    : public LocalMatrixDefault< PetscLinearOperator< DomainSpace, RangeSpace >::LocalMatrixTraits >
+    {
+      typedef LocalMatrix ThisType;
+      typedef LocalMatrixDefault< PetscLinearOperator< DomainSpace, RangeSpace >::LocalMatrixTraits >  BaseType;
+
+      typedef PetscLinearOperator< DomainSpace, RangeSpace > PetscLinearOperatorType;
+
+
+    public:
+      typedef PetscInt                                        DofIndexType;
+      typedef std::vector< DofIndexType >                     IndexVectorType;
+      typedef DomainSpace                                     DomainSpaceType;
+      typedef RangeSpace                                      RangeSpaceType;
+      typedef typename DomainSpaceType::BaseFunctionSetType   DomainBaseFunctionSetType;
+      typedef typename RangeSpaceType::BaseFunctionSetType    RangeBaseFunctionSetType;
+
+      enum { littleCols  = DomainSpaceType::localBlockSize };
+      enum { littleRows  = RangeSpaceType ::localBlockSize };
+
+      typedef Dune::FieldMatrix< typename DomainSpaceType :: RangeFieldType, 
+                  littleRows, littleCols > LittleBlockType ;
+
+      typedef Dune::DynamicMatrix< LittleBlockType > DynamicMatrixType;
+
+    private:
+
+      // needed for .mapEach below
+      template< typename PetscMapping >
+      struct PetscAssignFunctor
+      {
+        explicit PetscAssignFunctor ( const PetscMapping &petscMapping, IndexVectorType &indices )
+        : petscMapping_( petscMapping ),
+          indices_( indices )
+        {}
+
+        template< typename T >
+        void operator() ( const std::size_t localIndex, T globalIndex ) { indices_[ localIndex ] = petscMapping_.globalMapping( globalIndex ); }
+
+      private:
+        const PetscMapping &petscMapping_;
+        IndexVectorType &indices_;
+      };
+
+    public:
+
+      LocalMatrix ( const PetscLinearOperatorType &petscLinOp, 
+                    const DomainSpaceType &domainSpace, 
+                    const RangeSpaceType &rangeSpace )
+      : BaseType( domainSpace, rangeSpace ),
+        petscLinearOperator_( petscLinOp ),
+        localMatrix_( rangeSpace.blockMapper().maxNumDofs(), 
+                      domainSpace.blockMapper().maxNumDofs() )
+      {}
+
+      void init ( const RowEntityType &rowEntity, const ColEntityType &colEntity ) 
+      {
+        // call initialize on base class 
+        BaseType :: init( rowEntity, colEntity );
+
+        if( littleRows > 1 )
+        {
+          // set all values to 0 
+          localMatrix_ = 0;
+        }
+
+        setupIndices( rangeSpace().blockMapper(), petscLinearOperator_.rowDofMapping(), rowEntity, rowIndices_ );
+        setupIndices( domainSpace().blockMapper(), petscLinearOperator_.colDofMapping(), colEntity, colIndices_ );
+      }
+
+      inline void add ( const int localRow, const int localCol, const RangeFieldType &value )
+      {
+        if( littleRows > 1 ) 
+        {
+          const int row = (int) localRow / littleRows;
+          const int col = (int) localCol / littleCols;
+          const int lRow = localRow%littleRows;
+          const int lCol = localCol%littleCols;
+
+          // add value to local storage 
+          localMatrix_[ row ][ col ][ lRow ][ lCol ] += value ;
+        }
+        else
+         ::Dune::Petsc::MatSetValue( petscMatrix(), globalRowIndex( localRow ), globalColIndex( localCol ) , value, ADD_VALUES );
+      }
+
+      ~LocalMatrix() 
+      {
+        if( littleRows > 1 ) 
+        {
+          const int colSize = columns() * littleCols ;
+
+          PetscScalar v[ colSize ];
+          PetscInt c[ colSize ] ;
+
+          const int nrows = rows();
+          const int cols = columns();
+          for( int row = 0 ; row < nrows; ++ row ) 
+          {
+            // set row indices 
+            PetscInt idxm[ 1 ] = { PetscInt( globalRowIndex( row ) * littleRows ) };
+            for(int lr = 0; lr < littleRows; ++lr ) 
+            {
+              for(int col = 0, colG = 0; col < cols ; ++col ) 
+              {
+                PetscInt colIdx = globalColIndex( col ) * littleCols ;
+                for( int lc = 0; lc < littleCols ; ++ lc, ++ colG, ++colIdx ) 
+                {
+                  v[ colG ] = localMatrix_[ row ][ col ][ lr ][ lc ];
+                  c[ colG ] = colIdx ;
+                }
+              }
+
+              ::Dune::Petsc::MatSetValues( petscMatrix(), 1, idxm, colSize, c, v, ADD_VALUES );
+            }
+          }
+        }
+      }
+
+    private:
+      LocalMatrix ();
+
+
+      Mat& petscMatrix () { return petscLinearOperator_.petscMatrix_; }
+      const Mat& petscMatrix () const { return petscLinearOperator_.petscMatrix_; }
+
+      // Used to setup row/column indices. DofMapper is the DUNE DoF mapper
+      template< typename DofMapper, typename PetscMapping, typename Entity > 
+      void setupIndices ( const DofMapper &dofMapper, const PetscMapping &petscMapping, const Entity &entity, IndexVectorType &indices )
+      {
+        indices.resize( dofMapper.numDofs( entity ) );
+        dofMapper.mapEach( entity, PetscAssignFunctor< PetscMapping >( petscMapping, indices ) );
+      }
+
+      const int rows() const { return rowIndices_.size(); }
+      const int columns() const { return colIndices_.size(); }
+
+      DofIndexType globalRowIndex( const int localRow ) const 
+      { 
+        assert( localRow < static_cast< int >( rowIndices_.size() ) );
+        return rowIndices_[ localRow ]; 
+      }
+
+      DofIndexType globalColIndex( const int localCol ) const 
+      { 
+        assert( localCol < static_cast< int >( colIndices_.size() ) );
+        return colIndices_[ localCol ]; 
+      }
+
+      /*
+       * data fields
+       */
+      const PetscLinearOperatorType &petscLinearOperator_;
+      IndexVectorType rowIndices_;
+      IndexVectorType colIndices_;
+
+      DynamicMatrixType localMatrix_; 
+    };
+
+
+  } // namespace Fem
+
+} // namespace Dune
+
+#endif // #if defined HAVE_PETSC
+
+#endif // #ifndef DUNE_FEM_PETSCLINEAROPERATOR_HH
