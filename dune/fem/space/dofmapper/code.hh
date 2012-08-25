fea9413dec8faa6a479406e6009ddf99ac25ddba
Andreas Dedner <A.S.Dedner@warwick.ac.uk>
2012-08-25 23:16:36 +0000

    get rid of some unsigned/signed warnings
    git-svn-id: https://dune.mathematik.uni-freiburg.de/svn/dune-fem/trunk@7534 4a6c9bf5-c326-0410-8cc6-c7cabc8ddcf0

--- dune/fem/space/dofmapper/code.hh
+++ dune/fem/space/dofmapper/code.hh
@@ -111,13 +111,13 @@ namespace Dune
 
       const unsigned int &operator[] ( unsigned int i ) const
       {
-        assert( i < end() - begin() );
+        assert( (ptrdiff_t)i < end() - begin() );
         return begin()[ i ];
       }
 
       unsigned int &operator[] ( unsigned int i )
       {
-        assert( i < end() - begin() );
+        assert( (ptrdiff_t)i < end() - begin() );
         return begin()[ i ];
       }
     };
