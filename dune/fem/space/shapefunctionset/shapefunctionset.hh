3704017881f60199209969897d835d1ca3dcadf8
Christoph Gersbacher <christoph.gersbacher@mathematik.uni-freiburg.de>
2012-09-05 16:08:17 +0200

    Add missing private copy constructor and assignment operator.

--- dune/fem/space/shapefunctionset/shapefunctionset.hh
+++ dune/fem/space/shapefunctionset/shapefunctionset.hh
@@ -1,11 +1,14 @@
 #ifndef DUNE_FEM_SHAPEFUNCTIONSET_SHAPEFUNCTIONSET_HH
 #define DUNE_FEM_SHAPEFUNCTIONSET_SHAPEFUNCTIONSET_HH
 
+//- C++ includes
+#include <cstddef>
+
+//- dune-common includes
+#include <dune/common/bartonnackmanifcheck.hh>
+
 //- dune-geometry includes
 #include <dune/geometry/type.hh>
-
-//- dune-fem includes
-#include <dune/fem/misc/bartonnackmaninterface.hh>
 
 
 namespace Dune
@@ -17,19 +20,33 @@ namespace Dune
     // ShapeFunctionSet
     // ----------------
 
-    template< class Traits >
+    /**
+     * \brief interface class for shape function sets
+     *
+     * \tparam  FunctionSpace   Function space
+     * \tparam  Implementation  Implementation of this interface
+     *
+     * \note FunctionSpace::dimDomain has to be equal to type().dim().
+     */
+    template< class FunctionSpace, class Implementation >
     class ShapeFunctionSet
     {
+      // this type
+      typedef ShapeFunctionSet< FunctionSpace, Implementation > ThisType;
+
     public:
+      //! \brief function space type
+      typedef FunctionSpace FunctionSpaceType;
       //! \brief type of real implementation
-      typedef typename Traits::ShapeFunctionSetType ImplementationType;
+      typedef Implementation ImplementationType;
+
       //! \brief return reference to implementation
       ImplementationType &impl () { return static_cast< ImplementationType & >( *this ); }
       //! \brief return const reference to implementation
       const ImplementationType &impl () const { return static_cast< const ImplementationType & >( *this ); }
       
-      //! \brief function space type
-      typedef typename Traits::FunctionSpaceType FunctionSpaceType;
+      //! \brief domain type
+      typedef typename FunctionSpaceType::DomainType DomainType;
       //! \brief range type
       typedef typename FunctionSpaceType::RangeType RangeType;
       //! \brief jacobian range type
@@ -62,7 +79,8 @@ namespace Dune
        *  \code
        *  struct Functor
        *  {
-       *    void operator() ( const int shapeFunction, RangeType &value );
+       *    template< class Value >
+       *    void operator() ( const int shapeFunction, const Value &value );
        *  };
        *  \endcode
        */
@@ -83,7 +101,8 @@ namespace Dune
        *  \code
        *  struct Functor
        *  {
-       *    void operator() ( const int shapeFunction, JacobianRangeType &jacobian );
+       *    template< class Jacobian >
+       *    void operator() ( const int shapeFunction, const Jacobian &jacobian );
        *  };
        *  \endcode
        */
@@ -104,7 +123,8 @@ namespace Dune
        *  \code
        *  struct Functor
        *  {
-       *    void operator() ( const int shapeFunction, HessianRangeType &hessian );
+       *    template< class Hessian >
+       *    void operator() ( const int shapeFunction, const Hessian &hessian );
        *  };
        *  \endcode
        */
@@ -113,6 +133,12 @@ namespace Dune
       {
         CHECK_AND_CALL_INTERFACE_IMPLEMENTATION( impl().hessianEach( x, functor ) );
       }
+
+    private:
+      // forbid copy constructor
+      ShapeFunctionSet ( const ThisType & );
+      // forbid assignment operator 
+      ThisType &operator= ( const ThisType & );
     };
 
   } // namespace Fem
