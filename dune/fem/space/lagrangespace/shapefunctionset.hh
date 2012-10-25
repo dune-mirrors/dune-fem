743d44445bb1c21433fdddc0fbae74c6178202b0
Christoph Gersbacher <christoph.gersbacher@mathematik.uni-freiburg.de>
2012-10-25 17:49:34 +0200

    Add missing typedef.

--- dune/fem/space/lagrangespace/shapefunctionset.hh
+++ dune/fem/space/lagrangespace/shapefunctionset.hh
@@ -183,6 +183,7 @@ namespace Dune
                                          typename Traits::RangeType
                                        > BaseType;
 
+      typedef typename Traits::ScalarShapeFunctionSetType ScalarShapeFunctionSetType;
       typedef typename Traits::ScalarShapeFunctionFactoryType ScalarShapeFunctionFactoryType;
 
     public:
