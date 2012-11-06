c37aae02f0f226ebdb7e90d0cdb248b0d5306fea
Christoph Gersbacher <christoph.gersbacher@mathematik.uni-freiburg.de>
2012-11-06 12:02:53 +0100

    Uncrustify file.

--- dune/fem/space/discontinuousgalerkin/legendrepoly.hh
+++ dune/fem/space/discontinuousgalerkin/legendrepoly.hh
@@ -1,8 +1,10 @@
-#ifndef DUNE_FEM_LEGENDREPOLY_HH
-#define DUNE_FEM_LEGENDREPOLY_HH
+#ifndef DUNE_FEM_SPACE_DISCONTINUOUSGALERKIN_LEGENDREPOLY_HH
+#define DUNE_FEM_SPACE_DISCONTINUOUSGALERKIN_LEGENDREPOLY_HH
 
+// C++ inlcudes
 #include <cassert>
 #include <iostream>
+
 
 namespace Dune
 {
@@ -10,58 +12,56 @@ namespace Dune
   namespace Fem 
   {  
 
-    struct LegendrePoly
+    class LegendrePoly
     {
-      static double evaluate(const int num, const double x)
-      {  
-        assert(0<=num && num<maxPol);
+    protected:
+      static const int maxPol = 11;
+
+      static const double factor[ maxPol ][ maxPol ];
+      static const double weight[ maxPol ];
+
+    public:
+      static double evaluate ( const int num, const double x )
+      {
+        assert( 0 <= num && num < maxPol );
+
         double phi = factor[ num ][ num ];
-        for(int i=num-1; i>=0; --i )
-        {
+        for( int i = num-1; i >= 0; --i )
           phi = phi * x + factor[ num ][ i ];
-        }
         return weight[ num ] * phi;
       }
       
-      static double jacobian(const int num, const double x)
+      static double jacobian ( const int num, const double x )
       { 
-        assert(0<=num && num<maxPol);
-        double phi=0.0;
-        if (num>=1)
+        assert( 0 <= num && num < maxPol );
+
+        double phi = 0.;
+        if( num >= 1 )
         {
           phi = factor[ num ][ num ] * num;
-          for(int i=num-1 ; i>=1; --i )
-          {
+          for( int i = num-1; i >= 1; --i )
             phi = phi * x + factor[ num ][ i ] * i;
-          }
         }
         return weight[ num ] * phi;
       }
          
-      static double hessian(const int num, const double x)
+      static double hessian ( const int num, const double x )
       { 
-        assert(0<=num && num<maxPol);
-        double phi=0.0;    
+        assert( 0 <= num && num < maxPol );
 
-        if (num >= 2)
+        double phi=0.;
+        if( num >= 2 )
         {
           phi = factor[ num ][ num ] * num * ( num-1 );
-          for(int i = num-1; i>=2; --i )
-          {
-            phi=phi*x+factor[num][i]*i*(i-1);
-          }
+          for( int i = num-1; i >= 2; --i )
+            phi = phi * x + factor[ num ][ i ] * i * (i-1);
         }
         return weight[ num ] * phi;
       }
-      
-    protected:
-      enum { maxPol = 11 };
-      static const double factor[ maxPol ][ maxPol ];
-      static const double weight[ maxPol ];
     };
 
   } // namespace Fem 
 
 } // namespace Dune 
 
-#endif // #ifndef DUNE_FEM_LEGENDREPOLY_HH
+#endif // #ifndef DUNE_FEM_SPACE_DISCONTINUOUSGALERKIN_LEGENDREPOLY_HH
