ae8a85b5deea3693ea645768d0c16042ed452c6d
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2012-11-20 15:31:05 +0100

    add global method axpy to namespace Dune::Fem

--- dune/fem/space/basisfunctionset/functor.hh
+++ dune/fem/space/basisfunctionset/functor.hh
@@ -12,6 +12,33 @@ namespace Dune
   namespace Fem
   {
 
+    // axpy
+    // ----
+
+    template< class T >
+    inline void axpy ( const T &a, const T &x, T &y )
+    {
+      y += a*x;
+    }
+
+    template< class K, int SIZE >
+    inline void axpy ( const typename FieldTraits< K >::field_type &a,
+                       const FieldVector< K, SIZE > &x,
+                       FieldVector< K, SIZE > &y )
+    {
+      y.axpy( a, x );
+    }
+
+    template< class K, int ROWS, int COLS >
+    inline void axpy ( const typename FieldTraits< K >::field_type &a,
+                       const FieldMatrix< K, ROWS, COLS > &x,
+                       FieldMatrix< K, ROWS, COLS > &y )
+    {
+      y.axpy( a, x );
+    }
+
+
+
     // scalarProduct
     // -------------
 
@@ -45,7 +72,7 @@ namespace Dune
 
       void operator() ( const std::size_t i, const Value &v )
       {
-        value_.axpy( vector_[ i ], v );
+        axpy( vector_[ i ], v, value_ );
       }
 
     private:
