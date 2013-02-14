c1df5995c399849f13d59a6c373f252bc3b24314
Stefan Girke <stefan.girke@gmx.de>
2013-02-14 19:24:00 +0100

    non-const lvalue reference binded to temporary

--- dune/fem/pass/selection.hh
+++ dune/fem/pass/selection.hh
@@ -322,7 +322,8 @@ namespace Dune
     static typename TupleAccessTraits< Nil > :: NonConstType
     get( Tuple &arg )
     {
-      return Nil();
+			static Nil nilDummy;
+      return nilDummy;
     }
   };
 
