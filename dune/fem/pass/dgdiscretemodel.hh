4daf4ae264eeea2cefc84d5cc8b08452445e5201
Christoph Gersbacher <christoph.gersbacher@mathematik.uni-freiburg.de>
2013-03-01 17:05:40 +0100

    Add compatibility header.

---
+++ dune/fem/pass/dgdiscretemodel.hh
@@ -0,0 +1,4 @@
+#warning #include <dune/fem/pass/dgdiscretemodel.hh> is deprecated.
+#warning Use #include <dune/fem/pass/localdg/discretemodel.hh> instead.
+
+#include <dune/fem/pass/localdg/discretemodel.hh>
