90e6144dd44f8e89006cda8fec1ebf65f1f57223
Christoph Gersbacher <christoph.gersbacher@mathematik.uni-freiburg.de>
2013-03-04 18:09:14 +0100

    Yet another bug fix in auto-generated code...

--- dune/fem/pass/common/scripts/tupleutility.lua
+++ dune/fem/pass/common/scripts/tupleutility.lua
@@ -75,13 +75,13 @@ function tuple_push_back ( i )
 end
 
 function tuple_push_front ( i )
-  body = "template< class T" .. i .. ( ( (i == 1) ) and "" or ( ", " .. general_list( i-1, "class T" ) ) ) .. " >\n"
-  body = body .."inline " .. tuple( i ) .. " tuple_push_front ( const " .. tuple( i-1 ) .. " &t, T" .. i .. " t" .. i .. " )" .. "\n"
+  body = "template< " .. general_list( i, "class T" ) .. " >\n"
+  body = body .."inline " .. tuple( i ) .. " tuple_push_front ( const " .. tuple( i-1, 2 ) .. " &t, T1 t1 )" .. "\n"
   body = body .. "{" .. "\n"
   if i == 1 then
-    body = body .. "  return " .. tuple( i ) .. "( t" .. i .. " );" .. "\n"
+    body = body .. "  return " .. tuple( i ) .. "( t1 );" .. "\n"
   else
-    body = body .. "  return " .. tuple( i ) .. "( t" .. i .. ", " .. tuple_get_elements( i-1 ) .. " );" .. "\n"
+    body = body .. "  return " .. tuple( i ) .. "( t1, " .. tuple_get_elements( i-1, 1 ) .. " );" .. "\n"
   end
   body = body .. "}"
   return body
