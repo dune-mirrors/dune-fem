916a0f1eb7e51e25a55658bb112129392ad22297
Christoph Gersbacher <christoph.gersbacher@mathematik.uni-freiburg.de>
2013-03-19 14:44:42 +0100

    Fix warning.

--- dune/fem/pass/insertfunctionpass.hh
+++ dune/fem/pass/insertfunctionpass.hh
@@ -1,4 +1,4 @@
 #warning #include <dune/fem/pass/insertfunctionpass.hh> is deprecated.
-#warning Use #include <dune/fem/pass/common/insertfunction.hh> instead.
+#warning Use #include <dune/fem/pass/insertfunction.hh> instead.
 
 #include <dune/fem/pass/insertfunction.hh>
