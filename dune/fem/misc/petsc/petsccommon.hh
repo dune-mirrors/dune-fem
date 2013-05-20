69ccdca0fddd62ce6569bce67d4fa6c532cc48aa
Andreas Dedner <a.s.dedner@warwick.ac.uk>
2013-05-20 10:07:15 +0100

    need a getvalue method

--- dune/fem/misc/petsc/petsccommon.hh
+++ dune/fem/misc/petsc/petsccommon.hh
@@ -218,6 +218,8 @@ namespace Dune
     inline void MatSetValue ( Mat v, PetscInt i, PetscInt j, PetscScalar va, InsertMode mode ) { ErrorCheck( ::MatSetValue( v, i, j, va, mode ) ); }
     inline void MatSetValues ( Mat mat, PetscInt m, const PetscInt idxm[], PetscInt n, const PetscInt idxn[], const PetscScalar v[], InsertMode addv )
       { ErrorCheck( ::MatSetValues( mat, m, idxm, n, idxn, v, addv ) ); }
+    inline void MatGetValues ( Mat mat, PetscInt m, const PetscInt idxm[], PetscInt n, const PetscInt idxn[], PetscScalar v[] )
+      { ErrorCheck( ::MatGetValues( mat, m, idxm, n, idxn, v ) ); }
     inline void MatView ( Mat mat, PetscViewer viewer ) { ErrorCheck( ::MatView( mat, viewer ) ); }
     inline void MatZeroEntries ( Mat mat ) { ErrorCheck( ::MatZeroEntries( mat ) ); }
     inline void PetscBarrier ( PetscObject obj ) { ErrorCheck( ::PetscBarrier( obj ) ); }
