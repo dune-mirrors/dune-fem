a386398c660640ec7e73a4243ff3e67acce45f48
Andreas Dedner <a.s.dedner@warwick.ac.uk>
2013-05-30 21:59:37 +0100

    need to fix the parallelscalarproduct class this is just a first hack, correct implementation to follow

---
+++ dune/fem/function/common/dgmatrixsetup.hh
@@ -0,0 +1,135 @@
+#ifndef DUNE_DGMATRIXSETUP_HH
+#define DUNE_DGMATRIXSETUP_HH
+
+#include <dune/common/timer.hh>
+
+#include <dune/fem/function/common/scalarproducts.hh>
+#include <dune/fem/space/common/commoperations.hh>
+#include <dune/fem/misc/functor.hh>
+
+#if HAVE_DUNE_ISTL
+#include <dune/istl/operators.hh>
+#include <dune/fem/operator/matrix/istlmatrix.hh>
+#include <dune/fem/operator/matrix/preconditionerwrapper.hh>
+#endif
+
+namespace Dune 
+{
+  namespace Fem
+  {
+    ////////////////////////////////////////////////////////////
+    //
+    //  Setup of matrix structure 
+    //
+    ////////////////////////////////////////////////////////////
+    /**  \brief Setup Matrix structure for DG operators by including
+     * elements and it's neighbors. 
+    */
+    class ElementAndNeighbors
+    {
+    public:
+      //! create entries for element and neighbors 
+      template <class SpaceImp,    
+                class RowMapperType,
+                class DiscreteFunctionType>
+      static inline void setup(const SpaceImp& space,    
+                               const RowMapperType& rowMapper,
+                               const DiscreteFunctionType* )
+      {
+        typedef typename SpaceImp :: GridPartType GridPartType;
+        const GridPartType& gridPart = space.gridPart();
+
+        typedef Fem::ParallelScalarProduct<DiscreteFunctionType> ParallelScalarProductType;
+        typedef typename ParallelScalarProductType :: BuildProxyType BuildProxyType;
+        
+        ParallelScalarProductType scp (space);
+
+        std::auto_ptr<BuildProxyType> buildProxy = scp.buildProxy();
+
+        // define used types 
+        typedef typename GridPartType :: GridType GridType;
+        typedef typename GridPartType :: template Codim<0> :: EntityType    EntityType;
+        typedef typename GridPartType :: template Codim<0> :: IteratorType  IteratorType;
+
+        // we need All_Partition here to insert overlap entities 
+        // only for diagonal 
+        IteratorType endit = gridPart.template end<0>(); 
+        for(IteratorType it = gridPart.template begin<0>(); it != endit; ++it)
+        {
+          const EntityType & en = *it;
+          // add all column entities to row  
+          fill( gridPart,en,rowMapper,*buildProxy );
+        }
+
+        // insert size as last ghost 
+        buildProxy->insert( rowMapper.size() );
+      }
+
+    protected:
+      template <class GK, class SlaveDofProviderImp>
+      struct FillSlaveFunctor
+      {
+        typedef SlaveDofProviderImp SlaveDofProvider;
+        typedef GK GlobalKey;
+
+        FillSlaveFunctor ( SlaveDofProvider &slaves ) 
+        : slaves_( slaves ) 
+        {}
+
+        template < class ColGlobal >
+        void operator() ( const std::size_t colLocal, const ColGlobal &colGlobal )
+        {
+          slaves_.insert( colGlobal );
+        }
+
+      private:
+        SlaveDofProvider &slaves_;
+      };
+
+      //! create entries for element and neighbors 
+      template <class GridPartImp,
+                class EntityImp,
+                class RowMapperImp,
+                class ParallelScalarProductType>
+      static inline void fill(const GridPartImp& gridPart,
+                       const EntityImp& en,
+                       const RowMapperImp& rowMapper,
+                       ParallelScalarProductType& slaveDofs)
+      {
+#if HAVE_MPI 
+        typedef FillSlaveFunctor< typename RowMapperImp::GlobalKeyType, ParallelScalarProductType > SlaveFillFunctorType;
+        // if entity is not interior, insert into overlap entries 
+        if(en.partitionType() != InteriorEntity)
+          rowMapper.mapEach( en, SlaveFillFunctorType( slaveDofs ) );
+
+        // insert neighbors 
+        typedef typename GridPartImp::template Codim<0>::EntityPointerType EntityPointerType; 
+        typedef typename GridPartImp:: IntersectionIteratorType IntersectionIteratorType;
+        typedef typename IntersectionIteratorType :: Intersection IntersectionType;
+        IntersectionIteratorType endnit = gridPart.iend(en);
+        for(IntersectionIteratorType nit = gridPart.ibegin(en);
+            nit != endnit; ++nit)
+        {
+          // get intersection 
+          const IntersectionType& inter = *nit;
+
+          if( inter.neighbor() )
+          {
+            // get neighbor 
+            EntityPointerType ep = inter.outside();
+            const EntityImp& nb = *ep;
+            // check partition type 
+            if( nb.partitionType() != InteriorEntity )
+            {
+              rowMapper.mapEach( nb, SlaveFillFunctorType( slaveDofs ) );
+            }
+          }
+        }
+#endif
+      }
+    };
+
+  } // end namespace Fem
+
+} // end namespace Dune 
+#endif
