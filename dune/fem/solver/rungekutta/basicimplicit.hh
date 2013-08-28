e6a97005c949d1c7f775cf34ab1c781938546387
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2013-08-28 17:33:46 +0200

    add implicit ODE solver (based on previous work from Tobi)

---
+++ dune/fem/solver/rungekutta/basicimplicit.hh
@@ -0,0 +1,183 @@
+#ifndef DUNE_FEM_SOLVER_RUNGEKUTTA_BASICIMPLICIT_HH
+#define DUNE_FEM_SOLVER_RUNGEKUTTA_BASICIMPLICIT_HH
+
+//- system includes 
+#include <cassert>
+#include <sstream>
+#include <vector>
+
+//- dune-common includes
+#include <dune/common/dynmatrix.hh>
+#include <dune/common/dynvector.hh>
+#include <dune/common/nullptr.hh>
+
+//- dune-fem includes
+#include <dune/fem/solver/odesolver.hh>
+
+namespace DuneODE 
+{
+
+  /** \brief Implicit RungeKutta ODE solver. */
+  template< class HelmholtzOperator, class NonlinearSolver, class TimeStepControl >
+  class BasicImplicitRungeKuttaSolver
+  : public OdeSolverInterface< typename HelmholtzOperator::DestinationType >
+  {
+    typedef BasicImplicitRungeKuttaSolver< HelmholtzOperator, NonlinearSolver, TimeStepControl > ThisType;
+    typedef OdeSolverInterface< typename HelmholtzOperator::DestinationType > BaseType;
+
+  public:
+    typedef typename BaseType::MonitorType MonitorType;
+
+    typedef HelmholtzOperator HelmholtzOperatorType;
+    typedef NonlinearSolver NonlinearSolverType;
+    typedef TimeStepControl TimeStepControlType;
+
+    typedef Dune::Fem::TimeProviderBase TimeProviderType;
+
+    typedef typename HelmholtzOperatorType::DestinationType DestinationType;
+
+    typedef typename DestinationType::DiscreteFunctionSpaceType SpaceType;
+      
+    /** \brief constructor 
+     *
+     *  \param[in]  helmholtzOp   Helmholtz operator \f$L\f$
+     *  \param[in]  timeProvider  time provider
+     *  \param[in]  butcherTable  butcher table to use
+     *  \param[in]  traits        additional traits
+     */
+    template< class ButcherTable >
+    BasicImplicitRungeKuttaSolver ( HelmholtzOperatorType &helmholtzOp,
+                                    const ButcherTable &butcherTable,
+                                    const TimeStepControl &timeStepControl = TimeStepControl() )
+    : helmholtzOp_( helmholtzOp ),
+      nonlinearSolver_( helmholtzOp_ ),
+      timeStepControl_( timeStepControl ),
+      stages_( butcherTable.stages() ),
+      alpha_( butcherTable.A() ),
+      gamma_( stages() ),
+      beta_( stages() ),
+      c_( butcherTable.c() ),
+      rhs_( "RK rhs", helmholtzOp_.space() ),
+      update_( stages(), nullptr )
+    {
+      // create intermediate functions
+      for( int i = 0; i < stages(); ++i )
+      {
+        std::ostringstream name;
+        name << "RK stage " << i;
+        update_[ i ] = new DestinationType( name.str(), helmholtzOp_.space() );
+      }
+
+      // compute coefficients
+      Dune::DynamicMatrix< double > AL( alpha_ );
+      for( int i = 0; i < stages(); ++i )
+      {
+        gamma_[ i ] = AL[ i ][ i ];
+        AL[ i ][ i ] = 0.0;
+      }
+
+      alpha_.invert();
+      alpha_.mtv( butcherTable.b(), beta_ );
+
+      alpha_.leftmultiply( AL );
+      for( int i = 0; i < stages(); ++i )
+        alpha_[ i ][ i ] = gamma_[ i ];
+
+      for( int i = 0; i < stages(); ++i )
+      {
+        gamma_[ i ] = 1.0;
+        for( int j = 0; j < i; ++j )
+          gamma_[ i ] -= alpha_[ i ][ j ];
+      }
+
+      delta_ = 1.0;
+      for( int i = 0; i < stages(); ++i )
+        delta_ -= beta_[ i ];
+    }
+
+    /** \brief destructor */
+    ~BasicImplicitRungeKuttaSolver ()
+    {
+      for( int i = 0; i < stages(); ++i )
+        delete update_[ i ];
+    }
+
+    //! apply operator once to get dt estimate 
+    void initialize ( const DestinationType &U0 )
+    {
+      helmholtzOp_( U0, *update_[ 0 ] );
+      timeStepControl_.initialTimeStepSize( helmholtzOp_.timeStepEstimate() );
+    }
+
+    using BaseType::solve;
+
+    //! solve the system 
+    void solve ( DestinationType &U, MonitorType &monitor )
+    {
+      monitor.reset();
+
+      const double time = timeStepControl_.time();
+      const double timeStepSize = timeStepControl_.timeStepSize();
+      assert( timeStepSize > 0.0 );
+
+      for( int s; s < stages(); ++s )
+      {
+        // assemble rhs of nonlinear equation
+        rhs_.assign( U );
+        rhs_ *= gamma_[ s ];
+
+        for( int k = 0; k < s; ++k )
+          rhs_.axpy( alpha_[ s ][ k ], *update_[ k ] );
+         
+        // setup Helmholtz operator
+        helmholtzOp_.setTime( time + c_[ s ]*timeStepSize );
+        helmholtzOp_.setLambda( alpha_[ s ][ s ]*timeStepSize );
+
+        // solve the system
+        nonlinearSolver_( rhs_, *update_[ s ] );
+
+        // on failure break solving
+        if( !nonlinearSolver_.converged() )
+          return timeStepControl_.reduceTimeStep( helmholtzOp_.timeStepEstimate(), updateMonitor( monitor ) );
+      }
+
+      // update solution
+      U *= delta_;
+      for( int s = 0; s < stages(); ++s )
+        U.axpy( beta_[ s ], *update_[ s ] );
+
+      // update time step size
+      timeStepControl_.timeStepEstimate( helmholtzOp_.timeStepEstimate(), updateMonitor( monitor ) );
+    }
+
+    int stages () const { return stages(); }
+
+    void description ( std::ostream &out ) const
+    {
+      out << "Generic " << stages() << "-stage implicit Runge-Kutta solver.\\\\" << std::endl;
+    }
+
+  protected:
+    const MonitorType &updateMonitor ( MonitorType &monitor ) const
+    {
+      monitor.newtonIterations_ = nonlinearSolver_.iterations();
+      monitor.linearSolverIterations_ = nonlinearSolver_.linearIterations();
+      return monitor;
+    }
+
+    HelmholtzOperatorType &helmholtzOp_;
+    NonlinearSolverType nonlinearSolver_;
+    TimeStepControl timeStepControl_;
+
+    int stages_;
+    double delta_;
+    Dune::DynamicMatrix< double > alpha_;
+    Dune::DynamicVector< double > gamma_, beta_, c_;
+
+    DestinationType rhs_;
+    std::vector< DestinationType * > update_;
+  };
+
+} // namespace DuneODE
+
+#endif // #ifndef DUNE_FEM_SOLVER_RUNGEKUTTA_BASICIMPLICIT_HH
---
+++ dune/fem/solver/rungekutta/implicit.hh
@@ -0,0 +1,313 @@
+#ifndef DUNE_FEM_SOLVER_RUNGEKUTTA_IMPLICIT_HH
+#define DUNE_FEM_SOLVER_RUNGEKUTTA_IMPLICIT_HH
+
+//- system includes 
+#include <cassert>
+#include <sstream>
+#include <vector>
+
+//- dune-fem includes
+#include <dune/fem/io/parameter.hh>
+#include <dune/fem/solver/rungekutta/basicimplicit.hh>
+#include <dune/fem/solver/timeprovider.hh>
+
+namespace DuneODE 
+{
+
+  // ImplicitRungeKuttaSolverParameters
+  // ----------------------------------
+
+  struct ImplicitRungeKuttaSolverParameters
+  : public Dune::Fem::LocalParameter< ImplicitRungeKuttaSolverParameters, ImplicitRungeKuttaSolverParameters >
+  { 
+    enum { noVerbosity = 0,  noConvergenceVerbosity = 1,
+           cflVerbosity = 2, fullVerbosity = 3 };
+
+    ImplicitRungeKuttaSolverParameters ()
+    : min_it( Dune::Fem::Parameter::getValue< int >( "fem.ode.miniterations" , 14 ) ),
+      max_it( Dune::Fem::Parameter::getValue< int >( "fem.ode.maxiterations" , 16 ) ),
+      sigma( Dune::Fem::Parameter::getValue< double >( "fem.ode.cflincrease" , 1.1 ) )
+    {}
+
+    /** \brief tolerance for the non-linear solver (should be larger than the tolerance for
+               the linear solver */
+    virtual double tolerance () const
+    {
+      return Dune::Fem::Parameter::getValue< double >( "fem.ode.tolerance" , 1e-6 );
+    }
+
+    virtual int iterations() const
+    {
+      return Dune::Fem::Parameter::getValue< int >( "fem.ode.iterations" , 1000 );
+    }
+
+    /** \brief verbosity level ( none, noconv, cfl, full )  */
+    virtual int verbose () const
+    {
+      static const std::string verboseTypeTable[] = { "none", "noconv", "cfl", "full" };
+      return Dune::Fem::Parameter::getEnum( "fem.ode.verbose", verboseTypeTable, 0 );
+    }
+
+    virtual double cflStart () const
+    {
+      return Dune::Fem::Parameter::getValue< double >( "fem.ode.cflStart", 1 );
+    }
+
+    virtual double cflMax () const
+    {
+      return Dune::Fem::Parameter::getValue< double >( "fem.ode.cflMax" , std::numeric_limits< double >::max() );
+    }
+
+    double initialDeltaT ( double dt ) const
+    {
+      return std::min( Dune::Fem::Parameter::getValue< double >( "fem.ode.initialdt", 987654321 ), dt );
+    }
+
+    /** \brief return multiplication factor for the current cfl number
+     *  \param[in] imOpTimeStepEstimate Time step estimate of the first ode solver
+     *  \param[in] exOpTimeStepEstimate Time step estimate of the second ode solver
+     *  \param[in] solver Iterative linear solver (ILS)
+     *  \param[in] converged Convergence of the ILS
+     *  \param[out] factor Multiplication factor for the current cfl number
+     *
+     *  \note Do not increase the cfl number of the implicit solver if its time step 
+     *    estimate is already larger than the one of the explicit solver
+     */
+    virtual bool cflFactor( const double imOpTimeStepEstimate,
+                            const double exOpTimeStepEstimate,
+                            const int numberOfLinearIterations,
+                            bool converged,
+                            double &factor) const
+    {
+      const int iter = numberOfLinearIterations; 
+      factor = 1.;
+      bool changed = false;
+      if (converged) 
+      {
+        if (iter < min_it) 
+        {
+          if( imOpTimeStepEstimate <= exOpTimeStepEstimate )
+          {
+            factor = sigma;
+            changed = true;
+          }
+        }
+        else if (iter > max_it) 
+        {
+          factor = (double)max_it/(sigma*(double)iter);
+          changed = true;
+        }
+      }
+      else
+      {
+        factor = 0.5;
+        changed = true;
+      }
+      return changed;
+    }
+
+    virtual void initTimeStepEstimate ( const double dtEstExpl, const double dtEstImpl, double &dtEst, double &cfl ) const
+    {
+      // initial time step already set to explicit time step
+      dtEst = dtEstExpl;
+
+      // heuristics for initial CFL number
+      cfl = 1.0;
+      if( (dtEstImpl > 0) && (dtEstExpl > dtEstImpl) )
+        cfl = dtEstExpl / dtEstImpl;
+    }
+
+    // number of minimal iterations that the linear solver should do 
+    // if the number of iterations done is smaller then the cfl number is increased  
+    int min_it;
+    // number of maximal iterations that the linear solver should do 
+    // if the number of iterations larger then the cfl number is decreased   
+    int max_it;
+    // factor for cfl number on increase (decrease is 0.5)
+    double sigma;
+  };
+
+
+
+  // ImplicitRungeKuttaTimeStepControl
+  // ---------------------------------
+
+  class ImplicitRungeKuttaTimeStepControl
+  {
+    typedef ImplicitRungeKuttaTimeStepControl ThisType;
+
+  public:
+    typedef Dune::Fem::TimeProviderBase TimeProviderType;
+    typedef ImplicitRungeKuttaSolverParameters ParametersType;
+
+    explicit ImplicitRungeKuttaTimeStepControl ( TimeProviderType &timeProvider,
+                                                 const ParametersType &parameters = ParametersType() )
+    : timeProvider_( timeProvider ),
+      parameters_( parameters.clone() ),
+      cfl_( parameters_->cflStart() ),
+      cflMax_( parameters_->cflMax() ),
+      verbose_( parameters_->verbose() ),
+      initialized_( false )
+    {}
+
+    double time () const { return timeProvider_.time(); }
+    double timeStepSize () const { return timeProvider_.deltaT(); }
+
+    void initialTimeStepSize ( double explicitEstimate )
+    {
+      timeProvider_.provideTimeStepEstimate( parameters().initialDeltaT( explicitEstimate ) );
+      initialized_ = true;
+    }
+
+    template< class Monitor >
+    void reduceTimeStep ( double explicitEstimate, const Monitor &monitor )
+    {
+      if( !initialized_ )
+        DUNE_THROW( Dune::InvalidStateException, "ImplicitRungeKuttaSolver must be initialized before first solve." );
+
+      double factor( 1 );
+      parameters().cflFactor( explicitEstimate, explicitEstimate, monitor.newtonIterations_, false, factor );
+
+      if( !((factor >= std::numeric_limits< double >::min()) && (factor < 1.0)) )
+        DUNE_THROW( Dune::InvalidStateException, "invalid cfl factor: " << factor );
+
+      cfl_ *= factor;
+      timeProvider_.provideTimeStepEstimate( cfl_ * explicitEstimate );
+
+      if( (verbose_ >= ImplicitRungeKuttaSolverParameters::cflVerbosity) && (Dune::Fem::MPIManager::rank() == 0) )
+      {
+        Dune::derr << "Implicit Runge-Kutta step failed to convergence." << std::endl;
+        Dune::derr << "New cfl number: " << cfl_
+                   << ", iterations per time step: ILS = " << monitor.linearSolverIterations_
+                   << ", INLS = " << monitor.newtonIterations_
+                   << std::endl;
+      }
+
+      timeProvider_.provideTimeStepEstimate( factor * timeStepSize() );
+      timeProvider_.invalidateTimeStep();
+    }
+
+    template< class Monitor >
+    void timeStepEstimate ( double explicitEstimate, const Monitor &monitor )
+    {
+      if( !initialized_ )
+        DUNE_THROW( Dune::InvalidStateException, "ImplicitRungeKuttaSolver must be initialized before first solve." );
+
+      double factor( 1 );
+      parameters().cflFactor( explicitEstimate, explicitEstimate, monitor.newtonIterations_, true, factor );
+      if( !((factor >= std::numeric_limits< double >::min()) && (factor <= std::numeric_limits< double >::max())) )
+        DUNE_THROW( Dune::InvalidStateException, "invalid cfl factor: " << factor );
+
+      const double oldCfl = cfl_;
+      cfl_ = std::min( cflMax_, factor * cfl_ );
+      timeProvider_.provideTimeStepEstimate( cfl_ * explicitEstimate );
+
+      if( (cfl_ != oldCfl) && (verbose_ >= ImplicitRungeKuttaSolverParameters::cflVerbosity) && (Dune::Fem::MPIManager::rank() == 0) )
+      {
+        Dune::derr << "New cfl number: " << cfl_
+                   << ", iterations per time step: ILS = " << monitor.linearSolverIterations_
+                   << ", INLS = " << monitor.newtonIterations_
+                   << std::endl;
+      }
+    }
+
+  private:
+    const ParametersType &parameters () const
+    {
+      assert( parameters_ );
+      return *parameters_;
+    }
+
+    TimeProviderType &timeProvider_;
+    const ParametersType *parameters_;
+    double cfl_, cflMax_;
+    int verbose_;
+    bool initialized_;
+  };
+
+
+
+  /** \brief Implicit RungeKutta ODE solver. */
+  template< class HelmholtzOperator, class NonlinearSolver >
+  class ImplicitRungeKuttaSolver
+  : public BasicImplicitRungeKuttaSolver< HelmholtzOperator, NonlinearSolver, ImplicitRungeKuttaTimeStepControl >
+  {
+    typedef ImplicitRungeKuttaSolver< HelmholtzOperator, NonlinearSolver > ThisType;
+    typedef BasicImplicitRungeKuttaSolver< HelmholtzOperator, NonlinearSolver, ImplicitRungeKuttaTimeStepControl > BaseType;
+
+  public:
+    typedef HelmholtzOperator HelmholtzOperatorType;
+    typedef typename BaseType::TimeStepControlType TimeStepControlType;
+
+
+    typedef typename TimeStepControlType::TimeProviderType TimeProviderType;
+    typedef typename TimeStepControlType::ParametersType ParametersType;
+
+    /** \brief constructor 
+     *
+     *  \param[in]  helmholtzOp   Helmholtz operator \f$L\f$
+     *  \param[in]  timeProvider  time provider
+     *  \param[in]  butcherTable  butcher table to use
+     *  \param[in]  traits        additional traits
+     */
+    template< class ButcherTable >
+    ImplicitRungeKuttaSolver ( HelmholtzOperatorType &helmholtzOp,
+                               TimeProviderType &timeProvider,
+                               int order = 1,
+                               const ParametersType &parameters = ParametersType() )
+    : BaseType( helmholtzOp, butcherTable( order ), TimeStepControlType( timeProvider, parameters ) )
+    {}
+
+  protected:
+    class ButcherTable
+    {
+      friend class ImplicitRungeKuttaSolver< HelmholtzOperator, NonlinearSolver >;
+      Dune::DynamicMatrix< double > A_;
+      Dune::DynamicVector< double > b_, c_;
+
+      explicit ButcherTable ( int stages )
+      : A_( stages, stages ),
+        b_( stages ),
+        c_( stages )
+      {}
+
+    public:
+      int stages () const { return b.size(); }
+      const Dune::DynamicMatrix< double > &A () const { return A_; }
+      const Dune::DynamicVector< double > &b () const { return b_; }
+      const Dune::DynamicVector< double > &c () const { return c_; }
+    };
+
+    static ButcherTable butcherTable ( int order )
+    {
+      switch( order )
+      {
+      case 1:
+        {
+          ButcherTable bt( 1 );
+          bt.A_[ 0 ][ 0 ] = 1.0;
+          bt.b_[ 0 ] = 1.0;
+          bt.c_[ 0 ] = 1.0;
+          return bt;
+        }
+
+      case 2:
+        {
+          ButcherTable bt( 2 );
+          bt.A_[ 0 ][ 0 ] = 0.5 * (1.0 - 1.0 / std::sqrt( 3.0 ));
+          bt.A_[ 0 ][ 1 ] = 0.0;
+          bt.A_[ 1 ][ 0 ] = 1.0 / std::sqrt( 3.0 );
+          bt.A_[ 1 ][ 1 ] = 0.5 * (1.0 - 1.0 / std::sqrt( 3.0 ));
+          bt.b_[ 0 ] = 0.5;
+          bt.b_[ 1 ] = 0.5;
+          bt.c_[ 0 ] = 0.5 * (1.0 - 1.0 / std::sqrt( 3.0 ));
+          bt.c_[ 0 ] = 0.5 * (1.0 + 1.0 / std::sqrt( 3.0 ));
+          return bt;
+        }
+      }
+    }
+  };
+
+} // namespace DuneODE
+
+#endif // #ifndef DUNE_FEM_SOLVER_RUNGEKUTTA_IMPLICIT_HH
