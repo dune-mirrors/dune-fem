7fe3e33ea95ee30b11543a113554d96a9694f5f7
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2013-09-05 21:37:58 +0200

    add domainEntity / rangeEntity

--- dune/fem/operator/common/localmatrixwrapper.hh
+++ dune/fem/operator/common/localmatrixwrapper.hh
@@ -9,60 +9,69 @@ namespace Dune
   namespace Fem
   {
 
-    template< class LocalMatrixStackImp >
+    // Internal Forward Declarations
+    // -----------------------------
+
+    template< class LocalMatrixStack >
     class LocalMatrixWrapper;
 
 
-    template< class LocalMatrixStackImp >
+
+    // LocalMatrixWrapperTraits
+    // ------------------------
+
+    template< class LocalMatrixStack >
     struct LocalMatrixWrapperTraits
     {
-      typedef LocalMatrixStackImp LocalMatrixStackType;
+      typedef LocalMatrixStack LocalMatrixStackType;
 
-      typedef typename LocalMatrixStackImp :: ObjectType WrappedLocalMatrixType;
+      typedef typename LocalMatrixStack::ObjectType WrappedLocalMatrixType;
 
       typedef LocalMatrixWrapper< LocalMatrixStackType > LocalMatrixType;
 
-      typedef typename WrappedLocalMatrixType :: RangeFieldType RangeFieldType;
-      
-      typedef typename WrappedLocalMatrixType :: DomainSpaceType DomainSpaceType;
-      typedef typename WrappedLocalMatrixType :: RangeSpaceType RangeSpaceType;
+      typedef typename WrappedLocalMatrixType::RangeFieldType RangeFieldType;
 
-      typedef typename WrappedLocalMatrixType :: LittleBlockType LittleBlockType;
+      typedef typename WrappedLocalMatrixType::DomainSpaceType DomainSpaceType;
+      typedef typename WrappedLocalMatrixType::RangeSpaceType RangeSpaceType;
+
+      typedef typename WrappedLocalMatrixType::LittleBlockType LittleBlockType;
     };
 
 
-    template< class LocalMatrixStackImp >
+
+    // LocalMatrixWrapper
+    // ------------------
+
+    template< class LocalMatrixStack >
     class LocalMatrixWrapper
-    : public LocalMatrixInterface< LocalMatrixWrapperTraits< LocalMatrixStackImp > >
+    : public LocalMatrixInterface< LocalMatrixWrapperTraits< LocalMatrixStack > >
     {
+      typedef LocalMatrixWrapper< LocalMatrixStack > ThisType;
+      typedef LocalMatrixInterface< LocalMatrixWrapperTraits< LocalMatrixStack > > BaseType;
+
     public:
       //! type of the local matrix stack
-      typedef LocalMatrixStackImp LocalMatrixStackType;
+      typedef LocalMatrixStack LocalMatrixStackType;
 
       //! type of the traits
       typedef LocalMatrixWrapperTraits< LocalMatrixStackType > Traits;
 
-    private:
-      typedef LocalMatrixWrapper< LocalMatrixStackType > ThisType;
-      typedef LocalMatrixInterface< Traits > BaseType;
-
-    public:
       //! type of the wrapped local matrix
-      typedef typename Traits :: WrappedLocalMatrixType WrappedLocalMatrixType;
+      typedef typename Traits::WrappedLocalMatrixType WrappedLocalMatrixType;
 
-      typedef typename Traits :: RangeFieldType RangeFieldType;
+      typedef typename Traits::RangeFieldType RangeFieldType;
 
-      typedef typename BaseType :: DomainSpaceType DomainSpaceType;
-      typedef typename BaseType :: RangeSpaceType RangeSpaceType;
+      typedef typename BaseType::DomainSpaceType DomainSpaceType;
+      typedef typename BaseType::RangeSpaceType RangeSpaceType;
 
-      typedef typename BaseType :: DomainBasisFunctionSetType
-        DomainBasisFunctionSetType;
-      typedef typename BaseType :: RangeBasisFunctionSetType
-        RangeBasisFunctionSetType;
+      typedef typename BaseType::DomainBasisFunctionSetType DomainBasisFunctionSetType;
+      typedef typename BaseType::RangeBasisFunctionSetType RangeBasisFunctionSetType;
+
+      typedef typename BaseType::DomainEntityType DomainEntityType;
+      typedef typename BaseType::RangeEntityType RangeEntityType;
 
     private:
-      typedef typename LocalMatrixStackType :: PointerType
-        WrappedLocalMatrixPtrType;
+      typedef typename LocalMatrixStackType::PointerType WrappedLocalMatrixPtrType;
 
     private:
       // ObjectPointer to the actual local matrix
@@ -74,15 +83,14 @@ namespace Dune
 
     public:
       //! constructor creating an uninitialized local matrix
-      inline explicit LocalMatrixWrapper ( LocalMatrixStackType &stack )
+      explicit LocalMatrixWrapper ( LocalMatrixStackType &stack )
       : localMatrixPtr_( stack.getObject() ),
         localMatrix_( *localMatrixPtr_ )
-      {
-      }
-      
+      {}
+
       //! constructor initializing the wrapped local matrix
       template< class DomainEntityType, class RangeEntityType >
-      inline LocalMatrixWrapper( LocalMatrixStackType &stack,
+      LocalMatrixWrapper( LocalMatrixStackType &stack,
                                  const DomainEntityType &domainEntity,
                                  const RangeEntityType &rangeEntity )
       : localMatrixPtr_( stack.getObject() ),
@@ -96,158 +104,147 @@ namespace Dune
        *
        *  \param[in]  other  LocalMatrixWrapper to copy
        */
-      inline LocalMatrixWrapper ( const ThisType &other )
+      LocalMatrixWrapper ( const ThisType &other )
       : localMatrixPtr_( other.localMatrixPtr_ ),
         localMatrix_( *localMatrixPtr_ )
-      {
-      }
+      {}
 
       /** \brief destructor */
-      inline ~LocalMatrixWrapper ( )
+      ~LocalMatrixWrapper ( )
       {
-        // call finalize on local matrix implementation 
+        // call finalize on local matrix implementation
         // (e.g. needed for PETSc to add values to the real matrix)
         localMatrix().finalize();
       }
 
     private:
       // prohibit assignment
-      inline ThisType &operator= ( const ThisType & );
+      ThisType &operator= ( const ThisType & );
 
     public:
       /** \copydoc Dune::Fem::LocalMatrixInterface::init */
-      template< class DomainEntityType, class RangeEntityType >
-      inline void init ( const DomainEntityType &domainEntity,
-                         const RangeEntityType &rangeEntity )
+      void init ( const DomainEntityType &domainEntity, const RangeEntityType &rangeEntity )
       {
         localMatrix().init( domainEntity, rangeEntity );
       }
 
       /** \copydoc Dune::Fem::LocalMatrixInterface::add */
-      inline void add ( const int localRow,
-                        const int localCol,
-                        const RangeFieldType &value )
+      void add ( int localRow, int localCol, const RangeFieldType &value )
       {
         localMatrix().add( localRow, localCol, value );
       }
-      
+
       /** \copydoc Dune::Fem::LocalMatrixInterface::set */
-      inline void set ( const int localRow,
-                        const int localCol,
-                        const RangeFieldType &value )
+      void set ( int localRow, int localCol, const RangeFieldType &value )
       {
         localMatrix().set( localRow, localCol, value );
       }
-      
+
       /** \copydoc Dune::Fem::LocalMatrixInterface::unitRow */
-      inline void unitRow ( const int localRow ) DUNE_DEPRECATED
+      void unitRow ( const int localRow ) DUNE_DEPRECATED
       {
-        localMatrix().unitRow( localRow ); 
+        localMatrix().unitRow( localRow );
       }
 
       /** \copydoc Dune::Fem::LocalMatrixInterface::clearRow */
-      inline void clearRow ( const int localRow )
+      void clearRow ( const int localRow )
       {
         localMatrix().clearRow( localRow );
       }
 
       /** \copydoc Dune::Fem::LocalMatrixInterface::clearRow */
-      inline void clearCol ( const int localCol )
+      void clearCol ( const int localCol )
       {
         localMatrix().clearCol( localCol );
       }
-      
+
       /** \copydoc Dune::Fem::LocalMatrixInterface::get */
-      inline const RangeFieldType get ( const int localRow,
+      const RangeFieldType get ( const int localRow,
                                         const int localCol ) const
       {
         return localMatrix().get( localRow, localCol );
       }
 
       /** \copydoc Dune::Fem::LocalMatrixInterface::scale */
-      inline void scale ( const RangeFieldType& scalar ) 
+      void scale ( const RangeFieldType& scalar )
       {
         return localMatrix().scale( scalar );
       }
 
       /** \copydoc Dune::Fem::LocalMatrixInterface::clear */
-      inline void clear ()
+      void clear ()
       {
         return localMatrix().clear();
       }
 
       /** \copydoc Dune::Fem::LocalMatrixInterface::resort */
-      inline void resort ()
+      void resort ()
       {
         return localMatrix().resort();
       }
 
       /** \copydoc Dune::Fem::LocalMatrixInterface::rows */
-      inline int rows () const
+      int rows () const
       {
         return localMatrix().rows();
       }
 
       /** \copydoc Dune::Fem::LocalMatrixInterface::columns */
-      inline int columns () const
+      int columns () const
       {
         return localMatrix().columns();
       }
 
       /** \copydoc Dune::Fem::LocalMatrixInterface::multiplyAdd */
-      template <class DomainLocalFunctionImp, 
+      template <class DomainLocalFunctionImp,
                 class RangeLocalFunctionImp>
-      inline void multiplyAdd(const DomainLocalFunctionImp& dLf,
+      void multiplyAdd(const DomainLocalFunctionImp& dLf,
                               RangeLocalFunctionImp& rLf)
       {
-        localMatrix().multiplyAdd( dLf, rLf); 
+        localMatrix().multiplyAdd( dLf, rLf);
       }
-      
+
       /** \copydoc Dune::Fem::LocalMatrixInterface::domainSpace */
-      inline const DomainSpaceType &domainSpace () const
+      const DomainSpaceType &domainSpace () const
       {
         return localMatrix().domainSpace();
       }
 
       /** \copydoc Dune::Fem::LocalMatrixInterface::rangeSpace */
-      inline const RangeSpaceType &rangeSpace () const
+      const RangeSpaceType &rangeSpace () const
       {
         return localMatrix().rangeSpace();
       }
-      
+
+      const DomainEntityType &domainEntity () const { return localMatrix().domainEntity(); }
+      const RangeEntityType &rangeEntity () const { return localMatrix().rangeEntity(); }
+
       /** \copydoc Dune::Fem::LocalMatrixInterface::domainBasisFunctionSet */
-      inline const DomainBasisFunctionSetType &domainBasisFunctionSet () const
+      const DomainBasisFunctionSetType &domainBasisFunctionSet () const
       {
         return localMatrix().domainBasisFunctionSet();
       }
 
       /** \copydoc Dune::Fem::LocalMatrixInterface::rangeBasisFunctionSet */
-      inline const RangeBasisFunctionSetType &rangeBasisFunctionSet () const
+      const RangeBasisFunctionSetType &rangeBasisFunctionSet () const
       {
         return localMatrix().rangeBasisFunctionSet();
       }
 
     protected:
-      inline const WrappedLocalMatrixType &localMatrix () const
-      {
-        return localMatrix_;
-      }
-
-      inline WrappedLocalMatrixType &localMatrix ()
-      {
-        return localMatrix_;
-      }
+      const WrappedLocalMatrixType &localMatrix () const { return localMatrix_; }
+      WrappedLocalMatrixType &localMatrix () { return localMatrix_; }
     };
 
   } // namespace Fem
 
-#if DUNE_FEM_COMPATIBILITY  
-  // put this in next version 1.4 
+#if DUNE_FEM_COMPATIBILITY
+  // put this in next version 1.4
 
-  using Fem :: LocalMatrixWrapper ;
+  using Fem::LocalMatrixWrapper;
 
 #endif // DUNE_FEM_COMPATIBILITY
-  
+
 } // namespace Dune
 
 #endif // #ifndef DUNE_FEM_LOCALMATRIXWRAPPER_HH
