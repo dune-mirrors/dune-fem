3df1ebe8474b47df4da51efbe83e10ac0031911b
Robert Kloefkorn <robertk@ucar.edu>
2013-10-09 13:15:57 -0600

    fix version bump.

--- dune/fem/version.hh
+++ dune/fem/version.hh
@@ -4,8 +4,6 @@
 #include <dune/common/deprecated.hh>
 
 // for all future versions use the usual DUNE_DEPRECATED
-#define DUNE_VERSION_DEPRECATED_1_4(m) DUNE_DEPRECATED
-#define DUNE_VERSION_DEPRECATED_1_5(m) DUNE_DEPRECATED
 #define DUNE_VERSION_DEPRECATED_1_6(m) DUNE_DEPRECATED
 #define DUNE_VERSION_DEPRECATED_1_7(m) DUNE_DEPRECATED
 #define DUNE_VERSION_DEPRECATED_1_8(m) DUNE_DEPRECATED
@@ -13,6 +11,9 @@
 
 // current version should fail 
 #define DUNE_VERSION_DEPRECATED_1_4(newmethod) \
+  dune_remove_deprecated_method__use_##newmethod##_instead
+
+#define DUNE_VERSION_DEPRECATED_1_5(newmethod) \
   dune_remove_deprecated_method__use_##newmethod##_instead
 
 #define DUNE_VERSION_DEPRECATED(major,minor,newmethod) \
