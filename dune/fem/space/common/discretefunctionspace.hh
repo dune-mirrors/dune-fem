241e7510b8ad4bf94cb5034738cde39c7eb54c41
Robert Kloefkorn <robertk@ucar.edu>
2013-10-19 11:38:19 -0600

    Revert DofManager reference changes.

--- dune/fem/space/common/discretefunctionspace.hh
+++ dune/fem/space/common/discretefunctionspace.hh
@@ -620,6 +620,9 @@ namespace Dune
       typedef AllGeomTypes< IndexSetType, GridType > AllGeometryTypes;
       const AllGeometryTypes allGeomTypes_;
 
+      // reference to dof manager 
+      DofManagerType& dofManager_;
+
       // communication manager 
       const InterfaceType commInterface_;
       const CommunicationDirection commDirection_;
@@ -635,6 +638,7 @@ namespace Dune
         lfFactory_( asImp() ),
         lfStorage_( lfFactory_ ),
         allGeomTypes_( gridPart.indexSet() ),
+        dofManager_( DofManagerType :: instance( gridPart.grid() ) ),
         commInterface_( commInterface ),
         commDirection_( commDirection ),
         communicator_( 0 )
@@ -651,7 +655,7 @@ namespace Dune
       /** \copydoc Dune::Fem::DiscreteFunctionSpaceInterface::sequence */
       inline int sequence () const
       { 
-        return DofManagerType :: instance( grid() ).sequence();
+        return dofManager_.sequence();
       }
 
       /** \brief default implementation of the method order 
