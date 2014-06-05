c625198fb407ddc768c248ccff11f13f6a5a5d32
Tobias Malkmus <tomalk@mathematik.uni-freiburg.de>
2014-06-05 09:18:14 +0200

    allow construction of mutable localfunction from const DiscreteFunction
    This was always possible and should be removed in the future.

--- dune/fem/function/localfunction/mutable.hh
+++ dune/fem/function/localfunction/mutable.hh
@@ -49,7 +49,6 @@ namespace Dune
       //! type of BasisFunctionSet
       typedef typename BaseType::BasisFunctionSetType BasisFunctionSetType;
 
-
       //! cast from ConstLocalFunction
       MutableLocalFunction( const ConstLocalFunction< DiscreteFunctionType > &constLocalFunction ) DUNE_DEPRECATED
       : BaseType( constLocalFunction.basisFunctionSet(), LocalDofVectorType( constLocalFunction.discreteFunction_.localDofVectorAllocator() ) ),
@@ -58,13 +57,17 @@ namespace Dune
         discreteFunction().getLocalDofs( constLocalFunction.entity(), localDofVector() );
       }
 
-
-      //! Constructor creating empty local function from given discrete function 
-      explicit MutableLocalFunction ( DiscreteFunctionType &discreteFunction ) 
+      //! Constructor creating empty local function from given discrete function
+      explicit MutableLocalFunction ( DiscreteFunctionType &discreteFunction )
       : BaseType( LocalDofVectorType( discreteFunction.localDofVectorAllocator() ) ),
         discreteFunction_( &discreteFunction )
-      {
-      }
+      {}
+
+      //! Constructor creating empty local function from given discrete function 
+      explicit MutableLocalFunction ( const DiscreteFunctionType &discreteFunction )
+      : BaseType( LocalDofVectorType( discreteFunction.localDofVectorAllocator() ) ),
+        discreteFunction_( &const_cast<DiscreteFunctionType &>( discreteFunction ) )
+      {}
 
       //! Constructor creating local function from given discrete function and entity, not empty
       explicit MutableLocalFunction ( DiscreteFunctionType &discreteFunction, const EntityType &entity ) 
