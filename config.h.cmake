3aaa616779a54aa7bee9d138faf4436d6979f5ba
Stefan Girke <stefan.girke@gmx.de>
2014-08-01 15:34:42 +0200

    [cmake] adds verion macros to config.h
    Conflicts:
    	dune/fem/space/common/dofmanager.hh

---
+++ config.h.cmake
@@ -0,0 +1,15 @@
+/* begin dune-fem */
+
+/* Define to the version of dune-fem */
+#define DUNE_FEM_VERSION "${DUNE_FEM_VERSION}"
+
+/* Define to the major version of dune-fem */
+#define DUNE_FEM_VERSION_MAJOR ${DUNE_FEM_VERSION_MAJOR}
+
+/* Define to the minor version of dune-fem */
+#define DUNE_FEM_VERSION_MINOR ${DUNE_FEM_VERSION_MINOR}
+
+/* Define to the revision of dune-fem */
+#define DUNE_FEM_VERSION_REVISION ${DUNE_FEM_VERSION_REVISION}
+
+/* end dune-fem */
