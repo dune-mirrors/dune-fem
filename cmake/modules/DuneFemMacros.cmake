afa607a318452d75d8eb63231d10d64bb6c28180
Stefan Girke <stefan.girke@gmx.de>
2014-10-14 15:23:37 +0200

    [cmake] add xdr stuff to DuneFemMacros.cmake

--- cmake/modules/DuneFemMacros.cmake
+++ cmake/modules/DuneFemMacros.cmake
@@ -21,6 +21,26 @@ else(HAVE_ENDIAN_HEADER_HH)
 endif(HAVE_ENDIAN_HEADER_HH)
 add_definitions("-DSYSTEM_ENDIAN_HEADER=${SYSTEM_ENDIAN_HEADER}")
 
+#todo: there seems to be no cmake equivalent for
+#xdr issues in dune-common. Thus, the following lines are only for completeness
+#Please check whether it works and/or delete this stuff.
+find_path(DUNE_PATH_XDR
+  NAMES rpc/rpc.h )
+if(DUNE_PATH_XDR)
+  include(CheckFunctionExists)
+  check_function_exists(xdr_uint64_t XDR_UINT64_FUNC_EXISTS)
+  if( XDR_UINT64_FUNC_EXISTS )
+    set(XDR_UINT64_FUNC "xdr_uint64_t"
+      CACHE STRING "xdr_unit64_t routine")
+  else(XDR_UINT64_FUNC_EXISTS )
+    check_function_exists(xdr_u_int64_t XDR_U_INT64_FUNC_EXISTS)
+    if( XDR_U_INT64_FUNC_EXISTS )
+      set(XDR_UINT64_FUNC "xdr_u_int64_t"
+        CACHE STRING "xdr_unit64_t routine")
+    endif(XDR_U_INT64_FUNC_EXISTS )
+  endif(XDR_UINT64_FUNC_EXISTS )
+endif(DUNE_PATH_XDR)
+
 
 
 
