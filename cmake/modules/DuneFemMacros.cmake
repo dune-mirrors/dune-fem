2e4c14514741de7a05ef44fdf57f163023bc2368
Stefan Girke <stefan.girke@gmx.de>
2014-10-14 10:00:55 +0200

    [cmake] enable experimental grid extensions again, because dune-grid uses "wrong" compiler definition

--- cmake/modules/DuneFemMacros.cmake
+++ cmake/modules/DuneFemMacros.cmake
@@ -1,4 +1,8 @@
-message(AUTHOR_WARNING "TODO: Implement module test.")
 
+#make libdunefem known locally
 set(LOCAL_LIBS "${PROJECT_BINARY_DIR}/lib/libdunefem.a"
 		CACHE STRING "path to local libs in dune-fem" )
+
+
+message(AUTHOR_WARNING "TODO. Improve module test.")
+
