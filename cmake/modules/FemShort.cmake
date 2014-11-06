e3bc8afd800403da09464f6b88acea3d98f2e23b
Stefan Girke <stefan.girke@gmx.de>
2014-11-06 11:54:22 +0100

    [cmake] more advanced dune_add_test() to handle failed tests

--- cmake/modules/FemShort.cmake
+++ cmake/modules/FemShort.cmake
@@ -37,26 +37,74 @@ endfunction(dune_add_subdirs)
 
 # usage
 #
+# simple version:
+#
 # dune_add_test( <test1> [<test2>...] 
-#                [TEST <test_1> [test_2] ]
-#                [DEPENDENCY <dep1> [dep_2] ]
+#                [FAILTEST <ftest1> ...]
+#                [COMPILEFAILTEST <cftest1> ... ] )
 #
 # Simply adds all test to the cmake testing framework.
+#
+# key words:
+# FAILTEST         the following test should fail to be successfull
+# COMPILFAILTEST   the following test does not compile 
+#
+#
+# advanced version (needed in dune-fem):
+#
+# dune_add_test( <test1> [<test2>...] 
+#                [NO_DEPENDENCY] <ntest1> [<ntest2>...] 
+#                [FAILTEST [NO_DEPENDENCY] <ftest1> ...]
+#                [COMPILEFAILTEST <cftest1> ... ]
+#                [DEPENDENCY_ONLY <dep1> [dep_2] ] )
+#
+# arguments as above and:
+#
+# DEPENDENCY_ONLY  do not add targets to the test, but add them as dependencies
+# NO_DEPENDENCY    add targets to test, but not as dependency. Should be combined 
+#                  with targets or FAILTEST targets...
 #
 # All of these tests are not build during make all.
 # Build these targets with make test.
 function(dune_add_test)
   include(CMakeParseArguments)
-  cmake_parse_arguments(ADD_TEST "" "" "TEST;DEPENDENCY" ${ARGN})
-    
-  foreach(i ${ADD_TEST_UNPARSED_ARGUMENTS} ${ADD_TEST_TEST})
-    add_test(${i} ${i})
-  endforeach(i ${ADD_TEST_UNPARSED_ARGUMENTS} ${ADD_TEST_TEST})
 
-  if(ADD_TEST_UNPARSED_ARGUMENTS)
+  #parse first level
+  cmake_parse_arguments(ADD_TEST "" "" 
+    "FAILTEST;COMPILEFAILTEST;DEPENDENCY_ONLY" ${ARGN})
+  
+
+  #parse second level
+  cmake_parse_arguments(ADD_TEST_UNPARSED_ARGUMENTS "" "" "NO_DEPENDENCY" ${ADD_TEST_UNPARSED_ARGUMENTS}) 
+  cmake_parse_arguments(ADD_TEST_FAILTEST "" "" "NO_DEPENDENCY" ${ADD_TEST_FAILTEST}) 
+  cmake_parse_arguments(ADD_TEST_COMPILEFAILTEST "" "" "NO_DEPENDENCY" ${ADD_TEST_COMPILEFAILTEST}) 
+
+  #delete key words form first level
+  set(ADD_TEST_UNPARSED_ARGUMENTS "${ADD_TEST_UNPARSED_ARGUMENTS_UNPARSED_ARGUMENTS};${ADD_TEST_UNPARSED_ARGUMENTS_NO_DEPENDENCY}")
+  set(ADD_TEST_FAILTEST "${ADD_TEST_FAILTEST_UNPARSED_ARGUMENTS};${ADD_TEST_FAILTEST_NO_DEPENDENCY}")
+  set(ADD_TEST_COMPILEFAILTEST "${ADD_TEST_COMPILEFAILTEST_UNPARSED_ARGUMENTS};${ADD_TEST_COMPILEFAILTEST_NO_DEPENDENCY}")
+  
+  #add tests 
+  foreach(i ${ADD_TEST_UNPARSED_ARGUMENTS} ${ADD_TEST_FAILTEST})
+    add_test(${i} ${i})
+  endforeach(i ${ADD_TEST_UNPARSED_ARGUMENTS} ${ADD_TEST_FAILTEST})
+  # compile tests that should fail
+  foreach(i ${ADD_TEST_COMPILEFAILTEST})
+    add_test(NAME ${i}
+    COMMAND ${CMAKE_COMMAND} --build . --target ${_TEST} --config $<CONFIGURATION>)
+  endforeach(i ${ADD_TEST_COMPILEFAILTEST})
+
+  set(DEP_TESTS ${ADD_TEST_UNPARSED_ARGUMENTS_UNPARSED_ARGUMENTS} 
+                ${ADD_TEST_FAILTEST_UNPARSED_ARGUMENTS} )
+  if(DEP_TESTS)
     add_directory_test_target(_test_target)
-    add_dependencies(${_test_target} ${ADD_TEST_UNPARSED_ARGUMENTS} ${ADD_TEST_DEPENDENCY} ${COMPILE_XFAIL_TESTS})
+    add_dependencies(${_test_target} ${DEP_TESTS})
   endif()
+
+  #Set properties for failing tests
+  set_tests_properties(${ADD_TEST_FAILTEST} ${ADD_TEST_COMPILEFAILTEST}
+    PROPERTIES WILL_FAIL true)
+
 endfunction(dune_add_test)
 
 # 
