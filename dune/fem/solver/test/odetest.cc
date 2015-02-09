40ab9cf3a3b98e068de0a06fd200d72af258966f
Robert K <rokl@iris.no>
2015-02-09 17:06:27 +0100

    only one ode test.

--- dune/fem/solver/test/odetest.cc
+++ dune/fem/solver/test/odetest.cc
@@ -1,27 +1,40 @@
-// *****************************
+// ****************************************
 //
-// Solve the ODE dy/dt = F(y,t)
+// Solve the system of ODEs dy/dt = F(y,t)
+//                         F(y,0) = 0
 //
-// *****************************
+// ****************************************
 
 // standard includes
 #include <config.h>
 #include <iostream>
 
 // dune includes
-#include <dune/fem/misc/mpimanager.hh>
+#include <dune/common/fvector.hh>
 #include <dune/fem/solver/timeprovider.hh>
 #include <dune/fem/operator/common/spaceoperatorif.hh>
+
+#include <dune/fem/solver/odesolver.hh>
+
 #include <dune/fem/solver/rungekutta/explicit.hh>
+//#include <dune/fem/solver/rungekutta/implicit.hh>
 
+#include <dune/fem/io/parameter.hh>
 
-// Faked data structure for our unknown,
-// nothing more than a "tuned up" double
-class myDest {
-  struct SpaceDummy
-  {
-    int size () const { return 1; }
+static const int systemSize = 5;
+
+// Data structure for our unknown: fieldvector of
+// dimension N (N = number of ODEs) with some additional methods.
+// In scalar case (N=1) we have nothing more
+// than a simple, tuned up double.
+template <int N>
+class myDest : public Dune::FieldVector<double, N> {
+  typedef myDest< N > ThisType;
+private:
+  struct SpaceDummy {
+    int size () const { return N; }
   };
+  typedef Dune::FieldVector<double, N> BaseType;
 
 public:
   typedef double DomainFieldType;
@@ -29,43 +42,50 @@ public:
   typedef SpaceDummy DiscreteFunctionSpaceType;
 
   myDest(std::string, const SpaceDummy&, const double* u = 0) {
+    clear();
   }
+
   myDest() {
+    clear();
   }
-  RangeFieldType operator[](int i) const {
-    return d_;
+
+  void clear() {
+    BaseType::operator=(0.);
   }
-  RangeFieldType& operator[](int i) {
-    return d_;
-  }
+
   void assign(const myDest& other) {
-    d_ = other.d_;
+    BaseType::operator=(other);
   }
+
   void axpy(RangeFieldType l, const myDest& other) {
-    d_ += other.d_*l;
-  }
-  myDest& operator*=(RangeFieldType l) {
-    d_ *= l;
-    return *this;
-  }
-  myDest& operator+=(const myDest& other) {
-    d_ += other.d_;
-    return *this;
-  }
-  myDest& operator-=(const myDest& other) {
-    d_ += other.d_;
-    return *this;
+    BaseType::axpy(l, other);
   }
 
-  RangeFieldType scalarProductDofs ( const myDest &other ) const { return d_ * other.d_; }
+  double operator()(int i) const {
+    if (i<0 || i>N-1) {
+      std::cout << "ERROR: Accessing element " << i << std::endl;
+    }
+    return (*this)[i];
+  }
 
-private:
-  RangeFieldType d_;
+  double* leakPointer() { return &this->operator[](0); }
+  const double* leakPointer() const { return &this->operator[](0); }
+
+  double scalarProductDofs ( const ThisType &other ) const
+  {
+    double scp = 0;
+    for( std::size_t i=0; i < N; ++i )
+      scp += (*this)[ i ] * other[ i ];
+
+    return scp;
+  }
+
 };
 
 
 // implement right hand side F(y,t)
-class myRHS : public Dune::Fem::SpaceOperatorInterface<myDest> {
+// here: system of three ODEs
+class myRHS : public Dune::Fem::SpaceOperatorInterface< myDest<systemSize> > {
 public:
   myRHS() {
   }
@@ -74,16 +94,39 @@ public:
     return space_;
   }
 
-  void operator()(const DestinationType& x,
-                  DestinationType& y) const {
-    y[0]=2.0*t_;
-    //y[0]=3.0*t_*t_;
-    //y[0]=x[0];
+  void operator()(const DestinationType& X,
+                  DestinationType& Y) const
+  {
+    const double* x = X.leakPointer();
+    double* y = Y.leakPointer();
+    this->operator()( x, y );
+  }
+
+  void operator()(const double* x,
+                  double* y) const
+  {
+    y[0] = 2.0*t_;
+    y[1] = 3.0*t_*t_;
+    y[2] = x[2];
+    y[3] = 5.0 * x[ 3 ] - 3.0;
+    y[4] = std::cos( x[4] );
+  }
+
+  static DestinationType exact (const double t) {
+    DestinationType exact;
+    exact[ 0 ] = t * t;
+    exact[ 1 ] = t * t * t;
+    exact[ 2 ] = 0 ;
+    exact[ 3 ] = -3.0/5.0 * std::exp( 5.0 * t ) + 3.0/5.0;
+    exact[ 4 ] = std::sin( t );
+    return exact;
   }
 
   void setTime(const double time) {
     t_=time;
   }
+
+  double timeStepEstimate() const { return 0.01; }
 
 private:
   SpaceType space_;
@@ -91,33 +134,29 @@ private:
 };
 
 
-int main( int argc, char ** argv )
+template <class OdeSolverType>
+void solve(const bool verbose)
 {
-  Dune::Fem::MPIManager::initialize( argc, argv );
+  typedef myRHS SpaceOperatorType;
+  typedef SpaceOperatorType::DestinationType DestinationType;
+  SpaceOperatorType spaceOperator;
 
   // problem data
-  const double initialData = 1.0;
   const double startTime = 0.0;
   const double endTime = 2.0;
 
   // options
-  const double stepSize = 0.01;
+  const double stepSize = spaceOperator.timeStepEstimate();
   const double cfl = 1.;
-  const int order = 2;
-
-  // types
-  typedef myRHS SpaceOperatorType;
-  typedef SpaceOperatorType::DestinationType DestinationType;
-  typedef DuneODE::ExplicitRungeKuttaSolver<DestinationType> OdeSolverType;
+  const int order = Dune::Fem::Parameter::getValue("fem.ode.order", int(2));
 
   // create solver
   Dune::Fem::DefaultTimeProvider tp( startTime, cfl );
-  SpaceOperatorType spaceOperator;
   OdeSolverType odeSolver( spaceOperator, tp, order );
 
-  // initialize solution vector
+  // initialize solution vector, same initial data for all components
   DestinationType U;
-  U[0] = initialData;
+  U.clear(); // set initial data U(0) = 0
 
   // initialize odesolver
   odeSolver.initialize( U );
@@ -127,10 +166,61 @@ int main( int argc, char ** argv )
     // do calculation
     odeSolver.solve(U);
 
-    // print out solution
-    std::cout << tp.time()
-              << " " << U[0]
-              << std::endl;
+    if( verbose )
+    {
+      // print out solution
+      std::cout << "time = " << tp.time() << ", U = "
+                << U
+                << std::endl;
+    }
+  }
+
+  // print out solution
+  std::cout << "Result (t = " << tp.time() << ") U = " << U << std::endl;
+  DestinationType exact = spaceOperator.exact( tp.time() );
+  std::cout << "         exact U = " << exact << std::endl;
+  exact -= U;
+  exact /= U.two_norm();
+
+  std::cout << "Zwo norm: " << exact.two_norm() << std::endl;
+  if( exact.two_norm() > 1e-2 )
+  {
+    std::cerr << "ERROR: ode solver did not converge!" << std::endl;
+    assert( false );
+    std::abort();
+  }
+}
+
+
+int main( int argc, char **argv )
+{
+  Dune::Fem::MPIManager::initialize( argc, argv );
+  Dune::Fem::Parameter::append( argc, argv );
+  if( argc == 1 )
+    Dune::Fem::Parameter::append("parameter");
+
+  const bool verbose = Dune::Fem::Parameter::verbose();
+
+  // types
+  typedef myRHS SpaceOperatorType;
+  typedef SpaceOperatorType::DestinationType DestinationType;
+
+  // explicit RungeKutta (dune impl)
+  {
+    typedef DuneODE::ExplicitRungeKuttaSolver<DestinationType> OdeSolverType;
+    solve< OdeSolverType > ( verbose );
+  }
+
+  // explicit RungeKutta (pardg impl)
+  {
+    typedef DuneODE::ExplicitOdeSolver<DestinationType> OdeSolverType;
+    solve< OdeSolverType > ( verbose );
+  }
+
+  // implicit RungeKutta (pardg impl)
+  {
+    typedef DuneODE::ImplicitOdeSolver<DestinationType> OdeSolverType;
+    solve< OdeSolverType > ( verbose );
   }
 
   return 0;
