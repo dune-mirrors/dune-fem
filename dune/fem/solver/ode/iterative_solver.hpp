e9e80ba539fb25eb1a986ee2ac3d715fe85c6f84
Marco Agnese <m.agnese13@imperial.ac.uk>
2016-01-22 17:37:32 +0000

    [cleanup] remove deprecated method

--- dune/fem/solver/ode/iterative_solver.hpp
+++ dune/fem/solver/ode/iterative_solver.hpp
@@ -2,22 +2,12 @@
 #define ITERATIVE_SOLVER_HPP
 
 #include <iostream>
-#include <dune/common/deprecated.hh>
-
-
 
 class IterativeSolver
 {
 public:
   enum class ToleranceCriteria {absolute,relative,residualReduction};
   void set_tolerance(double tol, ToleranceCriteria crtieria = ToleranceCriteria::relative);
-  void set_tolerance(double tol, bool relative)
-      DUNE_DEPRECATED_MSG("IterativeSolver::set_tolerance is deprecated, see fem/solver/ode/iterative_solver.hpp for newer versions")
-  {
-    set_tolerance(tol,
-      (relative)?ToleranceCriteria::relative:ToleranceCriteria::absolute);
-  }
-
   void set_max_number_of_iterations(int iter);
   void set_output(std::ostream &os);
   int number_of_iterations() const;
@@ -31,7 +21,5 @@ protected:
   int max_num_of_iterations, num_of_iterations;
   std::ostream *os;
 };
-
-
 
 #endif
