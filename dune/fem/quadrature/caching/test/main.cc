d504514403e1608c1c30c9c7c2f94de96c34f5b1
Marco Agnese <m.agnese13@imperial.ac.uk>
2016-03-14 12:52:33 +0000

    [cleanup] remove empty test (issue 8)

--- dune/fem/quadrature/caching/test/main.cc
+++ dune/fem/quadrature/caching/test/main.cc
@@ -1,6 +1,5 @@
 #include <config.h>
 
-#include "refelem_test.hh"
 #include "cache_test.hh"
 #include "twist_test.hh"
 #include "point_test.hh"
@@ -8,10 +7,9 @@
 using namespace Dune;
 using namespace Fem;
 
-int main() {
+int main()
+{
 
-  //Suite suite("Tests for caching");
-  Fem::ReferenceElement_Test().run();
   Fem::TwistProvider_Test().run();
   Fem::PointProvider_Test().run();
   Fem::CacheProvider_Test().run();
