501697d52797d1d5b854f60372fa880bc15fb770
Marco Agnese <m.agnese13@imperial.ac.uk>
2016-05-05 17:48:40 +0100

    [tupleforeach] replace implementation of for_each
    The previous version of for_each( tuple ) was not order preserving.
    This new implementation is now order aware and allows iteration over const tuple (needed for direct solvers)

--- dune/fem/common/tupleforeach.hh
+++ dune/fem/common/tupleforeach.hh
@@ -2,27 +2,61 @@
 #define DUNE_FEM_COMMON_TUPLEFOREACH_HH
 
 #include <tuple>
-#include <type_traits>
-#include <utility>
 
 namespace Dune
 {
-
   namespace Fem
   {
 
-    template< class Tuple, class Functor, std::size_t ... I >
-    void for_each ( Tuple &t, Functor functor, std::index_sequence< I ... > )
+    template< typename Functor, std::size_t R >
+    struct ForEachImpl
     {
-      std::ignore = std::make_tuple(
-        ( functor( std::get< I >( t ), std::integral_constant< std::size_t, I >() ), I )
-        ... );
+      template< typename ... T >
+      ForEachImpl( std::tuple< T ... >& t, Functor&& functor)
+      {
+        constexpr std::size_t I = sizeof ... ( T ) - R - 1;
+        functor( std::get< I >( t ), I );
+        ForEachImpl< Functor, R-1 >( t, std::move( functor ) );
+      }
+
+      template< typename ... T >
+      ForEachImpl( const std::tuple< T ... >& t, Functor&& functor)
+      {
+        constexpr std::size_t I = sizeof ... ( T ) - R - 1;
+        functor( std::get< I >( t ), I );
+        ForEachImpl< Functor, R-1 >( t, std::move( functor ) );
+      }
+
+    };
+
+    template< typename Functor >
+    struct ForEachImpl< Functor, 0 >
+    {
+      template< typename ... T >
+      ForEachImpl( std::tuple< T ... >& t, Functor&& functor )
+      {
+        constexpr std::size_t I = sizeof ... ( T ) - 1;
+        functor( std::get< I > ( t ), I );
+      }
+
+      template< typename ... T >
+      ForEachImpl( const std::tuple< T ... >& t, Functor&& functor )
+      {
+        constexpr std::size_t I = sizeof ... ( T ) - 1;
+        functor( std::get< I > ( t ), I );
+      }
+    };
+
+    template< typename Functor, typename ... T >
+    void for_each( std::tuple< T ... >& t, Functor functor )
+    {
+      ForEachImpl< Functor, sizeof ... ( T ) - 1 >( t, std::move( functor ) );
     }
 
-    template< class Functor, class ... T >
-    void for_each ( std::tuple< T ... > &t, Functor functor )
+    template< typename Functor, typename ... T >
+    void for_each( const std::tuple< T ... >& t, Functor functor )
     {
-      for_each( t, std::move( functor ), std::index_sequence_for< T ... >() );
+      ForEachImpl< Functor, sizeof ... ( T ) - 1 >( t, std::move( functor ) );
     }
 
   }
