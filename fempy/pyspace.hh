2c8e7a52ea0755cd7e7e08032c646db7be82c285
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2016-05-10 17:15:33 +0200

    add space database

---
+++ fempy/pyspace.hh
@@ -0,0 +1,32 @@
+#ifndef DUNE_FEMPY_PYSPACE_HH
+#define DUNE_FEMPY_PYSPACE_HH
+
+#include <dune/fempy/grid.hh>
+#include <dune/fempy/pybind11/pybind11.h>
+
+namespace Dune
+{
+
+  namespace FemPy
+  {
+
+    // registerSpace
+    // -------------
+
+    template< class Space >
+    void registerSpace ( pybind11::module module )
+    {
+      typedef LeafGrid< typename Space::GridPartType > Grid;
+
+      pybind11::class_< Space > cls( module, "Space" );
+
+      cls.def( "__init__", [] ( Space &instance, const Grid &grid ) {
+          new( &instance ) Space( *grid.gridPart() );
+        }, pybind11::keep_alive< 1, 2 >() );
+    }
+
+  } // namespace FemPy
+
+} // namespace Dune
+
+#endif // #ifndef DUNE_FEMPY_PYSPACE_HH
