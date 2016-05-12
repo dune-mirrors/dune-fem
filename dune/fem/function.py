c962512fbc438cfaacdd65e049286d371321274f
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2016-05-12 14:45:16 +0200

    regain support for math expressions

---
+++ dune/fem/function.py
@@ -0,0 +1,11 @@
+import math
+
+class MathExpression(object):
+    def __init__(self, expr):
+        self.expr = expr
+    def __call__(self,x):
+        dimD = len(x)
+        x0 = x[0]
+        x1 = 0 if dimD < 2 else x[1]
+        x2 = 0 if dimD < 3 else x[2]
+        return [eval(e) for e in self.expr]
