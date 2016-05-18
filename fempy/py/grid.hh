961b1308be9d6918962a93bdde35f39076fcf7cd
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2016-05-18 12:54:15 +0200

    migrate geometry registration into separate file

--- fempy/py/grid.hh
+++ fempy/py/grid.hh
@@ -12,6 +12,7 @@
 #include <dune/fempy/function/simplegridfunction.hh>
 #include <dune/fempy/function/virtualizedgridfunction.hh>
 #include <dune/fempy/grid.hh>
+#include <dune/fempy/py/grid/geometry.hh>
 #include <dune/fempy/py/gridfunction.hh>
 #include <dune/fempy/py/vtk.hh>
 #include <dune/fempy/pybind11/functional.h>
@@ -23,76 +24,6 @@ namespace Dune
 
   namespace FemPy
   {
-
-    // PyCornerRange
-    // -------------
-
-    template< class Geometry >
-    struct PyCorners
-    {
-      PyCorners ( const Geometry &geometry, pybind11::object ref )
-        : geometry_( geometry ), ref_( ref )
-      {}
-
-      const Geometry &geometry () { return geometry_; }
-
-    private:
-      const Geometry &geometry_;
-      pybind11::object ref_;
-    };
-
-
-    // PyCornerIterator
-    // ----------------
-
-    template< class Geometry >
-    struct PyCornerIterator
-    {
-      PyCornerIterator ( const PyCorners< Geometry > corners ) : corners_( corners ) {}
-
-      typename Geometry::GlobalCoordinate next ()
-      {
-        if( index_ == corners_.geometry().corners() )
-          throw pybind11::stop_iteration();
-        return corners_.geometry().corner( index_++ );
-      }
-
-    private:
-      PyCorners< Geometry > corners_;
-      int index_ = 0;
-    };
-
-
-
-    // registerGridGeometry
-    // --------------------
-
-    template< class Geometry >
-    void registerGridGeometry ( pybind11::module module )
-    {
-      static const std::string clsName = "Geometry" + std::to_string( Geometry::mydimension );
-      pybind11::class_< Geometry > cls( module, clsName.c_str() );
-
-      pybind11::class_< PyCornerIterator< Geometry > > itCls( cls, "CornerIterator" );
-      itCls.def( "__iter__", [] ( PyCornerIterator< Geometry > &it ) -> PyCornerIterator< Geometry > & { return it; } );
-      itCls.def( "__next__", &PyCornerIterator< Geometry >::next );
-
-      pybind11::class_< PyCorners< Geometry > > cCls( cls, "Corners" );
-      cCls.def( "__iter__", [] ( const PyCorners< Geometry > &c ) { return PyCornerIterator< Geometry >( c ); } );
-
-      cls.def_property_readonly( "corners", [] ( pybind11::object geo ) {
-          return PyCorners< Geometry >( geo.cast< const Geometry & >(), geo );
-        } );
-
-      cls.def_property_readonly( "center", &Geometry::center );
-      cls.def_property_readonly( "volume", &Geometry::volume );
-
-      cls.def_property_readonly( "affine", &Geometry::affine );
-
-      cls.def( "position", &Geometry::global );
-      cls.def( "integrationElement", &Geometry::integrationElement );
-    }
-
 
     template< class Entity >
     void registerGridEntity ( pybind11::module module )
