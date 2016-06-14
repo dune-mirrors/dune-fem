2f1c2635f683aa9b6c8e1366382232c1970855e0
lloyd.connellan <l.connellan@warwick.ac.uk>
2016-06-14 15:53:36 +0100

    preliminary changes for schemes

---
+++ fempy/py/scheme.hh
@@ -0,0 +1,37 @@
+#ifndef DUNE_FEMPY_PY_SCHEME_HH
+#define DUNE_FEMPY_PY_SCHEME_HH
+
+#include <dune/fempy/function/virtualizedgridfunction.hh>
+#include <dune/fempy/py/common/numpyvector.hh>
+#include <dune/fempy/py/function/discrete.hh>
+#include <dune/fempy/pybind11/pybind11.h>
+
+namespace Dune
+{
+
+  namespace FemPy
+  {
+    // registerScheme
+    // -------------
+
+    template< class Scheme >
+    void registerScheme ( pybind11::module module )
+    {
+        typedef typename Scheme::GridPartType GridPart;
+        typedef typename Scheme::ModelType ModelType;
+        typedef typename Scheme::DiscreteFunctionType DiscreteFunction;
+
+        >> Export discrete function <<
+
+        pybind11::class_< Scheme > cls( module, "Scheme" );
+
+        cls.def( "__init__", [] ( Scheme &instance, GridPartType &gridPart, const ModelType& model, const std::string &prefix ) {
+          new( &instance ) Scheme( gridPart, model, prefix );
+        }, pybind11::keep_alive< 1, 3 >() );
+        cls.def("solve",  &Scheme::solve);
+        cls.def("solution", &Scheme::solution);
+    }
+  }
+}
+
+#endif
