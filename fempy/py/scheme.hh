f2f163609acfdb080761db9607c7527f50fed153
Lloyd <l.connellan@warwick.ac.uk>
2016-06-19 12:47:08 +0100

    setvelocity and solve methods working now

--- fempy/py/scheme.hh
+++ fempy/py/scheme.hh
@@ -30,9 +30,9 @@ namespace Dune
 
         cls.def( "__init__", [] ( Scheme &instance, GridPart &gridPart, const ModelType& model, const std::string &prefix ) {
           new( &instance ) Scheme( gridPart, model, prefix );
-        }, pybind11::keep_alive< 1, 3 >(), pybind11::keep_alive<1,2>() );
-        cls.def("solve",  &Scheme::solve);
-        cls.def("error",[] (Scheme &scheme)
+        }, pybind11::keep_alive< 1, 3 >(), pybind11::keep_alive< 1, 2 >() );
+        cls.def("solve", [] (Scheme &scheme) { return scheme.solve(true); });
+        cls.def("error", [] (Scheme &scheme)
         {
           const auto& gridExactSolution = scheme.exactSolution();
           Dune::Fem::L2Norm< GridPart > norm( scheme.solution().space().gridPart() );
