904b2f7a1d53b7e4f30c6b60e7b030600c0fdafa
dedner <a.s.dedner@warwick.ac.uk>
2016-06-28 17:20:19 +0100

    minor change

--- modules/DuneFempyMacros.cmake
+++ modules/DuneFempyMacros.cmake
@@ -13,3 +13,14 @@ function(add_python_target file)
   )
   add_custom_target(${file}.pyc ALL DEPENDS ${OUT})
 endfunction()
+
+function(add_gmshgeo_target file)
+  set(OUT ${CMAKE_CURRENT_BINARY_DIR}/${file}.msh)
+  set(IN ${CMAKE_CURRENT_SOURCE_DIR}/${file}.geo)
+  add_custom_command(OUTPUT ${OUT}
+    DEPENDS ${IN}
+    COMMAND gmsh -2 -o ${OUT} ${IN}
+    VERBATIM
+  )
+  add_custom_target(${file}.msh ALL DEPENDS ${OUT})
+endfunction()
