a23f0c314effdd7b6e87f517953432d00d98b032
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2016-07-02 22:34:30 +0200

    use pair of (dimGrid, dimWorld) instead of extra argument dimWorld in dune.ufl.Space

--- dune/ufl.py
+++ dune/ufl.py
@@ -2,10 +2,14 @@ from __future__ import absolute_import
 import ufl
 
 class Space(ufl.VectorElement):
-    def __init__(self, dimDomain, dimRange, dimWorld=None):
-        if not dimWorld:
-            dimWorld = dimDomain
-        dimWorld = int(dimWorld)
+    def __init__(self, dimDomain, dimRange):
+        if isinstance(dimDomain, tuple):
+            if len(dimDomain) != 2:
+                raise Exception('dimDomain tuple must contain exactly two elements.')
+            dimWorld = int(dimDomain[1])
+            dimDomain = dimDomain[0]
+        else:
+            dimWorld = int(dimDomain)
         if dimDomain == 1:
             cell = ufl.Cell("interval", dimWorld)
         elif dimDomain == 2:
