7373424f455c62c892123dc63eff42a2513f3fff
dedner <a.s.dedner@warwick.ac.uk>
2016-07-26 12:41:49 +0100

    export the 'force' class variable from the generator This allows for easy recompilation of shared libraries, e.g., add   create.schemeGenerator.force = True to rebuild all scheme libraries

--- dune/fem/create.py
+++ dune/fem/create.py
@@ -2,3 +2,8 @@ from .gridpart         import create as gridpart
 from .space            import create as space
 from .discretefunction import create as discretefunction
 from .scheme           import create as scheme
+
+from .gridpart         import myGenerator as gridpartGenerator
+from .space            import myGenerator as spaceGenerator
+from .discretefunction import myGenerator as discretefunctionGenerator
+from .scheme           import myGenerator as schemeGenerator
