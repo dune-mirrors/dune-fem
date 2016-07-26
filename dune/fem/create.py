49e96de14dfdc9e2f4ab4f73ab1658978e37c1c0
dedner <a.s.dedner@warwick.ac.uk>
2016-07-26 14:02:32 +0100

    add a create.rebuild method to rebuild all shared libraries

--- dune/fem/create.py
+++ dune/fem/create.py
@@ -1,9 +1,15 @@
+from .grid             import leafGrid as leafGrid
 from .gridpart         import create as gridpart
 from .space            import create as space
 from .discretefunction import create as discretefunction
 from .scheme           import create as scheme
 
+from .grid             import myGenerator as gridGenerator
 from .gridpart         import myGenerator as gridpartGenerator
 from .space            import myGenerator as spaceGenerator
 from .discretefunction import myGenerator as discretefunctionGenerator
 from .scheme           import myGenerator as schemeGenerator
+
+def rebuild():
+    gridGenerator.force=gridpartGenerator.force=spaceGenerator.force=\
+            discretefunctionGenerator.force=schemeGenerator.force = True
