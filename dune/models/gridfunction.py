1b71269f9ae25cde8d2281712d14b5e33809a5f0
lloyd.connellan <l.connellan@warwick.ac.uk>
2016-08-20 11:56:50 +0100

    add gridfunction class with evaluate method (interpolate still needs work)

--- dune/models/gridfunction.py
+++ dune/models/gridfunction.py
@@ -8,28 +8,39 @@ import types
 from dune import comm
 from dune.source import SourceWriter
 
-def importFunction(grid, code):
-    compilePath = os.path.join(os.path.dirname(__file__), "../generated")
+def gridFunction(grid, code):
+    compilePath = os.path.join(os.path.dirname(__file__), '../generated')
 
     signature = 'tmp'
 
+    dimRange = grid.dimensionworld
     if not isinstance(grid, types.ModuleType):
         grid = grid._module
     name = 'gridfunction_' + signature + '_' + grid._typeHash
     cname = 'GridFunction_' + signature + '_' + grid._typeHash
 
     if comm.rank == 0:
-        if not os.path.isfile(os.path.join(compilePath, name + ".so")):
+        if not os.path.isfile(os.path.join(compilePath, name + '.so')):
             writer = SourceWriter(compilePath + '/gridfunction.hh')
             writer.emit(grid._includes)
             writer.emit('')
             writer.emit('#include <dune/corepy/pybind11/pybind11.h>')
             writer.emit('#include <dune/corepy/pybind11/extensions.h>')
             writer.emit('')
+            writer.emit('#include <dune/fem/space/common/functionspace.hh>')
+            writer.emit('#include <dune/fem/space/common/interpolate.hh>')
+            writer.emit('')
 
             writer.typedef(grid._typeName, 'GridPart')
-            writer.typedef("typename GridPart::template Codim< 0 >::EntityType", "EntityType")
-           # writer.typedef('Dune::FemPy::VirtualizedGridFunction< GridPart, Dune::FieldVector< double, ' + str(coefficient['dimRange']) + ' > >, GridFunction')
+            writer.emit('static const int dimRange = ' + str(dimRange) + ';')
+            writer.emit('static const int dimDomain = GridPart::dimensionworld;')
+            writer.typedef('typename Dune::Fem::FunctionSpace< double, double, dimDomain, dimRange >', 'FunctionSpaceType')
+            writer.typedef('typename GridPart::template Codim< 0 >::EntityType', 'EntityType')
+            writer.typedef('typename FunctionSpaceType::DomainType', 'DomainType')
+            writer.typedef('typename EntityType::Geometry::LocalCoordinate', 'LocalCoordinateType')
+            writer.typedef('typename FunctionSpaceType::RangeType', 'RangeType')
+            writer.typedef('typename FunctionSpaceType::JacobianRangeType', 'JacobianRangeType')
+            writer.typedef('typename FunctionSpaceType::HessianRangeType', 'HessianRangeType')
             writer.openStruct(cname, targs=(['class Grid'] + ['class... Coefficients']))
 
             writer.openConstMethod('bool init', args=['const EntityType &entity'])
@@ -41,9 +52,9 @@ def importFunction(grid, code):
             writer.emit('return *entity_;')
             writer.closeConstMethod()
 
-            writer.openMethod('void evaluate')
+            writer.openConstMethod('void evaluate', args=['const DomainType &x', 'RangeType &value'])
             writer.emit(code)
-            writer.closeMethod()
+            writer.closeConstMethod()
 
             writer.section('private')
             writer.openConstMethod('void initCoefficients', targs=['std::size_t... i'], args=['std::index_sequence< i... >'])
@@ -60,17 +71,24 @@ def importFunction(grid, code):
             writer.openPythonModule(name)
             writer.emit('')
             writer.emit('// export function class')
-            writer.emit('auto cls = pybind11::class_< GridFunction >( module, "GridFunction" );')
+            writer.emit('auto cls = pybind11::class_< GridFunction >( module, "GridFunction");')
             writer.emit('')
-            writer.emit('cls.def( "evaluate", [] ( GridFunction &gf ) { return 1; } );')
+            writer.emit('cls.def_property_readonly( "dimRange", [] ( GridFunction & ) -> int { return RangeType::dimension; } );')
+            writer.emit('cls.def( "evaluate", [] ( const GridFunction &gf, const LocalCoordinateType &x ) {')
+            writer.emit('RangeType value;')
+            writer.emit('gf.evaluate( x, value );')
+            writer.emit('return value; } );')
+            #writer.emit('cls.def( "interpolate", [] ( DF &df, const GridFunction &gf ) {')
+            #writer.emit('Dune::Fem::interpolate( gf, df );')
+            #writer.emit('} );')
             writer.emit('module.def( "get", [] () { return new GridFunction(); } );')
             writer.closePythonModule(name)
 
             writer.close()
 
-            cmake = subprocess.Popen(["cmake", "--build", "../../..", "--target", "gridfunction"], cwd=compilePath)
+            cmake = subprocess.Popen(['cmake', '--build', '../../..', '--target', 'gridfunction'], cwd=compilePath)
             cmake.wait()
-            os.rename(os.path.join(compilePath, "gridfunction.so"), os.path.join(compilePath, name + ".so"))
+            os.rename(os.path.join(compilePath, 'gridfunction.so'), os.path.join(compilePath, name + '.so'))
 
         comm.barrier()
-        return importlib.import_module("dune.generated." + name)
+        return importlib.import_module('dune.generated.' + name)
