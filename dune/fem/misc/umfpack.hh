95ca20a67af95fb748d0c5685ffd823bcea70d7b
Tobias Malkmus <tomalk@mathematik.uni-freiburg.de>
2016-08-30 10:39:42 +0200

    [umfpack] add specialization for Fem::Double

--- dune/fem/misc/umfpack.hh
+++ dune/fem/misc/umfpack.hh
@@ -4,11 +4,13 @@
 #include <complex>
 
 #if HAVE_DUNE_ISTL
-#include <dune/ist/umfpack.hh>
-#else
+#include <dune/istl/umfpack.hh>
+#endif
 
 #if HAVE_SUITESPARSE_UMFPACK
 #include <umfpack.h>
+
+#include <dune/fem/misc/double.hh>
 
 namespace Dune
 {
@@ -16,6 +18,7 @@ namespace Dune
   // UMFPackMethodChooser
   // --------------------
 
+#if not HAVE_DUNE_ISTL
   template< class T >
   struct UMFPackMethodChooser {};
 
@@ -130,11 +133,67 @@ namespace Dune
       umfpack_zi_symbolic( m, n, cs, ri, val, nullptr, args ... );
     }
   };
+#endif // #if not HAVE_DUNE_ISTL
+
+  template< >
+  struct UMFPackMethodChooser< Fem::Double >
+  {
+    template< class... A >
+    static void defaults ( A ... args )
+    {
+      umfpack_di_defaults( args ... );
+    }
+    template< class ... A >
+    static void free_numeric ( A ... args )
+    {
+      umfpack_di_free_numeric( args ... );
+    }
+    template< class ... A >
+    static void free_symbolic ( A ... args )
+    {
+      umfpack_di_free_symbolic( args ... );
+    }
+    template< class ... A >
+    static int load_numeric ( A ... args )
+    {
+      return umfpack_di_load_numeric( args ... );
+    }
+    template< class ... A >
+    static void numeric ( A ... args )
+    {
+      umfpack_di_numeric( args ... );
+    }
+    template< class ... A >
+    static void report_info ( A ... args )
+    {
+      umfpack_di_report_info( args ... );
+    }
+    template< class ... A >
+    static void report_status ( A ... args )
+    {
+      umfpack_di_report_status( args ... );
+    }
+    template< class ... A >
+    static int save_numeric ( A ... args )
+    {
+      return umfpack_di_save_numeric( args ... );
+    }
+    template< class ... A >
+    static void solve ( int m, const int *cs, const int *ri, const Fem::Double *val, Fem::Double *x, const Fem::Double *b, A ... args )
+    {
+      umfpack_di_solve( m, cs, ri, reinterpret_cast< const double * >(val),
+          reinterpret_cast< double * >( x ),
+          reinterpret_cast< const double * >( b ), args ... );
+    }
+    template< class ... A >
+    static void symbolic ( A ... args )
+    {
+      umfpack_di_symbolic( args ... );
+    }
+  };
 
 } // namespace Dune
 
 #endif // #if HAVE_SUITESPARSE_UMFPACK
-
-#endif // #if HAVE_DUNE_ISTL
 
 #endif // #ifndef DUNE_FEM_MISC_UMFPACK_HH
