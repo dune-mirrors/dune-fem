3cf8483ef9f5cf6f71f2bc4f96eaad4f35c01a6b
lloyd.connellan <l.connellan@warwick.ac.uk>
2016-09-01 14:10:57 +0100

    implement correct registerGridFunction

--- dune/models/gridfunction.py
+++ dune/models/gridfunction.py
@@ -11,13 +11,20 @@ from dune.source import SourceWriter
 def gridFunction(grid, code):
     compilePath = os.path.join(os.path.dirname(__file__), '../generated')
 
-    signature = 'tmp'
-
     dimRange = grid.dimensionworld
     if not isinstance(grid, types.ModuleType):
         grid = grid._module
-    name = 'gridfunction_' + signature + '_' + grid._typeHash
-    cname = 'GridFunction_' + signature + '_' + grid._typeHash
+
+    gfnumber = 0
+    check = 0
+    while check == 0:
+        name = 'gridfunction_' + str(gfnumber) + '_' + grid._typeHash
+        if os.path.isfile(os.path.join(compilePath, name + '.so')):
+            gfnumber += 1
+        else:
+            check = 1
+    gfname = 'GridFunction_' + str(gfnumber) + '_' + grid._typeHash
+    locname = 'LocalFunction_' + str(gfnumber) + '_' + grid._typeHash
 
     if comm.rank == 0:
         if not os.path.isfile(os.path.join(compilePath, name + '.so')):
@@ -32,20 +39,46 @@ def gridFunction(grid, code):
             writer.emit('#include <dune/fempy/py/grid/function.hh>')
             writer.emit('')
 
-            writer.typedef(grid._typeName, 'GridPart')
+            writer.openStruct(locname, targs=(['class GridPart'] + ['class Range']))
+            writer.typedef('GridPart', 'GridPartType')
+            writer.typedef('Range', 'RangeType')
+            writer.typedef('typename GridPart::template Codim< 0 >::EntityType', 'EntityType')
+            writer.typedef('typename EntityType::Geometry::LocalCoordinate', 'LocalCoordinateType')
+
+            writer.openConstMethod('const EntityType &entity')
+            writer.emit('return *entity_;')
+            writer.closeConstMethod()
+
+            writer.openConstMethod('void evaluate', args=['const PointType &x', 'RangeType &value'], targs=['class PointType'])
+            writer.emit(code)
+            writer.closeConstMethod()
+
+            writer.openConstMethod('void jacobian' args=['const PointType &x', 'RangeType &value'], targs=['class PointType'])
+            writer.emit('// not implemented')
+            writer.closeConstMethod
+
+            writer.emit('mutable const EntityType *entity_ = nullptr;')
+            writer.closeStruct()
+
+
+            writer.openStruct(gfname, targs=(['class GridPart'] + ['class Range'] + ['class... Coefficients']))
+
+            writer.typedef('GridPart', 'GridPartType')
+            writer.typedef('Range', 'RangeType')
+            writer.typedef(locname + '< GridPart, Range >', 'LocalFunctionType')
             writer.emit('static const int dimRange = ' + str(dimRange) + ';')
             writer.emit('static const int dimDomain = GridPart::dimensionworld;')
             writer.typedef('typename Dune::Fem::FunctionSpace< double, double, dimDomain, dimRange >', 'FunctionSpaceType')
             writer.typedef('typename GridPart::template Codim< 0 >::EntityType', 'EntityType')
             writer.typedef('typename FunctionSpaceType::DomainType', 'DomainType')
             writer.typedef('typename EntityType::Geometry::LocalCoordinate', 'LocalCoordinateType')
-            writer.typedef('typename FunctionSpaceType::RangeType', 'RangeType')
             writer.typedef('typename FunctionSpaceType::JacobianRangeType', 'JacobianRangeType')
             writer.typedef('typename FunctionSpaceType::HessianRangeType', 'HessianRangeType')
-            writer.openStruct(cname, targs=(['class GridPart'] + ['class RangeType'] + ['class... Coefficients']))
 
-            writer.openConstMethod('bool init', args=['const EntityType &entity'])
+            writer.openConstMethod('bool init', args=['const EntityType &entity'] + ['const GridPartType &gridPart'] + ['const LocalFunctionType &localFunction'])
             writer.emit('entity_ = &entity;')
+            writer.emit('gridPart_ = &gridPart;')
+            writer.emit('localFunction_ = &localFunction;')
             writer.emit('return true;')
             writer.closeConstMethod()
 
@@ -53,8 +86,16 @@ def gridFunction(grid, code):
             writer.emit('return *entity_;')
             writer.closeConstMethod()
 
-            writer.openConstMethod('void evaluate', args=['const DomainType &x', 'RangeType &value'])
-            writer.emit(code)
+            writer.openConstMethod('GridPartType &gridPart')
+            writer.emit('return *gridPart_;')
+            writer.closeConstMethod()
+
+            writer.openConstMethod('LocalFunctionType &localFunction', args=['const EntityType &entity'])
+            writer.emit('return *localFunction_;')
+            writer.closeConstMethod()
+
+            writer.openConstMethod('std::string name')
+            writer.emit('return "' + name + '";')
             writer.closeConstMethod()
 
             writer.section('private')
@@ -63,11 +104,18 @@ def gridFunction(grid, code):
             writer.closeConstMethod()
             writer.emit('')
             writer.emit('mutable const EntityType *entity_ = nullptr;')
+            writer.emit('mutable const GridPartType *gridPart_ = nullptr;')
+            writer.emit('mutable const LocalFunctionType *localFunction_ = nullptr;')
             writer.emit('mutable std::tuple< Coefficients... > coefficients_;')
             writer.closeStruct()
 
             writer.emit('')
-            writer.typedef(cname + '< GridPart, RangeType >', 'GridFunction')
+            writer.typedef(grid._typeName, 'GridPart')
+            writer.emit('static const int dimRange = ' + str(dimRange) + ';')
+            writer.emit('static const int dimDomain = GridPart::dimensionworld;')
+            writer.typedef('typename Dune::Fem::FunctionSpace< double, double, dimDomain, dimRange >', 'FunctionSpaceType')
+            writer.typedef('typename FunctionSpaceType::RangeType', 'RangeType')
+            writer.typedef(gfname + '< GridPart, RangeType >', 'GridFunction')
 
             writer.openNameSpace('Dune')
             writer.openNameSpace('FemPy')
@@ -75,17 +123,11 @@ def gridFunction(grid, code):
             writer.emit('')
             writer.emit('// export function class')
             writer.emit('')
-            writer.emit('pybind11::class_< GridFunction > registerGridFunction( module, "GridFunction" );')
+            writer.emit('pybind11::class_< GridFunction > cls = registerGridFunction< GridFunction >( module, "GridFunction" );')
             writer.emit('module.def( "get", [] () { return new GridFunction; } );')
             writer.closePythonModule(name)
             writer.closeNameSpace('FemPy')
             writer.closeNameSpace('Dune')
-
-            #writer.emit('cls.def_property_readonly( "dimRange", [] ( GridFunction & ) -> int { return RangeType::dimension; } );')
-            #writer.emit('cls.def( "evaluate", [] ( const GridFunction &gf, const LocalCoordinateType &x ) {')
-            #writer.emit('RangeType value;')
-            #writer.emit('gf.evaluate( x, value );')
-            #writer.emit('return value; } );')
 
             writer.close()
 
