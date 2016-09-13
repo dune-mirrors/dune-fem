e4431cbaa2a929c9ced1b06b657a06b04d98c5dc
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2016-09-13 16:06:54 +0200

    remove old construction method for geometry grid part

--- fempy/py/gridpart.hh
+++ fempy/py/gridpart.hh
@@ -1,12 +1,7 @@
 #ifndef DUNE_FEMPY_PY_GRIDPART_HH
 #define DUNE_FEMPY_PY_GRIDPART_HH
 
-#include <string>
-#include <utility>
-
 #include <dune/corepy/grid/gridview.hh>
-
-#include <dune/fem/gridpart/geometrygridpart.hh>
 
 #include <dune/fempy/py/grid/gridpart.hh>
 
@@ -26,22 +21,6 @@ namespace Dune
     {
       detail::registerGridPart< GridPart >( module, cls );
 
-      auto clsGW = pybind11::class_< typename GridPart::GridViewType >( module, "GridView" );
-      clsGW.def( pybind11::init< GridPart & >() );
-      pybind11::implicitly_convertible< GridPart, typename GridPart::GridViewType >();
-      CorePy::registerGridView< typename GridPart::GridViewType >( module, cls );
-    }
-
-    template< class Holder, class AliasType, class ... Args >
-    void registerGridPart ( pybind11::handle module, pybind11::class_< Dune::Fem::GeometryGridPart< Args ... >, Holder, AliasType > &cls )
-    {
-      typedef Dune::Fem::GeometryGridPart< Args ... > GridPart;
-      typedef typename GridPart::GridFunctionType GridFunctionType;
-      cls.def( "__init__", [] ( GridPart &instance,  GridFunctionType &gf ) {
-          new (&instance)GridPart( gf );
-        }, pybind11::keep_alive< 1, 2 >() );
-
-      detail::registerGridPart< GridPart >( module, cls );
       auto clsGW = pybind11::class_< typename GridPart::GridViewType >( module, "GridView" );
       clsGW.def( pybind11::init< GridPart & >() );
       pybind11::implicitly_convertible< GridPart, typename GridPart::GridViewType >();
