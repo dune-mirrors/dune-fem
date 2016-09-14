5a39b4998ab604ac19cde663b31c91d761814679
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2016-09-14 14:42:59 +0200

    let CMake generate __init__.py for dune module

--- modules/DuneFempyMacros.cmake
+++ modules/DuneFempyMacros.cmake
@@ -1,19 +1,3 @@
-find_package(PythonInterp 2.7 REQUIRED)
-find_package(PythonLibs 2.7 REQUIRED)
-
-include_directories("${PYTHON_INCLUDE_DIRS}")
-
-function(add_python_target file)
-  set(OUT ${CMAKE_CURRENT_BINARY_DIR}/${file}.pyc)
-  set(IN ${CMAKE_CURRENT_SOURCE_DIR}/${file}.py)
-  add_custom_command(OUTPUT ${OUT}
-    DEPENDS ${IN}
-    COMMAND ${PYTHON_EXECUTABLE} -c "import py_compile; py_compile.compile('${IN}',cfile='${OUT}')"
-    VERBATIM
-  )
-  add_custom_target(${file}.pyc ALL DEPENDS ${OUT})
-endfunction()
-
 function(add_gmshgeo_target file)
   set(OUT ${CMAKE_CURRENT_BINARY_DIR}/${file}.msh)
   set(IN ${CMAKE_CURRENT_SOURCE_DIR}/${file}.geo)
@@ -24,3 +8,5 @@ function(add_gmshgeo_target file)
   )
   add_custom_target(${file}.msh ALL DEPENDS ${OUT})
 endfunction()
+
+declare_python_dune_module(MODULE dune-fempy INIT "import .femmpi")
