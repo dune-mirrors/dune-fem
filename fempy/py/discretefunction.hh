a50cec6d508862dd32b14d65cbe77a0d04620dd3
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2016-10-10 17:09:48 +0200

    add method "copy" to DiscreteFunction

--- fempy/py/discretefunction.hh
+++ fempy/py/discretefunction.hh
@@ -24,40 +24,40 @@ namespace Dune
   namespace FemPy
   {
 
-    // interpolant
-    // -----------
-    template< class DF, class GF, std::enable_if_t< std::is_base_of< Fem::HasLocalFunction, GF >::value, int > = 0 >
-    void interpolant ( DF &df, std::string name, const GF &gf )
-    {
-      Fem::interpolate( gf, df );
-    }
-
-    template< class DF >
-    void interpolant ( DF &df, typename DF::RangeType value )
-    {
-      return interpolant( df, simpleGridFunction( df.space().gridPart(), [ value ] ( typename DF::DomainType ) { return value; }, 0 ) );
-    }
-
-
-    // registerDiscreteFunction
-    // -------------
-
     namespace detail
     {
-      template< class DF, class Cls >
-      void registerDFConstructor( Cls &cls, std::false_type )
+
+      // registerDFConstructor
+      // ---------------------
+
+      template< class DF, class Holder, class Alias >
+      void registerDFConstructor ( pybind11::class_< DF, Holder, Alias > &cls, std::false_type )
       {}
-      template<class DF, class Cls >
-      void registerDFConstructor( Cls &cls, std::true_type )
+
+      template< class DF, class Holder, class Alias >
+      void registerDFConstructor ( pybind11::class_< DF, Holder, Alias > &cls, std::true_type )
       {
+        using pybind11::operator""_a;
+
         typedef typename DF::DiscreteFunctionSpaceType Space;
-        cls.def( "__init__", [] ( DF &instance, Space &space, const std::string &name ) {
-            new( &instance ) DF( std::move(name), space );
-          }, pybind11::keep_alive< 1, 2 >() );
+        cls.def( "__init__", [] ( DF &self, const Space &space, std::string name ) {
+            new (&self) DF( std::move( name ), space );
+          }, "space"_a, "name"_a, pybind11::keep_alive< 1, 2 >() );
       }
 
+      template< class DF, class Holder, class Alias >
+      void registerDFConstructor ( pybind11::class_< DF, Holder, Alias > &cls )
+      {
+        registerDFConstructor( cls, std::is_constructible< DF, const std::string &, const typename DF::DiscreteFunctionSpaceType & >() );
+      }
+
+
+
+      // registerDFBuffer
+      // ----------------
+
       template <class DF, class Cls>
-      auto registerDFBuffer(Cls &cls,int)
+      auto registerDFBuffer( Cls &cls, int )
       -> decltype(std::declval<DF>().dofVector().array().data(),void())
       {
         typedef typename DF::RangeType Value;
@@ -73,10 +73,15 @@ namespace Dune
             );
           }); // ????  pybind11::keep_alive<0,1>() );
       }
-      template <class DF,class Cls>
-      void registerDFBuffer(Cls &cls,long)
-      {
-      }
+
+      template< class DF, class Cls >
+      void registerDFBuffer ( Cls &cls, long )
+      {}
+
+
+
+      // registerDiscreteFunction
+      // ------------------------
 
       template< class DF, class Cls >
       void registerDiscreteFunction ( pybind11::module module, Cls &cls )
@@ -87,7 +92,7 @@ namespace Dune
         typedef typename GridPart::GridType Grid;
         typedef typename Value::field_type FieldType;
 
-        detail::registerGridFunction< DF >( module, cls);
+        detail::registerGridFunction< DF >( module, cls );
 
         detail::clsVirtualizedGridFunction< GridPart, Value >( module ).def( "__init__", [] ( VirtualizedGridFunction< GridPart, Value > &instance, DF &df ) {
             new (&instance) VirtualizedGridFunction< GridPart, Value >( pyGridFunction( df ) );
@@ -99,11 +104,18 @@ namespace Dune
           }, pybind11::keep_alive< 1, 2 >() );
         pybind11::implicitly_convertible< DF, VirtualizedRestrictProlong< Grid > >();
 
-        cls.def_property_readonly( "space", [](DF &df) -> const typename DF::DiscreteFunctionSpaceType& {return df.space();} );
-        cls.def_property_readonly( "size", [](DF &df) {return df.size();} );
-        cls.def("clear", [] (DF &instance) { instance.clear(); } );
+        cls.def_property_readonly( "space", [] ( DF &df ) -> const typename DF::DiscreteFunctionSpaceType & { return df.space(); } );
+        cls.def_property_readonly( "size", [] ( DF &df ) { return df.size(); } );
+        cls.def( "clear", [] ( DF &instance ) { instance.clear(); } );
 
-        registerDFConstructor< DF, Cls >( cls, std::is_constructible< DF, const std::string&, const Space& >() );
+        registerDFConstructor( cls );
+
+        cls.def( "copy", [] ( DF &self ) {
+            pybind11::object copy = pybind11::cast( new DF( self ), pybind11::return_value_policy::take_ownership );
+            // keep alive discrete function space until copy dies, too
+            pybind11::detail::keep_alive_impl( copy, pybind11::detail::get_object_handle( &self.space(), pybind11::detail::get_type_info( typeid( Space ) ) ) );
+            return copy;
+          } );
 
         cls.def( "assign", [] ( DF &instance, const DF &other ) { instance.assign(other); } );
 
@@ -112,43 +124,53 @@ namespace Dune
             Fem::interpolate( gf, df );
           } );
         cls.def( "interpolate", [] ( DF &df, typename Space::RangeType value ) {
-            Fem::interpolate(
-                  simpleGridFunction( df.space().gridPart(), [ value ] ( typename DF::DomainType ) { return value; },0
-            ), df );
+            const auto gf = simpleGridFunction( df.space().gridPart(), [ value ] ( typename DF::DomainType ) { return value; }, 0 );
+            Fem::interpolate( gf, df );
           } );
 
         typedef typename DF::DofVectorType DofVector;
-        if (!pybind11::already_registered<DofVector>())
+        if( !pybind11::already_registered< DofVector >() )
         {
-          auto clsDof = pybind11::class_<DofVector>( module, "DofVector");
+          auto clsDof = pybind11::class_< DofVector >( module, "DofVector" );
         }
+
         cls.def( "dofVector", [] ( DF &instance ) -> DofVector&{ return instance.dofVector(); },
                  pybind11::return_value_policy::reference_internal );
         cls.def( "assign", [] ( DF &instance, const DofVector &other ) { instance.dofVector() = other; } );
 
-        registerDFBuffer<DF>(cls,0);
+        registerDFBuffer< DF >( cls, 0 );
       }
-    }
-    template< class DF, class Holder, class AliasType >
-    void registerDiscreteFunction ( pybind11::module module, pybind11::class_<DF,Holder,AliasType> &cls )
+
+    } // namespace detail
+
+
+
+    // registerDiscreteFunction
+    // ------------------------
+
+    template< class DF, class Holder, class Alias >
+    void registerDiscreteFunction ( pybind11::module module, pybind11::class_< DF, Holder, Alias > &cls )
     {
-      detail::registerDiscreteFunction<DF>(module,cls);
+      detail::registerDiscreteFunction< DF >( module, cls );
     }
+
     // special registry for numpy df since they require a constructor
     // taking the dof vector
-    template< class Space, class Field, class Holder, class AliasType >
+    template< class Space, class Field, class Holder, class Alias >
     void registerDiscreteFunction ( pybind11::module module,
-          pybind11::class_<Dune::Fem::VectorDiscreteFunction<Space,Dune::FemPy::NumPyVector<Field>>,
-          Holder,AliasType> &cls )
+          pybind11::class_< Dune::Fem::VectorDiscreteFunction< Space, Dune::FemPy::NumPyVector< Field > >, Holder, Alias > &cls )
     {
+      using pybind11::operator""_a;
+
       typedef Dune::Fem::VectorDiscreteFunction<Space,Dune::FemPy::NumPyVector<Field>> DF;
       detail::registerDiscreteFunction<DF>(module,cls);
       typedef typename DF::VectorType VectorType;
-      cls.def( "__init__", [] ( DF &instance, Space &space, const std::string &name, pybind11::buffer &dof ) {
-          VectorType *vec = new VectorType(dof);
-          new( &instance ) DF( std::move(name), space, *vec );
-        }, pybind11::keep_alive< 1, 2 >(), pybind11::keep_alive< 1, 4 >() );
+      cls.def( "__init__", [] ( DF &self, Space &space, std::string name, pybind11::buffer dof ) {
+          VectorType *vec = new VectorType( std::move( dof ) );
+          new (&self) DF( std::move( name ), space, *vec );
+        }, "space"_a, "name"_a, "dof"_a, pybind11::keep_alive< 1, 2 >(), pybind11::keep_alive< 1, 4 >() );
     }
+
   } // namespace FemPy
 
 } // namespace Dune
