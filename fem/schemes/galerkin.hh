f6dc2f6b6d89d9c31341bf31d175cf2c9aaaef1e
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2016-10-13 08:33:49 +0200

    add (untested) DGDiffusionModelIntegrands

--- fem/schemes/galerkin.hh
+++ fem/schemes/galerkin.hh
@@ -8,6 +8,8 @@
 #include <utility>
 #include <vector>
 
+#include <dune/common/ftraits.hh>
+
 #include <dune/grid/common/rangegenerators.hh>
 
 #include <dune/fem/function/localfunction/temporary.hh>
@@ -204,7 +206,7 @@ namespace Dune
 
       private:
         template< class Integrands >
-        static std::true_type chkHasSkeletonIntegrand ( const Integrands &, decltype( std::declval< const Integrands & >().skeleton( std::declval< const SurfaceQuadraturePointType & >(), std::declval< const ValueType & >(), std::declval< const ValueType & >() ) ) * = nullptr );
+        static std::true_type chkHasSkeletonIntegrand ( const Integrands &, decltype( std::declval< const Integrands & >().skeleton( std::declval< const SurfaceQuadraturePointType & >(), std::declval< const ValueType & >(), std::declval< const SurfaceQuadraturePointType & >(), std::declval< const ValueType & >() ) ) * = nullptr );
 
         static std::false_type chkHasSkeletonIntegrand ( ... );
 
@@ -248,7 +250,7 @@ namespace Dune
         template< class Integrands, class QP, class W, std::enable_if_t< HasSkeletonIntegrand< Integrands >::value, int > = 0 >
         static void addSkeletonIntegrand ( const Integrands &integrands, const QP &qpIn, const QP &qpOut, const RangeFieldType &weight, const ValueType &uIn, const ValueType &uOut, W &wIn, W &wOut )
         {
-          std::pair< ValueType, ValueType > integrand = integrands.skeleton( qpIn, uIn, uOut );
+          std::pair< ValueType, ValueType > integrand = integrands.skeleton( qpIn, uIn, qpOut, uOut );
           std::get< 0 >( integrand.first ) *= weight;
           std::get< 1 >( integrand.first ) *= weight;
           wIn.axpy( qpIn, std::get< 0 >( integrand.first ), std::get< 1 >( integrand.first ) );
@@ -268,16 +270,16 @@ namespace Dune
         template< class Integrands, class QP, class J, std::enable_if_t< HasSkeletonIntegrand< Integrands >::value, int > = 0 >
         static void addLinearizedSkeletonIntegrand ( const Integrands &integrands, const QP &qpIn, const QP &qpOut, const RangeFieldType &weight, const ValueType &uIn, const ValueType &uOut, const ValueVectorType &phiIn, std::size_t colsIn, const ValueVectorType &phiOut, std::size_t colsOut, J &jInIn, J &jOutIn )
         {
-          auto integrand = integrands.linearizedSkeleton( qpIn, uIn, uOut );
+          auto integrand = integrands.skeleton( qpIn, uIn, qpOut, uOut );
           for( std::size_t col = 0; col < colsIn; ++col )
           {
-            ValueType intPhiIn = integrand.first.first( std::make_tuple( std::get< 0 >( phiIn )[ col ], std::get< 1 >( phiIn )[ col ] ) );
-            jInIn.column( col ).axpy( std::get< 0 >( phiIn ), std::get< 1 >( phiIn ), std::get< 0 >( intPhiIn ), std::get< 1 >( intPhiIn ), weight );
+            std::pair< ValueType, ValueType > intPhi = integrand.first( std::make_tuple( std::get< 0 >( phiIn )[ col ], std::get< 1 >( phiIn )[ col ] ) );
+            jInIn.column( col ).axpy( std::get< 0 >( phiIn ), std::get< 1 >( phiIn ), std::get< 0 >( intPhi.first ), std::get< 1 >( intPhi.first ), weight );
           }
           for( std::size_t col = 0; col < colsOut; ++col )
           {
-            ValueType intPhiIn = integrand.first.second( std::make_tuple( std::get< 0 >( phiOut )[ col ], std::get< 1 >( phiOut )[ col ] ) );
-            jOutIn.column( col ).axpy( std::get< 0 >( phiIn ), std::get< 1 >( phiIn ), std::get< 0 >( intPhiIn ), std::get< 1 >( intPhiIn ), weight );
+            std::pair< ValueType, ValueType > intPhi = integrand.second( std::make_tuple( std::get< 0 >( phiOut )[ col ], std::get< 1 >( phiOut )[ col ] ) );
+            jOutIn.column( col ).axpy( std::get< 0 >( phiIn ), std::get< 1 >( phiIn ), std::get< 0 >( intPhi.first ), std::get< 1 >( intPhi.first ), weight );
           }
         }
 
@@ -287,19 +289,15 @@ namespace Dune
           auto integrand = integrands.linearizedSkeleton( qpIn, uIn, uOut );
           for( std::size_t col = 0; col < colsIn; ++col )
           {
-            ValueType intPhiIn = integrand.first.first( std::make_tuple( std::get< 0 >( phiIn )[ col ], std::get< 1 >( phiIn )[ col ] ) );
-            jInIn.column( col ).axpy( std::get< 0 >( phiIn ), std::get< 1 >( phiIn ), std::get< 0 >( intPhiIn ), std::get< 1 >( intPhiIn ), weight );
-
-            ValueType intPhiOut = integrand.second.first( std::make_tuple( std::get< 0 >( phiIn )[ col ], std::get< 1 >( phiIn )[ col ] ) );
-            jInOut.column( col ).axpy( std::get< 0 >( phiOut ), std::get< 1 >( phiOut ), std::get< 0 >( intPhiOut ), std::get< 1 >( intPhiOut ), weight );
+            std::pair< ValueType, ValueType > intPhi = integrand.first( std::make_tuple( std::get< 0 >( phiIn )[ col ], std::get< 1 >( phiIn )[ col ] ) );
+            jInIn.column( col ).axpy( std::get< 0 >( phiIn ), std::get< 1 >( phiIn ), std::get< 0 >( intPhi.first ), std::get< 1 >( intPhi.first ), weight );
+            jInOut.column( col ).axpy( std::get< 0 >( phiOut ), std::get< 1 >( phiOut ), std::get< 0 >( intPhi.second ), std::get< 1 >( intPhi.second ), weight );
           }
           for( std::size_t col = 0; col < colsOut; ++col )
           {
-            ValueType intPhiIn = integrand.first.second( std::make_tuple( std::get< 0 >( phiOut )[ col ], std::get< 1 >( phiOut )[ col ] ) );
-            jOutIn.column( col ).axpy( std::get< 0 >( phiIn ), std::get< 1 >( phiIn ), std::get< 0 >( intPhiIn ), std::get< 1 >( intPhiIn ), weight );
-
-            ValueType intPhiOut = integrand.second.second( std::make_tuple( std::get< 0 >( phiOut )[ col ], std::get< 1 >( phiOut )[ col ] ) );
-            jOutOut.column( col ).axpy( std::get< 0 >( phiOut ), std::get< 1 >( phiOut ), std::get< 0 >( intPhiOut ), std::get< 1 >( intPhiOut ), weight );
+            std::pair< ValueType, ValueType > intPhi = integrand.second( std::make_tuple( std::get< 0 >( phiOut )[ col ], std::get< 1 >( phiOut )[ col ] ) );
+            jOutIn.column( col ).axpy( std::get< 0 >( phiIn ), std::get< 1 >( phiIn ), std::get< 0 >( intPhi.first ), std::get< 1 >( intPhi.first ), weight );
+            jOutOut.column( col ).axpy( std::get< 0 >( phiOut ), std::get< 1 >( phiOut ), std::get< 0 >( intPhi.second ), std::get< 1 >( intPhi.second ), weight );
           }
         }
 
@@ -848,20 +846,20 @@ namespace Dune
       {
         RangeType source( 0 );
         model().source( x, std::get< 0 >( u ), std::get< 1 >( u ), source );
-        JacobianRangeType diffusiveFlux( 0 );
-        model().diffusiveFlux( x, std::get< 0 >( u ), std::get< 1 >( u ), diffusiveFlux );
-        return std::make_tuple( source, diffusiveFlux );
+        JacobianRangeType dFlux( 0 );
+        model().diffusiveFlux( x, std::get< 0 >( u ), std::get< 1 >( u ), dFlux );
+        return std::make_tuple( source, dFlux );
       }
 
       template< class Point >
       auto linearizedInterior ( const Point &x, const ValueType &u ) const
       {
-        return [ this, &x, &u ] ( const ValueType &phi ) {
+        return [ this, x, u ] ( const ValueType &phi ) {
             RangeType source( 0 );
             model().linSource( std::get< 0 >( u ), std::get< 1 >( u ), x, std::get< 0 >( phi ), std::get< 1 >( phi ), source );
-            JacobianRangeType diffusiveFlux( 0 );
-            model().linDiffusiveFlux( std::get< 0 >( u ), std::get< 1 >( u ), x, std::get< 0 >( phi ), std::get< 1 >( phi ), diffusiveFlux );
-            return std::make_tuple( source, diffusiveFlux );
+            JacobianRangeType dFlux( 0 );
+            model().linDiffusiveFlux( std::get< 0 >( u ), std::get< 1 >( u ), x, std::get< 0 >( phi ), std::get< 1 >( phi ), dFlux );
+            return std::make_tuple( source, dFlux );
           };
       }
 
@@ -876,7 +874,7 @@ namespace Dune
       template< class Point >
       auto linearizedBoundary ( const Point &x, const ValueType &u ) const
       {
-        return [ this, &x, &u ] ( const ValueType &phi ) {
+        return [ this, x, u ] ( const ValueType &phi ) {
             RangeType alpha( 0 );
             model().linAlpha( std::get< 0 >( u ), x, std::get< 0 >( phi ), alpha );
             return std::make_tuple( alpha, 0 );
@@ -887,6 +885,229 @@ namespace Dune
 
     private:
       const Model *model_;
+    };
+
+
+
+    // DGDiffusionModelIntegrands
+    // --------------------------
+
+    template< class Model >
+    struct DGDiffusionModelIntegrands
+    {
+      typedef typename Model::GridPartType GridPartType;
+
+      typedef typename GridPartType::template Codim< 0 >::EntityType EntityType;
+      typedef typename GridPartType::IntersectionType IntersectionType;
+
+      typedef typename Model::RangeType RangeType;
+      typedef typename Model::JacobianRangeType JacobianRangeType;
+
+      typedef typename FieldTraits< RangeType >::field_type RangeFieldType;
+
+      typedef std::tuple< RangeType, JacobianRangeType > ValueType;
+
+      DGDiffusionModelIntegrands ( const Model &model, RangeFieldType penalty )
+        : model_( &model ), penalty_( penalty )
+      {}
+
+      bool init ( const EntityType &entity )
+      {
+        intersection_ = nullptr;
+        return model().init( entity );
+      }
+
+      bool init ( const IntersectionType &intersection )
+      {
+        intersection_ = &intersection;
+        if( intersection.boundary() )
+        {
+          const EntityType inside = intersection.inside();
+          beta_ = intersection.geometry().volume() / inside.geometry().volume();
+          return (model().hasNeumanBoundary() && model().init( inside ));
+        }
+        else if( intersection.neighbor() )
+        {
+          const auto volIn = intersection.inside().geometry().volume();
+          const auto volOut = intersection.outside().geometry().volume();
+          beta_ = intersection.geometry().volume() / std::min( volIn, volOut );
+          return true;
+        }
+      }
+
+      template< class Point >
+      ValueType interior ( const Point &x, const ValueType &u ) const
+      {
+        RangeType source( 0 );
+        model().source( x, std::get< 0 >( u ), std::get< 1 >( u ), source );
+        JacobianRangeType dFlux( 0 );
+        model().diffusiveFlux( x, std::get< 0 >( u ), std::get< 1 >( u ), dFlux );
+        return std::make_tuple( source, dFlux );
+      }
+
+      template< class Point >
+      auto linearizedInterior ( const Point &x, const ValueType &u ) const
+      {
+        return [ this, x, u ] ( const ValueType &phi ) {
+            RangeType source( 0 );
+            model().linSource( std::get< 0 >( u ), std::get< 1 >( u ), x, std::get< 0 >( phi ), std::get< 1 >( phi ), source );
+            JacobianRangeType dFlux( 0 );
+            model().linDiffusiveFlux( std::get< 0 >( u ), std::get< 1 >( u ), x, std::get< 0 >( phi ), std::get< 1 >( phi ), dFlux );
+            return std::make_tuple( source, dFlux );
+          };
+      }
+
+      template< class Point >
+      ValueType boundary ( const Point &x, const ValueType &u ) const
+      {
+        RangeType alpha( 0 );
+        model().alpha( x, std::get< 0 >( u ), alpha );
+        return std::make_tuple( alpha, 0 );
+      }
+
+      template< class Point >
+      auto linearizedBoundary ( const Point &x, const ValueType &u ) const
+      {
+        return [ this, x, u ] ( const ValueType &phi ) {
+            RangeType alpha( 0 );
+            model().linAlpha( std::get< 0 >( u ), x, std::get< 0 >( phi ), alpha );
+            return std::make_tuple( alpha, 0 );
+          };
+      }
+
+      template< class Point >
+      std::pair< ValueType, ValueType > skeleton ( const Point &xIn, const ValueType &uIn, const Point &xOut, const ValueType &uOut ) const
+      {
+        const EntityType inside = intersection().inside();
+        const EntityType outside = intersection().outside();
+
+        const RangeFieldType half = RangeFieldType( 1 ) / RangeFieldType( 2 );
+        const auto normal = intersection.unitOuterNormal( xIn.localPosition() );
+
+        ValueType uJump;
+        std::get< 0 >( uJump ) = std::get< 0 >( uOut ) - std::get< 0 >( uIn );
+        for( int i = 0; i < RangeType::dimension; ++i )
+        {
+          std::get< 1 >( uJump )[ i ] = normal;
+          std::get< 1 >( uJump )[ i ] *= std::get< 0 >( uJump )[ i ];
+        }
+
+        model().init( outside );
+        JacobianRangeType dFluxOut( 0 ), dFluxPrimeOut( 0 );
+        model().diffusiveFlux( xOut, std::get< 0 >( uOut ), std::get< 1 >( uJump ), dFluxPrimeOut );
+        model().diffusiveFlux( xOut, 0, 0, dFluxOut );
+        dFluxPrimeOut -= dFluxOut;
+        model().diffusiveFlux( xOut, std::get< 0 >( uOut ), std::get< 1 >( uOut ), dFluxOut );
+
+        model().init( inside );
+        JacobianRangeType dFluxIn( 0 ), dFluxPrimeIn( 0 );
+        model().diffusiveFlux( xIn, std::get< 0 >( uIn ), std::get< 1 >( uJump ), dFluxPrimeIn );
+        model().diffusiveFlux( xIn, 0, 0, dFluxIn );
+        dFluxPrimeIn -= dFluxIn;
+        model().diffusiveFlux( xIn, std::get< 0 >( uIn ), std::get< 1 >( uIn ), dFluxIn );
+
+        RangeType int0 = std::get< 0 >( uJump );
+        int0 *= beta_;
+        (dFluxIn + dFluxOut).usmv( -half, normal, int0 );
+
+        dFluxPrimeIn *= -half;
+        dFluxPrimeOut *= -half;
+
+        return std::make_pair( ValueType( int0, dFluxPrimeOut ), ValueType( -int0, dFluxPrimeOut ) );
+      }
+
+      template< class Point >
+      auto linearizedSkeleton ( const Point &xIn, const ValueType &uIn, const Point &xOut, const ValueType &uOut ) const
+      {
+        const auto normal = intersection.unitOuterNormal( xIn.localPosition() );
+
+        ValueType uJump;
+        std::get< 0 >( uJump ) = std::get< 0 >( uOut ) - std::get< 0 >( uIn );
+        for( int i = 0; i < RangeType::dimension; ++i )
+        {
+          std::get< 1 >( uJump )[ i ] = normal;
+          std::get< 1 >( uJump )[ i ] *= std::get< 0 >( uJump )[ i ];
+        }
+
+        auto intIn = [ this, xIn, uIn, xOut, uOut, normal, uJump ] ( const ValueType &phiIn ) {
+          const EntityType inside = intersection().inside();
+          const EntityType outside = intersection().outside();
+
+          const RangeFieldType half = RangeFieldType( 1 ) / RangeFieldType( 2 );
+
+          ValueType phiJump;
+          std::get< 0 >( phiJump ) = std::get< 0 >( phiIn );
+          for( int i = 0; i < RangeType::dimension; ++i )
+          {
+            std::get< 1 >( phiJump )[ i ] = normal;
+            std::get< 1 >( phiJump )[ i ] *= -std::get< 0 >( phiJump )[ i ];
+          }
+
+          model().init( outside );
+          JacobianRangeType dFluxPrimeOut( 0 );
+          model().linDiffusiveFlux( std::get< 0 >( uOut ), std::get< 1 >( uJump ), xOut, 0, std::get< 1 >( phiJump ), dFluxPrimeOut );
+
+          model().init( inside );
+          JacobianRangeType dFluxIn( 0 ), dFluxPrimeIn( 0 );
+          model().linDiffusiveFlux( std::get< 0 >( uIn ), std::get< 1 >( uJump ), xIn, std::get< 0 >( phiIn ), std::get< 1 >( phiJump ), dFluxPrimeIn );
+          model().linDiffusiveFlux( std::get< 0 >( uIn ), std::get< 1 >( uIn ), xIn, std::get< 0 >( phiIn ), std::get< 1 >( phiIn ), dFluxIn );
+
+          RangeType int0 = std::get< 0 >( phiJump );
+          int0 *= beta_;
+          dFluxIn.usmv( -half, normal, int0 );
+
+          dFluxPrimeIn *= -half;
+          dFluxPrimeOut *= -half;
+
+          return std::make_pair( ValueType( int0, dFluxPrimeOut ), ValueType( -int0, dFluxPrimeOut ) );
+        };
+
+        auto intOut = [ this, xIn, uIn, xOut, uOut, normal, uJump ] ( const ValueType &phiOut ) {
+          const EntityType inside = intersection().inside();
+          const EntityType outside = intersection().outside();
+
+          const RangeFieldType half = RangeFieldType( 1 ) / RangeFieldType( 2 );
+          const auto normal = intersection.unitOuterNormal( xIn.localPosition() );
+
+          ValueType phiJump;
+          std::get< 0 >( phiJump ) = std::get< 0 >( phiOut );
+          for( int i = 0; i < RangeType::dimension; ++i )
+          {
+            std::get< 1 >( phiJump )[ i ] = normal;
+            std::get< 1 >( phiJump )[ i ] *= std::get< 0 >( phiJump )[ i ];
+          }
+
+          model().init( outside );
+          JacobianRangeType dFluxOut( 0 ), dFluxPrimeOut( 0 );
+          model().linDiffusiveFlux( std::get< 0 >( uOut ), std::get< 1 >( uJump ), xOut, std::get< 0 >( phiOut ), std::get< 1 >( phiJump ), dFluxPrimeOut );
+          model().linDiffusiveFlux( std::get< 0 >( uOut ), std::get< 1 >( uOut ), xOut, std::get< 0 >( phiOut ), std::get< 1 >( phiOut ), dFluxOut );
+
+          model().init( inside );
+          JacobianRangeType dFluxPrimeIn( 0 );
+          model().linDiffusiveFlux( std::get< 0 >( uIn ), std::get< 1 >( uJump ), xIn, 0, std::get< 1 >( phiJump ), dFluxPrimeIn );
+
+          RangeType int0 = std::get< 0 >( phiJump );
+          int0 *= beta_;
+          dFluxOut.usmv( -half, normal, int0 );
+
+          dFluxPrimeIn *= -half;
+          dFluxPrimeOut *= -half;
+
+          return std::make_pair( ValueType( int0, dFluxPrimeOut ), ValueType( -int0, dFluxPrimeOut ) );
+        };
+
+        return std::make_pair( intIn, intOut );
+      }
+
+      const Model &model () const { return *model_; }
+
+    private:
+      const IntersectionType &intersection () const { assert( intersection_ ); return *intersection_; }
+
+      const Model *model_;
+      RangeFieldType penalty_;
+      const IntersectionType *intersection_ = nullptr;
+      RangeFieldType beta_;
     };
 
   } // namespace Fem
