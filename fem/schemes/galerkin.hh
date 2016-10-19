0dc54974dd6946086ec1317249a1b32415ad5251
dedner <a.s.dedner@warwick.ac.uk>
2016-10-19 08:53:06 +0100

    moved the storage to space and removed the key from the scheme

--- fem/schemes/galerkin.hh
+++ fem/schemes/galerkin.hh
@@ -817,7 +817,7 @@ namespace Dune
         typedef typename UsedSolverType::LinearInverseOperatorType LinearInverseOperatorType;
         NewtonInverseOperator< typename GalerkinOperatorType::JacobianOperatorType, LinearInverseOperatorType > invOp( galerkinOperator_, parameter_ );
         invOp( rhs_, solution );
-        // std::cout << "Linear Iterations: " << invOp.linearIterations() << std::endl;
+        std::cout << "Linear Iterations: " << invOp.linearIterations() << std::endl;
       }
 
       template< class GridFunction >
