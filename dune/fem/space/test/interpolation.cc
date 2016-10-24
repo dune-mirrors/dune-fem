418191ff1c6b3a12e25a29bb3f2a94bab804725d
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2016-10-24 09:34:17 +0200

    refactor interpolation test and allow checking multiple spaces (at once)

--- dune/fem/space/test/interpolation.cc
+++ dune/fem/space/test/interpolation.cc
@@ -5,16 +5,17 @@
 #include <cstdlib>
 #include <iostream>
 #include <string>
-#include <sstream>
 
 // dune-common includes
 #include <dune/common/exceptions.hh>
+
+// dune-grid includes
+#include <dune/grid/io/file/dgfparser/dgfparser.hh>
 
 // dune-fem includes
 #include <dune/fem/function/adaptivefunction.hh>
 #include <dune/fem/function/common/gridfunctionadapter.hh>
 #include <dune/fem/gridpart/adaptiveleafgridpart.hh>
-#include <dune/fem/io/file/dataoutput.hh>
 #include <dune/fem/io/parameter.hh>
 #include <dune/fem/misc/l2norm.hh>
 #include <dune/fem/misc/mpimanager.hh>
@@ -22,9 +23,10 @@
 #include <dune/fem/space/common/functionspace.hh>
 #include <dune/fem/space/common/interpolate.hh>
 #include <dune/fem/space/discontinuousgalerkin.hh>
+#include <dune/fem/space/lagrange.hh>
 
 #include "../../test/exactsolution.hh"
-#include "../../test/testgrid.hh"
+
 
 // polynomial order
 static const int polOrder = POLORDER;
@@ -37,71 +39,34 @@ typedef Dune::GridSelector::GridType GridType;
 
 
 
-// DataOutputParameters
-// --------------------
+// interpolationError
+// ------------------
 
-struct DataOutputParameters
-: public Dune::Fem::LocalParameter< Dune::Fem::DataOutputParameters, DataOutputParameters >
+template< class DiscreteFunctionSpace >
+double interpolationError ( const DiscreteFunctionSpace &discreteFunctionSpace )
 {
-  DataOutputParameters ( const int step )
-  : step_( step )
-  { }
-
-  DataOutputParameters ( const DataOutputParameters &other )
-  : step_( other.step_ )
-  { }
-
-  std::string prefix () const
-  {
-    std::stringstream s;
-    s << "projection-" << step_ << "-";
-    return s.str();
-  }
-
-private:
-  int step_;
-};
-
-
-
-// algorithm
-// ---------
-
-template< class GridType >
-double algorithm ( GridType &grid, const int step )
-{
-  // create grid part
-  typedef Dune::Fem::AdaptiveLeafGridPart< GridType > GridPartType;
-  GridPartType gridPart( grid );
-
-  // function space type
-  typedef typename GridPartType::ctype DomainFieldType;
-  static const int dimDomain = GridPartType::dimensionworld;
-  typedef Dune::Fem::FunctionSpace< DomainFieldType, double, dimDomain, dimRange > FunctionSpaceType;
+  typedef typename DiscreteFunctionSpace::GridPartType GridPartType;
+  const GridPartType &gridPart = discreteFunctionSpace.gridPart();
 
   // create exact solution
-  typedef Dune::Fem::ExactSolution< FunctionSpaceType > ExactSolutionType;
+  typedef Dune::Fem::ExactSolution< typename DiscreteFunctionSpace::FunctionSpaceType > ExactSolutionType;
   ExactSolutionType exactSolution;
   typedef Dune::Fem::GridFunctionAdapter< ExactSolutionType, GridPartType > GridExactSolutionType;
   GridExactSolutionType gridExactSolution( "exact solution", exactSolution, gridPart, 5 );
 
-  // create discrete function space
-  typedef Dune::Fem::DiscontinuousGalerkinSpace< FunctionSpaceType, GridPartType, polOrder > DiscreteFunctionSpaceType;
-  DiscreteFunctionSpaceType discreteFunctionSpace( gridPart );
-
   // create discrete function
-  typedef Dune::Fem::AdaptiveDiscreteFunction< DiscreteFunctionSpaceType > DiscreteFunctionType;
+  typedef Dune::Fem::AdaptiveDiscreteFunction< DiscreteFunctionSpace > DiscreteFunctionType;
   DiscreteFunctionType solution( "solution", discreteFunctionSpace );
   solution.clear();
 
   // perform the interpolation
   interpolate( gridExactSolution, solution );
 
-  typedef Dune::Capabilities::hasSingleGeometryType< GridType > HasSingleGeometryType;
-  if( dimDomain == 2 && HasSingleGeometryType::v && HasSingleGeometryType::topologyId == 0 )
+  typedef Dune::Capabilities::hasSingleGeometryType< typename GridPartType::GridType > HasSingleGeometryType;
+  if( (DiscreteFunctionSpace::dimDomain == 2) && HasSingleGeometryType::v && (HasSingleGeometryType::topologyId == 0) )
   {
     typename DiscreteFunctionType::HessianRangeType h1, h2;
-    typename DiscreteFunctionType::DomainType x(0.5);
+    typename DiscreteFunctionType::DomainType x( 0.5 );
 
     solution.hessian( x, h1 );
     exactSolution.hessian( x, h2 );
@@ -116,23 +81,69 @@ double algorithm ( GridType &grid, const int step )
     std::cout<<std::sqrt( norm )<<std::endl;
   }
 
-#if 0
-  // prepare output
-  typedef std::tuple< const DiscreteFunctionType *, GridExactSolutionType * > IOTupleType;
-  IOTupleType ioTuple( &solution, &gridExactSolution );
-
-  // data output
-  typedef Dune::Fem::DataOutput< GridType, IOTupleType > DataOutputType;
-  DataOutputType dataOutput( grid, ioTuple, DataOutputParameters( step ) );
-  dataOutput.write();
-#endif
-
   // compute error
   Dune::Fem::L2Norm< GridPartType > l2norm( gridPart );
   return l2norm.distance( gridExactSolution, solution );
 }
 
 
+
+// interpolationErrors
+// -------------------
+
+template< class... DiscreteFunctionSpace >
+std::array< double, sizeof...( DiscreteFunctionSpace ) > interpolationErrors ( const DiscreteFunctionSpace &... discreteFunctionSpace )
+{
+  return {{ interpolationError( discreteFunctionSpace )... }};
+}
+
+
+
+// printValues
+// -----------
+
+template< std::size_t n >
+void printValues ( std::string prefix, std::array< double, n > values )
+{
+  if( Dune::Fem::MPIManager::rank() != 0 )
+    return;
+
+  std::cout << prefix << "( " << values[ 0 ];
+  for( std::size_t i = 1; i < n; ++i )
+    std::cout << ", " << values[ i ];
+  std::cout << " )" << std::endl;
+}
+
+
+
+// eocLoop
+// -------
+
+template< class... DiscreteFunctionSpace >
+void eocLoop ( GridType &grid, int steps, const DiscreteFunctionSpace &... discreteFunctionSpace )
+{
+  const std::size_t n = sizeof...( DiscreteFunctionSpace );
+  std::array< double, n > oldErrors = interpolationErrors( discreteFunctionSpace... );
+  printValues( "L2 error[ 0 ] = ", oldErrors );
+
+  for( int step = 1; step <= steps ; ++step )
+  {
+    Dune::Fem::GlobalRefine::apply( grid, Dune::DGFGridInfo< GridType >::refineStepsForHalf() );
+
+    const std::array< double, n > newErrors = interpolationErrors( discreteFunctionSpace... );
+    std::array< double, n > eocs;
+    std::transform( oldErrors.begin(), oldErrors.end(), newErrors.begin(), eocs.begin(), [] ( double eold, double enew ) { return log( eold / enew ) / M_LN2; } );
+    oldErrors = newErrors;
+
+    printValues( "L2 error[ " + std::to_string( step ) + " ] = ", oldErrors );
+    printValues( "L2 EOC[ " + std::to_string( step ) + " ] = ", eocs );
+  }
+}
+
+
+
+// main
+// ----
 
 int main ( int argc, char **argv )
 try
@@ -144,48 +155,43 @@ try
   Dune::Fem::Parameter::append( argc, argv );
   Dune::Fem::Parameter::append( "parameter" );
 
-  if( argc < 2 )
+  if( argc < 1 )
   {
     std::cerr << "Usage: " << argv[ 0 ] << " [initial refinement level] [steps]" << std::endl;
     return 0;
   }
 
   // initial refinement level
-  int refCount = 1;
-  if( argc > 1 )
-    refCount = std::max( refCount, std::atoi( argv[ 1 ] ) );
+  const int refCount = (argc > 1 ? std::max( 0, std::atoi( argv[ 1 ] ) ) : 1);
 
   // number of EOC steps
-  int steps = 1;
-  if( argc > 2 )
-    steps = std::max( steps, std::atoi( argv[ 2 ] ) );
+  const int steps = (argc > 2 ? std::max( 0, std::atoi( argv[ 2 ] ) ) : 1);
 
   // create grid
-  GridType &grid = Dune::Fem::TestGrid::grid();
-  const int refineStepsForHalf = Dune::Fem::TestGrid::refineStepsForHalf();
-  Dune::Fem::GlobalRefine::apply( grid, refineStepsForHalf );
+  Dune::GridPtr< GridType > grid( std::to_string( GridType::dimension ) + "dgrid.dgf" );
+  grid->loadBalance();
+  Dune::Fem::GlobalRefine::apply( *grid, refCount * Dune::DGFGridInfo< GridType >::refineStepsForHalf() );
 
-  // compute DG L2-projection
-  double oldError = algorithm( grid, 0 );
-  std::cout << "L2 error[ 0 ] = " << oldError << std::endl;
-  for( int step = 1; step < steps ; ++step )
-  {
-    Dune::Fem::GlobalRefine::apply( grid, refineStepsForHalf );
+  // create grid part
+  typedef Dune::Fem::AdaptiveLeafGridPart< GridType > GridPartType;
+  GridPartType gridPart( *grid );
 
-    const double newError = algorithm( grid, step );
-    const double eoc = log( oldError / newError ) / M_LN2;
-    if( Dune::Fem::MPIManager::rank() == 0 )
-    {
-      std::cout << "L2 error[ " << step << " ] = " << newError << std::endl;
-      std::cout << "Eoc[ " << step << " ] = " << eoc << std::endl;
-    }
-    oldError = newError;
-  }
+  // function space type
+  typedef typename GridPartType::ctype DomainFieldType;
+  static const int dimDomain = GridPartType::dimensionworld;
+  typedef Dune::Fem::FunctionSpace< DomainFieldType, double, dimDomain, dimRange > FunctionSpaceType;
+
+  // create discrete function spaces
+  Dune::Fem::DiscontinuousGalerkinSpace< FunctionSpaceType, GridPartType, polOrder > discontinuousGalerkinSpace( gridPart );
+  Dune::Fem::LagrangeDiscreteFunctionSpace< FunctionSpaceType, GridPartType, polOrder > lagrangeSpace( gridPart );
+
+  // perform eoc loop
+  eocLoop( *grid, steps, discontinuousGalerkinSpace, lagrangeSpace );
 
   return 0;
 }
-catch( Dune::Exception e )
+catch( const Dune::Exception &e )
 {
-  std::cerr << e.what() << std::endl;
+  std::cerr << e << std::endl;
   return 1;
 }
