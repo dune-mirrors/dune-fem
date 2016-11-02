e84a324bb58b8102f9d34b8f16ea78f12f91c9a1
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2016-11-02 08:59:02 +0100

    use FieldMatrixHelper instead of MatrixHelper< DuneCoordTraits >

--- fem/gridpart/geometrygridpart/geometry.hh
+++ fem/gridpart/geometrygridpart/geometry.hh
@@ -208,7 +208,7 @@ namespace Dune
       typedef typename HostGridPartType::IntersectionType HostIntersectionType;
 
       // Helper class to compute a matrix pseudo inverse
-      typedef Impl::MatrixHelper< GenericGeometry::DuneCoordTraits< ctype > > MatrixHelper;
+      typedef Impl::FieldMatrixHelper< ctype > MatrixHelper;
 
       MyGeometryImpl ( const MyGeometryImpl< GridFamily::dimension, cdim, GridFamily > elementGeo,
                        const GridFunctionType *gridFunction,
@@ -331,7 +331,7 @@ namespace Dune
       typedef typename HostGridPartType::IntersectionType HostIntersectionType;
 
       // Helper class to compute a matrix pseudo inverse
-      typedef Impl::MatrixHelper< GenericGeometry::DuneCoordTraits< ctype > > MatrixHelper;
+      typedef Impl::FieldMatrixHelper< ctype > MatrixHelper;
 
       MyGeometryImpl ( const HostGeometryType &hostGeometry,
                        const HostEntityType &hostEntity,
