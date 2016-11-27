92a94443d5d21cc73ca66ee0bb40fcb0a1c485cf
Robert K <robertk@posteo.org>
2016-11-27 21:43:16 +0100

    [bugfix] fix classic mixup of domain and range space.

--- dune/fem/operator/linear/petscoperator.hh
+++ dune/fem/operator/linear/petscoperator.hh
@@ -190,8 +190,8 @@ namespace Dune
           petscDest_.reset();
 
           // update dof mappings
-          rowSlaveDofs_.update( rangeSpace() );
-          colSlaveDofs_.update( domainSpace() );
+          rowSlaveDofs_.update( domainSpace() );
+          colSlaveDofs_.update( rangeSpace() );
 
           /*
           * initialize the row and column petsc dof mappings
