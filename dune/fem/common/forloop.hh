19731611c1ed328221e180fd8146f6b592bfc08c
Robert K <robertk@posteo.org>
2016-11-29 11:26:49 +0100

    [bugfix] revert last commit to fully disable warnings.

--- dune/fem/common/forloop.hh
+++ dune/fem/common/forloop.hh
@@ -1,17 +1,29 @@
 #ifndef DUNE_FEM_FORLOOP_HH
 #define DUNE_FEM_FORLOOP_HH
 
+#include <utility>
+
 #include <dune/common/deprecated.hh>
-#include <dune/common/forloop.hh>
+#include <dune/common/hybridutilities.hh>
+#include <dune/common/std/utility.hh>
 
 namespace Dune
 {
 
   namespace Fem {
 
-    DUNE_NO_DEPRECATED_BEGIN
-    using Dune::ForLoop;
-    DUNE_NO_DEPRECATED_END
+  template< template< int > class Operation, int first, int last >
+  struct ForLoop
+  {
+    static_assert( (first <= last), "Fem::Fem::ForLoop: first > last" );
+
+    template<typename... Args>
+    static void apply(Args&&... args)
+    {
+      Dune::Hybrid::forEach(Std::make_index_sequence<last+1-first>{},
+        [&](auto i){Operation<i+first>::apply(args...);});
+    }
+  };
 
   } // end namespace Fem
 } // end namespace Dune
