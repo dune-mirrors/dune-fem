017d0b85ee374a45e719d247f61be0a8cad14fd2
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2016-12-16 23:22:06 +0100

    [bugfix] make LocalMatrixColumn compile

--- fem/operator/common/localmatrixcolumn.hh
+++ fem/operator/common/localmatrixcolumn.hh
@@ -22,23 +22,23 @@ namespace Dune
 
       typedef typename LocalMatrixType::RangeFieldType RangeFieldType;
 
-      LocalMatrixEntry ( const LocalMatrixType &localMatrix, unsigned int row, unsigned int column )
-        : localMatrix_( localMatrix ), row_( row ), column_( column )
+      LocalMatrixEntry ( LocalMatrixType &localMatrix, unsigned int row, unsigned int col )
+        : localMatrix_( localMatrix ), row_( row ), col_( col )
       {}
 
-      operator RangeFieldType () const { return localMatrix_.get( row_, column_ ); }
+      operator RangeFieldType () const { return localMatrix_.get( row_, col_ ); }
 
       ThisType &operator= ( const RangeFieldType &value ) { localMatrix_.set( row_, col_, value ); return *this; }
 
       ThisType &operator+= ( const RangeFieldType &value ) { localMatrix_.add( row_, col_, value ); return *this; }
       ThisType &operator-= ( const RangeFieldType &value ) { localMatrix_.add( row_, col_, -value ); return *this; }
 
-      ThisType &operator*= ( const RangeField &value ) { localMatrix_.set( row_, col_, localMatrix_.get( row_, col_ ) * value ); return *this; }
-      ThisType &operator/= ( const RangeField &value ) { localMatrix_.set( row_, col_, localMatrix_.get( row_, col_ ) / value ); return *this; }
+      ThisType &operator*= ( const RangeFieldType &value ) { localMatrix_.set( row_, col_, localMatrix_.get( row_, col_ ) * value ); return *this; }
+      ThisType &operator/= ( const RangeFieldType &value ) { localMatrix_.set( row_, col_, localMatrix_.get( row_, col_ ) / value ); return *this; }
 
     private:
       LocalMatrixType &localMatrix_;
-      unsigned int row_, column_;
+      unsigned int row_, col_;
     };
 
 
@@ -55,19 +55,19 @@ namespace Dune
       typedef LocalMatrix LocalMatrixType;
 
       typedef typename LocalMatrixType::RangeFieldType RangeFieldType;
-
-      typedef typename LocalMatrixType::RangeType RangeType;
-      typedef typename LocalMatrixType::JacobianRangeType JacobianRangeType;
-
       typedef typename LocalMatrixType::RangeBasisFunctionSetType BasisFunctionSetType;
 
-      LocalMatrixColumn ( LocalMatrixType &localMatrix, unsigned int column )
-        : localMatrix_( localMatrix ), column_( column )
+      typedef typename BasisFunctionSetType::RangeType RangeType;
+      typedef typename BasisFunctionSetType::JacobianRangeType JacobianRangeType;
+      typedef typename BasisFunctionSetType::HessianRangeType HessianRangeType;
+
+      LocalMatrixColumn ( LocalMatrixType &localMatrix, unsigned int col )
+        : localMatrix_( localMatrix ), col_( col )
       {}
 
-      RangeFieldType operator[] ( unsigned int row ) const { return localMatrix_.get( row, column_ ); }
+      RangeFieldType operator[] ( unsigned int row ) const { return localMatrix_.get( row, col_ ); }
 
-      LocalMatrixEntry< LocalMatrix > operator[] ( unsigned int row ) { return LocalMatrixEntry( localMatrix_, row, col_ ); }
+      LocalMatrixEntry< LocalMatrixType > operator[] ( unsigned int row ) { return LocalMatrixEntry< LocalMatrixType >( localMatrix_, row, col_ ); }
 
       template< class Point, class... Factor >
       void axpy ( const Point &x, Factor &&... factor )
@@ -75,11 +75,11 @@ namespace Dune
         basisFunctionSet().axpy( x, std::forward< Factor >( factor )..., *this );
       }
 
-      BasisFunctionSetType &basisFunctionSet () const { return localMatrix_.rangeBasisFunctionSet(); }
+      const BasisFunctionSetType &basisFunctionSet () const { return localMatrix_.rangeBasisFunctionSet(); }
 
     private:
       LocalMatrixType &localMatrix_;
-      unsigned int column_;
+      unsigned int col_;
     };
 
   } // namespace Fem
