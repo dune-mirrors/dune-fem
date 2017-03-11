56b16e24779a9bea33282f67af955cc9382cc437
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2017-03-11 15:45:23 +0100

    handle "dot" in latex output

--- dune/ufl/latex.py
+++ dune/ufl/latex.py
@@ -35,6 +35,9 @@ class Expr2Latex(Transformer):
     def division(self, expr, left, right):
         return "\\frac{" + left + "}{" + right + "}"
 
+    def dot(self, expr, left, right):
+        return left + "\\cdot" + right
+
     def float_value(self, expr):
         value = expr.value();
         if value < 0:
