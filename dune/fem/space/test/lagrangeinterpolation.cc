0c7eb8f812e415ef150f52d73453aa309b50f1e3
Robert K <robertk@posteo.org>
2017-04-28 13:01:58 +0200

    [bugfix] make compile.

--- dune/fem/space/test/lagrangeinterpolation.cc
+++ dune/fem/space/test/lagrangeinterpolation.cc
@@ -369,7 +369,7 @@ double algorithm( MyGridType &grid, DiscreteFunctionType &solution, int turn )
 
   GridPartType part( grid );
   DiscreteFunctionSpaceType discreteFunctionSpace( part );
-  ExactSolution f;
+  const ExactSolution f;
 
   //! perform Lagrange interpolation
   interpolate( gridFunctionAdapter( f, part, discreteFunctionSpace.order() + 2 ), solution );
