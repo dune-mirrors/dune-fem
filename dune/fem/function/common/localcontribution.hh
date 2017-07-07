86df5437c2e8c88d5865cb9181a49491ef0b24a6
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2017-07-07 22:18:07 +0200

    [bugfix] remove legacy forward declaration

--- dune/fem/function/common/localcontribution.hh
+++ dune/fem/function/common/localcontribution.hh
@@ -24,14 +24,6 @@ namespace Dune
 
 
 
-    // Internal Forward Declarations
-    // -----------------------------
-
-    template< class DiscreteFunction, template< class > class AssemblyOperation >
-    class LocalContribution;
-
-
-
     namespace Assembly
     {
 
