77bb57d200b60723992cc34bf11bde3e484da508
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2017-09-15 14:16:19 +0200

    generalize vectorial expressions to general FieldVector "scalars"

--- dune/fem/storage/subvector.hh
+++ dune/fem/storage/subvector.hh
@@ -1,6 +1,8 @@
 #ifndef DUNE_FEM_SUBVECTOR_HH
 #define DUNE_FEM_SUBVECTOR_HH
 
+#include <cassert>
+
 #include <algorithm>
 
 #include <dune/common/densevector.hh>
@@ -13,11 +15,23 @@ namespace Dune
 
   namespace Fem
   {
-    // forward declaration
-    template< class K, class M > class SubVector;
-  }
 
-  // specialization of DenseMatVecTraits for SubVector
+    // Internal Forward Declarations
+    // -----------------------------
+
+    template< class K, class M >
+    class SubVector;
+
+    template< class BaseMatrix, class IndexMapper >
+    class SubRowMatrix;
+
+  } // namespace Fem
+
+
+
+  // DenseMatVecTraits for SubVector
+  // -------------------------------
+
   template< class K, class M >
   struct DenseMatVecTraits< Fem::SubVector< K, M > >
   {
@@ -28,12 +42,66 @@ namespace Dune
     typedef std::decay_t< decltype( std::declval< K >()[ std::declval< size_type >() ] ) > value_type;
   };
 
+
+
+  // DenseMatVecTraits for SubRowMatrix
+  // ----------------------------------
+
+  template< class BaseMatrix, class IndexMapper >
+  struct DenseMatVecTraits< Fem::SubRowMatrix< BaseMatrix, IndexMapper > >
+  {
+    typedef Fem::SubRowMatrix< BaseMatrix, IndexMapper > derived_type;
+    typedef BaseMatrix container_type;
+
+    typedef typename DenseMatVecTraits< BaseMatrix >::row_type row_type;
+
+    typedef typename DenseMatVecTraits< BaseMatrix >::row_reference row_reference;
+    typedef typename DenseMatVecTraits< BaseMatrix >::const_row_reference const_row_reference;
+
+    typedef typename DenseMatVecTraits< BaseMatrix >::size_type size_type;
+    typedef typename DenseMatVecTraits< BaseMatrix >::value_type value_type;
+  };
+
+  template< class BaseMatrix, class IndexMapper >
+  struct DenseMatVecTraits< Fem::SubRowMatrix< const BaseMatrix, IndexMapper > >
+  {
+    typedef Fem::SubRowMatrix< BaseMatrix, IndexMapper > derived_type;
+    typedef BaseMatrix container_type;
+
+    typedef typename DenseMatVecTraits< BaseMatrix >::row_type row_type;
+
+    typedef typename DenseMatVecTraits< BaseMatrix >::const_row_reference row_reference;
+    typedef typename DenseMatVecTraits< BaseMatrix >::const_row_reference const_row_reference;
+
+    typedef typename DenseMatVecTraits< BaseMatrix >::size_type size_type;
+    typedef typename DenseMatVecTraits< BaseMatrix >::value_type value_type;
+  };
+
+
+
+  // FieldTraits for SubVector
+  // -------------------------
+
   template< class K, class M >
   struct FieldTraits< Fem::SubVector< K, M > >
   {
     typedef typename FieldTraits< typename DenseMatVecTraits< Fem::SubVector< K, M > >::value_type >::field_type field_type;
     typedef typename FieldTraits< typename DenseMatVecTraits< Fem::SubVector< K, M > >::value_type >::real_type real_type;
   };
+
+
+
+  // FieldTraits for SubRowMatrix
+  // ----------------------------
+
+  template< class BaseMatrix, class IndexMapper >
+  struct FieldTraits< Fem::SubRowMatrix< BaseMatrix, IndexMapper > >
+  {
+    typedef typename FieldTraits< typename DenseMatVecTraits< Fem::SubRowMatrix< BaseMatrix, IndexMapper > >::value_type >::field_type field_type;
+    typedef typename FieldTraits< typename DenseMatVecTraits< Fem::SubRowMatrix< BaseMatrix, IndexMapper > >::value_type >::real_type real_type;
+  };
+
+
 
   namespace Fem
   {
@@ -188,9 +256,20 @@ namespace Dune
         : baseVector_( other.baseVector_ ), indexMapper_( other.indexMapper_ )
       {}
 
+      using BaseType::operator=;
+
       //! Copy entries
       ThisType& operator=( const ThisType & other)
       {
+        assert( size() == other.size() );
+        std::copy( other.begin(), other.end(), this->begin() );
+        return *this;
+      }
+
+      template< class T >
+      ThisType &operator= ( const DenseVector< T > &other )
+      {
+        assert( size() == other.size() );
         std::copy( other.begin(), other.end(), this->begin() );
         return *this;
       }
@@ -218,6 +297,59 @@ namespace Dune
       IndexMapperType indexMapper_;
     };
 
+
+
+    // SubRowMatrix
+    // ------------
+
+    template< class BaseMatrix, class IndexMapper >
+    class SubRowMatrix
+      : public DenseMatrix< SubRowMatrix< BaseMatrix, IndexMapper > >
+    {
+      typedef SubVector< BaseMatrix, IndexMapper > ThisType;
+      typedef DenseMatrix< SubRowMatrix< BaseMatrix, IndexMapper > > BaseType;
+
+    public:
+      typedef typename BaseType::size_type size_type;
+      typedef typename BaseType::value_type value_type;
+
+      typedef typename BaseType::const_row_reference const_row_reference;
+      typedef typename BaseType::row_reference row_reference;
+
+      typedef BaseMatrix BaseMatrixType;
+      typedef IndexMapper IndexMapperType;
+
+      explicit SubRowMatrix ( BaseMatrixType &baseMatrix, IndexMapperType &&indexMapper )
+        : baseMatrix_( baseMatrix ), indexMapper_( std::move( indexMapper ) )
+      {}
+
+      using BaseType::operator=;
+
+      ThisType &operator= ( const ThisType &other )
+      {
+        assert( mat_rows() == other.N() );
+        std::copy( other.begin(), other.end(), this->begin() );
+        return *this;
+      }
+
+      template< class T >
+      ThisType &operator= ( const DenseMatrix< T > &other )
+      {
+        assert( mat_rows() == other.N() );
+        std::copy( other.begin(), other.end(), this->begin() );
+        return *this;
+      }
+
+      const_row_reference mat_access ( size_type i ) const { return baseMatrix_[ indexMapper_[ i ] ]; }
+      row_reference mat_access ( size_type i ) { return baseMatrix_[ indexMapper_[ i ] ]; }
+
+      size_type mat_rows () const { return indexMapper_.size(); }
+      size_type mat_cols () const { return baseMatrix_.N(); }
+
+    private:
+      BaseMatrixType &baseMatrix_;
+      IndexMapperType indexMapper_;
+    };
 
   } // namespace Fem
 
