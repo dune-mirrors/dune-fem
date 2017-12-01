9d83294795771213c6475b48923dd9494451cd3a
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2017-12-01 19:39:06 +0100

    add model compiler for integrands model

--- modules/UseModelCompiler.cmake
+++ modules/UseModelCompiler.cmake
@@ -23,3 +23,18 @@ function(dune_fem_add_elliptic_models)
     set_property(SOURCE ${CMAKE_CURRENT_BINARY_DIR}/${base}.hh PROPERTY GENERATED 1)
   endforeach()
 endfunction()
+
+function(dune_fem_add_integrands_models)
+  foreach(source ${ARGN})
+    get_filename_component(base ${source} NAME_WE)
+    add_custom_command(
+        OUTPUT ${base}.hh
+        DEPENDS ${source}
+        COMMAND ${PYTHON_EXECUTABLE} -c "${DUNE_BUILD_SET_PYTHON_PATH}; from dune.models.integrands.compiler import main; main()" ${source} -o ${CMAKE_CURRENT_BINARY_DIR}/${base}.hh
+        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+        COMMENT "Compiling integrands model ${base}..."
+        VERBATIM
+      )
+    set_property(SOURCE ${CMAKE_CURRENT_BINARY_DIR}/${base}.hh PROPERTY GENERATED 1)
+  endforeach()
+endfunction()
