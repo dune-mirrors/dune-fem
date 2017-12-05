0a905f80ead6591bdb3beeea53608373dfee22e6
Janick Gerstenberger <janick.gerstenberger@gmail.com>
2017-12-05 11:51:00 +0100

    [stencil.hh] make `Partition` a template argument for `DiagonalStencil`, `DiagonalAndNeighborStencil`

--- dune/fem/operator/common/stencil.hh
+++ dune/fem/operator/common/stencil.hh
@@ -181,11 +181,12 @@ namespace Dune
      *  \tparam  RangeSpace   type of discrete function space for the range
      *
      */
-    template <class DomainSpace, class RangeSpace>
+    template <class DomainSpace, class RangeSpace, class Partition = Dune::Partitions::InteriorBorder>
     struct DiagonalStencil : public Stencil<DomainSpace,RangeSpace>
     {
       typedef Stencil<DomainSpace,RangeSpace> BaseType;
     public:
+      typedef Partition                                 PartitionType;
       typedef typename BaseType::DomainEntityType       DomainEntityType;
       typedef typename BaseType::RangeEntityType        RangeEntityType;
       typedef typename BaseType::DomainGlobalKeyType    DomainGlobalKeyType;
@@ -196,7 +197,7 @@ namespace Dune
       DiagonalStencil(const DomainSpace &dSpace, const RangeSpace &rSpace)
         : BaseType( dSpace, rSpace )
       {
-        for (const auto& entity : elements( dSpace.gridPart(), Dune::Partitions::interiorBorder ) )
+        for (const auto& entity : elements( dSpace.gridPart(), PartitionType{} ) )
           BaseType::fill(entity,entity);
       }
     };
@@ -210,11 +211,12 @@ namespace Dune
      *  \tparam  RangeSpace   type of discrete function space for the range
      *
      */
-    template <class DomainSpace, class RangeSpace>
+    template <class DomainSpace, class RangeSpace, class Partition = Dune::Partitions::InteriorBorder>
     struct DiagonalAndNeighborStencil : public Stencil<DomainSpace,RangeSpace>
     {
       typedef Stencil<DomainSpace,RangeSpace> BaseType;
     public:
+      typedef Partition                                 PartitionType;
       typedef typename BaseType::DomainEntityType       DomainEntityType;
       typedef typename BaseType::RangeEntityType        RangeEntityType;
       typedef typename BaseType::DomainGlobalKeyType    DomainGlobalKeyType;
@@ -226,7 +228,7 @@ namespace Dune
                                  bool onlyNonContinuousNeighbors = false)
         : BaseType( dSpace, rSpace )
       {
-        for (const auto & entity: elements( dSpace.gridPart(), Dune::Partitions::interiorBorder ) )
+        for (const auto & entity: elements( dSpace.gridPart(), PartitionType{} ) )
         {
           BaseType::fill(entity,entity);
           for (const auto & intersection: intersections(dSpace.gridPart(), entity) )
