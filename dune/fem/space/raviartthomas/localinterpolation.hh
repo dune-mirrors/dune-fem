8a2305b16b67dc05a058b555a859ebcbf04eaa75
Janick Gerstenberger <janick.gerstenberger@gmail.com>
2018-02-20 16:28:38 +0100

    add alternative local interpolation for `RaviartThomasSpace`
    `RaviartThomasLocalInterpolation` allows independent interpolation for
    the interior and each edge. This is necessary for e.g. a H(div)
    projection of a dg space.

---
+++ dune/fem/space/raviartthomas/localinterpolation.hh
@@ -0,0 +1,348 @@
+#ifndef DUNE_FEM_SPACE_RAVIARTTHOMAS_LOCALINTERPOLATION_HH
+#define DUNE_FEM_SPACE_RAVIARTTHOMAS_LOCALINTERPOLATION_HH
+
+// C++ includes
+#include <cassert>
+#include <vector>
+#include <utility>
+
+// dune-common includes
+#include <dune/common/fvector.hh>
+#include <dune/common/typetraits.hh>
+
+// dune-geometry includes
+#include <dune/geometry/type.hh>
+
+// dune-fem includes
+#include <dune/fem/space/basisfunctionset/piolatransformation.hh>
+
+
+namespace Dune
+{
+
+  namespace Fem
+  {
+
+    namespace Impl
+    {
+
+      // forward declarations
+      // --------------------
+
+      template< unsigned int, class, class, int, int > struct RaviartThomasLocalFiniteElement;
+
+
+      // RaviartThomasLocalInterpolationBasis
+      // ------------------------------------
+
+      /*
+       *  These are mostly copies from the interpolation implementations in dune-localfunctions
+       *  (dune/localfunctions/raviartthomas/raviartthomas * /raviartthomas * localinterpolation.hh)
+       */
+
+      template< class LocalFiniteElement >
+      struct RaviartThomasLocalInterpolationBasis
+      {
+        static_assert( AlwaysFalse< LocalFiniteElement >::value,
+                       "`RaviartThomasLocalInterpolationBasis` not implemented for your choice." );
+      };
+
+
+      // 0th order
+      template< unsigned int id, class Domain, class Range, int dim >
+      struct RaviartThomasLocalInterpolationBasis< RaviartThomasLocalFiniteElement< id, Domain, Range, dim, 0 > >
+      {
+        using DomainType = FieldVector< Domain, dim >;
+        using FaceDomainType = FieldVector< Domain, dim-1 >;
+        using RangeType = FieldVector< Range, dim >;
+        using RangeFieldType = Range;
+
+        RaviartThomasLocalInterpolationBasis () = default;
+        explicit RaviartThomasLocalInterpolationBasis ( unsigned int orientations ) : orientations_( orientations ) {}
+
+        void trace ( int facet, const FaceDomainType& x, std::vector< std::pair< int, RangeFieldType > >& basis ) const
+        {
+          assert( basis.size() >= size( 1 ) );
+          basis[ 0 ] = std::make_pair( facet, sign( facet ) );
+        }
+
+        void interior ( const DomainType& x, std::vector< std::pair< int, RangeType > >& basis ) const {}
+
+        constexpr auto size ( int codim ) const -> std::size_t { assert( codim < 2 ); return (codim == 0) ? 0 : 1; }
+        constexpr int order ( int codim ) const { assert( codim < 2 ); return (codim == 0) ? 0 : 1; }
+
+      private:
+        auto sign ( int facet ) const -> RangeFieldType { return (orientations_ & (1u << facet)) ? -1.0 : 1.0; }
+
+        unsigned int orientations_ = 0;
+      };
+
+      // 1st order, 2d Simplex
+      template< class Domain, class Range >
+      struct RaviartThomasLocalInterpolationBasis< RaviartThomasLocalFiniteElement< Dune::Impl::SimplexTopology< 2 >::type::id, Domain, Range, 2, 1 > >
+      {
+        using DomainType = FieldVector< Domain, 2 >;
+        using FaceDomainType = FieldVector< Domain, 1 >;
+        using RangeType = FieldVector< Range, 2 >;
+        using RangeFieldType = Range;
+
+        RaviartThomasLocalInterpolationBasis () = default;
+        explicit RaviartThomasLocalInterpolationBasis ( unsigned int orientations ) : orientations_( orientations ) {}
+
+        void trace ( int facet, const FaceDomainType& x, std::vector< std::pair< int, RangeFieldType > >& basis ) const
+        {
+          assert( basis.size() >= size( 1 ) );
+          const RangeFieldType temp = (facet==1) ? 1.0 - 2.0*x : 2.0*x - 1.0;
+          basis[ 0 ] = std::make_pair( facet, sign( facet ) );
+          basis[ 1 ] = std::make_pair( facet+3, temp );
+        }
+
+        void interior ( const DomainType& x, std::vector< std::pair< int, RangeType > >& basis ) const
+        {
+          assert( basis.size() >= size( 0 ) );
+          basis[ 0 ] = std::make_pair( 6, RangeType{ 1.0, 0.0 } );
+          basis[ 1 ] = std::make_pair( 7, RangeType{ 0.0, 1.0 } );
+        }
+
+        constexpr auto size ( int codim ) const -> std::size_t { assert( codim < 2 ); return (codim == 0) ? 2 : 2; }
+        constexpr int order ( int codim ) const { assert( codim < 2 ); return (codim == 0) ? 8 : 4; }
+
+
+      private:
+        auto sign ( int facet ) const -> RangeFieldType { return (orientations_ & (1u << facet)) ? -1.0 : 1.0; }
+
+        unsigned int orientations_ = 0;
+      };
+
+      // 1st order, 2d Cube
+      template< class Domain, class Range >
+      struct RaviartThomasLocalInterpolationBasis< RaviartThomasLocalFiniteElement< Dune::Impl::CubeTopology< 2 >::type::id, Domain, Range, 2, 1 > >
+      {
+        using DomainType = FieldVector< Domain, 2 >;
+        using FaceDomainType = FieldVector< Domain, 1 >;
+        using RangeType = FieldVector< Range, 2 >;
+        using RangeFieldType = Range;
+
+        RaviartThomasLocalInterpolationBasis () = default;
+        explicit RaviartThomasLocalInterpolationBasis ( unsigned int orientations ) : orientations_( orientations ) {}
+
+        void trace ( int facet, const FaceDomainType& x, std::vector< std::pair< int, RangeFieldType > >& basis ) const
+        {
+          assert( basis.size() >= size( 1 ) );
+          const RangeFieldType temp = (facet > 0 && facet < 3 ) ? 1.0 - 2.0*x : 2.0*x - 1.0;
+          basis[ 0 ] = std::make_pair( 2*facet  , sign( facet ) );
+          basis[ 1 ] = std::make_pair( 2*facet+1, temp );
+        }
+
+        void interior ( const DomainType& x, std::vector< std::pair< int, RangeType > >& basis ) const
+        {
+          assert( basis.size() >= size( 0 ) );
+          basis[ 0 ] = std::make_pair(  8, RangeType{  1.0,  0.0 } );
+          basis[ 1 ] = std::make_pair(  9, RangeType{  0.0,  1.0 } );
+          basis[ 2 ] = std::make_pair( 10, RangeType{ x[1],  0.0 } );
+          basis[ 3 ] = std::make_pair( 11, RangeType{  0.0, x[0] } );
+        }
+
+        constexpr auto size ( int codim ) const -> std::size_t { assert( codim < 2 ); return (codim == 0) ? 4 : 2; }
+        constexpr int order ( int codim ) const { assert( codim < 2 ); return (codim == 0) ? 3 : 3; }
+
+      private:
+        auto sign ( int facet ) const -> RangeFieldType { return (orientations_ & (1u << facet)) ? -1.0 : 1.0; }
+
+        unsigned int orientations_ = 0;
+      };
+
+      // 1st order, 3d Cube
+      template< class Domain, class Range >
+      struct RaviartThomasLocalInterpolationBasis< RaviartThomasLocalFiniteElement< Dune::Impl::CubeTopology< 3 >::type::id, Domain, Range, 3, 1 > >
+      {
+        using DomainType = FieldVector< Domain, 3 >;
+        using FaceDomainType = FieldVector< Domain, 2 >;
+        using RangeType = FieldVector< Range, 3 >;
+        using RangeFieldType = Range;
+
+        RaviartThomasLocalInterpolationBasis () = default;
+        explicit RaviartThomasLocalInterpolationBasis ( unsigned int orientations ) : orientations_( orientations ) {}
+
+        void trace ( int facet, const FaceDomainType& x, std::vector< std::pair< int, RangeFieldType > >& basis ) const
+        {
+          assert( basis.size() >= size( 1 ) );
+
+          const RangeFieldType tempX = 2.0*x[0] - 1.0;
+          const RangeFieldType tempY = 2.0*x[1] - 1.0;
+
+          basis[ 0 ] = std::make_pair( facet, sign( facet ) );
+
+          switch( facet )
+          {
+            case 0:
+            case 5:
+              basis[ 1 ] = std::make_pair( facet+ 6,        tempX );
+              basis[ 2 ] = std::make_pair( facet+12,        tempY );
+              basis[ 3 ] = std::make_pair( facet+18,  tempX*tempY );
+              break;
+            case 1:
+            case 4:
+              basis[ 1 ] = std::make_pair( facet+ 6,       -tempX );
+              basis[ 2 ] = std::make_pair( facet+12,       -tempY );
+              basis[ 3 ] = std::make_pair( facet+18, -tempX*tempY );
+              break;
+            case 2:
+              basis[ 1 ] = std::make_pair( facet+ 6,       -tempX );
+              basis[ 2 ] = std::make_pair( facet+12,        tempY );
+              basis[ 3 ] = std::make_pair( facet+18, -tempX*tempY );
+              break;
+            case 3:
+              basis[ 1 ] = std::make_pair( facet+ 6,        tempX );
+              basis[ 2 ] = std::make_pair( facet+12,       -tempY );
+              basis[ 3 ] = std::make_pair( facet+18,  tempX*tempY );
+              break;
+          }
+        }
+
+        void interior ( const DomainType& x, std::vector< std::pair< int, RangeType > >& basis ) const
+        {
+          assert( basis.size() >= size( 0 ) );
+          basis[  0 ] = std::make_pair( 24, RangeType{       1.0,       0.0,       0.0 } );
+          basis[  1 ] = std::make_pair( 25, RangeType{       0.0,       1.0,       0.0 } );
+          basis[  2 ] = std::make_pair( 26, RangeType{       0.0,       0.0,       1.0 } );
+          basis[  3 ] = std::make_pair( 27, RangeType{      x[1],       0.0,       0.0 } );
+          basis[  4 ] = std::make_pair( 28, RangeType{      x[2],       0.0,       0.0 } );
+          basis[  5 ] = std::make_pair( 29, RangeType{       0.0,      x[0],       0.0 } );
+          basis[  6 ] = std::make_pair( 30, RangeType{       0.0,      x[2],       0.0 } );
+          basis[  7 ] = std::make_pair( 31, RangeType{       0.0,       0.0,      x[0] } );
+          basis[  8 ] = std::make_pair( 32, RangeType{       0.0,       0.0,      x[1] } );
+          basis[  9 ] = std::make_pair( 33, RangeType{ x[1]*x[2],       0.0,       0.0 } );
+          basis[ 10 ] = std::make_pair( 34, RangeType{       0.0, x[0]*x[2],       0.0 } );
+          basis[ 11 ] = std::make_pair( 35, RangeType{       0.0,       0.0, x[1]*x[2] } );
+        }
+
+        constexpr auto size ( int codim ) const -> std::size_t { assert( codim < 2 ); return (codim == 0) ? 12 : 4; }
+        constexpr int order ( int codim ) const { assert( codim < 2 ); return (codim == 0) ? 3 : 3; }
+
+      private:
+        auto sign ( int facet ) const -> RangeFieldType { return (orientations_ & (1u << facet)) ? -1.0 : 1.0; }
+
+        unsigned int orientations_ = 0;
+      };
+
+    }
+
+    template< class GridPart, class LocalFiniteElement, int dimRange = GridPart::dimension >
+    class RaviartThomasLocalInterpolation
+    {
+    public:
+      using GridPartType = GridPart;
+      using LocalFiniteElementType = LocalFiniteElement;
+
+      using EntityType = typename GridPartType::template Codim< 0 >::EntityType;
+
+    protected:
+      using LocalInterpolationBasisType = Impl::RaviartThomasLocalInterpolationBasis< LocalFiniteElementType >;
+
+      using Geometry = typename EntityType::Geometry;
+      using ReferenceElementType = ReferenceElement< Geometry >;
+
+      using TransformationType = PiolaTransformation< Geometry, dimRange >;
+      using InverseTransformationType = typename TransformationType::InverseTransformationType;
+
+      using RangeType = typename LocalInterpolationBasisType::RangeType;
+      using RangeFieldType = typename LocalInterpolationBasisType::RangeFieldType;
+
+      using VolumeQuadratures = QuadratureRules< typename Geometry::ctype, ReferenceElementType::dimension >;
+      using FaceQuadratures = QuadratureRules< RangeFieldType, ReferenceElementType::dimension-1 >;
+
+    public:
+      explicit RaviartThomasLocalInterpolation ( const EntityType& entity )
+        : RaviartThomasLocalInterpolation( entity, 0, -1 )
+      {}
+
+      RaviartThomasLocalInterpolation ( const EntityType& entity, unsigned int orientations, int order = -1 )
+        : geometry_( entity.geometry() ),
+          refElement_( Dune::referenceElement( geometry_ ) ),
+          localBasis_( orientations ),
+          order_( order )
+      {}
+
+      template< class LocalFunction, class LocalDofVector >
+      void interior ( const LocalFunction& lf, LocalDofVector& dofs ) const
+      {
+        if ( !hasInterior() )
+          return;
+
+        assert( dofs.size() >= localBasis().size( 0 ) + referenceElement().size( 1 ) * localBasis().size( 1 ) );
+        interior_.resize( localBasis().size( 0 ) );
+
+        for( const auto& qp : getQuadrature( (order_ == -1) ? localBasis().order( 0 ) : order_ ) )
+        {
+          auto invPiola = inverseTransformation( qp.position() );
+
+          localBasis().interior( qp.position(), interior_ );
+          auto value = invPiola.apply( lf( qp.position() ) );
+
+          for ( const auto& base : interior_ )
+            dofs[ base.first ] += qp.weight() * (value * base.second);
+        }
+      }
+
+      template< class LocalFunction, class LocalDofVector >
+      void trace ( int facet, const LocalFunction& lf, LocalDofVector& dofs ) const
+      {
+        assert( dofs.size() >= localBasis().size( 0 ) + referenceElement().size( 1 ) * localBasis().size( 1 ) );
+        traces_.resize( localBasis().size( 1 ) );
+
+        auto embedding = referenceElement().template geometry< 1 >( facet );
+        auto normal = referenceElement().integrationOuterNormal( facet );
+
+        for ( const auto& qp : getQuadrature( facet, (order_ == -1) ? localBasis().order( 1 ) : order_ ) )
+        {
+          auto p = embedding.global( qp.position() );
+          auto invPiola = inverseTransformation( p );
+
+          localBasis().trace( facet, qp.position(), traces_ );
+          auto value = invPiola.apply( lf( p ) );
+
+          for ( const auto& base : traces_ )
+            dofs[ base.first ] += qp.weight() * (value * normal) * base.second;
+        }
+      }
+
+      template< class LocalFunction, class LocalDofVector >
+      void operator() ( const LocalFunction& lf, LocalDofVector& dofs ) const
+      {
+        for ( int facet : range( referenceElement().size( 1 ) ) )
+          trace( facet, lf, dofs );
+        interior( lf, dofs );
+      }
+
+      bool hasInterior () const { return localBasis().size( 0 ) > 0; }
+
+    protected:
+      auto geometry () const -> const Geometry& { return geometry_; }
+      auto referenceElement () const -> const ReferenceElementType& { return refElement_; }
+      auto localBasis () const -> const LocalInterpolationBasisType& { return localBasis_; }
+
+      template< class Point >
+      auto transformation ( const Point& p ) const { return TransformationType( geometry(), p ); }
+
+      template< class Point >
+      auto inverseTransformation ( const Point& p ) const { return InverseTransformationType( geometry(), p ); }
+
+      auto getQuadrature ( int order ) const { return VolumeQuadratures::rule( referenceElement().type(), order ); }
+      auto getQuadrature ( int facet, int order ) const { return FaceQuadratures::rule( referenceElement().type( facet, 1 ), order ); }
+
+    private:
+      Geometry geometry_;
+      ReferenceElementType refElement_;
+      LocalInterpolationBasisType localBasis_;
+      const int order_;
+
+      mutable std::vector< std::pair< int, RangeFieldType > > traces_;
+      mutable std::vector< std::pair< int, RangeType  > > interior_;
+    };
+
+  } // namespace Fem
+
+} // namespace Dune
+
+#endif // #ifndef DUNE_FEM_SPACE_RAVIARTTHOMAS_LOCALINTERPOLATION_HH
---
+++ dune/fem/space/test/test-raviartthomasinterpolation.cc
@@ -0,0 +1,269 @@
+#include <config.h>
+
+#include <cmath>
+
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+#include <dune/common/classname.hh>
+#include <dune/common/ftraits.hh>
+#include <dune/common/hybridutilities.hh>
+
+#include <dune/grid/io/file/dgfparser/dgfparser.hh>
+
+#include <dune/fem/common/localcontribution.hh>
+#include <dune/fem/common/bindguard.hh>
+#include <dune/fem/function/adaptivefunction.hh>
+#include <dune/fem/function/common/gridfunctionadapter.hh>
+#include <dune/fem/function/common/localcontribution.hh>
+#include <dune/fem/gridpart/leafgridpart.hh>
+#include <dune/fem/misc/mpimanager.hh>
+#include <dune/fem/space/common/interpolate.hh>
+#include <dune/fem/space/common/uniquefacetorientation.hh>
+#include <dune/fem/test/exactsolution.hh>
+
+#include <dune/fem/misc/l2norm.hh>
+#include <dune/fem/misc/h1norm.hh>
+
+#include <dune/fem/space/raviartthomas/space.hh>
+#include <dune/fem/space/raviartthomas/localinterpolation.hh>
+#include <dune/fem/space/test/checklocalinterpolation.hh>
+
+// dgfUnitCube
+// -----------
+
+inline static std::string dgfUnitCube ( int dimWorld, int cells )
+{
+  std::string dgf = "DGF\nINTERVAL\n";
+  for( int i = 0; i < dimWorld; ++i )
+    dgf += " 0";
+  dgf += "\n";
+  for( int i = 0; i < dimWorld; ++i )
+    dgf += " 1";
+  dgf += "\n";
+  for( int i = 0; i < dimWorld; ++i )
+    dgf += (" " + std::to_string( cells ));
+  dgf += "\n#\n";
+  return dgf;
+}
+
+
+
+// Real
+// ----
+
+template< class T >
+using Real = typename Dune::FieldTraits< typename T::RangeFieldType >::real_type;
+
+
+
+// ErrorTuple
+// ----------
+
+template< class T >
+struct ErrorTuple;
+
+template< class... T >
+struct ErrorTuple< std::tuple< T... > >
+{
+  typedef std::tuple< std::pair< Real< T >, Real< T > >... > Type;
+};
+
+
+
+// Type Definitions
+// ----------------
+
+typedef Dune::GridSelector::GridType GridType;
+typedef Dune::Fem::LeafGridPart< GridType > GridPartType;
+
+static const int dimRange = GridPartType::dimensionworld;
+
+typedef Dune::Fem::GridFunctionSpace< GridPartType, Dune::FieldVector< typename GridPartType::ctype, dimRange > > FunctionSpaceType;
+
+typedef std::tuple<
+    Dune::Fem::RaviartThomasSpace< FunctionSpaceType, GridPartType, 0 >,
+    Dune::Fem::RaviartThomasSpace< FunctionSpaceType, GridPartType, 1 >
+  > DiscreteFunctionSpacesType;
+
+typedef ErrorTuple< DiscreteFunctionSpacesType >::Type ErrorTupleType;
+
+
+// AlternateInterpolation
+// ----------------------
+
+template< class DiscreteFunctionSpace >
+struct AlternateInterpolation
+{
+  using GridPartType = typename DiscreteFunctionSpace::GridPartType;
+  using FunctionSpaceType = typename DiscreteFunctionSpace::FunctionSpaceType;
+  using LocalFiniteElementType = typename DiscreteFunctionSpace::LocalFiniteElementType;
+
+  using LocalInterpolationType =
+    Dune::Fem::RaviartThomasLocalInterpolation< GridPartType, LocalFiniteElementType, FunctionSpaceType::dimRange >;
+
+  explicit AlternateInterpolation ( const GridPartType& gridPart )
+    : orientations_( gridPart )
+  {}
+
+  template< class DomainFunction, class RangeFunction >
+  void operator() ( const DomainFunction& u, RangeFunction& v ) const
+  {
+    v.clear();
+
+    Dune::Fem::ConstLocalFunction< DomainFunction > uLocal( u );
+    Dune::Fem::SetLocalContribution< RangeFunction > vLocal( v );
+
+    for ( const auto& entity : Dune::elements( u.space().gridPart(), Dune::Partitions::all ) )
+    {
+      auto guard = bindGuard( std::tie( uLocal, vLocal ), entity );
+      auto interpolation = localInterpolation( entity );
+
+      interpolation( [&] ( const auto& x ) { return uLocal.evaluate( x ); }, vLocal );
+    }
+  }
+
+  template< class Entity >
+  auto localInterpolation ( const Entity& entity ) const
+  {
+    return LocalInterpolationType( entity, orientations_( entity ) );
+  }
+
+private:
+  Dune::Fem::UniqueFacetOrientation< GridPartType > orientations_;
+};
+
+// checkLocalInterpolation
+// -----------------------
+
+template< class DiscreteFunctionSpace, class Interpolation >
+void checkLocalInterpolation ( const DiscreteFunctionSpace& space, const Interpolation& interpolation )
+{
+  std::cout << ">>> Testing interpolation " << Dune::className< Interpolation >()
+            << " of " << Dune::className< DiscreteFunctionSpace >() << ":" << std::endl;
+
+  for( auto entity : space )
+  {
+    auto bSet = space.basisFunctionSet( entity );
+    auto interpolation_ = interpolation.localInterpolation( entity );
+
+    for( std::size_t i = 0; i < bSet.size(); ++i )
+    {
+      LocalBasis< DiscreteFunctionSpace > local( bSet, i );
+
+      std::vector< typename DiscreteFunctionSpace::RangeFieldType > phii( bSet.size(), 0 );
+      interpolation_( [&] ( const auto& x ) { return local.evaluate( x ); }, phii );
+
+      for( std::size_t j = 0; j < phii.size(); ++j )
+        if( std::abs( phii[ j ] -( i == j ))  > 1e-8 )
+        {
+          std::cerr << "Local interpolation error for basis function: "<< j <<" with diff: " <<  phii[ j ] <<  std::endl;
+          std::abort();
+        }
+    }
+  }
+  std::cout <<"... done." <<std::endl;
+}
+
+// algorithm
+// ---------
+
+template< class DiscreteFunctionSpace >
+std::pair< Real< DiscreteFunctionSpace >, Real< DiscreteFunctionSpace > >
+algorithm ( typename DiscreteFunctionSpace::GridPartType &gridPart )
+{
+  DiscreteFunctionSpace space( gridPart );
+  Dune::Fem::AdaptiveDiscreteFunction< DiscreteFunctionSpace > u( "solution", space );
+
+  // interpolate a function
+  Dune::Fem::ExactSolution< typename DiscreteFunctionSpace::FunctionSpaceType > uExact;
+  const auto uGridExact = gridFunctionAdapter( "exact solution", uExact, gridPart, 3 );
+
+  AlternateInterpolation< DiscreteFunctionSpace > interpolation( gridPart );
+  interpolation( uGridExact, u );
+
+  checkLocalInterpolation( space, interpolation );
+
+  Dune::Fem::L2Norm< GridPartType > l2norm( gridPart );
+  Dune::Fem::H1Norm< GridPartType > h1norm( gridPart );
+
+  return std::make_pair( l2norm.distance( uGridExact, u ), h1norm.distance( uGridExact, u ) );
+}
+
+
+
+// eoc
+// ---
+
+template< class T, class U >
+std::pair< T, U > eoc ( const std::pair< T, U > &e_old, const std::pair< T, U > &e_new )
+{
+  using std::log;
+  return std::make_pair( log( e_old.first / e_new.first ) / log( 2 ), log( e_old.second / e_new.second ) / log( 2 ) );
+}
+
+
+// print
+// -----
+
+template< class L2Error, class L2Eoc, class H1Error, class H1Eoc >
+void print ( L2Error &&l2Error, L2Eoc &&l2Eoc, H1Error &&h1Error, H1Eoc &&h1Eoc )
+{
+  std::cout << std::setw( 12 ) << std::setprecision( 6 ) << l2Error;
+  std::cout << std::setw( 12 ) << std::setprecision( 2 ) << l2Eoc;
+  std::cout << std::setw( 12 ) << std::setprecision( 6 ) << h1Error;
+  std::cout << std::setw( 12 ) << std::setprecision( 2 ) << h1Eoc;
+  std::cout << std::endl;
+}
+
+
+
+// main
+// ----
+
+int main ( int argc, char **argv )
+{
+  Dune::Fem::MPIManager::initialize( argc, argv );
+
+  // construct unit cube
+  typedef typename Dune::GridSelector::GridType GridType;
+  std::istringstream dgf( dgfUnitCube( GridType::dimensionworld, 4 ) );
+  Dune::GridPtr< GridType > grid( dgf );
+
+  // create leaf grid part
+  typedef Dune::Fem::LeafGridPart< GridType > GridPartType;
+  GridPartType gridPart( *grid );
+
+  auto indices = std::make_index_sequence< std::tuple_size< DiscreteFunctionSpacesType >::value >();
+
+  std::array< ErrorTupleType, 4 > errors;
+  for( ErrorTupleType &e : errors )
+  {
+    Dune::Hybrid::forEach( indices, [ &gridPart, &e ] ( auto &&idx ) {
+        const std::size_t i = std::decay_t< decltype( idx ) >::value;
+        std::get< i >( e ) = algorithm< std::tuple_element_t< i, DiscreteFunctionSpacesType > >( gridPart );
+      } );
+    grid->globalRefine(1);
+  }
+
+  Dune::Hybrid::forEach( indices, [ &errors ] ( auto &&idx ) {
+      const std::size_t i = std::decay_t< decltype( idx ) >::value;
+      std::cout << ">>> Testing " << Dune::className< std::tuple_element_t< i, DiscreteFunctionSpacesType > >() << ":" << std::endl;
+      std::cout << std::endl;
+      print( "L2 Error", "L2 EOC", "H1 Error", "H1 EOC" );
+      print( std::get< i >( errors[ 0 ] ).first, "---", std::get< i >( errors[ 0 ] ).second, "---" );
+      for( std::size_t j = 1; j < errors.size(); ++j )
+      {
+        auto eocs = eoc( std::get< i >( errors[ j-1 ] ), std::get< i >( errors[ j ] ) );
+        print( std::get< i >( errors[ j ] ).first, eocs.first, std::get< i >( errors[ j ] ).second, eocs.second );
+      }
+      std::cout << std::endl;
+    } );
+
+  return 0;
+}
