04e389294405c612bf3a1da65129ba87162f929b
Robert K <robertk@posteo.org>
2018-06-04 11:04:10 +0000

    Merge branch 'bugfix/istl-operator' into 'master'
    [bugfix][ISTLLinearOperator] Also provide types of local blocks as
    
    See merge request dune-fem/dune-fem!231
    
    (cherry picked from commit 0817cfa56e1686b8050b1f2634dee31eef39a426)
    
    a80ab97d [bugfix][ISTLLinearOperator] Also provide types of local blocks as
    cf011be9 [cleanup][Double] has_nan --> HasNan
    e5da0c22 [bugfix][ISTL-AMG+UMFPack] disable UMFPack when matrix block is not
    333f73a7 [bugfix][ISTLMatrix] change storage type for LocalMatrix pointers to

--- dune/fem/operator/linear/istloperator.hh
+++ dune/fem/operator/linear/istloperator.hh
@@ -14,8 +14,10 @@ namespace Dune
 
   namespace Fem
   {
+    template< class DomainFunction, class RangeFunction >
+    struct ISTLLinearOperator;
 
-    //! ISTLMatrixOperator
+    //! ISTLMatrixOperator (any discrete function, ISTL spec below)
     template< class DomainFunction, class RangeFunction >
     struct ISTLLinearOperator
     : public ISTLMatrixObject< typename DomainFunction::DiscreteFunctionSpaceType, typename RangeFunction::DiscreteFunctionSpaceType >,
@@ -25,6 +27,59 @@ namespace Dune
       typedef typename RangeFunction::DiscreteFunctionSpaceType RangeSpaceType;
       typedef ISTLLinearOperator< DomainFunction, RangeFunction > ThisType;
       typedef ISTLMatrixObject< DomainSpaceType, RangeSpaceType > BaseType;
+
+      static constexpr bool assembled = true;
+
+      using BaseType::apply;
+      using BaseType::communicate;
+
+      //! constructor
+      //! \param domainSpace space defining domain of operator
+      //! \param rangeSpace  space defining range of operator
+      //! \param param ISTL matrix parameters for preconditioning
+      //!         - Preconditioning: {0,1,2,3,4,5,6} put -1 to get info
+      //!         - Pre-iteration: number of iteration of preconditioner
+      //!         - Pre-relaxation: relaxation factor
+      ISTLLinearOperator( const std::string & ,
+                          const DomainSpaceType &domainSpace,
+                          const RangeSpaceType &rangeSpace,
+                          const MatrixParameter& param = ISTLMatrixParameter() )
+        : BaseType( domainSpace, rangeSpace, param )
+      {}
+
+      virtual void operator()( const DomainFunction &arg, RangeFunction &dest ) const
+      {
+        apply( arg, dest );
+      }
+
+      const BaseType &systemMatrix() const
+      {
+        return *this;
+      }
+
+      BaseType &systemMatrix()
+      {
+        return *this;
+      }
+    };
+
+    //! ISTLMatrixOperator
+    template< class DomainFunctionSpace, class RangeFunctionSpace,
+              class DomainBlock, class RangeBlock >
+    struct ISTLLinearOperator< ISTLBlockVectorDiscreteFunction< DomainFunctionSpace, DomainBlock >,
+                               ISTLBlockVectorDiscreteFunction< RangeFunctionSpace, RangeBlock > >
+    : public ISTLMatrixObject< DomainFunctionSpace, RangeFunctionSpace, DomainBlock, RangeBlock >,
+      public AssembledOperator< ISTLBlockVectorDiscreteFunction< DomainFunctionSpace, DomainBlock >,
+                                ISTLBlockVectorDiscreteFunction< RangeFunctionSpace, RangeBlock > >
+    {
+      typedef DomainFunctionSpace DomainSpaceType;
+      typedef RangeFunctionSpace  RangeSpaceType;
+
+      typedef ISTLBlockVectorDiscreteFunction< DomainFunctionSpace, DomainBlock > DomainFunction;
+      typedef ISTLBlockVectorDiscreteFunction< RangeFunctionSpace, RangeBlock >   RangeFunction;
+
+      typedef ISTLLinearOperator< DomainFunction, RangeFunction > ThisType;
+      typedef ISTLMatrixObject< DomainSpaceType, RangeSpaceType, DomainBlock, RangeBlock > BaseType;
 
       static constexpr bool assembled = true;
 
