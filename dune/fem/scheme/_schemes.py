615055098ed0e6c12ca3ade2db43924158ca1d34
dedner <a.s.dedner@warwick.ac.uk>
2018-07-02 22:48:01 +0100

    fix an issue with wrong include list for scheme generation - need to check other generated modules that the includes of parameters are before the includes for the new types

--- dune/fem/scheme/_schemes.py
+++ dune/fem/scheme/_schemes.py
@@ -152,10 +152,10 @@ def galerkin(integrands, space=None, solver=None, parameters={}, virtualize=None
     if virtualize is None:
         virtualize = integrands.virtualized
 
-    includes = ["dune/fem/schemes/galerkin.hh","dune/fem/schemes/dirichletwrapper.hh"]
+    includes = integrands._includes
     includes += space._includes + dfIncludes + solverIncludes
-    includes += integrands._includes
     includes += ["dune/fempy/parameter.hh"]
+    includes += ["dune/fem/schemes/galerkin.hh","dune/fem/schemes/dirichletwrapper.hh"]
 
     spaceType = space._typeName
     valueType = 'std::tuple< typename ' + spaceType + '::RangeType, typename ' + spaceType + '::JacobianRangeType >'
