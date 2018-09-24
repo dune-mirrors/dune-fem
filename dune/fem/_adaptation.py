149b28e83035c361c34d19fd32bea2adae60da27
dedner <a.s.dedner@warwick.ac.uk>
2018-09-24 17:35:16 +0100

    improve dune.fem.adapt function (can simply pass in a single list/tuple or directly dune.fem.adapt(df1,df2,df3,...)

--- dune/fem/_adaptation.py
+++ dune/fem/_adaptation.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 __metaclass__ = type
 
 import hashlib
+import warnings
 
 from dune.generator.generator import SimpleGenerator
 
@@ -24,13 +25,38 @@ def module(grid):
     return module
 
 
-def adapt(grid, *args, **kwargs):
-    module(grid).gridAdaptation(grid).adapt(*args, **kwargs)
+def _adaptArguments(first,*args):
+    try: # first see if first argument is a discrete function (should be only method)
+        hgrid = first.grid.hierarchicalGrid
+        args = list([*args,first])
+    except AttributeError:
+        pass
+    try: # if its not a df test if it is a list/tuple
+        if len(first)>1:
+            assert len(args)==0,\
+                   "only one list of discrete functions can be passed into the adaptation method"
+            hgrid = first[0].grid.hierarchicalGrid
+            args = first
+    except TypeError: # okay apparently its a hgrid object (should be deprecated)
+        hgrid = first
+        if len(args)==1:
+            args = args[0]
+        warnings.warn("""
+              passing in the hierarchical grid as first argument to the
+              'dune.fem.adapt' function is not required and deprecated.
+              """)
+    # make sure all args are over the same grid
+    assert all([a.grid.hierarchicalGrid==hgrid for a in args]),\
+            "all discrete functions must be over the same hierarchical grid"
+    return hgrid,args
 
+def adapt(first, *args):
+    hgrid,args = _adaptArguments(first,*args)
+    module(hgrid).gridAdaptation(hgrid).adapt(args)
 
-def loadBalance(grid, *args, **kwargs):
-    module(grid).gridAdaptation(grid).loadBalance(*args, **kwargs)
-
+def loadBalance(first, *args):
+    hgrid,args = _adaptArguments(first,*args)
+    module(hgrid).gridAdaptation(hgrid).loadBalance(args)
 
 if __name__ == "__main__":
     import doctest
