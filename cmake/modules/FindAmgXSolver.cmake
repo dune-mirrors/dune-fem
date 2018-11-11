d07ffd80c31e8dd92dc116c70fa5b76ff7630cf0
Robert K <robertk@posteo.org>
2018-11-11 21:07:36 +0100

    [feature][cmake] Added checks for ViennaCL and AMGX solver wrapper. Also, a cmake varialbe PETSC_ROOT can now be passed to config.opts

---
+++ cmake/modules/FindAmgXSolver.cmake
@@ -0,0 +1,76 @@
+# - Try to find AmgX
+# Once done this will define
+#
+#  AMGXSOLVER_FOUND        - system has PETSc
+#  AMGXSOLVER_INCLUDES     - the PETSc include directories
+#  AMGXSOLVER_LIBRARIES    - Link these to use PETSc
+#
+# Redistribution and use is allowed according to the terms of the BSD license.
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+#
+
+cmake_policy(VERSION 3.3)
+
+include(DuneMPI)
+
+set(AMGX_VALID_COMPONENTS
+    C
+    CXX)
+
+SET(CUDA_DIR "$ENV{CUDA_DIR}" CACHE PATH "The path to CUDA.")
+find_package(CUDA)
+
+if(${CUDA_FOUND})
+    SET(CUDA_LIBRARIES ${CUDA_LIBRARIES} ${CUDA_CUBLAS_LIBRARIES} ${CUDA_cusparse_LIBRARY} ${CUDA_cusolver_LIBRARY})
+    #MESSAGE("-- Finding CUDA - Success")
+    message("Cuda = ${CUDA_LIBRARY_DIRS} ${CUDA_LIBRARIES}")
+
+# search AMGX header
+find_path(AMGX_INCLUDE_DIR amgx_c.h amgx_capi.h
+  PATHS ${AMGX_ROOT} ${AMGX_DIR}
+  PATH_SUFFIXES include include/amgx
+  NO_DEFAULT_PATH
+  DOC "Include directory of AMGX")
+
+find_library(AMGX_LIBRARY
+    NAMES "amgx" "amgxsh"
+    PATHS ${AMGX_ROOT}
+    PATH_SUFFIXES "lib" "lib" "lib/${CMAKE_LIBRARY_ARCHITECTURE}"
+    NO_DEFAULT_PATH)
+
+find_path(AMGX_WRAPPER_INCLUDE_DIR AmgXSolver.hpp
+  PATHS ${AMGX_WRAPPER_ROOT} ${AMGX_WRAPPER_DIR}
+  PATH_SUFFIXES include
+  NO_DEFAULT_PATH
+  DOC "Include directory of AMGX_WRAPPER")
+
+find_library(AMGX_WRAPPER_LIBRARY
+    NAMES "AmgXWrapper"
+    PATHS ${AMGX_WRAPPER_ROOT}
+    PATH_SUFFIXES "lib" "lib" "lib/${CMAKE_LIBRARY_ARCHITECTURE}"
+    NO_DEFAULT_PATH)
+
+message("amg = ${AMGX_LIBRARY}")
+message("amg wrapper = ${AMGX_WRAPPER_LIBRARY}")
+
+if( AMGX_WRAPPER_LIBRARY )
+  set(AMGXSOLVER_INCLUDE_DIRS ${AMGX_WRAPPER_INCLUDE_DIR} ${AMGX_INCLUDE_DIR})
+  #  set(AMGXSOLVER_LIBRARIES ${AMGX_WRAPPER_LIBRARY} ${AMGX_LIBRARY} ${CUDA_LIBRARY_DIRS})
+  set(AMGXSOLVER_LIBRARIES ${AMGXSOLVER_LIBRARIES} ${AMGX_WRAPPER_LIBRARY})
+  set(AMGXSOLVER_LIBRARIES ${AMGXSOLVER_LIBRARIES} ${AMGX_LIBRARY})
+  set(AMGXSOLVER_LIBRARIES ${AMGXSOLVER_LIBRARIES} ${CUDA_LIBRARIES})
+  set(AMGXSOLVER_FOUND TRUE)
+  message("AMG Libraries = ${AMGXSOLVER_LIBRARIES}")
+endif()
+
+#set HAVE_AMGXSOLVER for config.h
+set(HAVE_AMGXSOLVER ${AMGXSOLVER_FOUND})
+
+# register all AMGX related flags
+if(AMGXSOLVER_FOUND)
+  dune_register_package_flags(LIBRARIES "${AMGXSOLVER_LIBRARIES}"
+                              INCLUDE_DIRS "${AMGXSOLVER_INCLUDE_DIRS}")
+endif()
+
+# endif CUDA_FOUND
+endif()
---
+++ cmake/modules/FindViennaCL.cmake
@@ -0,0 +1,47 @@
+# - Try to find AmgX
+# Once done this will define
+#
+#  VIENNACL_FOUND        - system has ViennaCL
+#  VIENNACL_INCLUDES     - the ViennaCL include directories
+#
+# Redistribution and use is allowed according to the terms of the BSD license.
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+#
+
+cmake_policy(VERSION 3.3)
+
+include(DuneMPI)
+
+find_package(OpenCL)
+
+# search VIENNACL header
+find_path(VIENNACL_INCLUDE_DIR version.hpp
+  PATHS ${VIENNACL_ROOT} ${VIENNACL_DIR}
+  PATH_SUFFIXES include include/viennacl
+  NO_DEFAULT_PATH
+  DOC "Include directory of VIENNACL")
+
+if( VIENNACL_INCLUDE_DIR )
+  set(VIENNACL_INCLUDE_DIRS ${VIENNACL_INCLUDE_DIR})
+
+  set(VIENNACL_FOUND TRUE)
+endif()
+
+#set HAVE_VIENNACL for config.h
+set(HAVE_VIENNACL ${VIENNACL_FOUND})
+
+#set HAVE_OPENCL for config.h
+set(HAVE_OPENCL ${OpenCL_FOUND})
+
+# register all VIENNACL related flags
+if(VIENNACL_FOUND)
+  dune_register_package_flags(INCLUDE_DIRS "${VIENNACL_INCLUDE_DIRS}")
+endif()
+
+# register all OpenCL related flags
+if(OpenCL_FOUND)
+  message("opencl = ${OpenCL_LIBRARIES}")
+  dune_register_package_flags(
+    LIBRARIES "${OpenCL_LIBRARIES}"
+    INCLUDE_DIRS "${OpenCL_INCLUDE_DIRS}")
+endif()
