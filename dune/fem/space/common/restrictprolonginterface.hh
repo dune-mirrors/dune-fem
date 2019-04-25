c28e485e11df8965014d83132b6e8c26a40827fc
Robert K <robertk@posteo.org>
2019-04-25 12:15:52 +0200

    [bugfix][RestrictProlongDefault] add missing empty default method for restrictFinalize.

--- dune/fem/space/common/restrictprolonginterface.hh
+++ dune/fem/space/common/restrictprolonginterface.hh
@@ -178,6 +178,10 @@ namespace Dune
 
       void initialize () {}
       void finalize   () {}
+
+      //! restrictFinalize is for some spaces where a local matrix inversion is carried out after restriction.
+      template <class Entity>
+      void restrictFinalize( const Entity& father ) const {}
     };
 
 
