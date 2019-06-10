fd98d21a82b7c266f31c946bd1bdfdaa2ef3e61c
dedner <a.s.dedner@warwick.ac.uk>
2019-06-10 21:41:13 +0100

    small change to rundocker script

--- scripts/rundocker.sh
+++ scripts/rundocker.sh
@@ -8,6 +8,7 @@ then
   docker run -it --rm -v $PWD:/host -v dunepy:/dunepy \
     -v /tmp/.X11-unix:/tmp/.X11-unix:ro --device /dev/dri \
     -e userId=$(id -u) -e groupId=$(id -g) $dockerName
+  xhost -si:localuser:$USER
 elif [ $(uname) = "Darwin" ]
 then
   echo "on MAC: for X forwarding remember to run 'ipconfig getifaddr en0)' in separate terminal"
@@ -16,6 +17,7 @@ then
     -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
     -e DISPLAY=$(ipconfig getifaddr en0):0 --net=host \
     -e userId=$(id -u) -e groupId=$(id -g) $dockerName
+  xhost -si:localuser:$USER
 else
   echo "uname=$(uname) not recognized - specific settings for 'Linux' and 'Darwin' available"
   echo "starting docker container without X forwarding."
