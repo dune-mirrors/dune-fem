62f55672e45212cd55b6c2a3dea96288c20199b5
Robert K <robertk@posteo.org>
2020-08-25 15:52:51 +0200

    [cleanup][AdaptationMethod] limit output in thread parallel runs.

--- dune/fem/space/common/adaptationmanager.hh
+++ dune/fem/space/common/adaptationmanager.hh
@@ -149,7 +149,7 @@ namespace Dune
     AdaptationMethod ( const GridType &grid, const ParameterReader &parameter = Parameter::container() )
       : adaptationMethod_(generic)
     {
-      const bool output = ( Parameter :: verbose() );
+      const bool output = ( Parameter :: verbose() && ThreadManager::isMaster() );
       int am = 1;
       const std::string methodNames [] = { "none", "generic", "callback" };
       am = parameter.getEnum("fem.adaptation.method", methodNames, am);
