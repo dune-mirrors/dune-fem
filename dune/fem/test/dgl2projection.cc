2664c3f88ffc11c7a8d2c4ff7fddf5db45690410
Robert K <robertk@posteo.org>
2020-10-24 14:09:27 +0200

    [cleanup][dgl2projection] Merge all dg2lprojection tests into a single test to reduce compile time.

--- dune/fem/test/dgl2projection.cc
+++ dune/fem/test/dgl2projection.cc
@@ -1,3 +1,4 @@
+// undef NDEBUG so we can use assert every time.
 #undef NDEBUG
 
 #include <config.h>
@@ -12,33 +13,29 @@
 #include <dune/fem/misc/h1norm.hh>
 #include <dune/fem/misc/lpnorm.hh>
 
-#if defined USE_BLOCKVECTORFUNCTION
-#include <dune/fem/function/blockvectorfunction.hh>
-#elif defined USE_VECTORFUNCTION
 #include <dune/common/dynvector.hh>
-#include <dune/fem/function/vectorfunction.hh>
-#elif defined USE_BLOCKVECTORDISCRETEFUNCTION
+#include <dune/fem/function/adaptivefunction.hh>
 #include <dune/fem/function/blockvectordiscretefunction.hh>
 #include <dune/fem/function/blockvectors/referenceblockvector.hh>
-#elif HAVE_PETSC && defined USE_PETSCDISCRETEFUNCTION
-#include <dune/fem/function/petscdiscretefunction.hh>
-#elif defined USE_COMBINEDFUNCTION
-#undef DIMRANGE
-#define DIMRANGE 1
-#include <dune/fem/function/adaptivefunction.hh>
+#include <dune/fem/function/blockvectorfunction.hh>
 #include <dune/fem/function/combinedfunction.hh>
-#else
-#include <dune/fem/function/adaptivefunction.hh>
-#endif
+#include <dune/fem/function/vectorfunction.hh>
+#include <dune/fem/function/petscdiscretefunction.hh>
 
-#if defined  USE_FILTEREDGRID
 #include <dune/fem/gridpart/filter/radialfilter.hh>
 #include <dune/fem/gridpart/filter/basicfilterwrapper.hh>
 #include <dune/fem/gridpart/filteredgridpart.hh>
-#endif
-#if defined  USE_IDGRIDPART
 #include <dune/fem/gridpart/idgridpart.hh>
-#endif
+
+
+#include <complex>
+#include <dune/fem/misc/double.hh>
+
+#include <dune/fem/space/discontinuousgalerkin.hh>
+#include <dune/fem/space/common/dofstorage.hh>
+#include <dune/fem/space/combinedspace.hh>
+#include <dune/fem/space/lagrange.hh>
+#include <dune/fem/space/finitevolume.hh>
 
 #include "testgrid.hh"
 #include "dfspace.hh"
@@ -49,61 +46,105 @@ using namespace Dune;
 using namespace Fem;
 
 typedef GridSelector::GridType MyGridType;
-// typedef HierarchicGridPart< MyGridType >  ContainedGridPartType;
 typedef Fem :: DGAdaptiveLeafGridPart< MyGridType > ContainedGridPartType;
-//typedef IdBasedLeafGridPart< MyGridType > ContainedGridPartType;
 
-// use filtered grid for testing
-#if defined  USE_FILTEREDGRID
-  typedef Fem :: RadialFilter< double, MyGridType :: dimensionworld > FilterImplType;
-  typedef Fem :: BasicFilterWrapper< ContainedGridPartType, FilterImplType > FilterType ;
-  typedef Fem :: FilteredGridPart<ContainedGridPartType, FilterType, true > GridPartType;
-#elif defined  USE_IDGRIDPART
-  typedef Fem:: IdGridPart< ContainedGridPartType > GridPartType;
-#else
-  typedef ContainedGridPartType  GridPartType ;
-#endif
+template <class DiscreteFunctionType>
+void algorithm( DiscreteFunctionType& solution )
+{
+  typedef typename DiscreteFunctionType :: DiscreteFunctionSpaceType  DiscreteFunctionSpaceType;
+  typedef typename DiscreteFunctionSpaceType :: GridPartType GridPartType;
 
-typedef TestFunctionSpace FunctionSpaceType;
-typedef TestDiscreteFunctionSpace< GridPartType > DiscreteFunctionSpaceType;
+  // just to check that it is there
+  typedef Fem::DiscreteFunctionTraits< DiscreteFunctionType > Traits;
+  typedef typename Traits::DofType DofType;
+  DofType val = 0;
+  val = val + val;
 
-#if defined USE_BLOCKVECTORFUNCTION
-typedef Fem :: ISTLBlockVectorDiscreteFunction< DiscreteFunctionSpaceType >
-  DiscreteFunctionType;
-#elif defined USE_VECTORFUNCTION
-typedef Fem :: ManagedDiscreteFunction
-  < Fem :: VectorDiscreteFunction
-    < DiscreteFunctionSpaceType,
-  #if defined USE_DOFTYPE_INT && not defined USE_COMPLEX   // note: no conversion from int to std::complex implemented in std
-        Dune :: DynamicVector< int >
-  #else
-        Dune :: DynamicVector< FunctionSpaceType :: RangeFieldType >
-  #endif
-  > >  DiscreteFunctionType;
-#elif defined USE_BLOCKVECTORDISCRETEFUNCTION
-typedef Dune::Fem::ReferenceBlockVector< FunctionSpaceType::RangeFieldType, DiscreteFunctionSpaceType::localBlockSize >
-  BlockVectorType;
-typedef Dune::Fem::BlockVectorDiscreteFunction< DiscreteFunctionSpaceType, BlockVectorType >
-  DiscreteFunctionType;
-#elif HAVE_PETSC && defined USE_PETSCDISCRETEFUNCTION
-typedef Dune::Fem::PetscDiscreteFunction< DiscreteFunctionSpaceType >
-  DiscreteFunctionType;
-#elif defined USE_COMBINEDFUNCTION
-typedef Fem :: AdaptiveDiscreteFunction< DiscreteFunctionSpaceType >
-  ContainedDiscreteFunctionType;
-typedef Fem :: CombinedDiscreteFunction< ContainedDiscreteFunctionType, DIMRANGE >
-  DiscreteFunctionType;
-#else
-typedef Fem :: AdaptiveDiscreteFunction< DiscreteFunctionSpaceType >
-  DiscreteFunctionType;
-#endif
+  typedef ExactSolution< typename DiscreteFunctionSpaceType::FunctionSpaceType > ExactSolutionType;
+  typedef Fem :: FunctionSpace< double, double, GridPartType::dimensionworld, 1 > WeightFunctionSpaceType;
+  typedef WeightFunction< WeightFunctionSpaceType > WeightFunctionType;
 
-typedef ExactSolution< FunctionSpaceType > ExactSolutionType;
-typedef Fem :: FunctionSpace< double, double, GridSelector::dimworld, 1 > WeightFunctionSpaceType;
-typedef WeightFunction< WeightFunctionSpaceType > WeightFunctionType;
+  const GridPartType& gridPart = solution.space().gridPart();
 
-typedef Fem::DiscreteFunctionTraits< DiscreteFunctionType > Traits;
-typedef typename Traits::DofType DofType;
+  ExactSolutionType exactSolution;
+  solution.clear();
+
+  // perform the L2Projection
+  interpolate( exactSolution, solution );
+
+  LPNorm< GridPartType > lpnorm( gridPart, 2.0 );
+  L2Norm< GridPartType > l2norm( gridPart );
+  L1Norm< GridPartType > l1norm( gridPart );
+  H1Norm< GridPartType > h1norm( gridPart );
+
+  // weighted norm stuff
+  WeightFunctionType weightFunctionExact;
+  typedef GridFunctionAdapter< WeightFunctionType, GridPartType>
+    DiscreteWeightFunctionType;
+
+  typedef typename DiscreteFunctionType::RangeFieldType RangeFieldType ;
+
+  DiscreteWeightFunctionType weightFunction( "weight", weightFunctionExact, gridPart );
+
+  WeightedLPNorm< DiscreteWeightFunctionType > wLpnorm( weightFunction, 2.0 );
+  WeightedL2Norm< DiscreteWeightFunctionType > wL2norm( weightFunction );
+
+  // check all norms
+  {
+    // check lp norm
+    RangeFieldType lperror  = lpnorm.distance( exactSolution, solution );
+    RangeFieldType lperror2 = lpnorm.distance( solution, exactSolution );
+    assert( std::abs( lperror - lperror2 ) < 1e-10 );
+
+    // check l2 norm
+    RangeFieldType error  = l2norm.distance( exactSolution, solution );
+    RangeFieldType error2 = l2norm.distance( solution, exactSolution );
+    assert( std::abs( error - error2 ) < 1e-10 );
+
+    // compare lp(p=2) and l2 norm
+    assert( std::abs( lperror - error ) < 1e-10 );
+  }
+
+  // check l1 norm
+  {
+    RangeFieldType error  = l1norm.distance( exactSolution, solution );
+    RangeFieldType error2 = l1norm.distance( solution, exactSolution );
+    assert( std::abs( error - error2 ) < 1e-10 );
+  }
+
+  // check h1 norm
+  {
+    RangeFieldType error  = h1norm.distance( exactSolution, solution );
+    RangeFieldType error2 = h1norm.distance( solution, exactSolution );
+    assert( std::abs( error - error2 ) < 1e-10 );
+  }
+
+  // check weighted lp norm
+  {
+    // check lp norm
+    RangeFieldType lperror  = wLpnorm.distance( exactSolution, solution );
+    RangeFieldType lperror2 = wLpnorm.distance( solution, exactSolution );
+    assert( std::abs( lperror - lperror2 ) < 1e-10 );
+
+    // check l2 norm
+    RangeFieldType error  = wL2norm.distance( exactSolution, solution );
+    RangeFieldType error2 = wL2norm.distance( solution, exactSolution );
+    assert( std::abs( error - error2 ) < 1e-10 );
+
+    // compare lp(p=2) and l2 norm
+    assert( std::abs( lperror - error ) < 1e-10 );
+  }
+}
+
+// default run algorithm
+template <class GridPartType, class DiscreteFunctionSpaceType, class DiscreteFunctionType>
+void run(MyGridType& grid)
+{
+  GridPartType gridPart( grid );
+  DiscreteFunctionSpaceType discreteFunctionSpace( gridPart );
+  DiscreteFunctionType solution( "solution", discreteFunctionSpace );
+  algorithm( solution );
+}
 
 // main program
 int main(int argc, char ** argv)
@@ -115,87 +156,156 @@ int main(int argc, char ** argv)
     const int step = TestGrid :: refineStepsForHalf();
     grid.globalRefine( 2*step );
 
-#ifdef  USE_FILTEREDGRID
-    ContainedGridPartType containedGridPart( grid );
-    FilterType filter( containedGridPart );
-    GridPartType gridPart( containedGridPart, filter );
-#else
-    GridPartType gridPart( grid );
+    // test grid width calculation
+    {
+      typedef ContainedGridPartType  GridPartType ;
+      GridPartType gridPart( grid );
+      // add check for grid width
+      std::cout << "Grid width: " << GridWidth :: calcGridWidth( gridPart ) << std::endl;
+    }
+
+    // AdaptiveDiscreteFunction
+    {
+      // use standard grid part
+      typedef ContainedGridPartType  GridPartType ;
+      typedef TestDiscreteFunctionSpace< GridPartType > DiscreteFunctionSpaceType;
+      typedef Fem :: AdaptiveDiscreteFunction< DiscreteFunctionSpaceType > DiscreteFunctionType;
+      run< GridPartType, DiscreteFunctionSpaceType, DiscreteFunctionType > ( grid );
+    }
+
+    // AdaptiveDiscreteFunction + FV space
+    {
+      // use standard grid part
+      typedef ContainedGridPartType  GridPartType ;
+      typedef FiniteVolumeSpace< TestFunctionSpace, GridPartType, 0 > DiscreteFunctionSpaceType;
+      typedef Fem :: AdaptiveDiscreteFunction< DiscreteFunctionSpaceType > DiscreteFunctionType;
+      run< GridPartType, DiscreteFunctionSpaceType, DiscreteFunctionType > ( grid );
+    }
+
+    // AdaptiveDiscreteFunction + DG HierarchicLegendre
+    {
+      // use standard grid part
+      typedef ContainedGridPartType  GridPartType ;
+      typedef HierarchicLegendreDiscontinuousGalerkinSpace<
+        TestFunctionSpace, GridPartType, 2 > DiscreteFunctionSpaceType;
+      typedef Fem :: AdaptiveDiscreteFunction< DiscreteFunctionSpaceType > DiscreteFunctionType;
+      run< GridPartType, DiscreteFunctionSpaceType, DiscreteFunctionType > ( grid );
+    }
+
+    // ISTLBlockVectorDiscreteFunction
+    {
+      // use standard grid part
+      typedef ContainedGridPartType  GridPartType ;
+      typedef TestDiscreteFunctionSpace< GridPartType > DiscreteFunctionSpaceType;
+      typedef Fem :: ISTLBlockVectorDiscreteFunction< DiscreteFunctionSpaceType > DiscreteFunctionType;
+      run< GridPartType, DiscreteFunctionSpaceType, DiscreteFunctionType > ( grid );
+    }
+
+    // ManagedDiscreteFunction + int
+    {
+      typedef ContainedGridPartType  GridPartType ;
+      typedef TestDiscreteFunctionSpace< GridPartType > DiscreteFunctionSpaceType;
+      typedef Dune :: DynamicVector< int >  VectorType;
+      typedef Fem :: ManagedDiscreteFunction< Fem :: VectorDiscreteFunction < DiscreteFunctionSpaceType, VectorType> > DiscreteFunctionType;
+      run< GridPartType, DiscreteFunctionSpaceType, DiscreteFunctionType > ( grid );
+    }
+
+    // ManagedDiscreteFunction + RangeFieldType
+    {
+      typedef ContainedGridPartType  GridPartType ;
+      typedef TestDiscreteFunctionSpace< GridPartType > DiscreteFunctionSpaceType;
+      typedef Dune :: DynamicVector< typename DiscreteFunctionSpaceType :: RangeFieldType >  VectorType;
+      typedef Fem :: ManagedDiscreteFunction< Fem :: VectorDiscreteFunction < DiscreteFunctionSpaceType, VectorType> > DiscreteFunctionType;
+      run< GridPartType, DiscreteFunctionSpaceType, DiscreteFunctionType > ( grid );
+    }
+
+    /*
+    // CombinedDiscreteFunction
+    {
+      // use standard grid part
+      typedef ContainedGridPartType  GridPartType ;
+      typedef TestDiscreteFunctionSpace< GridPartType > DiscreteFunctionSpaceType;
+      typedef Fem :: AdaptiveDiscreteFunction< DiscreteFunctionSpaceType >
+        ContainedDiscreteFunctionType;
+      typedef Fem :: CombinedDiscreteFunction< ContainedDiscreteFunctionType, 5 >
+        DiscreteFunctionType;
+      run< GridPartType, DiscreteFunctionSpaceType, DiscreteFunctionType > ( grid );
+    }
+    */
+
+    // BlockVectorDiscreteFunction
+    {
+      // use standard grid part
+      typedef ContainedGridPartType  GridPartType ;
+      typedef TestDiscreteFunctionSpace< GridPartType > DiscreteFunctionSpaceType;
+      typedef Dune::Fem::ReferenceBlockVector< typename DiscreteFunctionSpaceType::RangeFieldType, DiscreteFunctionSpaceType::localBlockSize >
+        BlockVectorType;
+      typedef Dune::Fem::BlockVectorDiscreteFunction< DiscreteFunctionSpaceType, BlockVectorType >
+        DiscreteFunctionType;
+      run< GridPartType, DiscreteFunctionSpaceType, DiscreteFunctionType > ( grid );
+    }
+
+#if HAVE_PETSC
+    // PetscDiscreteFunction
+    {
+      // use standard grid part
+      typedef ContainedGridPartType  GridPartType ;
+      typedef TestDiscreteFunctionSpace< GridPartType > DiscreteFunctionSpaceType;
+      typedef Dune::Fem::PetscDiscreteFunction< DiscreteFunctionSpaceType >
+        DiscreteFunctionType;
+      run< GridPartType, DiscreteFunctionSpaceType, DiscreteFunctionType > ( grid );
+    }
 #endif
 
-    // add check for grid width
-    std::cout << "Grid width: "
-      << GridWidth :: calcGridWidth( gridPart ) << std::endl;
-
-    DiscreteFunctionSpaceType discreteFunctionSpace( gridPart );
-    ExactSolutionType exactSolution;
-    DiscreteFunctionType solution( "solution", discreteFunctionSpace );
-    solution.clear();
-
-    // perform the L2Projection
-    interpolate( exactSolution, solution );
-
-    LPNorm< GridPartType > lpnorm( gridPart, 2.0 );
-    L2Norm< GridPartType > l2norm( gridPart );
-    L1Norm< GridPartType > l1norm( gridPart );
-    H1Norm< GridPartType > h1norm( gridPart );
-
-    // weighted norm stuff
-    WeightFunctionType weightFunctionExact;
-    typedef GridFunctionAdapter< WeightFunctionType, GridPartType>
-      DiscreteWeightFunctionType;
-
-    typedef DiscreteFunctionType::RangeFieldType RangeFieldType ;
-
-    DiscreteWeightFunctionType weightFunction( "weight", weightFunctionExact, gridPart );
-
-    WeightedLPNorm< DiscreteWeightFunctionType > wLpnorm( weightFunction, 2.0 );
-    WeightedL2Norm< DiscreteWeightFunctionType > wL2norm( weightFunction );
-
-    // check all norms
+    // AdaptiveDiscreteFunction + FilteredGridPart
     {
-      // check lp norm
-      RangeFieldType lperror  = lpnorm.distance( exactSolution, solution );
-      RangeFieldType lperror2 = lpnorm.distance( solution, exactSolution );
-      assert( std::abs( lperror - lperror2 ) < 1e-10 );
+      // use standard grid part
+      typedef Fem :: RadialFilter< double, MyGridType :: dimensionworld > FilterImplType;
+      typedef Fem :: BasicFilterWrapper< ContainedGridPartType, FilterImplType > FilterType ;
+      typedef Fem :: FilteredGridPart<ContainedGridPartType, FilterType, true > GridPartType;
+      typedef TestDiscreteFunctionSpace< GridPartType > DiscreteFunctionSpaceType;
+      typedef Fem :: AdaptiveDiscreteFunction< DiscreteFunctionSpaceType > DiscreteFunctionType;
 
-      // check l2 norm
-      RangeFieldType error  = l2norm.distance( exactSolution, solution );
-      RangeFieldType error2 = l2norm.distance( solution, exactSolution );
-      assert( std::abs( error - error2 ) < 1e-10 );
+      ContainedGridPartType containedGridPart( grid );
+      FilterType filter( containedGridPart );
+      GridPartType gridPart( containedGridPart, filter );
 
-      // compare lp(p=2) and l2 norm
-      assert( std::abs( lperror - error ) < 1e-10 );
+      DiscreteFunctionSpaceType discreteFunctionSpace( gridPart );
+      DiscreteFunctionType solution( "solution", discreteFunctionSpace );
+      algorithm( solution );
     }
 
-    // check l1 norm
+    // ISTLBlockVectorDiscreteFunction + FilteredGridPart
     {
-      RangeFieldType error  = l1norm.distance( exactSolution, solution );
-      RangeFieldType error2 = l1norm.distance( solution, exactSolution );
-      assert( std::abs( error - error2 ) < 1e-10 );
+      // use standard grid part
+      typedef Fem :: RadialFilter< double, MyGridType :: dimensionworld > FilterImplType;
+      typedef Fem :: BasicFilterWrapper< ContainedGridPartType, FilterImplType > FilterType ;
+      typedef Fem :: FilteredGridPart<ContainedGridPartType, FilterType, true > GridPartType;
+      typedef TestDiscreteFunctionSpace< GridPartType > DiscreteFunctionSpaceType;
+      typedef Fem :: ISTLBlockVectorDiscreteFunction< DiscreteFunctionSpaceType > DiscreteFunctionType;
+
+      ContainedGridPartType containedGridPart( grid );
+      FilterType filter( containedGridPart );
+      GridPartType gridPart( containedGridPart, filter );
+
+      DiscreteFunctionSpaceType discreteFunctionSpace( gridPart );
+      DiscreteFunctionType solution( "solution", discreteFunctionSpace );
+      algorithm( solution );
     }
 
-    // check h1 norm
+    // AdaptiveDiscreteFunction + IdGridPart
     {
-      RangeFieldType error  = h1norm.distance( exactSolution, solution );
-      RangeFieldType error2 = h1norm.distance( solution, exactSolution );
-      assert( std::abs( error - error2 ) < 1e-10 );
-    }
+      // use standard grid part
+      typedef Fem:: IdGridPart< ContainedGridPartType > GridPartType;
+      typedef TestDiscreteFunctionSpace< GridPartType > DiscreteFunctionSpaceType;
+      typedef Fem :: AdaptiveDiscreteFunction< DiscreteFunctionSpaceType > DiscreteFunctionType;
 
-    // check weighted lp norm
-    {
-      // check lp norm
-      RangeFieldType lperror  = wLpnorm.distance( exactSolution, solution );
-      RangeFieldType lperror2 = wLpnorm.distance( solution, exactSolution );
-      assert( std::abs( lperror - lperror2 ) < 1e-10 );
+      ContainedGridPartType containedGridPart( grid );
+      GridPartType gridPart( containedGridPart );
 
-      // check l2 norm
-      RangeFieldType error  = wL2norm.distance( exactSolution, solution );
-      RangeFieldType error2 = wL2norm.distance( solution, exactSolution );
-      assert( std::abs( error - error2 ) < 1e-10 );
-
-      // compare lp(p=2) and l2 norm
-      assert( std::abs( lperror - error ) < 1e-10 );
+      DiscreteFunctionSpaceType discreteFunctionSpace( gridPart );
+      DiscreteFunctionType solution( "solution", discreteFunctionSpace );
+      algorithm( solution );
     }
 
     return 0;
