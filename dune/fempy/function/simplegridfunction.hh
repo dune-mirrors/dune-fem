26a23056d17c7b200ddf12f56aedb49d5fa92ef4
Robert K <robertk@posteo.org>
2021-06-08 15:46:16 +0200

    [bugfix][SimpleGridFunction] Fix #126 (missing typedef GridPartType).

--- dune/fempy/function/simplegridfunction.hh
+++ dune/fempy/function/simplegridfunction.hh
@@ -32,6 +32,8 @@ namespace Dune
       typedef SimpleLocalFunction< GridPart, LocalEvaluator > This;
 
     public:
+      typedef GridPart GridPartType;
+
       typedef typename GridPart::template Codim< 0 >::EntityType EntityType;
       typedef typename EntityType::Geometry::LocalCoordinate LocalCoordinateType;
       typedef std::decay_t< std::result_of_t< LocalEvaluator( EntityType, LocalCoordinateType ) > > Value_;
