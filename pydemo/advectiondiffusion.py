f6ddaa7806b969e7b2573786d90a7d7f75581487
Robert K <robertk@posteo.org>
2021-10-26 15:36:44 +0200

    [bugfix][testing] Add MOLGalerkin to tests again and also use non-virtualized integrands on some combinations.

--- pydemo/advectiondiffusion.py
+++ pydemo/advectiondiffusion.py
@@ -77,7 +77,7 @@ def compute(space,epsilon,weakBnd,skeleton, mol=None):
                               "newton.verbose": True, "newton.linear.verbose": False}
                }
     if mol == 'mol':
-        scheme = molSolutionScheme([form==rhs,strongBC], **solver)
+        scheme = molSolutionScheme([form==rhs,strongBC], virtualize=False, **solver)
     else:
         scheme = solutionScheme([form==rhs,strongBC], **solver)
 
@@ -114,7 +114,7 @@ for i in range(10):
   print(i,"dgSpace, 1e-5, True, True")
   gridView = newGridView()
   space    = dgSpace(gridView, order=2, storage=storage)
-  eoc = compute(space,1e-5,True,True) # , 'mol')
+  eoc = compute(space,1e-5,True,True, 'mol')
   test = test and (eoc[-1]-(space.order+1)) > -0.1
 
   print(i,"dgSpace, 1, True, True")
