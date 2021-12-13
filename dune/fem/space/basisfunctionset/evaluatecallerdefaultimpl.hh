547727906b11890c2d367e25c81ae67a5600d0ce
Robert K <robertk@posteo.org>
2021-12-13 09:56:53 +0100

    [feature][codegen] Add templated default implementations.

--- dune/fem/space/basisfunctionset/evaluatecallerdefaultimpl.hh
+++ dune/fem/space/basisfunctionset/evaluatecallerdefaultimpl.hh
@@ -14,39 +14,106 @@ namespace Codegen {
 //  evaluate and store results in a vector
 //
 /////////////////////////////////////////////////////////////////////////
-template <class BaseFunctionSet, class Geometry, int dimRange, int numRows, int numCols>
+template <class BaseFunctionSet, class Geometry, int dimR, int numRows, int numCols>
 struct EvaluateRanges
 {
   template< class QuadratureType,
             class RangeVectorType,
-            class LocalDofVectorType,
-            class RangeFactorType>
+            class RangeFactorType,
+            class LocalDofVectorType>
   static void eval( const QuadratureType& quad,
-                    const RangeVectorType& rangeStorage,
+                    const RangeVectorType& rangeStorageTransposed,
                     const LocalDofVectorType& dofs,
-                    RangeFactorType &rangeFactors )
+                    RangeFactorType &rangeVector)
   {
-    std::cerr << "ERROR: wrong code generated for VectorialBaseFunctionSet::evaluateRanges< "
-              << dimRange << " , " << numRows << " , " << numCols << " >!" << std::endl;
+    std::cout << "Default implementation of EvaluateRanges" << std::endl;
     std::abort();
   }
 };
 
-template <class BaseFunctionSet, int dimRange, int numRows, int numCols>
-struct EvaluateRanges<BaseFunctionSet, EmptyGeometry, dimRange, numRows, numCols >
+template <class BaseFunctionSet, int dimR, int numRows, int numCols>
+struct EvaluateRanges<BaseFunctionSet, EmptyGeometry, dimR, numRows, numCols >
 {
+  static const int dimRange = dimR ;      // dimRange
+  static const int quadNop  = numRows ;   // number of quadrature points
+  static const int scalarBasis = numCols; // number of scalar basis functions
+  static const int lanes = 4;             // simdWidth
+  static const int basisLoop = (scalarBasis / lanes) * lanes;
+  static const int nResult = dimRange * quadNop;
+  static const int dofSkip = dimRange * lanes;
+
   template< class QuadratureType,
             class RangeVectorType,
-            class LocalDofVectorType,
-            class RangeFactorType>
+            class RangeFactorType,
+            class LocalDofVectorType>
   static void eval( const QuadratureType& quad,
-                    const RangeVectorType& rangeStorage,
+                    const RangeVectorType& rangeStorageTransposed,
                     const LocalDofVectorType& dofs,
-                    RangeFactorType &rangeFactors)
+                    RangeFactorType &rangeVector)
   {
-    std::cerr << "ERROR: wrong code generated for VectorialBaseFunctionSet::evaluateRanges< "
-              << "EmptyGeo, " << dimRange << " , " << numRows << " , " << numCols << " >!" << std::endl;
-    std::abort();
+    typedef double Field;
+    static thread_local std::vector< Field > memory( nResult );
+    Field* resultTmp = memory.data();
+
+    for( int i=0; i < nResult; ++i ) resultTmp[ i ] = 0;
+
+    Field* result[ dimRange ];
+    for( int i=0; i<dimRange; ++i )
+      result[ i ] = resultTmp + i*quadNop;
+
+    const Field* baseData = rangeStorageTransposed.data();
+    if constexpr ( basisLoop > 0 )
+    {
+      for( int col = 0, dof = 0 ; col < basisLoop; col += lanes, dof += dofSkip )
+      {
+        const Field* base[ lanes ];
+        for( int l=0; l<lanes; ++l )
+          base[ l ] = baseData + ((col + l )*quadNop);
+
+        evalRangesLoop( &dofs[ dof ], base, result );
+      }
+    }
+
+    if constexpr ( scalarBasis > basisLoop )
+    {
+      // remainder iteration
+      for( int col = basisLoop, dof = basisLoop*dimRange ; col < scalarBasis ; ++col )
+      {
+        const Field* base0 = baseData + (col * quadNop);
+        // Loop over all quadrature points
+        for(int qp = 0; qp < quadNop ; ++qp )
+        {
+          const double phi0 = base0[ qp ];
+          for( int i=0; i<dimRange; ++i, ++dof )
+            result[ i ][ qp ] += phi0 * dofs[ dof ] ;
+        }
+      }
+    }
+
+    // store result
+    for(int qp = 0; qp < quadNop ; ++qp )
+    {
+      auto& res = rangeVector[ qp ];
+      for( int i=0; i<dimRange; ++i )
+        res[ i ] = result[ i ][ qp ];
+    }
+  }
+
+private:
+  static void evalRangesLoop(const double* dofs,
+                             const double** __restrict__ basis,
+                             double** __restrict__ result)
+  {
+    // Loop over all quadrature points
+    for(int qp = 0; qp < quadNop ; ++qp )
+    {
+      for( int l=0, dof = 0; l<lanes; ++l )
+      {
+        const double base = basis[ l ][ qp ];
+        for( int i=0; i<dimRange; ++i, ++dof )
+          result[ i ][ qp ] += base * dofs[ dof ];
+      }
+    }
   }
 };
 
