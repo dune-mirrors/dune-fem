e1a150aa9237a86667be9625937235b065b299ca
Samuel Burbulla <samuel.burbulla@mathematik.uni-stuttgart.de>
2022-05-16 12:38:09 +0200

    Use find_package and shared linker flags to link openmp on mac correctly.

--- cmake/modules/DuneFemMacros.cmake
+++ cmake/modules/DuneFemMacros.cmake
@@ -34,10 +34,11 @@ include(CheckCXXSourceCompiles)
 set(USE_OPENMP ON CACHE BOOL "whether we are using OpenMP.")
 # if open mp should be used perform cmake check
 if(USE_OPENMP)
-  include(FindOpenMP)
+  find_package(OpenMP)
   if(OPENMP_FOUND)
     # add flags to compiler flags
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
+    set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} ${OpenMP_CXX_LIBRARIES}")
   endif(OPENMP_FOUND)
 endif(USE_OPENMP)
 
