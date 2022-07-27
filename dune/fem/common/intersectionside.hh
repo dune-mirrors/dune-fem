2f66c8b8c337b50cf211531e7b6bb599e044fff1
Samuel Burbulla <samuel.burbulla@mathematik.uni-stuttgart.de>
2022-07-27 10:21:24 +0200

    Use PriorityTag from dune-common.

--- dune/fem/common/intersectionside.hh
+++ dune/fem/common/intersectionside.hh
@@ -9,26 +9,22 @@ namespace Dune
   {
     enum class IntersectionSide : std::size_t { in = 0u, out = 1u };
 
-    template <int N> struct priority : priority<N-1> {};
-    template <> struct priority<0> {};
-
-
     template<class GF, class Intersection>
-    auto bindIntersection(GF& gf, const Intersection& intersection, IntersectionSide side, priority<2>)
+    auto bindIntersection(GF& gf, const Intersection& intersection, IntersectionSide side, PriorityTag<2>)
      -> decltype(gf.bind(intersection, side))
     {
       gf.bind(intersection, side);
     }
 
     template<class GF, class Intersection>
-    auto bindIntersection(GF& gf, const Intersection& intersection, IntersectionSide side, priority<1>)
+    auto bindIntersection(GF& gf, const Intersection& intersection, IntersectionSide side, PriorityTag<1>)
      -> decltype(gf.bind(intersection.impl().hostIntersection(), side))
     {
       gf.bind(intersection.impl().hostIntersection(), side);
     }
 
     template<class GF, class Intersection>
-    auto bindIntersection(GF& gf, const Intersection& intersection, IntersectionSide side, priority<0>)
+    auto bindIntersection(GF& gf, const Intersection& intersection, IntersectionSide side, PriorityTag<0>)
      -> decltype(gf.bind(intersection.inside()))
     {
       // store local copy to avoid problems with casting to temporary types
@@ -40,7 +36,7 @@ namespace Dune
     template<class GF, class Intersection>
     void defaultIntersectionBind(GF &gf, const Intersection &intersection, IntersectionSide side)
     {
-      bindIntersection(gf, intersection, side, priority<2>{});
+      bindIntersection(gf, intersection, side, PriorityTag<2>{});
     }
   }
 }
