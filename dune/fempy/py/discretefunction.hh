969c4396ee934dc9b4d8f29c3317d117df8e3ad8
Johannes Kasimir <johannes.kasimir@math.lu.se>
2022-09-20 14:00:11 +0200

    [feature] expose communicate method on discrete functions

--- dune/fempy/py/discretefunction.hh
+++ dune/fempy/py/discretefunction.hh
@@ -378,6 +378,7 @@ namespace Dune
           } );
 
         cls.def( "clear", [] ( DF &self ) { self.clear(); } );
+        cls.def( "communicate", [] ( DF &self ) { self.communicate(); } );
         // assign is added in __init__ of space
         //cls.def( "assign", [] ( DF &self, const DF &other ) { self.assign( other ); }, "other"_a );
         cls.def( "scalarProductDofs", [] ( DF &self, const DF &other ) { return self.scalarProductDofs( other ); }, "other"_a );
