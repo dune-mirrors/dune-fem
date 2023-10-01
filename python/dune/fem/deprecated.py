176eef9f0d8eadb8d5001fc34eefac191fd68677
dedner <a.s.dedner@warwick.ac.uk>
2023-10-01 09:55:23 +0100

    1. print name of function calling the function with the deprecation warning 2. set filter to 'always' which will print the deprecation message not once but everytime

--- python/dune/fem/deprecated.py
+++ python/dune/fem/deprecated.py
@@ -1,4 +1,10 @@
 import warnings
-warnings.filterwarnings("default", category=DeprecationWarning)
+# warnings.filterwarnings("default", category=DeprecationWarning)
+# warnings.simplefilter("default", category=DeprecationWarning)
+warnings.simplefilter("always", category=DeprecationWarning)
 def deprecated(msg):
-    warnings.warn(msg, DeprecationWarning, stacklevel=2)
+    msg += """
+To disable this message add 'warnings.filterwarnings("ignore", category=DeprecationWarning)'
+to your script after importing all dune.fem modules.
+"""
+    warnings.warn(msg, DeprecationWarning, stacklevel=3)
