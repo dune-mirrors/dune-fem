8298ae0b2f64f19829bd2a65a335d09c98bbc70e
Robert K <robertk@posteo.org>
2024-02-24 13:02:06 +0100

    [feature][Operator] Add method nonLinear to Operator interface.

--- dune/fem/operator/common/operator.hh
+++ dune/fem/operator/common/operator.hh
@@ -59,6 +59,9 @@ namespace Dune
        *  \note The default implementation is empty.
        */
       virtual void finalize () {}
+
+      /**Return @c true if the Operator is nonlinear and @c false otherwise (default is true). */
+      virtual bool nonLinear () const { return true; }
     };
 
     /** \class LinearOperator
@@ -94,6 +97,12 @@ namespace Dune
       virtual bool positiveDefinite() const
       {
 	      return false;
+      }
+
+      /* \copydoc Dune::Fem::Operator::nonLinear */
+      virtual bool nonLinear () const
+      {
+        return false;
       }
     };
 
@@ -242,6 +251,7 @@ namespace Dune
 
     using BaseType::operator();
     using BaseType::finalize;
+    using BaseType::nonLinear;
 
   protected:
     /** \brief The method apply calls the application operator. The method
