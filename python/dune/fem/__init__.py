178a1f440f10b67f986445e9b23edd446db2e3a5
A Dedner <a.s.dedner@warwick.ac.uk>
2026-05-06 09:21:08 +0100

    [minor] layout

--- python/dune/fem/__init__.py
+++ python/dune/fem/__init__.py
@@ -17,7 +17,7 @@ from dune.fem.deprecated import deprecated
 
 __cite_dune_module_as__ = """
 @article{dunefem:10,
-  author = {Dedner, A. and Kl{\"o}fkorn, R. and Nolte, M. and Ohlberger, M.},
+  author = {Dedner, A. and Kl{\\"o}fkorn, R. and Nolte, M. and Ohlberger, M.},
   title = {{A Generic Interface for Parallel and Adaptive Scientific Computing:
   Abstraction Principles and the DUNE-FEM Module}},
   journal = {Computing},
@@ -27,18 +27,16 @@ __cite_dune_module_as__ = """
   number = {3--4},
   doi={10.1007/s00607-010-0110-3}
 }
-
-@software{dunefempy:20,
-  author       = {Dedner, Andreas and
-                  Kloefkorn, Robert and
-                  Nolte, Martin},
+@software{dunefempy:26,
+  author       = {Dedner, A. and
+                  Kl{\\"o}fkorn, R. and
+                  Nolte, M.},
   title        = {Python Bindings for the DUNE-FEM module},
-  month        = mar,
-  year         = 2020,
+  year         = 2026,
   publisher    = {Zenodo},
-  version      = {v2.7.0},
-  doi          = {10.5281/zenodo.3706994},
-  url          = {https://doi.org/10.5281/zenodo.3706994},
+  version      = {v2.12.x},
+  doi          = {10.5281/zenodo.3706994}
+}
 """
 
 
