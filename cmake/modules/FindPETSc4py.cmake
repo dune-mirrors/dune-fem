b2283db803f276e8728a27721195405023da7a1f
A Dedner <a.s.dedner@warwick.ac.uk>
2026-06-19 11:44:47 +0100

    [fix]

--- cmake/modules/FindPETSc4py.cmake
+++ cmake/modules/FindPETSc4py.cmake
@@ -1,4 +1,5 @@
 # FindPETSc4py.cmake
+execute_process(COMMAND ${DUNE_PYTHON_VIRTUALENV_EXECUTABLE} -m pip list)
 execute_process(COMMAND ${DUNE_PYTHON_VIRTUALENV_EXECUTABLE} -c
   "import petsc4py,sys; print(petsc4py.get_include())"
   OUTPUT_VARIABLE PETSC4PY_INCLUDE OUTPUT_STRIP_TRAILING_WHITESPACE)
@@ -8,4 +9,7 @@ if(PETSC4PY_INCLUDE)
   set(PETSC4PY_FOUND TRUE)
 else()
   set(PETSC4PY_FOUND FALSE)
+file(APPEND ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeError.log
+  "Determing location of PETSc4py failed:\n"
+  "using Python ${DUNE_PYTHON_VIRTUALENV_EXECUTABLE}\n\n")
 endif()
